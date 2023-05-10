9c6ac2ff538dcbd03e41d12ab7dc36b8c5e605ae
ysh <happysh_s2@naver.com>
2023-05-11 04:34:46 +0900

    [Fix] .swiftlint.yml에서 클래스 네이밍 최소길이 제한 해제

---
+++ travelPlan/Source/Common/Extension/UIColor+AppPalette.swift
@@ -0,0 +1,16 @@
+//
+//  ColorStyles+UIColor.swift
+//  travelPlan
+//
+//  Created by 양승현 on 2023/05/11.
+//
+
+import UIKit
+
+extension UIColor {
+  enum YG {
+    static let gray00Background = UIColor(
+      red: 0.975, green: 0.975, blue: 0.975, alpha: 1)
+
+  }
+}
