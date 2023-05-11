1577d966140ffef9048a9d20ccfb0e75f02736a2
ysh <happysh_s2@naver.com>
2023-05-12 04:59:36 +0900

    [Refactor] FeedNavigationBar에서 isCheckedNotification 처리 로직을 제거
    원래 FeedNavigationBar에서 isCheckedNotification 여부 로직(서버로부터 알림이 있는지, 영구 저장소에 값 저장되있는거 확인 등)을 담당하는 함수들을 설계했다가 FeedViewModel에서 처리 후 변경된 값만 전달해 주도록 리펙터링 했습니다.

--- travelPlan/Source/Feature/Feed/View/FeedNavigationBar/FeedNavigationBar.swift
+++ travelPlan/Source/Feature/Feed/View/FeedNavigationBar/FeedNavigationBar.swift
@@ -53,13 +53,7 @@ final class FeedNavigationBar: UIView {
   }
   
   /// 사용자가 알림을 확인 했는지 여부를 체크하고 이에 따른 notificationIcon 상태를 rendering 합니다.
-  /// FeedViewController를 생성하기 이전에 최초로 사용자가 알림을 확인했는지 여부를 파악해야 합니다.
-  /// 추후 서버 통신할 때, 로그인 유저에게 알림을 보낸 여부 또한 확인해야 합니다.
-  /// 이 상태를 저장해 둔 후 FeedViewController가 보여질 때마다 실시간으로 이 상태를 갱신해야 합니다.
-  private var isCheckedNotification: Bool = {
-    /// userDefaults나 전역 변수를 통해 사용자에게 알림이 있거나 확인하지 않았던 알림이 있을 경우를 확인합니다.
-    false
-  }()
+  private var isCheckedNotification: Bool
   
   /// notificationButton과 Icon, searchButton의 경우 isCheckedNotification의 확인이 된 후에야 레이아웃 지정이 가능합니다.
   private lazy var notificationRedIcon = UIView().set {
@@ -72,19 +66,23 @@ final class FeedNavigationBar: UIView {
   
   // MARK: - Initialization
   private override init(frame: CGRect) {
+    //임시 값
+    isCheckedNotification = false
     super.init(frame: frame)
-    translatesAutoresizingMaskIntoConstraints = false
-    setupUI()
-    backgroundColor = Constant.backgroundColor
-    bind()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
-  convenience init() {
+  /// 계산된 뷰를
+  convenience init(isCheckedNotification: Bool) {
     self.init(frame: .zero)
+    self.isCheckedNotification = isCheckedNotification
+    translatesAutoresizingMaskIntoConstraints = false
+    setupUI()
+    backgroundColor = Constant.backgroundColor
+    bind()
   }
   
   deinit {
@@ -114,16 +112,9 @@ extension FeedNavigationBar {
       bottomAnchor.constraint(equalTo: navi.bottomAnchor)])
   }
   
-  func updateIsCheckedNotification() {
-    /// 이 곳에서 앱 내부에서 기록하고 있던( 앱 종료 후에도 정보 갖고 있어야 하기 때문입니다. )
-    /// 객체의 상태도 업데이트 하고 isCheckedNotification 또한 업데이트 합니다.
-    isCheckedNotification = isCheckedNotification ? false : true
-    updateFeedNavigationBarNotificationCheckState()
+  func updateIsCheckedNotification(_ value: Bool) {
+    isCheckedNotification = value
     updateNotificationIconColor()
-  }
-  
-  private func updateFeedNavigationBarNotificationCheckState() {
-    /// userDefaults에 저장된 feed navigation bar notification check state를 갱신합니다.
   }
   
   private func updateNotificationIconColor() {
