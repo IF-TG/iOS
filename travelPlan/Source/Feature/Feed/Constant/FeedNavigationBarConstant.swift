0ca2a12673b9e4cf0ec84f0e0135d13e114f3adf
ysh <happysh_s2@naver.com>
2023-05-12 04:23:15 +0900

    [Feat] 알림 여부에 따른 red effect추가

--- travelPlan/Source/Feature/Feed/Constant/FeedNavigationBarConstant.swift
+++ travelPlan/Source/Feature/Feed/Constant/FeedNavigationBarConstant.swift
@@ -9,6 +9,9 @@ import UIKit
 
 extension FeedNavigationBar {
   struct Constant {
+    
+    static let backgroundColor: UIColor = .white
+    
     struct Title {
       static let text = "ㅇㅕ ㄱㄴ"
       static let textColor: UIColor = .yg.primary
@@ -16,9 +19,10 @@ extension FeedNavigationBar {
       static let leadingSpacing: CGFloat = 30
       static let topSpacing: CGFloat = 10
       static let bottomSpacing: CGFloat = 10
+      static let fontName: String = "SFProText-Semibold"
     }
-    struct UserPostSearch {
-      static let image = UIImage()
+    struct postSearch {
+      static let image = UIImage(named: "search")
       static let size = CGSize(width: 24, height: 24)
       static let trailingSpacing: CGFloat = 19.3
       static let topSpacing: CGFloat = 10.5
@@ -28,13 +32,20 @@ extension FeedNavigationBar {
     }
     
     struct Notification {
-      static let image = UIImage()
+      static let image = UIImage(named: "notification")
       static let size = CGSize(width: 24, height: 24)
-      static let trailingSpacing: CGFloat = 23.8
       static let topSpacaing: CGFloat = 9.33
       static let bottomSpacing: CGFloat = 9.33
       static let normalColor: UIColor = .yg.gray5
       static let highlightColor: UIColor = .yg.gray5.withAlphaComponent(0.5)
+    }
+    
+    struct NotificationIcon {
+      static let width: CGFloat = 5
+      static let height: CGFloat = 5
+      static let topSpacing: CGFloat = 9.33
+      static let trailingSpacing: CGFloat = 23.8 - width
+      static let color: UIColor = UIColor(hex: "#FF2216")
     }
   }
 }
--- travelPlan/Source/Feature/Feed/View/FeedNavigationBar/FeedNavigationBar.swift
+++ travelPlan/Source/Feature/Feed/View/FeedNavigationBar/FeedNavigationBar.swift
@@ -19,7 +19,7 @@ final class FeedNavigationBar: UIView {
     $0.translatesAutoresizingMaskIntoConstraints = false
     $0.textColor = Constant.Title.textColor
     $0.font = UIFont(
-      name: "SFProText-Semibold",
+      name: Constant.Title.fontName,
       size: Constant.Title.fontSize)
     $0.numberOfLines = 0
     $0.textAlignment = .center
@@ -30,18 +30,44 @@ final class FeedNavigationBar: UIView {
       attributes: [.kern: -0.41, .paragraphStyle: paragraphStyle])
   }
   
-  private let userPostSearchButton = UIButton().set {
+  private lazy var postSearchButton = UIButton().set {
     $0.translatesAutoresizingMaskIntoConstraints = false
-    let image = UIImage(named: "search")
-    $0.setImage(image?.setColor(Constant.UserPostSearch.normalColor), for: .normal)
-    $0.setImage(image?.setColor(Constant.UserPostSearch.highlightColor), for: .highlighted)
+    let image = Constant.postSearch.image
+    $0.setImage(
+      image?.setColor(Constant.postSearch.normalColor),
+      for: .normal)
+    $0.setImage(
+      image?.setColor(Constant.postSearch.highlightColor),
+      for: .highlighted)
   }
   
-  private let notificationButton = UIButton().set {
+  private lazy var notificationButton = UIButton().set {
     $0.translatesAutoresizingMaskIntoConstraints = false
-    let image = UIImage(named: "notification")
-    $0.setImage(image?.setColor(UIColor(hex: "#484848")), for: .normal)
-    $0.setImage(image?.setColor(UIColor(hex: "#484848").withAlphaComponent(0.5)), for: .highlighted)
+    let image = Constant.Notification.image
+    $0.setImage(
+      image?.setColor(Constant.Notification.normalColor),
+      for: .normal)
+    $0.setImage(
+      image?.setColor(Constant.Notification.highlightColor),
+      for: .highlighted)
+  }
+  
+  /// 사용자가 알림을 확인 했는지 여부를 체크하고 이에 따른 notificationIcon 상태를 rendering 합니다.
+  /// FeedViewController를 생성하기 이전에 최초로 사용자가 알림을 확인했는지 여부를 파악해야 합니다.
+  /// 추후 서버 통신할 때, 로그인 유저에게 알림을 보낸 여부 또한 확인해야 합니다.
+  /// 이 상태를 저장해 둔 후 FeedViewController가 보여질 때마다 실시간으로 이 상태를 갱신해야 합니다.
+  private var isCheckedNotification: Bool = {
+    /// userDefaults나 전역 변수를 통해 사용자에게 알림이 있거나 확인하지 않았던 알림이 있을 경우를 확인합니다.
+    false
+  }()
+  
+  /// notificationButton과 Icon, searchButton의 경우 isCheckedNotification의 확인이 된 후에야 레이아웃 지정이 가능합니다.
+  private lazy var notificationIcon = UIView().set {
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    $0.layer.cornerRadius = Constant.NotificationIcon.width/2
+    let redColor = Constant.NotificationIcon.color
+    $0.backgroundColor = isCheckedNotification ? Constant
+      .backgroundColor : redColor
   }
   
   // MARK: - Initialization
@@ -49,7 +75,7 @@ final class FeedNavigationBar: UIView {
     super.init(frame: frame)
     translatesAutoresizingMaskIntoConstraints = false
     setupUI()
-    backgroundColor = .white
+    backgroundColor = Constant.backgroundColor
     bind()
   }
   
@@ -70,7 +96,7 @@ final class FeedNavigationBar: UIView {
 extension FeedNavigationBar {
   
   private func bind() {
-    userPostSearchButton.tap.sink {
+    postSearchButton.tap.sink {
       self.delegate?.didTapUserPostSearch()
     }.store(in: &subscriptions)
     notificationButton.tap.sink {
@@ -87,19 +113,38 @@ extension FeedNavigationBar {
       trailingAnchor.constraint(equalTo: navi.trailingAnchor),
       bottomAnchor.constraint(equalTo: navi.bottomAnchor)])
   }
+  
+  func updateIsCheckedNotification() {
+    /// 이 곳에서 앱 내부에서 기록하고 있던( 앱 종료 후에도 정보 갖고 있어야 하기 때문입니다. )
+    /// 객체의 상태도 업데이트 하고 isCheckedNotification 또한 업데이트 합니다.
+    isCheckedNotification = isCheckedNotification ? false : true
+    updateFeedNavigationBarNotificationCheckState()
+    updateNotificationIconColor()
+  }
+  
+  private func updateFeedNavigationBarNotificationCheckState() {
+    /// userDefaults에 저장된 feed navigation bar notification check state를 갱신합니다.
+  }
+  
+  private func updateNotificationIconColor() {
+    let redColor = Constant.NotificationIcon.color
+    notificationIcon
+      .backgroundColor = isCheckedNotification ? .white : redColor
+  }
 }
 
 // MARK: - LayoutSupport
 extension FeedNavigationBar: LayoutSupport {
   func addSubviews() {
-    _=[appTitle, userPostSearchButton, notificationButton]
+    _=[appTitle, postSearchButton, notificationButton, notificationIcon]
       .map { addSubview($0) }
   }
   
   func setConstraints() {
     _=[appTitleConstraint,
        notificationButtonConstraint,
-       userPostSearchButtonConstraint]
+       postSearchButtonConstraint,
+       notificationIconConstraint]
       .map { NSLayoutConstraint.activate($0) }
   }
 }
@@ -117,16 +162,16 @@ fileprivate extension FeedNavigationBar {
       constant: -Constant.Title.bottomSpacing)]
   }
   
-  var userPostSearchButtonConstraint: [NSLayoutConstraint] {
-    [userPostSearchButton.topAnchor.constraint(
+  var postSearchButtonConstraint: [NSLayoutConstraint] {
+    [postSearchButton.topAnchor.constraint(
       equalTo: topAnchor,
-      constant: Constant.UserPostSearch.topSpacing),
-     userPostSearchButton.trailingAnchor.constraint(
+      constant: Constant.postSearch.topSpacing),
+     postSearchButton.trailingAnchor.constraint(
       equalTo: notificationButton.leadingAnchor,
-      constant: -Constant.UserPostSearch.trailingSpacing),
-     userPostSearchButton.bottomAnchor.constraint(
+      constant: -Constant.postSearch.trailingSpacing),
+     postSearchButton.bottomAnchor.constraint(
       equalTo: bottomAnchor,
-      constant: -Constant.UserPostSearch.bottomSpacing)]
+      constant: -Constant.postSearch.bottomSpacing)]
   }
   
   var notificationButtonConstraint: [NSLayoutConstraint] {
@@ -134,10 +179,22 @@ fileprivate extension FeedNavigationBar {
       equalTo: topAnchor,
       constant: Constant.Notification.topSpacaing),
      notificationButton.trailingAnchor.constraint(
-      equalTo: trailingAnchor,
-     constant: -Constant.Notification.trailingSpacing),
+      equalTo: notificationIcon.leadingAnchor),
      notificationButton.bottomAnchor.constraint(
       equalTo: bottomAnchor,
       constant: -Constant.Notification.bottomSpacing)]
+  }
+  
+  var notificationIconConstraint: [NSLayoutConstraint] {
+    [notificationIcon.topAnchor.constraint(
+      equalTo: topAnchor,
+      constant: Constant.NotificationIcon.topSpacing),
+     notificationIcon.trailingAnchor.constraint(
+      equalTo: trailingAnchor,
+      constant: -Constant.NotificationIcon.trailingSpacing),
+     notificationIcon.widthAnchor.constraint(
+      equalToConstant: Constant.NotificationIcon.width),
+     notificationIcon.heightAnchor.constraint(
+      equalToConstant: Constant.NotificationIcon.height)]
   }
 }
