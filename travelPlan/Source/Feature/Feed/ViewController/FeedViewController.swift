5395226aa053911c069f3ac7be68ceadcdd90440
ysh <happysh_s2@naver.com>
2023-05-12 07:00:51 +0900

    [Feat] ViewModel 추가, FeedNavigationBar로부터 발생한 이벤트 VM한테 전달 기능 구현

--- travelPlan/Source/Feature/Feed/ViewController/FeedViewController.swift
+++ travelPlan/Source/Feature/Feed/ViewController/FeedViewController.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+import Combine
 
 class FeedViewController: UIViewController {
   // MARK: - Properties
   private let categoryPageView = CategoryPageView()
+  private var feedNavigationBar = FeedNavigationBar(isCheckedNotification: nil)
+  private let vm = FeedViewModel()
+  private var subscription = Set<AnyCancellable>()
   
-  private let feedNavigationBar = FeedNavigationBar()
+  private let tapPostSearch = PassthroughSubject<Void, FeedViewModelError>()
+  private let tapNotification = PassthroughSubject<Void, FeedViewModelError>()
+  private let appear = PassthroughSubject<Void, FeedViewModelError>()
   
   // MARK: - Lifecycle
+  override func viewWillAppear(_ animated: Bool) {
+    appear.send()
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     configureUI()
@@ -23,26 +33,76 @@ class FeedViewController: UIViewController {
 // MARK: - Helpers
 extension FeedViewController {
   private func configureUI() {
+    configureFeedNavigationBar()
     setupUI()
-    navigationController?.navigationBar.backgroundColor = .white
     view.backgroundColor = .white
+    bind()
+  }
+  private func configureFeedNavigationBar() {
     guard let naviBar = navigationController?.navigationBar else { return }
     feedNavigationBar.delegate = self
     feedNavigationBar.layoutFrom(naviBar)
   }
 }
 
+// MARK: - ViewBindCase
+extension FeedViewController: ViewBindCase {
+  typealias State = FeedViewControllerState
+  
+  func bind() {
+    let input = FeedViewControllerEvent(
+      appear: appear.eraseToAnyPublisher(),
+      didTapPostSearch: tapPostSearch.eraseToAnyPublisher(),
+      didTapNotification: tapNotification.eraseToAnyPublisher())
+    let output = vm.transform(input)
+    
+    output
+      .receive(on: RunLoop.main)
+      .sink { [weak self] result in
+        switch result {
+        case .finished:
+          break
+        case .failure(let error):
+          self?.handleError(error)
+        }
+      } receiveValue: { self.render($0) }
+      .store(in: &subscription)
+  }
+  func render(_ state: State) {
+    switch state {
+    case .none:
+      break
+    case .initFeedNaviBarIsCheckedNotification(let value):
+      feedNavigationBar.updateIsCheckedNotification(value)
+    case .updateNotificationRedIcon:
+      feedNavigationBar.updateIsCheckedNotification(false)
+    case .goToPostSearch:
+      // goto user post search naivgationController
+      print("DEBUG: User post search event occured")
+    case .goToNotification(let isChecked):
+      // goto notifiation with naivgationController
+      print("DEBUG: User notification event occured")
+      feedNavigationBar.updateIsCheckedNotification(isChecked)
+    }
+  }
+  
+  func handleError(_ error: FeedViewModelError) {
+    switch error {
+    case .none:
+      break
+    }
+  }
+}
+
+// MARK: - FeedNavigationBarDelegate
 extension FeedViewController: FeedNavigationBarDelegate {
-  func didTapUserPostSearch() {
-    // goto user post search naivgationController
-    print("DEBUG: User post search event occured")
+  func didTapPostSearch() {
+    tapPostSearch.send()
   }
 
   func didTapNotification() {
-    // goto notifiation with naivgationController
-    print("DEBUG: User notification event occured")
+    tapNotification.send()
   }
-
 }
 
 // MARK: - LayoutSupport
