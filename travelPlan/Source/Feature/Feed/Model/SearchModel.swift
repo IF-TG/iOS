de423c2a1fbd5a9651906e89e029883288f24da3
seokhyun <letushyun@gmail.com>
2023-05-12 15:40:51 +0900

    [Feat] mock model, vm 추가

---
+++ travelPlan/Source/Feature/Feed/Model/SearchModel.swift
@@ -0,0 +1,21 @@
+//
+//  RecommendationSearchTagModel.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 2023/05/10.
+//
+
+import Foundation
+
+struct SearchModel {
+  // MARK: - Properties
+  static let recommendationSearchKeyword = [
+    "추천1", "추천11", "추천111", "추천11", "추천", "추천111111", "추천11", "추천11", "추천", "추", "추천1",
+    "추천11", "추천", "추천1111"
+  ]
+  
+  static let recentSearchKeyword = [
+    "최근검색1", "최근검색2", "최근검색3", "최근검색4", "최근검색5"
+  ]
+  static let headerTitle = ["추천 검색", "최근 검색"]
+}
---
+++ travelPlan/Source/Feature/Feed/ViewModel/UserPostSearchViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  UserPostSearchViewModel.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 2023/05/10.
+//
+
+import Foundation
+
+final class UserPostSearchViewModel {
+  
+}
