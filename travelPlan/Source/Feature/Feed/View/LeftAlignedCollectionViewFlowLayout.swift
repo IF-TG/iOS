76968b521c296bb3f401ec6e40ac3fcc59cc8b9f
seokhyun <letushyun@gmail.com>
2023-05-12 15:39:57 +0900

    [Feat] Custom CollectionViewFlowLayout 객체 추가
    TagLabel의 문자열 길이에 따라 동적으로 변하는 Cell의 size를 계산하기 위해, customLayout객체를 만들어서 layout속성을 변경시켰습니다.

--- travelPlan/Source/Feature/Feed/View/LeftAlignedCollectionViewFlowLayout.swift
+++ travelPlan/Source/Feature/Feed/View/LeftAlignedCollectionViewFlowLayout.swift
@@ -18,7 +18,7 @@ class LeftAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
     var maxY: CGFloat = -1.0
     
     attributes.forEach { layoutAttribute in
-      if layoutAttribute.representedElementCategory == .cell { // cell일 경우
+      if layoutAttribute.representedElementCategory == .cell {
         // 한 cell의 y값이 이전 cell들이 들어갔던 line의 y값보다 크다면
         if layoutAttribute.frame.origin.y >= maxY {
           // x좌표 left에서 시작 (default값을 -1 주었기 때문에 처음은 무조건 실행)
