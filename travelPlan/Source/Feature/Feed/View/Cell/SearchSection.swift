3a12c6acb1f95d252562c4462314f0a864184e8b
seokhyun <letushyun@gmail.com>
2023-05-12 15:21:15 +0900

    [Feat] Cell Section 구분

---
+++ travelPlan/Source/Feature/Feed/View/Cell/SearchSection.swift
@@ -0,0 +1,13 @@
+//
+//  SearchSection.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 2023/05/10.
+//
+
+import Foundation
+
+enum SearchSection: Int, CaseIterable {
+  case recommendation
+  case recent
+}
