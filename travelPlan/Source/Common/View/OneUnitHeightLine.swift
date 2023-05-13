e1c775d53daee4c05284ab33df2df9e39ee4f729
ysh <happysh_s2@naver.com>
2023-05-14 01:00:57 +0900

    [Feat] height 1인 gray line UIView 추가

---
+++ travelPlan/Source/Common/View/OneUnitHeightLine.swift
@@ -0,0 +1,84 @@
+//
+//  OneUnitHeightLine.swift
+//  travelPlan
+//
+//  Created by 양승현 on 2023/05/14.
+//
+
+import UIKit
+
+final class OneUnitHeightLine: UIView {
+  // MARK: - Constants
+  private let height: CGFloat = 1
+  
+  // MARK: - Initialization
+  fileprivate override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  convenience init(_ color: UIColor = .yg.gray0) {
+    self.init(frame: .zero)
+  }
+}
+
+//MARK: -  Constants
+extension OneUnitHeightLine {
+  struct Spacing {
+    let leading: CGFloat
+    let trailing: CGFloat
+    let bottom: CGFloat
+    let top: CGFloat
+    init(leading: CGFloat = 0, trailing: CGFloat = 0, bottom: CGFloat = 0, top: CGFloat = 0) {
+      self.leading = leading
+      self.trailing = trailing
+      self.bottom = bottom
+      self.top = top
+    }
+  }
+}
+
+// MARK: - Helpers
+extension OneUnitHeightLine {
+  
+  fileprivate func configureUI(_ color: UIColor) {
+    translatesAutoresizingMaskIntoConstraints = false
+    heightAnchor.constraint(
+      equalToConstant: height).isActive = true
+    backgroundColor = color
+  }
+  
+  // superView만 필요한 경우
+  func lineConstraint(
+    from superView: UIView,
+    spacing: Spacing
+  ) {
+    superView.addSubview(self)
+    NSLayoutConstraint.activate([
+      leadingAnchor.constraint(
+        equalTo: superView.leadingAnchor,
+        constant: spacing.leading),
+      bottomAnchor.constraint(
+        equalTo: superView.bottomAnchor,
+        constant: -spacing.bottom),
+      trailingAnchor.constraint(
+        equalTo: superView.trailingAnchor,
+        constant: -spacing.trailing)
+    ])
+  }
+  // superView랑 line 위에 view가 있는 경우
+  func lineConstraint(
+    fromTopView topView: UIView,
+    superView: UIView,
+    spacing: Spacing
+  ) {
+    topAnchor.constraint(
+      equalTo: topView.bottomAnchor,
+      constant: spacing.top).isActive = true
+    lineConstraint(from: superView, spacing: spacing)
+  }
+  
+}
