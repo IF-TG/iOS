8bf048b791303d150c332ea2aa9ae04cd24b5289
seokhyun <letushyun@gmail.com>
2023-05-14 02:14:34 +0900

    [Fix] 최근 검색 Tag Cell의 버튼 layout이 잡히지 않는 문제를 해결했습니다.

--- travelPlan/Source/Feature/Feed/View/Cell/SearchTagCell.swift
+++ travelPlan/Source/Feature/Feed/View/Cell/SearchTagCell.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class SearchTagCell: UICollectionViewCell {
   // MARK: - Properties
-  var type: SearchSection?
+  var sectionType: SearchSection?
   
   static var id: String {
     return String(describing: self)
@@ -18,6 +18,8 @@ class SearchTagCell: UICollectionViewCell {
   private let tagLabel: UILabel = UILabel().set {
     $0.font = .systemFont(ofSize: 14)
     $0.textColor = .black
+    $0.numberOfLines = 1
+    $0.lineBreakMode = .byClipping
   }
   
   private var cancelButton: UIButton?
@@ -26,69 +28,86 @@ class SearchTagCell: UICollectionViewCell {
   override init(frame: CGRect) {
     super.init(frame: frame)
     setupUI()
-    contentView.backgroundColor = .systemBackground
-    contentView.layer.cornerRadius = 13
-    contentView.layer.borderColor = UIColor(hex: "D9D9D9").cgColor
-    contentView.layer.borderWidth = 1
+    setupStyles()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-  
-  override func layoutSubviews() {
-    super.layoutSubviews()
-    setupUI()
-  }
 }
 
 // MARK: - Helpers
 extension SearchTagCell {
+  private func setupStyles() {
+    self.contentView.backgroundColor = .systemBackground
+    self.contentView.layer.cornerRadius = 13
+    self.contentView.layer.borderColor = UIColor(hex: "D9D9D9").cgColor
+    self.contentView.layer.borderWidth = 1
+  }
+  
+  private func makeCancelButton() -> UIButton {
+    self.cancelButton = UIButton()
+    guard let cancelButton = self.cancelButton else { return UIButton() }
+    cancelButton.setImage(UIImage(systemName: "xmark"), for: .normal)
+    cancelButton.tintColor = UIColor(hex: "#484848")
+    return cancelButton
+  }
+  
   func configure(_ text: String) {
     tagLabel.text = text
+  }
+  
+  func initSectionType(with type: SearchSection) {
+    switch sectionType {
+    case .none:
+      self.sectionType = type
+      setupUI()
+    case .recent, .recommendation: return
+    }
   }
 }
 
 // MARK: - LayoutSupport
 extension SearchTagCell: LayoutSupport {
   func addSubviews() {
-    self.contentView.addSubview(tagLabel)
-//    if type == .recent {
-//      self.cancelButton = UIButton(type: .close)
-//      guard let cancelButton = self.cancelButton else { return }
-//      self.contentView.addSubview(cancelButton)
-//    }
+    switch sectionType {
+    case .recent:
+      // Cancel Button 추가
+      let cancelButton = makeCancelButton()
+      self.contentView.addSubview(cancelButton)
+      fallthrough
+    case .recommendation: self.contentView.addSubview(tagLabel)
+    case .none: break
+    }
   }
   
   func setConstraints() {
-    tagLabel.snp.makeConstraints {
-      $0.center.equalToSuperview() // case .recommendation: center 고정
+    switch sectionType {
+    case .recommendation:
+      tagLabel.snp.makeConstraints {
+        $0.leading.equalToSuperview().inset(13)
+        $0.trailing.equalToSuperview().inset(13)
+        $0.centerY.equalToSuperview()
+      }
+    case .recent:
+      guard let cancelButton = cancelButton else { return }
+      
+      tagLabel.snp.makeConstraints {
+        $0.leading.equalToSuperview().inset(13)
+        $0.centerY.equalToSuperview()
+      }
+      
+      cancelButton.snp.makeConstraints {
+        $0.leading.equalTo(tagLabel.snp.trailing).offset(4)
+        $0.trailing.equalToSuperview().inset(13)
+        $0.centerY.equalTo(tagLabel)
+        $0.size.equalTo(10)
+      }
+    case .none: break
     }
-    
-    
-    //    switch type {
-    //    case .recommendation:
-    //      tagLabel.snp.makeConstraints {
-    ////        $0.centerX.equalToSuperview()
-    ////        $0.leading.equalToSuperview().inset(13)
-    ////        $0.centerY.equalToSuperview()
-    ////        $0.top.equalToSuperview().inset(4)
-    //        $0.center.equalToSuperview()
-    //      }
-    //    case .recent: break
-    ////      tagLabel.snp.makeConstraints {
-    ////        $0.leading.equalToSuperview().inset(13)
-    ////        $0.centerY.equalToSuperview()
-    ////        $0.top.equalToSuperview().inset(4)
-    ////      }
-    ////
-    ////      guard let cancelButton = cancelButton else { return }
-    ////
-    ////      cancelButton.snp.makeConstraints {
-    ////        $0.leading.equalTo(tagLabel.snp.trailing).offset(4)
-    ////        $0.centerY.equalTo(tagLabel)
-    ////      }
-    //    case .none: break
-    //    }
   }
 }
+
+//protocol CancelButtonDelegate: AnyObject {
+//  func
+//}
