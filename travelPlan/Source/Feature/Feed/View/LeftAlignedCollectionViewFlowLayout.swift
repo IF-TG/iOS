34d0b4d0432daa8d39215b6a38bfc99440dd4e09
seokhyun <letushyun@gmail.com>
2023-05-14 02:11:16 +0900

    [Fix] text가 화면의 width를 넘어서는 경우, Cell의 layout이 망가지는 문제를 수정했습니다.

--- travelPlan/Source/Feature/Feed/View/LeftAlignedCollectionViewFlowLayout.swift
+++ travelPlan/Source/Feature/Feed/View/LeftAlignedCollectionViewFlowLayout.swift
@@ -8,17 +8,26 @@
 import UIKit
 
 class LeftAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
+  // MARK: - Properties
   override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
     // layout 속성값
     guard let attributes = super.layoutAttributesForElements(in: rect) else { return [] }
     
     // contentView의 left 여백
     var leftMargin = self.sectionInset.left
-    // cell 라인의 y의 default
+    // cell 라인 y의 start
     var maxY: CGFloat = -1.0
+    let edgeInset: CGFloat = 20
     
     attributes.forEach { layoutAttribute in
       if layoutAttribute.representedElementCategory == .cell {
+        
+        guard let collectionView = collectionView else { return }
+        // Label의 text가 화면을 넘어가는 경우, cell size 고정
+        if layoutAttribute.frame.width >= collectionView.frame.width - (edgeInset * 2) {
+          layoutAttribute.frame.size.width = collectionView.frame.width - (edgeInset * 2)
+        }
+        
         // 한 cell의 y값이 이전 cell들이 들어갔던 line의 y값보다 크다면
         if layoutAttribute.frame.origin.y >= maxY {
           // x좌표 left에서 시작 (default값을 -1 주었기 때문에 처음은 무조건 실행)
