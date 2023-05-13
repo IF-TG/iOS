e0bf181c2e000ac24e693b2224a84575b15363f5
seokhyun <letushyun@gmail.com>
2023-05-14 03:21:13 +0900

    [Refector] 중복 코드 함수화

--- travelPlan/Source/Feature/Feed/ViewController/UserPostSearchViewController.swift
+++ travelPlan/Source/Feature/Feed/ViewController/UserPostSearchViewController.swift
@@ -10,6 +10,8 @@ import SnapKit
 
 final class UserPostSearchViewController: UIViewController {
   // MARK: - Properties
+  let viewModel = UserPostSearchViewModel()
+  
   private var dataSource: String?
   
   private lazy var leftAlignedCollectionViewFlowLayout:
@@ -28,14 +30,15 @@ final class UserPostSearchViewController: UIViewController {
     $0.delegate = self
     $0.dataSource = self
     $0.collectionViewLayout = leftAlignedCollectionViewFlowLayout
+    
     $0.register(
       SearchTagCell.self,
       forCellWithReuseIdentifier: SearchTagCell.id
     )
     $0.register(
-      TitleSupplementaryView.self,
+      UserPostSearchHeaderView.self,
       forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
-      withReuseIdentifier: TitleSupplementaryView.id
+      withReuseIdentifier: UserPostSearchHeaderView.id
     )
   }
   
@@ -53,7 +56,19 @@ final class UserPostSearchViewController: UIViewController {
 
 // MARK: - Helpers
 extension UserPostSearchViewController {
-
+  private func fetchHeaderTitle(
+    _ headerView: UserPostSearchHeaderView,
+    at type: SearchSection
+  ) -> String {
+    headerView.initSectionType(with: type)
+    
+    switch type {
+    case .recommendation:
+      return viewModel.recommendationModel.headerTitle
+    case .recent:
+      return viewModel.recentModel.headerTitle
+    }
+  }
 }
 
 // MARK: - LayoutSupport
@@ -72,47 +87,56 @@ extension UserPostSearchViewController: LayoutSupport {
 
 // MARK: - UICollectionViewDelegateFlowLayout
 extension UserPostSearchViewController: UICollectionViewDelegateFlowLayout {
-  // cell의 size가 string값에 따라 달라지도록 작업
-    // label에 string 대입 후, label의 size를 이용
-  
-  // FIXME: - UILabel 사이즈 개선
+  // sizeFIXME: - UILabel 사이즈 개선
   func collectionView(
     _ collectionView: UICollectionView,
     layout collectionViewLayout: UICollectionViewLayout,
     sizeForItemAt indexPath: IndexPath
   ) -> CGSize {
-
-    let dummyLabel = UILabel()
-    dummyLabel.font = .systemFont(ofSize: 14)
-    
     let widthPadding: CGFloat = 13
     let heightPadding: CGFloat = 4
-
+    
     if indexPath.section == SearchSection.recommendation.rawValue {
-      dummyLabel.text = SearchModel.recommendationSearchKeyword[indexPath.item]
-      dummyLabel.sizeToFit()
+      let text = viewModel.recommendationModel.keywords[indexPath.item]
+      let textSize = (text as NSString)
+        .size(withAttributes: [.font: UIFont.systemFont(ofSize: 14)])
       
-      let labelSize = dummyLabel.frame.size
-
       return CGSize(
-        width: labelSize.width + (widthPadding * 2),
-        height: labelSize.height + (heightPadding * 2)
+        width: textSize.width + (widthPadding * 2),
+        height: textSize.height + (heightPadding * 2)
       )
     } else if indexPath.section == SearchSection.recent.rawValue {
-
-      dummyLabel.text = SearchModel.recentSearchKeyword[indexPath.item]
-      dummyLabel.sizeToFit()
+      let text = viewModel.recentModel.keywords[indexPath.item]
+      let textSize = (text as NSString)
+        .size(withAttributes: [.font: UIFont.systemFont(ofSize: 14)])
       
-      let labelSize = dummyLabel.frame.size
-      
-      let cancelButton = UIButton(type: .close)
+      // widthSizeFIXME: - 버튼의 width 계산하기
+      // AutoLayout을 사용할 수 없어서 문제를 해결하지 못하여, 임시 방편으로 buttonWidth 상수를 사용했습니다.
+      let buttonWidth: CGFloat = 10
       let componentPadding: CGFloat = 4
-
+      
+      let width = textSize.width + componentPadding + buttonWidth + (widthPadding * 2)
+      
       return CGSize(
-        width: labelSize.width + cancelButton.frame.width + componentPadding,
-        height: labelSize.height + (heightPadding * 2)
+        width: width,
+        height: textSize.height + (heightPadding * 2)
       )
     } else { return CGSize() }
+  }
+  
+  func collectionView(
+    _ collectionView: UICollectionView,
+    didSelectItemAt indexPath: IndexPath
+  ) {
+    guard let searchTagCell = collectionView.cellForItem(at: indexPath) as? SearchTagCell else { return }
+    
+    switch searchTagCell.sectionType {
+    case .recommendation:
+      print("추천 검색어: \(viewModel.recommendationModel.keywords[indexPath.item]) tapped")
+    case .recent:
+      print("추천 검색어: \(viewModel.recentModel.keywords[indexPath.item]) tapped")
+    case .none: return
+    }
   }
 }
 
@@ -127,9 +151,9 @@ extension UserPostSearchViewController: UICollectionViewDataSource {
     numberOfItemsInSection section: Int
   ) -> Int {
     if section == SearchSection.recommendation.rawValue {
-      return SearchModel.recommendationSearchKeyword.count
+      return viewModel.recommendationModel.keywords.count
     } else if section == SearchSection.recent.rawValue {
-      return SearchModel.recentSearchKeyword.count
+      return viewModel.recentModel.keywords.count
     } else { return 0 }
   }
   
@@ -145,14 +169,14 @@ extension UserPostSearchViewController: UICollectionViewDataSource {
     
     // 하나의 Cell class를 재사용해서 변형시키므로, section별로 Cell 구분화
     if indexPath.section == SearchSection.recommendation.rawValue {
-      searchTagCell.type = .recommendation
-      dataSource = SearchModel.recommendationSearchKeyword[indexPath.item]
+      searchTagCell.initSectionType(with: .recommendation)
+      self.dataSource = viewModel.recommendationModel.keywords[indexPath.item]
     } else if indexPath.section == SearchSection.recent.rawValue {
-      searchTagCell.type = .recent
-      dataSource = SearchModel.recentSearchKeyword[indexPath.item]
+      searchTagCell.initSectionType(with: .recent)
+      self.dataSource = viewModel.recentModel.keywords[indexPath.item]
     }
     
-    guard let dataSource = dataSource else { return searchTagCell }
+    guard let dataSource = self.dataSource else { return searchTagCell }
     searchTagCell.configure(dataSource)
     
     return searchTagCell
@@ -165,17 +189,26 @@ extension UserPostSearchViewController: UICollectionViewDataSource {
   ) -> UICollectionReusableView {
     switch kind {
     case UICollectionView.elementKindSectionHeader:
+      
+      // 최근 검색인 경우 전체삭제 버튼 추가
       guard let titleHeaderView = collectionView.dequeueReusableSupplementaryView(
         ofKind: kind,
-        withReuseIdentifier: TitleSupplementaryView.id,
+        withReuseIdentifier: UserPostSearchHeaderView.id,
         for: indexPath
-      ) as? TitleSupplementaryView else { return UICollectionReusableView() }
+      ) as? UserPostSearchHeaderView else { return UICollectionReusableView() }
       
-      titleHeaderView.prepare(title: SearchModel.headerTitle[indexPath.item])
+      var titleString = ""
+      
+      if indexPath.section == SearchSection.recommendation.rawValue {
+        titleString = fetchHeaderTitle(titleHeaderView, at: .recommendation)
+      } else if indexPath.section == SearchSection.recent.rawValue {
+        titleString = fetchHeaderTitle(titleHeaderView, at: .recent)
+      } else { return UICollectionReusableView() }
+      
+      titleHeaderView.prepare(title: titleString)
       
       return titleHeaderView
-    default:
-      return UICollectionReusableView()
+    default: return UICollectionReusableView()
     }
   }
 }
