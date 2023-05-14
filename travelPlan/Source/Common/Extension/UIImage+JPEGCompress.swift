7ae472ae05f8231983aee34d0c9879fba0253c1e
ysh <happysh_s2@naver.com>
2023-05-15 01:48:18 +0900

    [Feat] image 화질 압축 기능 추가

--- travelPlan/Source/Common/Extension/UIImage+JPEGCompress.swift
+++ travelPlan/Source/Common/Extension/UIImage+JPEGCompress.swift
@@ -5,4 +5,15 @@
 //  Created by 양승현 on 2023/05/15.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+  /// 이미지 compressionQuality로 해상도 조절
+  func compressJPEGImage(with compressionQuality: CGFloat = 0.5) -> UIImage? {
+    guard let imageData = self.jpegData(compressionQuality: compressionQuality),
+          let compressedImage = UIImage(data: imageData) else {
+      return nil
+    }
+    return compressedImage
+  }
+}
