a72fa3560c4d535a93b6d989b9380a95ce530e2c
ysh <happysh_s2@naver.com>
2023-05-15 01:51:19 +0900

    [Docs] UIImage compress 코드 리뷰

--- travelPlan/Source/Common/Extension/UIImage+JPEGCompress.swift
+++ travelPlan/Source/Common/Extension/UIImage+JPEGCompress.swift
@@ -9,6 +9,12 @@ import UIKit
 
 extension UIImage {
   /// 이미지 compressionQuality로 해상도 조절
+  /// # Example #
+  /// ```
+  /// Example:
+  /// let image = UIImage()
+  /// let compressedImg = image.compressJPEGImage(with: 0.7)
+  /// ```
   func compressJPEGImage(with compressionQuality: CGFloat = 0.5) -> UIImage? {
     guard let imageData = self.jpegData(compressionQuality: compressionQuality),
           let compressedImage = UIImage(data: imageData) else {
