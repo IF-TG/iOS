ec99220cef487cba81c3a62b632246dfaf587c31
seokhyun <letushyun@gmail.com>
2023-05-17 01:43:22 +0900

    [Refactor] Combine 도입

--- travelPlan/Source/Feature/Feed/ViewController/UserPostSearchViewController.swift
+++ travelPlan/Source/Feature/Feed/ViewController/UserPostSearchViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SnapKit
+import Combine
 
 final class UserPostSearchViewController: UIViewController {
   // MARK: - Properties
@@ -24,12 +25,11 @@ final class UserPostSearchViewController: UIViewController {
   
   private lazy var collectionView: UICollectionView = UICollectionView(
     frame: .zero,
-    collectionViewLayout: .init()
+    collectionViewLayout: leftAlignedCollectionViewFlowLayout
   ).set {
     $0.backgroundColor = .systemBackground
     $0.delegate = self
     $0.dataSource = self
-    $0.collectionViewLayout = leftAlignedCollectionViewFlowLayout
     
     $0.register(
       SearchTagCell.self,
@@ -42,11 +42,28 @@ final class UserPostSearchViewController: UIViewController {
     )
   }
   
+  private var subscriptions = Set<AnyCancellable>()
+  private let _viewDidLoad = PassthroughSubject<Void, Never>()
+  private let _didTapTagCell = PassthroughSubject<Void, Never>()
+  private let _didTapDeleteButton = PassthroughSubject<Void, Never>()
+  private let _didTapDeleteAllButton = PassthroughSubject<Void, Never>()
+  private let _didTapView = PassthroughSubject<Void, Never>()
+  private let _didTapSearchTextField = PassthroughSubject<Void, Never>()
+  private let _didTapCancelButton = PassthroughSubject<Void, Never>()
+  private let _didTapSearchButton = PassthroughSubject<Void, Never>()
+  private let _editingTextField = PassthroughSubject<Void, Never>()
+
   // MARK: - LifeCycle
   override func viewDidLoad() {
     super.viewDidLoad()
     self.view.backgroundColor = .white
     setupUI()
+    bind()
+//    BaseNavigationController.configureBackButtonItem(title: "헬로", target: self)
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
     leftAlignedCollectionViewFlowLayout.headerReferenceSize = CGSize(
       width: self.collectionView.frame.width,
       height: 60
@@ -56,6 +73,47 @@ final class UserPostSearchViewController: UIViewController {
 
 // MARK: - Helpers
 extension UserPostSearchViewController {
+  private func bind() {
+    let input = UserPostSearchEvent(
+      viewDidLoad: _viewDidLoad.eraseToAnyPublisher(),
+      didTapTagCell: _didTapTagCell.eraseToAnyPublisher(),
+      didTapDeleteButton: _didTapDeleteButton.eraseToAnyPublisher(),
+      didTapDeleteAllButton: _didTapDeleteAllButton.eraseToAnyPublisher(),
+      didTapView: _didTapView.eraseToAnyPublisher(),
+      didTapSearchTextField: _didTapSearchTextField.eraseToAnyPublisher(),
+      didTapCancelButton: _didTapCancelButton.eraseToAnyPublisher(),
+      didTapSearchButton: _didTapSearchButton.eraseToAnyPublisher(),
+      editingTextField: _editingTextField.eraseToAnyPublisher()
+    )
+    
+    let output = viewModel.transform(input)
+    output
+      .sink { result in
+        switch result {
+        case .finished:
+          print("completed")
+        case let .failure(error):
+          print("error: \(error.localizedDescription)")
+        }
+      } receiveValue: { [weak self] in
+        self?.render($0)
+      }
+      .store(in: &subscriptions)
+  }
+  
+  private func render(_ state: UserPostSearchState) {
+    switch state {
+    case .none: break
+    case .gotoBack:
+      print("DEBUG: UserPostSearchVC -> FeedVC")
+    case .gotoSearch:
+      print("DEBUG: UserPostSearchVC -> SearchResultVC")
+    case .deleteAllCells: break
+      
+    case .deleteCell: break
+    }
+  }
+  
   private func fetchHeaderTitle(
     _ headerView: UserPostSearchHeaderView,
     at type: SearchSection
@@ -68,6 +126,14 @@ extension UserPostSearchViewController {
     case .recent:
       return viewModel.recentModel.headerTitle
     }
+  }
+}
+
+// MARK: - Actions
+extension UserPostSearchViewController {
+  @objc private func didTapSearchButton(_ button: UIButton) {
+//    _didTapSearchButton.send(텍스트필드.text)
+    print("DEBUG: search button tapped!")
   }
 }
 
@@ -133,8 +199,9 @@ extension UserPostSearchViewController: UICollectionViewDelegateFlowLayout {
     switch searchTagCell.sectionType {
     case .recommendation:
       print("추천 검색어: \(viewModel.recommendationModel.keywords[indexPath.item]) tapped")
+//      self.navigationController?.pushViewController(DummyViewController(), animated: true)
     case .recent:
-      print("추천 검색어: \(viewModel.recentModel.keywords[indexPath.item]) tapped")
+      print("최근 검색어: \(viewModel.recentModel.keywords[indexPath.item]) tapped")
     case .none: return
     }
   }
@@ -150,11 +217,13 @@ extension UserPostSearchViewController: UICollectionViewDataSource {
     _ collectionView: UICollectionView,
     numberOfItemsInSection section: Int
   ) -> Int {
-    if section == SearchSection.recommendation.rawValue {
+    switch section {
+    case SearchSection.recommendation.rawValue:
       return viewModel.recommendationModel.keywords.count
-    } else if section == SearchSection.recent.rawValue {
+    case SearchSection.recent.rawValue:
       return viewModel.recentModel.keywords.count
-    } else { return 0 }
+    default: return 0
+    }
   }
   
   func collectionView(
@@ -162,20 +231,22 @@ extension UserPostSearchViewController: UICollectionViewDataSource {
     cellForItemAt indexPath: IndexPath
   ) -> UICollectionViewCell {
     
+//    viewModel.collectionView(collectionView, cellForItemAt: indexPath)
     guard let searchTagCell = collectionView.dequeueReusableCell(
       withReuseIdentifier: SearchTagCell.id,
       for: indexPath
     ) as? SearchTagCell else { return UICollectionViewCell() }
     
     // 하나의 Cell class를 재사용해서 변형시키므로, section별로 Cell 구분화
-    if indexPath.section == SearchSection.recommendation.rawValue {
+    switch indexPath.section {
+    case SearchSection.recommendation.rawValue:
       searchTagCell.initSectionType(with: .recommendation)
       self.dataSource = viewModel.recommendationModel.keywords[indexPath.item]
-    } else if indexPath.section == SearchSection.recent.rawValue {
+    case SearchSection.recent.rawValue:
       searchTagCell.initSectionType(with: .recent)
       self.dataSource = viewModel.recentModel.keywords[indexPath.item]
+    default: break
     }
-    
     guard let dataSource = self.dataSource else { return searchTagCell }
     searchTagCell.configure(dataSource)
     
@@ -190,20 +261,24 @@ extension UserPostSearchViewController: UICollectionViewDataSource {
     switch kind {
     case UICollectionView.elementKindSectionHeader:
       
-      // 최근 검색인 경우 전체삭제 버튼 추가
       guard let titleHeaderView = collectionView.dequeueReusableSupplementaryView(
         ofKind: kind,
         withReuseIdentifier: UserPostSearchHeaderView.id,
         for: indexPath
       ) as? UserPostSearchHeaderView else { return UICollectionReusableView() }
       
+//      viewModel.fetchHeaderTitle(titleHeaderView, at: .recommendation)
+      // viewModel에게 CollectionReusableView와 cell Type을 넘긴다.
+      
       var titleString = ""
       
-      if indexPath.section == SearchSection.recommendation.rawValue {
+      switch indexPath.section {
+      case SearchSection.recommendation.rawValue:
         titleString = fetchHeaderTitle(titleHeaderView, at: .recommendation)
-      } else if indexPath.section == SearchSection.recent.rawValue {
+      case SearchSection.recent.rawValue:
         titleString = fetchHeaderTitle(titleHeaderView, at: .recent)
-      } else { return UICollectionReusableView() }
+      default: break
+      }
       
       titleHeaderView.prepare(title: titleString)
       
--- travelPlan/Source/Feature/Feed/ViewModel/UserPostSearchViewModel.swift
+++ travelPlan/Source/Feature/Feed/ViewModel/UserPostSearchViewModel.swift
@@ -6,8 +6,51 @@
 //
 
 import Foundation
+import Combine
 
 final class UserPostSearchViewModel {
+  typealias Input = UserPostSearchEvent
+  typealias Output = AnyPublisher<State, Never>
+  typealias State = UserPostSearchState
+  
+  // MARK: - Properties
   var recommendationModel = RecommendationSearch()
   var recentModel = RecentSearch()
+}
+
+// MARK: - ViewModelCase
+extension UserPostSearchViewModel: ViewModelCase {
+  func transform(_ input: Input) -> Output {
+    let _viewDidLoad = input.viewDidLoad
+      .receive(on: RunLoop.main)
+      .map { _ -> State in return .none }
+      .eraseToAnyPublisher()
+    
+    return Publishers.MergeMany([
+      _viewDidLoad
+    ])
+    .eraseToAnyPublisher()
+  }
+}
+
+// MARK: - Input
+struct UserPostSearchEvent {
+  let viewDidLoad: AnyPublisher<Void, Never>
+  let didTapTagCell: AnyPublisher<Void, Never>
+  let didTapDeleteButton: AnyPublisher<Void, Never>
+  let didTapDeleteAllButton: AnyPublisher<Void, Never>
+  let didTapView: AnyPublisher<Void, Never>
+  let didTapSearchTextField: AnyPublisher<Void, Never>
+  let didTapCancelButton: AnyPublisher<Void, Never>
+  let didTapSearchButton: AnyPublisher<Void, Never>
+  let editingTextField: AnyPublisher<Void, Never>
+}
+
+// MARK: - State
+enum UserPostSearchState {
+  case none
+  case gotoBack
+  case gotoSearch
+  case deleteCell
+  case deleteAllCells
 }
