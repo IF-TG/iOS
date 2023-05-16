b3f6f98a61c372f0e76b481b56ee90c18ae5440f
ysh <happysh_s2@naver.com>
2023-05-16 20:27:45 +0900

    [Refactor] 불필요 코드 삭제

--- travelPlan/Source/Feature/Feed/Constant/FeedNavigationBarConstant.swift
+++ travelPlan/Source/Feature/Feed/Constant/FeedNavigationBarConstant.swift
@@ -7,36 +7,6 @@
 
 import UIKit
 
-extension FeedRightNaviContentView {
-  enum Constant {
-    static let backgroundColor: UIColor = .white
-    enum postSearch {
-      static let image = UIImage(named: "search")
-      static let size = CGSize(width: 24, height: 24)
-      static let trailingSpacing: CGFloat = 19.3
-      static let topSpacing: CGFloat = 10.5
-      static let bottomSpacing: CGFloat = 10.5
-      static let normalColor: UIColor = .yg.gray5
-      static let highlightColor: UIColor = .yg.gray5.withAlphaComponent(0.5)
-    }
-    enum Notification {
-      static let image = UIImage(named: "notification")
-      static let size = CGSize(width: 24, height: 24)
-      static let topSpacaing: CGFloat = 9.33
-      static let bottomSpacing: CGFloat = 9.33
-      static let normalColor: UIColor = .yg.gray5
-      static let highlightColor: UIColor = .yg.gray5.withAlphaComponent(0.5)
-    }
-    enum NotificationIcon {
-      static let width: CGFloat = 5
-      static let height: CGFloat = 5
-      static let topSpacing: CGFloat = 9.33
-      static let trailingSpacing: CGFloat = 5
-      static let color: UIColor = UIColor(hex: "#FF2216")
-    }
-  }
-}
-
 // MARK: - FeedPostDearchBarItem
 extension FeedPostSearchBarItem {
   enum Constant {
