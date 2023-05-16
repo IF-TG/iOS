5bdbaa29a2f80cb1d1d73ea378391b66cb9f02f8
ysh <happysh_s2@naver.com>
2023-05-16 20:23:23 +0900

    [Docs] isCheckedNotification -> userNotificationState 적용 + 주석 추가

--- travelPlan/Source/Feature/Feed/ViewModel/FeedViewModel.swift
+++ travelPlan/Source/Feature/Feed/ViewModel/FeedViewModel.swift
@@ -76,7 +76,8 @@ fileprivate extension FeedViewModel {
       .subscribe(on: RunLoop.main)
       .tryMap { _ -> State in
         /// 이곳에서 영구저장소에 사용자가 알림을 확인했는지 확인해야합니다.
-        return .initFeedNaviBarIsCheckedNotification(isCheckedNotification: true)
+        /// 임시적으로 지금은 뷰가 나타났을 때 알림이 왔다는 가정을 했습니다. .none으로 할 경우 redIcon 사라집니다.
+        return .viewAppear(userNotificationState: .notChecked)
       }.mapError { error in
         return error as? FeedErr ?? .none
       }.eraseToAnyPublisher()
@@ -90,7 +91,7 @@ fileprivate extension FeedViewModel {
         /// 이곳에서 영구저장소에 사용자가 알림을 확인했는지 확인해야 합니다.
         /// 그리고 확인된 값을 반환해야 합니다.
         /// 지금은 임시적으로 일관된 값을 반환합니다.
-        return .goToNotification(isCheckedNotification: true)
+        return .goToNotification
       }.mapError { return $0 as? FeedErr ?? .none }
       .eraseToAnyPublisher()
   }
