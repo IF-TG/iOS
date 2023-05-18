69dba205c5295477087f4710a09c1c1e91e6df07
ysh <happysh_s2@naver.com>
2023-05-18 21:18:24 +0900

    [Feat] FeatureViewController constant 추가

--- travelPlan/Source/Feature/Favorite/Constant/FavoriteViewControllerConstant.swift
+++ travelPlan/Source/Feature/Favorite/Constant/FavoriteViewControllerConstant.swift
@@ -5,4 +5,22 @@
 //  Created by 양승현 on 2023/05/18.
 //
 
-import Foundation
+import UIKit
+
+extension FavoriteViewController {
+  enum Constant {
+    static let bgColor: UIColor = .white
+    enum NavigationBar {
+      enum Title {
+        static let color: UIColor = .yg.gray7
+        static let font: UIFont = UIFont(
+          pretendard: .semiBold, size: 18)!
+      }
+      
+      enum Setting {
+        static let iconName: String = "favoriteNavigationBarSetting"
+        static let touchedColor: UIColor = .yg.gray7.withAlphaComponent(0.5)
+      }
+    }
+  }
+}
