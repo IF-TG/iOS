1ab79fc92ae6de9ba5b1b37efd47070a10b343ce
seokhyun <letushyun@gmail.com>
2023-05-18 23:14:07 +0900

    [Test] Mock 데이터 변경

--- travelPlan/Source/Feature/Feed/Model/SearchModel.swift
+++ travelPlan/Source/Feature/Feed/Model/SearchModel.swift
@@ -17,13 +17,14 @@ struct SearchSectionItemModel {
 
 struct RecommendationSearch {
   let headerTitle = "추천 검색"
-  let keywords = ["최근검색2222zzzzzzzzzzz", "최근검색22", "최근검색3333", "최근검색", "최근검색5"]
+  let keywords = [
+    "추천1", "추천22", "추천33333", "추천444"
+  ]
 }
 
 struct RecentSearch {
   let headerTitle = "최근 검색"
   let keywords = [
-    "추천1", "추천11", "추천11111111222222222222", "추천11", "추천", "추천111111", "추천11", "추천11",
-    "추천", "추천", "추천1", "추천11", "추천", "추천1111"
+    "최근검색2222zzzzzzzzzzz", "최근검색22", "최근검색3333", "최근검색", "최근검색5"
   ]
 }
