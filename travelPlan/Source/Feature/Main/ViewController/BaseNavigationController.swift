397bad389f3bc4752cbb827061fc52cb8abf8c25
seokhyun <letushyun@gmail.com>
2023-05-18 23:14:51 +0900

    [Test] Custom Navi 구현

--- travelPlan/Source/Feature/Main/ViewController/BaseNavigationController.swift
+++ travelPlan/Source/Feature/Main/ViewController/BaseNavigationController.swift
@@ -5,7 +5,7 @@
 //  Created by SeokHyun on 2023/05/15.
 //
 
-// import UIKit
+ import UIKit
 
 /*
  default
@@ -14,55 +14,55 @@
  두 개로 이루어져 있습니다.
  */
 
-// class BaseNavigationController: UINavigationController {
+class BaseNavigationController: UINavigationController {
   // MARK: - Properties
-//  private var backButtonAppearance: UIBarButtonItemAppearance {
-//    let backButtonAppearance = UIBarButtonItemAppearance()
-//    // backButton하단에 표출되는 text를 안보이게 설정
-//    backButtonAppearance.normal.titleTextAttributes = [
-//      .foregroundColor: UIColor.clear,
-//      .font: UIFont.systemFont(ofSize: 0.0)
-//    ]
-//
-//    return backButtonAppearance
-//  }
-
-//  private static let paddingBarButtonItem: UIBarButtonItem = {
-//      let spaceBarButtonItem = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
-//      spaceBarButtonItem.width = 11 // 11만큼 왼쪽 버튼과 여백
-//
-//      return spaceBarButtonItem
-//  }()
-
+  private var backButtonAppearance: UIBarButtonItemAppearance {
+    let backButtonAppearance = UIBarButtonItemAppearance()
+    // backButton하단에 표출되는 text를 안보이게 설정
+    backButtonAppearance.normal.titleTextAttributes = [
+      .foregroundColor: UIColor.clear,
+      .font: UIFont.systemFont(ofSize: 0.0)
+    ]
+    
+    return backButtonAppearance
+  }
+  
+  private static let makePaddingBarButtonItem: UIBarButtonItem = {
+    let spaceBarButtonItem = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+    spaceBarButtonItem.width = 10 // 10만큼 왼쪽 버튼과 여백
+    
+    return spaceBarButtonItem
+  }()
+  
   // MARK: - LifeCycle
-//  override func viewDidLoad() {
-//    super.viewDidLoad()
-//    setNavigationBarAppearance()
-//  }
-// }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setNavigationBarAppearance()
+  }
+}
 
 // MARK: - Helpers
-// extension BaseNavigationController {
-//  private func setNavigationBarAppearance() {
-//    let appearance = UINavigationBarAppearance()
-//    appearance.backgroundColor = .white
-//    appearance.shadowColor = .clear
-//
-////    let image = UIImage(named: "back.button")?.withRenderingMode(.alwaysOriginal)
-////    appearance.setBackIndicatorImage(image, transitionMaskImage: image)
-////    appearance.backButtonAppearance = backButtonAppearance
-//
-//    self.navigationBar.standardAppearance = appearance
-//    self.navigationBar.compactAppearance = appearance
-//    self.navigationBar.scrollEdgeAppearance = appearance
-//    self.navigationBar.isTranslucent = false
-//    // navigationItem의 버튼 색상을 .white로 지정
-////    self.navigationBar.tintColor = .yg
-//
-//    // Default: Back Indicator Image
-//
-//  }
-// }
+extension BaseNavigationController {
+  private func setNavigationBarAppearance() {
+    let appearance = UINavigationBarAppearance()
+    appearance.backgroundColor = .white
+    appearance.shadowColor = .clear
+    
+        let image = UIImage(named: "back.button")?.withRenderingMode(.alwaysOriginal)
+        appearance.setBackIndicatorImage(image, transitionMaskImage: image)
+    //    appearance.backButtonAppearance = backButtonAppearance
+    
+    self.navigationBar.standardAppearance = appearance
+    self.navigationBar.compactAppearance = appearance
+    self.navigationBar.scrollEdgeAppearance = appearance
+    self.navigationBar.isTranslucent = false
+    // navigationItem의 버튼 색상을 .white로 지정
+    //    self.navigationBar.tintColor = .yg
+    
+    // Default: Back Indicator Image
+    
+  }
+}
 
 // 1. rightBarButtonItem을 설정
 // 2.
