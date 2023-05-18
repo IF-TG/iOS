68f93135164f28b3ce703e8e94dea26fe6f06ab5
seokhyun <letushyun@gmail.com>
2023-05-18 23:17:11 +0900

    [Feat] SearchCell FooterView 추가

--- travelPlan/Source/Feature/Feed/View/Search/RecommendationSearchFooterView.swift
+++ travelPlan/Source/Feature/Feed/View/Search/RecommendationSearchFooterView.swift
@@ -1,5 +1,5 @@
 //
-//  RecommendationSearchHeaderView.swift
+//  RecommendationSearchFooterView.swift
 //  travelPlan
 //
 //  Created by SeokHyun on 2023/05/17.
@@ -7,18 +7,35 @@
 
 import UIKit
 
-class RecommendationSearchHeaderView: UICollectionReusableView {
+class RecommendationSearchFooterView: UICollectionReusableView {
   // MARK: - Properties
   static var id: String {
     return String(describing: self)
   }
   
+  private let underLineView = OneUnitHeightLine(color: .yg.gray0)
+  
   // MARK: - LifeCycle
   override init(frame: CGRect) {
     super.init(frame: frame)
+    setupUI()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: - LayoutSupport
+extension RecommendationSearchFooterView: LayoutSupport {
+  func addSubviews() {
+    self.addSubview(underLineView)
+  }
+  
+  func setConstraints() {
+    underLineView.setConstraint(
+      fromSuperView: self,
+      spacing: .init(leading: 20, trailing: 20, bottom: 10, top: 10)
+    )
   }
 }
