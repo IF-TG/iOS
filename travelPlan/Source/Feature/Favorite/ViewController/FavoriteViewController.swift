2b8a5440e4341e7070681aad5dec2b209aa4e30e
ysh <happysh_s2@naver.com>
2023-05-18 21:18:50 +0900

    [Feat] Feature title, setting button 구현

--- travelPlan/Source/Feature/Favorite/ViewController/FavoriteViewController.swift
+++ travelPlan/Source/Feature/Favorite/ViewController/FavoriteViewController.swift
@@ -8,22 +8,55 @@
 import UIKit
 
 class FavoriteViewController: UIViewController {
+  // MARK: - Properties
   
+  // MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    // Do any additional setup after loading the view.
-    view.backgroundColor = .systemPink.withAlphaComponent(0.3)
+    configureUI()
   }
   
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destination.
-   // Pass the selected object to the new view controller.
-   }
-   */
+}
+
+// MARK: - Private helpers
+private extension FavoriteViewController {
+  func configureUI() {
+    view.backgroundColor = Constant.bgColor
+    setNavigationBarTitle()
+    setNavigationRightBarItem()
+  }
   
+  func setNavigationBarTitle() {
+    let titleLabel = UILabel().set {
+      $0.text = "찜 목록"
+      $0.textColor = Constant.NavigationBar.Title.color
+      $0.font = Constant.NavigationBar.Title.font
+      $0.textAlignment = .center
+    }
+    navigationItem.titleView = titleLabel
+  }
+  
+  func setNavigationRightBarItem() {
+    let settingButton = UIButton().set {
+      let image = UIImage(
+        named: Constant.NavigationBar.Setting.iconName)
+      $0.setImage(image, for: .normal)
+      $0.setImage(
+        image!.setColor(Constant.NavigationBar.Setting.touchedColor),
+        for: .highlighted)
+      $0.addTarget(
+        self,
+        action: #selector(didTapSettingButton),
+        for: .touchUpInside)
+    }
+    navigationItem.rightBarButtonItem = UIBarButtonItem(
+      customView: settingButton)
+  }
+}
+
+// MARK: - Action
+extension FavoriteViewController {
+  @objc func didTapSettingButton() {
+    print("DEBUG: Tap setting button")
+  }
 }
