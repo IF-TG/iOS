ab5788c6ebe70b04b974e4a2698a24c6dc997e06
seokhyun <letushyun@gmail.com>
2023-05-21 03:25:15 +0900

    [Feat] Delegate 추가

--- travelPlan/Source/Feature/Feed/Protocol/SearchTagCellDelegate.swift
+++ travelPlan/Source/Feature/Feed/Protocol/SearchTagCellDelegate.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+import UIKit
+
+protocol SearchTagCellDelegate: AnyObject {
+  func didTapDeleteButton(item: Int, in section: Int)
+}
--- travelPlan/Source/Feature/Feed/Protocol/UserPostSearchHeaderViewDelegate.swift
+++ travelPlan/Source/Feature/Feed/Protocol/UserPostSearchHeaderViewDelegate.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+protocol UserPostSearchHeaderViewDelegate: AnyObject {
+  func didTapDeleteAllButton()
+}
