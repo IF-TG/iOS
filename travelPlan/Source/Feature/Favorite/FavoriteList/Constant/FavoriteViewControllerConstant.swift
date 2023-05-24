623d627925cfb371e579d6febafeaae052b383f6
ysh <happysh_s2@naver.com>
2023-05-24 12:58:12 +0900

    [Feat] Favorite의 첫 화면에 들어갈 header, cell을 구현
    한가지 이슈는 favorite list 의 header와 navigation edge gray line이 원활하게 layout되지 않습니다..... TT

--- travelPlan/Source/Feature/Favorite/FavoriteList/Constant/FavoriteViewControllerConstant.swift
+++ travelPlan/Source/Feature/Favorite/FavoriteList/Constant/FavoriteViewControllerConstant.swift
@@ -11,6 +11,7 @@ extension FavoriteViewController {
   enum Constant {
     static let bgColor: UIColor = .white
     enum NavigationBar {
+      static let spacing = UIConstantSpacing(bottom: 5)
       enum Title {
         static let color: UIColor = .yg.gray7
         static let font: UIFont = UIFont(
---
+++ travelPlan/Source/Feature/Favorite/FavoriteList/View/FavoriteListTableView/FavoriteListTableView.swift
@@ -0,0 +1,56 @@
+//
+//  FavoriteListTableView.swift
+//  travelPlan
+//
+//  Created by 양승현 on 2023/05/20.
+//
+
+import UIKit
+
+final class FavoriteListTableView: UITableView {
+  // MARK: - Properties
+  
+  // MARK: - Initialization
+  private override init(frame: CGRect, style: UITableView.Style) {
+    super.init(frame: frame, style: style)
+    configureUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  convenience init() {
+    self.init(frame: .zero)
+  }
+}
+
+// MARK: - Helpers
+extension FavoriteListTableView {
+  func layoutFrom(superView: UIView) {
+      superView.addSubview(self)
+    superView.layoutIfNeeded()
+    NSLayoutConstraint.activate([
+      leadingAnchor.constraint(
+        equalTo: superView.leadingAnchor),
+      topAnchor.constraint(
+        equalTo: superView.safeAreaLayoutGuide.topAnchor),
+      trailingAnchor.constraint(
+        equalTo: superView.trailingAnchor),
+      bottomAnchor.constraint(
+        equalTo: superView.safeAreaLayoutGuide.bottomAnchor)])
+  }
+}
+
+// MARK: - Private helpers
+private extension FavoriteListTableView {
+  func configureUI() {
+    translatesAutoresizingMaskIntoConstraints = false
+    register(
+      FavoriteListTableViewCell.self,
+      forCellReuseIdentifier: FavoriteListTableViewCell.id)
+    register(
+      FavoriteListHeaderView.self,
+      forHeaderFooterViewReuseIdentifier: FavoriteListHeaderView.id)
+  }
+}
