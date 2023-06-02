441305eb4b0ea1517f9674e512013b91d3e93817
seokhyun <letushyun@gmail.com>
2023-06-03 00:52:43 +0900

    [Feat] CompositionalLayout 적용

--- travelPlan/Source/Feature/Search/ViewController/SearchViewController.swift
+++ travelPlan/Source/Feature/Search/ViewController/SearchViewController.swift
@@ -11,26 +11,25 @@ import SnapKit
 final class SearchViewController: UIViewController {
   
   // MARK: - Properties
+  private let viewModel = SearchViewModel()
+  
   private let searchView: SearchView = SearchView()
   
   // LayoutFIXME: - CompositionalLayout으로 변경
-//  private let flowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout().set {
-//    $0.headerReferenceSize = CGSize(
-//      width: UIScreen.main.bounds.size.width,
-//      height: 72
-//    )
-//  }
-  
   private lazy var collectionView: UICollectionView = UICollectionView(
     frame: .zero,
-    collectionViewLayout: UICollectionViewFlowLayout()
+    collectionViewLayout: createLayout()
   ).set {
     $0.dataSource = self
     $0.delegate = self
     
     $0.register(
-      BestFestivalCell.self,
-      forCellWithReuseIdentifier: BestFestivalCell.id
+      SearchBestFestivalCell.self,
+      forCellWithReuseIdentifier: SearchBestFestivalCell.id
+    )
+    $0.register(
+      SearchFamousSpotCell.self,
+      forCellWithReuseIdentifier: SearchFamousSpotCell.id
     )
     $0.register(
       SearchHeaderView.self,
@@ -45,12 +44,84 @@ final class SearchViewController: UIViewController {
     setupUI()
     setupStyles()
   }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.isNavigationBarHidden = true
+  }
 }
 
 // MARK: - Helpers
 extension SearchViewController {
   private func setupStyles() {
     view.backgroundColor = .white
+  }
+  
+  private func createLayout() -> UICollectionViewCompositionalLayout {
+    return UICollectionViewCompositionalLayout { [weak self] section, _ in
+      guard let self = self else { return nil }
+      switch self.viewModel.models[section] {
+      case .bestFestival:
+        // item
+        let item = NSCollectionLayoutItem(layoutSize: .init(
+          widthDimension: .fractionalWidth(1),
+          heightDimension: .fractionalHeight(1)
+        ))
+        
+        // group
+        let group = NSCollectionLayoutGroup.horizontal(
+          layoutSize: .init(
+            widthDimension: .absolute(140),
+            heightDimension: .absolute(150)
+          ),
+          subitems: [item]
+        )
+        
+        // section
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.interGroupSpacing = 10
+        section.contentInsets = .init(top: 0, leading: 20, bottom: 0, trailing: 20)
+        section.boundarySupplementaryItems = [self.supplementaryHeaderItem()] // 헤더의 layout 처리
+        
+        // availableTODO: - 16버전은 사용 불가능, 버전에 따라 처리 해주어야 합니다.
+        section.supplementariesFollowContentInsets = false // false의 경우, item의 contentInset을 무시하고 size 설정
+        return section
+      case .famousSpot:
+        // item
+        let item = NSCollectionLayoutItem(layoutSize: .init(
+          widthDimension: .fractionalWidth(1),
+          heightDimension: .fractionalHeight(0.3)
+        ))
+        
+        // group
+        let group = NSCollectionLayoutGroup.vertical(
+          layoutSize: .init(
+            widthDimension: .fractionalWidth(0.87),
+            heightDimension: .absolute(360)
+          ),
+          subitems: [item]
+        )
+        group.interItemSpacing = .fixed(10)
+        // section
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPaging
+        section.interGroupSpacing = 20
+        section.contentInsets = .init(top: 5, leading: 0, bottom: 5, trailing: UIScreen.main.bounds.size.width*0.13)
+        section.boundarySupplementaryItems = [self.supplementaryHeaderItem()] // 헤더의 layout 처리
+        section.supplementariesFollowContentInsets = false
+        
+        return section
+      }
+    }
+  }
+  
+  private func supplementaryHeaderItem() -> NSCollectionLayoutBoundarySupplementaryItem {
+    return .init(
+      layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .estimated(74)),
+      elementKind: UICollectionView.elementKindSectionHeader,
+      alignment: .top
+    )
   }
 }
 
@@ -69,7 +140,7 @@ extension SearchViewController: LayoutSupport {
     }
     
     collectionView.snp.makeConstraints {
-      $0.top.equalTo(searchView)
+      $0.top.equalTo(searchView.snp.bottom)
       $0.leading.trailing.equalToSuperview()
       $0.bottom.equalTo(view.safeAreaLayoutGuide)
     }
@@ -78,24 +149,43 @@ extension SearchViewController: LayoutSupport {
 
 // MARK: - UICollectionViewDataSource
 extension SearchViewController: UICollectionViewDataSource {
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return viewModel.models.count
+  }
+  
   func collectionView(
     _ collectionView: UICollectionView,
     numberOfItemsInSection section: Int
   ) -> Int {
-    return 1
+    switch viewModel.models[section] {
+    case let .bestFestival(festivalItems):
+      return festivalItems.count
+    case let .famousSpot(spotItems):
+      return spotItems.count
+    }
   }
   
   func collectionView(
     _ collectionView: UICollectionView,
     cellForItemAt indexPath: IndexPath
   ) -> UICollectionViewCell {
-    guard let cell = collectionView.dequeueReusableCell(
-      withReuseIdentifier: BestFestivalCell.id,
-      for: indexPath
-    ) as? BestFestivalCell else { return UICollectionViewCell() }
-    
-    // modelTODO: - model indexing
-    return cell
+    switch viewModel.models[indexPath.section] {
+    case let .bestFestival(festivalItems):
+      guard let cell = collectionView.dequeueReusableCell(
+        withReuseIdentifier: SearchBestFestivalCell.id,
+        for: indexPath
+      ) as? SearchBestFestivalCell else { return UICollectionViewCell() }
+      
+      cell.configure(festivalItems[indexPath.item])
+      return cell
+    case let .famousSpot(spotItems):
+      guard let cell = collectionView.dequeueReusableCell(
+        withReuseIdentifier: SearchFamousSpotCell.id, for: indexPath
+      ) as? SearchFamousSpotCell else { return UICollectionViewCell() }
+      
+      cell.configure(spotItems[indexPath.item])
+      return cell
+    }
   }
   
   func collectionView(
@@ -111,27 +201,22 @@ extension SearchViewController: UICollectionViewDataSource {
         for: indexPath
       ) as? SearchHeaderView else { return UICollectionReusableView() }
       
+      let title = viewModel.models[indexPath.section].headerTitle
+      headerView.configure(title)
+      
       return headerView
     default: return UICollectionReusableView()
     }
   }
 }
 
-// MARK: - UICollectionViewDelegateFlowLayout
-extension SearchViewController: UICollectionViewDelegateFlowLayout {
-  func collectionView(
-    _ collectionView: UICollectionView,
-    layout collectionViewLayout: UICollectionViewLayout,
-    referenceSizeForHeaderInSection section: Int
-  ) -> CGSize {
-    return CGSize(width: UIScreen.main.bounds.size.width, height: 72)
-  }
+// MARK: - UICollectionViewDelegate
+extension SearchViewController: UICollectionViewDelegate {
   
-  func collectionView(
-    _ collectionView: UICollectionView,
-    layout collectionViewLayout: UICollectionViewLayout,
-    sizeForItemAt indexPath: IndexPath
-  ) -> CGSize {
-    return CGSize(width: 140, height: 150)
-  }
 }
+
+// searchBarTODO: - searchBar 위치에 따라 navigationBar 로 만들기
+// 1. searchBar의 y값이 0보다 작아지면, searchBar 상단에 고정
+
+// 2. scrollView의 현재 y값이 searchBar의 top constraint라면,
+  // ㄴ> searchBar 상단에 고정된 것 풀고 기존 위치에 고정
