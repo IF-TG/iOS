b2d0176fcc35dd8dbc93d9710ccff368e85b8b09
seokhyun <letushyun@gmail.com>
2023-06-06 17:31:52 +0900

    [Feat] headerView 더보기 버튼 추가

--- travelPlan/Source/Feature/Search/View/SearchHeaderView.swift
+++ travelPlan/Source/Feature/Search/View/SearchHeaderView.swift
@@ -22,6 +22,16 @@ final class SearchHeaderView: UICollectionReusableView {
     $0.text = "베스트 축제" // will erase
   }
   
+  private let lookingMoreButton: UIButton = .init().set {
+    $0.setTitle("더보기", for: .normal)
+    $0.titleLabel?.font = .systemFont(ofSize: 12, weight: .semibold)
+    $0.setTitleColor(.yg.gray4, for: .normal)
+    $0.setImage(UIImage(named: "plus"), for: .normal)
+    $0.layer.cornerRadius = 12
+    $0.layer.borderColor = UIColor.yg.gray0.cgColor
+    $0.layer.borderWidth = 1
+  }
+  
   // MARK: - LifeCycle
   override init(frame: CGRect) {
     super.init(frame: frame)
@@ -45,13 +55,22 @@ extension SearchHeaderView {
 extension SearchHeaderView: LayoutSupport {
   func addSubviews() {
     addSubview(headerLabel)
+    addSubview(lookingMoreButton)
   }
   
   func setConstraints() {
     headerLabel.snp.makeConstraints {
       $0.leading.equalToSuperview().inset(20)
+      $0.trailing.lessThanOrEqualTo(lookingMoreButton.snp.leading).offset(-20)
       $0.top.equalToSuperview().inset(30)
       $0.bottom.equalToSuperview().inset(20)
+    }
+    
+    lookingMoreButton.snp.makeConstraints {
+      $0.top.equalTo(headerLabel)
+      $0.width.equalTo(64)
+      $0.height.equalTo(24)
+      $0.trailing.equalToSuperview().inset(20)
     }
   }
 }
