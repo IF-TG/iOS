7c4ed11d436a9dc806addacc76ce8123ed71d751
seokhyun <letushyun@gmail.com>
2023-08-17 06:07:12 +0900

    [Refactor] ViewModelCase 프로토콜 준수

--- travelPlan/Source/Feature/Search/ViewModel/SearchBestFestivalCellViewModel.swift
+++ travelPlan/Source/Feature/Search/ViewModel/SearchBestFestivalCellViewModel.swift
@@ -49,10 +49,10 @@ class SearchBestFestivalCellViewModel {
   }
 }
 
-extension SearchBestFestivalCellViewModel {
-  // MARK: - Transform
+// MARK: - ViewModelCase
+extension SearchBestFestivalCellViewModel: ViewModelCase {
   func transform(_ input: Input) -> Output {
-    // 서버 저장 요청후 응답에 따라 output이 달라집니다.
+    // 서버 저장 요청 후 응답에 따라 output이 달라집니다.
     return input.didTapHeartButton
       .flatMap { [weak self] _ in
         guard let self = self else {
@@ -64,10 +64,13 @@ extension SearchBestFestivalCellViewModel {
       }
       .eraseToAnyPublisher()
   }
-  
+}
+
+// MARK: - Helpers
+extension SearchBestFestivalCellViewModel {
   /// 서버에 저장 요청.
   /// 성공 시 UI 변환, 실패 시, 변화 없음
-  func saveButtonState(id: Int) -> Future<State, ErrorType> {
+  private func saveButtonState(id: Int) -> Future<State, ErrorType> {
     // networkTODO: - id값을 통해 서버에 데이터 저장을 요청하고, 성공 시 하트버튼의 색깔을 변경해야 합니다.
     return Future { promise in
       // fake network. 추후 네트워크 통신 이후, promise로 값을 방출해야 합니다.
