9adc63eb1294aa619d3b39f0453ca28a72c52d26
seokhyun <letushyun@gmail.com>
2023-09-07 15:07:37 +0900

    [Design] 오토레이아웃 수정

--- travelPlan/Source/Feature/Feed/Constant/PostSearch/TagCell/PostRecentSearchTagCell+Constants.swift
+++ travelPlan/Source/Feature/Feed/Constant/PostSearch/TagCell/PostRecentSearchTagCell+Constants.swift
@@ -11,32 +11,31 @@ extension PostRecentSearchTagCell {
   enum Constants {
     enum ContentView {
       static let borderWidth: CGFloat = 1
-      static let cornerRadius: CGFloat = 13
+      static let cornerRadius: CGFloat = 15
     }
     
     enum TagLabel {
-      static let fontSize: CGFloat = 14
-      static let numberOfLines = 1
       enum Inset {
-        static let trailing: CGFloat = 13
-        static let leading: CGFloat = 13
-        static let top: CGFloat = 4
-        static let bottom: CGFloat = 4
+        static let leading: CGFloat = 10
+        static let topBottom: CGFloat = 4
+      }
+      enum Offset {
+        static let trailing: CGFloat = -4
       }
     }
     
     enum TagDeleteButton {
       enum Inset {
-        static let trailing: CGFloat = 13
+        static let trailing: CGFloat = 10
       }
-      enum Offset {
-        static let leading: CGFloat = 4
-      }
-      enum Size {
-        static let width: CGFloat = 10
-        static let height: CGFloat = 10
-      }
-      static let imageName = "xmark"
+      static let size: CGFloat = 18
+      static let imageName = "cancel"
     }
+    
+    static let targetSizeHeight: CGFloat = PostSearchCollectionViewCompositionalLayout
+      .Constants.Recent.Item.absoluteHeight
+    static let contentInsets: CGFloat = PostSearchCollectionViewCompositionalLayout
+      .Constants.Recent.Section.ContentInsets.leading + PostSearchCollectionViewCompositionalLayout
+      .Constants.Recent.Section.ContentInsets.trailing
   }
 }
--- travelPlan/Source/Feature/Feed/Constant/PostSearch/TagCell/PostRecommendationSearchTagCell+Constants.swift
+++ travelPlan/Source/Feature/Feed/Constant/PostSearch/TagCell/PostRecommendationSearchTagCell+Constants.swift
@@ -11,13 +11,11 @@ extension PostRecommendationSearchTagCell {
   enum Constants {
     enum ContentView {
       static let borderWidth: CGFloat = 1
-      static let cornerRadius: CGFloat = 13
+      static let cornerRadius: CGFloat = 15
       static let alphaComponent: CGFloat = 0.1
     }
     
     enum TagLabel {
-      static let fontSize: CGFloat = 14
-      static let numberOfLines = 1
       enum Inset {
         static let trailing: CGFloat = 13
         static let leading: CGFloat = 13
