ac153c4d0b90cf1000fd4060d8c99cf3658f9cbb
shcommit <happysh_s2@naver.com>
2023-09-13 15:48:26 +0900

    feat: TravelThemeType 생성 및 구현

--- travelPlan/Source/Common/Model/TravelThemeType.swift
+++ travelPlan/Source/Common/Model/TravelThemeType.swift
@@ -5,6 +5,72 @@
 //  Created by 양승현 on 2023/09/13.
 //
 
-enum TravelThemeType {
+import Foundation
+
+enum Season: String, CaseIterable {
+  case spring = "봄"
+  case summer = "여름"
+  case fall = "가을"
+  case winter = "겨울"
   
+  var count: Int {
+    return Self.allCases.count
+  }
+}
+
+enum TravelThemeType: CaseIterable {
+  case all
+  case season(Season)
+  case legion
+  case travelTheme
+  case partner
+  case categoryDevelop
+  
+  static var allCases: [TravelThemeType] {
+    return [
+      .all,
+      .season(Season.spring),
+      .legion,
+      .travelTheme,
+      .partner,
+      .categoryDevelop]
+  }
+}
+
+extension TravelThemeType: RawRepresentable {
+  init?(rawValue: String) {
+    switch rawValue {
+    case "전체":
+      self = .all
+    case "계절":
+      self = .season(Season.spring)
+    case "지역":
+      self = .legion
+    case "여행 테마":
+      self = .travelTheme
+    case "동반자":
+      self = .partner
+    case "???":
+      self = .categoryDevelop
+    default:
+      return nil
+    }
+  }
+  
+  var rawValue: String {
+    switch self {
+    case .all:
+      return "전체"
+    case .season(_):
+      return "계절"
+    case .legion:
+      return "지역"
+    case .travelTheme:
+      return "여행 테마"
+    case .partner:
+      return "동반자"
+    case .categoryDevelop:
+      return "???"
+    }
+  }
 }
