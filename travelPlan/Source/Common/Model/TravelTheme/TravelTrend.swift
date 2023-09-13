30eeb3539057e33adcd4e10346c0bc34baed25a7
shcommit <happysh_s2@naver.com>
2023-09-13 18:22:09 +0900

    [Feat] Travel Trend 추가

---
+++ travelPlan/Source/Common/Model/TravelTheme/TravelTrend.swift
@@ -0,0 +1,13 @@
+//
+//  TravelTrend.swift
+//  travelPlan
+//
+//  Created by 양승현 on 2023/09/13.
+//
+
+import Foundation
+
+enum TravelTrend: String, CaseIterable, Equatable {
+  case newest = "최신순"
+  case popularity = "인기순"
+}
