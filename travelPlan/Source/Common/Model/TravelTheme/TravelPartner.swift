21cd09e8c4193dc9c8efe456191bb49cb2fd66cb
shcommit <happysh_s2@naver.com>
2023-09-13 17:07:07 +0900

    [Feat] TravelPartner case 추가

--- travelPlan/Source/Common/Model/TravelTheme/TravelPartner.swift
+++ travelPlan/Source/Common/Model/TravelTheme/TravelPartner.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+enum TravelPartner: String, CaseIterable {
+  case alone = "혼자"
+  case family = "가족과 함께"
+  case parents = "부모님과 함께"
+  case children = "아이들과 함께"
+  case lover = "연인과 함께"
+  case friend = "친구와 함께"
+  case pet = "애완견과 함게"
+}
