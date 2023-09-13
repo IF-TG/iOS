cb61bbf8fdd6d82600c1eb83db73c2e6ec4017e4
shcommit <happysh_s2@naver.com>
2023-09-13 21:53:02 +0900

    [Style] 불필요한 코드 제거

--- travelPlan/Source/Common/Extension/Notification+.swift
+++ travelPlan/Source/Common/Extension/Notification+.swift
@@ -11,8 +11,4 @@ extension Notification.Name {
   /// FeedPage에서 정렬, 소정렬 눌렀을 때
   static let TravelCategoryDetailSelected = Notification
     .Name("travelCategoryDetailSelected")
-  
-  /// PostViewDetailCategoryHeaderView -> FeedViewController로 이벤트 전달 하기 위한 식별자
-  static let postViewDetailCategoryHeaderViewToFeedVC = Notification.Name(
-    "PostViewDetailCategoryHeaderViewToFeedVC")
 }
