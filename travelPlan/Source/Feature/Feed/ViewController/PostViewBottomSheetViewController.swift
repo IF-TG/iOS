80f6212c1c7bdea8f60ee61e9d86ee6576df61ad
shcommit <happysh_s2@naver.com>
2023-09-13 23:51:57 +0900

    [Feat] BottomSheet 보여질 때 애니메이션 추가

--- travelPlan/Source/Feature/Feed/ViewController/PostViewBottomSheetViewController.swift
+++ travelPlan/Source/Feature/Feed/ViewController/PostViewBottomSheetViewController.swift
@@ -33,7 +33,11 @@ final class PostViewBottomSheetViewController: UIViewController {
   }
   
   // MARK: - Properties
-  private let categoryView: UITableView = UITableView(frame: .zero).set {
+  private let indicatorBar = UIView(frame: .zero).set {
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    $0
+  }
+  private let categoryView = UITableView(frame: .zero).set {
     $0.translatesAutoresizingMaskIntoConstraints = false
     $0.register(
       PostViewBottomSheetCell.self,
@@ -41,31 +45,66 @@ final class PostViewBottomSheetViewController: UIViewController {
     $0.rowHeight = Constants.CategoryView.cellSize.height
   }
   
+  private let safeAreaBottomBackgroundView = UIView().set {
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    $0.backgroundColor = .white
+  }
+  
   // MARK: - Lifecycle
   override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
     super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    setupUI()
+    configureUI()
   }
   
   init() {
     super.init(nibName: nil, bundle: nil)
-    setupUI()
+    configureUI()
   }
   
   required init?(coder: NSCoder) {
     super.init(coder: coder)
+    configureUI()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(false)
+    UIView.animate(
+      withDuration: 0.40,
+      delay: 0,
+      options: .curveEaseOut
+    ) {
+      self.view.backgroundColor = UIColor(hex: "#000000").withAlphaComponent(0.1)
+      self.categoryView.transform = .identity
+      self.safeAreaBottomBackgroundView.transform = .identity
+    }
+  }
+  
+  // MARK: - Private helper
+  private func configureUI() {
     setupUI()
+    modalPresentationStyle = .overFullScreen
+    let height = UIScreen.main.bounds.height
+    view.backgroundColor = .clear
+    categoryView.transform = .init(translationX: 0, y: +height)
+    safeAreaBottomBackgroundView.transform = .init(translationX: 0, y: +height)
   }
 }
 
 // MARK: - LayoutSupport
 extension PostViewBottomSheetViewController: LayoutSupport {
   func addSubviews() {
-    view.addSubview(categoryView)
+    _=[categoryView,
+     safeAreaBottomBackgroundView
+    ].map { view.addSubview($0) }
   }
   
   func setConstraints() {
     NSLayoutConstraint.activate(categoryViewConstraints)
+    _=[categoryViewConstraints,
+       safeAreaBottomBackgroundViewConstraints
+    ].map {
+      NSLayoutConstraint.activate($0)
+    }
   }
 }
 
@@ -77,11 +116,25 @@ private extension PostViewBottomSheetViewController {
       categoryView.leadingAnchor.constraint(
         equalTo: view.leadingAnchor),
       categoryView.topAnchor.constraint(
-        equalTo: view.topAnchor,
+        greaterThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor,
         constant: Inset.top),
       categoryView.trailingAnchor.constraint(
         equalTo: view.trailingAnchor),
       categoryView.bottomAnchor.constraint(
-        equalTo: view.bottomAnchor)]
+        equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+      categoryView.heightAnchor.constraint(lessThanOrEqualToConstant: <#T##CGFloat#>)]
+  }
+  
+  var safeAreaBottomBackgroundViewConstraints: [NSLayoutConstraint] {
+    return [
+      safeAreaBottomBackgroundView.leadingAnchor.constraint(
+        equalTo: view.leadingAnchor),
+      safeAreaBottomBackgroundView.trailingAnchor.constraint(
+        equalTo: view.trailingAnchor),
+      safeAreaBottomBackgroundView.topAnchor.constraint(
+        equalTo: categoryView.bottomAnchor),
+      safeAreaBottomBackgroundView.bottomAnchor.constraint(
+        equalTo: view.bottomAnchor)
+    ]
   }
 }
