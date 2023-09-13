c2e3e41cdf8f4f9db772705ba0596f585f15a370
shcommit <happysh_s2@naver.com>
2023-09-13 15:56:15 +0900

    feat: Season 추가

--- travelPlan/Source/Common/Model/TravelTheme/Season.swift
+++ travelPlan/Source/Common/Model/TravelTheme/Season.swift
@@ -1,8 +1,19 @@
 //
-//  File.swift
+//  Season.swift
 //  travelPlan
 //
 //  Created by 양승현 on 2023/09/13.
 //
 
 import Foundation
+
+enum Season: String, CaseIterable {
+  case spring = "봄"
+  case summer = "여름"
+  case fall = "가을"
+  case winter = "겨울"
+  
+  static var count: Int {
+    Self.allCases.count
+  }
+}
