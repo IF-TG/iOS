37aaa1bb395f4b19999048d7a583fff05801043e
shcommit <happysh_s2@naver.com>
2023-09-24 19:02:32 +0900

    [Feat] FeedPostSearchFilterInfo 추가

---
+++ travelPlan/Source/Common/Model/TravelTheme/FeedPostSearchFilterInfo.swift
@@ -0,0 +1,11 @@
+//
+//  PostSearchFilterInfo.swift
+//  travelPlan
+//
+//  Created by 양승현 on 2023/09/24.
+//
+
+struct FeedPostSearchFilterInfo {
+  var travelTheme: TravelThemeType
+  var travelTrend: TravelTrend
+}
