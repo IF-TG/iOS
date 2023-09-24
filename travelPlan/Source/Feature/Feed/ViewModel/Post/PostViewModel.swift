f6415ba307237bdd8775c6c3d9e2778bf0a0998e
shcommit <happysh_s2@naver.com>
2023-09-24 19:39:04 +0900

    [Feat] PostViewAdapterDataSource 프로토콜 관련 trend, theme 준수 및 의존성 주입 로직 추가

--- travelPlan/Source/Feature/Feed/ViewModel/Post/PostViewModel.swift
+++ travelPlan/Source/Feature/Feed/ViewModel/Post/PostViewModel.swift
@@ -9,15 +9,20 @@ import Foundation
 
 final class PostViewModel {
   // MARK: - Properties
-  private let data: [PostModel]
+  private let data: [PostModel] = MockPostModel().initMockData()
+  
+  private(set) var travelTheme: TravelThemeType = .all
+  
+  private(set) var travelTrend: TravelTrend = .newest
   
     var count: Int {
       data.count
     }
   
   // MARK: - Properteis
-  init(data: [PostModel]) {
-    self.data = data
+  init(filterInfo: FeedPostSearchFilterInfo) {
+    self.travelTheme = filterInfo.travelTheme
+    self.travelTrend = filterInfo.travelTrend
   }
 }
 
