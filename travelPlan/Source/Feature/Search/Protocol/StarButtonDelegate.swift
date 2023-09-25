b4321852fa558b1e98a8c6fcde8ecf8ff91d112d
seokhyun <letushyun@gmail.com>
2023-09-26 05:20:13 +0900

    [Feat] StarButtonDelegate 프로토콜 추가

---
+++ travelPlan/Source/Feature/Search/Protocol/StarButtonDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  StarButtonDelegate.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 2023/09/24.
+//
+
+import Foundation
+
+protocol StarButtonDelegate: AnyObject {
+  func didTapStarButton()
+}
