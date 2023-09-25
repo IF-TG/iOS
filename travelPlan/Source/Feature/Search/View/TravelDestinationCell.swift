87685369bdeffa0a57f5a25004daf0c233b49ee9
seokhyun <letushyun@gmail.com>
2023-09-26 05:45:00 +0900

    [Feat] TravelDestinationCell 추가

--- travelPlan/Source/Feature/Search/View/TravelDestinationCell.swift
+++ travelPlan/Source/Feature/Search/View/TravelDestinationCell.swift
@@ -9,91 +9,25 @@ import UIKit
 import SnapKit
 import Combine
 
-final class TravelDestinationCell: UICollectionViewCell {
-  enum Constants {
-    enum ThumbnailImageView {
-      static let imageName = "tempThumbnail1"
-      static let cornerRadius: CGFloat = 3
-      enum Inset {
-        static let top: CGFloat = 5
-        static let bottom: CGFloat = 5
-      }
-    }
-    
-    enum LabelStackView {
-      enum Offset {
-        static let leading: CGFloat = 15
-        static let trailing: CGFloat = -20
-      }
-    }
-    enum HeartButton {
-      static let size: CGFloat = 24
-      enum Inset {
-        static let top: CGFloat = 5
-        static let trailing: CGFloat = 16
-      }
-    }
-    enum PlaceLabel {
-      static let fontSize: CGFloat = 16
-      static let numberOfLines = 2
-    }
-    enum CategoryLabel {
-      static let fontSize: CGFloat = 14
-      static let numberOfLines = 1
-    }
-    enum AreaLabel {
-      static let size: CGFloat = 14
-      static let numberOfLines = 1
-    }
-  }
+class TravelDestinationCell: UICollectionViewCell {
+  typealias Input = TravelDestinationCellViewModel.Input
+  typealias ErrorType = TravelDestinationCellViewModel.ErrorType
+  typealias State = TravelDestinationCellViewModel.State
   
   // MARK: - Properties
+  static var id: String {
+    return String(describing: self)
+  }
+  
   private var viewModel: TravelDestinationCellViewModel? {
     didSet {
       bind()
     }
   }
   
-  static var id: String {
-    return String(describing: self)
-  }
-  
-  private let thumbnailImageView: UIImageView = UIImageView().set {
-    $0.image = UIImage(named: Constants.ThumbnailImageView.imageName)
-    $0.layer.cornerRadius = Constants.ThumbnailImageView.cornerRadius
-    $0.contentMode = .scaleAspectFill
-    $0.layer.masksToBounds = true
-  }
-  
-  private lazy var starButton: SearchStarButton = .init(normalType: .empty).set {
-    $0.addTarget(self, action: #selector(didTapStarButton), for: .touchUpInside)
-  }
-  
-  private let titleLabel: UILabel = UILabel().set {
-    $0.font = .init(pretendard: .semiBold, size: Constants.PlaceLabel.fontSize)
-    $0.text = "n/a"
-    $0.textColor = .yg.gray6
-    $0.numberOfLines = Constants.PlaceLabel.numberOfLines
-  }
-  
-  private let secondLabel: UILabel = UILabel().set {
-    $0.font = .init(pretendard: .medium, size: Constants.CategoryLabel.fontSize)
-    $0.textColor = .yg.gray6
-    $0.text = "n/a"
-    $0.numberOfLines = Constants.CategoryLabel.numberOfLines
-  }
-  
-  private let thirdLabel: UILabel = UILabel().set {
-    $0.font = .init(pretendard: .medium, size: Constants.AreaLabel.size)
-    $0.text = "n/a"
-    $0.textColor = .yg.gray6
-    $0.numberOfLines = Constants.AreaLabel.numberOfLines
-  }
-  
-  private let labelStackView: UIStackView = UIStackView().set {
-    $0.axis = .vertical
-    $0.spacing = .zero
-    $0.alignment = .leading
+  private lazy var containerView: BaseDestinationView<LeftAlignThreeLabelsView> 
+  = .init(centerView: LeftAlignThreeLabelsView()).set {
+    $0.delegate = self
   }
   
   private lazy var input = Input()
@@ -111,17 +45,12 @@ final class TravelDestinationCell: UICollectionViewCell {
   
   override func prepareForReuse() {
     super.prepareForReuse()
-    thumbnailImageView.image = nil
+    
+    containerView.clearThumbnailImage()
     subscriptions.removeAll()
   }
-}
-
-extension TravelDestinationCell: ViewBindCase {
-  typealias Input = TravelDestinationCellViewModel.Input
-  typealias ErrorType = TravelDestinationCellViewModel.ErrorType
-  typealias State = TravelDestinationCellViewModel.State
   
-  func bind() {
+  private func bind() {
     guard let viewModel = self.viewModel else { return }
     
     let output = viewModel.transform(input)
@@ -140,15 +69,15 @@ extension TravelDestinationCell: ViewBindCase {
       .store(in: &subscriptions)
   }
   
-  func render(_ state: State) {
+  private func render(_ state: State) {
     switch state {
     case .changeButtonColor:
-      starButton.isSelected.toggle()
+      containerView.toggleStarButtonState()
     case .none: break
     }
   }
   
-  func handleError(_ error: ErrorType) {
+  private func handleError(_ error: ErrorType) {
     switch error {
     case .fatalError: print("DEBUG: fatalError occurred")
     case .networkError: print("DEBUG: networkError occurred")
@@ -157,56 +86,33 @@ extension TravelDestinationCell: ViewBindCase {
   }
 }
 
-// MARK: - Actions
-extension TravelDestinationCell {
-  @objc private func didTapStarButton() {
+// MARK: - StarButtonDelegate
+extension TravelDestinationCell: StarButtonDelegate {
+  func didTapStarButton() {
     input.didTapStarButton.send()
-    print("DEBUG: 버튼 변화됨!")
   }
 }
 
-// MARK: - Configure
+// MARK: - Helpers
 extension TravelDestinationCell {
   func configure(with viewModel: TravelDestinationCellViewModel) {
     self.viewModel = viewModel
     
-    thumbnailImageView.image = UIImage(named: viewModel.thumbnailImage ?? "tempProfile4")
-    titleLabel.text = viewModel.place
-    secondLabel.text = viewModel.category
-    thirdLabel.text = viewModel.location
+    containerView.configure(centerModel: viewModel.contentModel)
+    containerView.configure(imageURL: viewModel.imageURLString,
+                            isSelectedButton: viewModel.isSelectedButton)
   }
 }
 
 // MARK: - LayoutSupport
 extension TravelDestinationCell: LayoutSupport {
   func addSubviews() {
-    contentView.addSubview(thumbnailImageView)
-    contentView.addSubview(labelStackView)
-    contentView.addSubview(starButton)
-    _ = [titleLabel, secondLabel, thirdLabel]
-      .map { labelStackView.addArrangedSubview($0) }
+    contentView.addSubview(containerView)
   }
   
   func setConstraints() {
-    thumbnailImageView.snp.makeConstraints {
-      $0.leading.equalToSuperview()
-      $0.top.equalToSuperview().inset(Constants.ThumbnailImageView.Inset.top)
-      $0.bottom.equalToSuperview().inset(Constants.ThumbnailImageView.Inset.bottom)
-      $0.width.equalTo(thumbnailImageView.snp.height)
-    }
-    
-    labelStackView.snp.makeConstraints {
-      $0.leading.equalTo(thumbnailImageView.snp.trailing)
-        .offset(Constants.LabelStackView.Offset.leading)
-      $0.trailing.lessThanOrEqualTo(starButton.snp.leading)
-        .offset(Constants.LabelStackView.Offset.trailing)
-      $0.top.equalTo(thumbnailImageView)
-    }
-    
-    starButton.snp.makeConstraints {
-      $0.top.equalToSuperview().inset(Constants.HeartButton.Inset.top)
-      $0.trailing.equalToSuperview().inset(Constants.HeartButton.Inset.trailing)
-      $0.size.equalTo(Constants.HeartButton.size)
+    containerView.snp.makeConstraints {
+      $0.edges.equalTo(contentView)
     }
   }
 }
