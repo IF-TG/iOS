f6f823353e7b3ad9f97509edb719f29dd1e620e0
shcommit <happysh_s2@naver.com>
2023-09-25 22:42:17 +0900

    [Feat] layout.itemsize 초기화 시점 cell 데이터 대입할 수 있도록 알림 보내주는 객체 추가

--- travelPlan/Source/Feature/Feed/View/CategoryPageView/CategoryDetailView/CategoryDetailView.swift
+++ travelPlan/Source/Feature/Feed/View/CategoryPageView/CategoryDetailView/CategoryDetailView.swift
@@ -6,44 +6,57 @@
 //
 
 import UIKit
+import Combine
 
 final class CategoryDetailView: UICollectionView {
   // MARK: - Properties
-  /// CategoryView가 로드된 후 height가 지정되면 detailView cell의 height지정하기 위해
-  private var layout: UICollectionViewFlowLayout!
+  @Published private var isSetItemSize = false
   
-  // MARK: - Initialization
-  convenience init() {
-    let layoutObject = UICollectionViewFlowLayout()
-    layoutObject.minimumLineSpacing = 0
-    layoutObject.scrollDirection = .horizontal
-    self.init(
-      frame: .zero,
-      collectionViewLayout: layoutObject)
-    layout = layoutObject
+  var itemSizeSetNotifier: AnyPublisher<Void, Never> {
+    $isSetItemSize
+      .filter { $0 }
+      .map { _ -> Void in }
+      .eraseToAnyPublisher()
   }
   
-  private override init(
+  override var bounds: CGRect {
+    didSet {
+      if !isSetItemSize,
+         let layout = collectionViewLayout as? UICollectionViewFlowLayout
+      {
+        isSetItemSize.toggle()
+        layout.itemSize = bounds.size
+      }
+    }
+  }
+  
+  // MARK: - Initialization
+  override init(
     frame: CGRect,
     collectionViewLayout layout: UICollectionViewLayout
   ) {
     super.init(frame: frame, collectionViewLayout: layout)
-    translatesAutoresizingMaskIntoConstraints = false
     showsHorizontalScrollIndicator = false
     decelerationRate = .fast
+    isScrollEnabled = false
     register(
       CategoryDetailViewCell.self,
       forCellWithReuseIdentifier: CategoryDetailViewCell.id)
   }
   
+  convenience init() {
+    let layout = UICollectionViewFlowLayout().set {
+      $0.minimumLineSpacing = 0
+      $0.scrollDirection = .horizontal
+      $0.itemSize = CGSize(width: 50, height: 50)
+    }
+    self.init(
+      frame: .zero,
+      collectionViewLayout: layout)
+    translatesAutoresizingMaskIntoConstraints = false
+  }
+  
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
-  }
-}
-
-// MARK: - Helpers
-extension CategoryDetailView {
-  func setLayoutItemSize(_ size: CGSize) {
-    layout.itemSize = size
   }
 }
