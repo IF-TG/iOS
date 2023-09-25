082facf7e4befd7556783ccb235992279636723f
seokhyun <letushyun@gmail.com>
2023-09-26 03:35:59 +0900

    [Feat] SearchSectionType을 개별 파일로 분리

---
+++ travelPlan/Source/Feature/Search/Model/SearchSectionType.swift
@@ -0,0 +1,14 @@
+//
+//  SearchSectionType.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 2023/09/26.
+//
+
+import Foundation
+
+/// 해당 Type에 따라 SearchMoreDetail 데이터가 결정됩니다.
+enum SearchSectionType: Int {
+  case festival
+  case camping
+}
