465e2c9b3f1fdca8bb7298f37c93fe6e4ed7e477
shcommit <happysh_s2@naver.com>
2023-09-26 22:22:01 +0900

    [Feat] BaseLeftRoundProfileAreaView 생성 및 프로필, contentView 레이아웃 지정

---
+++ travelPlan/Source/Common/View/BaseLeftRoundProfileAreaView.swift
@@ -0,0 +1,157 @@
+//
+//  BaseLeftRoundProfileAreaView.swift
+//  travelPlan
+//
+//  Created by 양승현 on 2023/09/26.
+//
+
+import UIKit
+
+protocol BaseLeftRoundProfileAreaViewDelegate: AnyObject {
+  func baseLeftRoundProfileAreaView(
+    _ baseLeftRoundProfileAreaView: BaseLeftRoundProfileAreaView,
+    didSelectProfile profile: UIImageView)
+}
+
+class BaseLeftRoundProfileAreaView: UIView {
+  enum ProfileLayoutType {
+    case centerY
+    case top
+  }
+  
+  enum ProfileInfoType {
+    case small(ProfileLayoutType)
+    case medium(ProfileLayoutType)
+    
+    var size: CGSize {
+      switch self {
+      case .small:
+        return .init(width: 35, height: 35)
+      case .medium:
+        return .init(width: 40, height: 40)
+      }
+    }
+    
+    var radius: CGFloat {
+      switch self {
+      case .small:
+        return size.width/2
+      case .medium:
+        return size.width/2
+      }
+    }
+    
+    var layoutType: ProfileLayoutType {
+      switch self {
+      case .small(let type):
+        return type
+      case .medium(let type):
+        return type
+      }
+    }
+  }
+  
+  // MARK: - Properties
+  private var profileImageView: UIImageView!
+  
+  private var contentView: UIView
+  
+  weak var delegate: BaseLeftRoundProfileAreaViewDelegate?
+  
+  private var profileLayoutInfo: ProfileInfoType
+  
+  // MARK: - Lifecycle
+  init(
+    frame: CGRect,
+    contentView: UIView,
+    profileLayoutInfo: ProfileInfoType
+  ) {
+    self.profileLayoutInfo = profileLayoutInfo
+    self.contentView = contentView
+    super.init(frame: frame)
+    configureUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    profileLayoutInfo = .small(.centerY)
+    contentView = .init(frame: .zero)
+    super.init(coder: coder)
+    configureUI()
+  }
+  
+  // MARK: - Helper
+  func configure(with image: UIImage?) {
+    profileImageView.image = image
+  }
+  
+  // MARK: - Private helper
+  private func configureUI() {
+    translatesAutoresizingMaskIntoConstraints = false
+    makeProfileImageView()
+    setupUI()
+  }
+  
+  private func makeProfileImageView() {
+    profileImageView = UIImageView(frame: .zero).set {
+      $0.translatesAutoresizingMaskIntoConstraints = false
+      $0.backgroundColor = .lightGray.withAlphaComponent(0.5)
+      $0.clipsToBounds = true
+      $0.isUserInteractionEnabled = true
+      $0.contentMode = .scaleAspectFill
+      let gesture = UITapGestureRecognizer(target: self, action: #selector(didTapProfile))
+      $0.isUserInteractionEnabled = true
+      $0.addGestureRecognizer(gesture)
+      $0.layer.cornerRadius = profileLayoutInfo.radius
+    }
+  }
+  
+  // MARK: - Action
+  @objc func didTapProfile() {
+    print("DEBUG: Goto profile scene !!")
+    delegate?.baseLeftRoundProfileAreaView(self, didSelectProfile: profileImageView)
+  }
+}
+
+// MARK: - LayoutSupportable
+extension BaseLeftRoundProfileAreaView: LayoutSupport {
+  func addSubviews() {
+    _=[
+      profileImageView,
+      contentView
+    ].map {
+      addSubview($0)
+    }
+  }
+  
+  func setConstraints() {
+    _=[
+      profileImageViewConstraints,
+      contentViewConstraints
+    ].map {
+      NSLayoutConstraint.activate($0)
+    }
+  }
+}
+
+// MARK: - Private layoutSupportable
+private extension BaseLeftRoundProfileAreaView {
+  var profileImageViewConstraints: [NSLayoutConstraint] {
+    var constraints = [
+      profileImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+      profileImageView.widthAnchor.constraint(equalToConstant: profileLayoutInfo.size.width),
+      profileImageView.heightAnchor.constraint(equalToConstant: profileLayoutInfo.size.height)]
+    guard profileLayoutInfo.layoutType == .top else {
+      return constraints + [profileImageView.centerYAnchor.constraint(equalTo: centerYAnchor)]
+    }
+    return constraints + [profileImageView.topAnchor.constraint(equalTo: topAnchor)]
+  }
+  
+  var contentViewConstraints: [NSLayoutConstraint] {
+    return [
+      contentView.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor),
+      contentView.topAnchor.constraint(equalTo: topAnchor),
+      contentView.trailingAnchor.constraint(equalTo: trailingAnchor),
+      contentView.bottomAnchor.constraint(equalTo: bottomAnchor)
+    ]
+  }
+}
