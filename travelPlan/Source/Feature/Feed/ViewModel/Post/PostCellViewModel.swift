61a2ccce14021cb0ff158aaa724e8d07a3ba61e4
shcommit <happysh_s2@naver.com>
2023-09-30 19:50:45 +0900

    [Feat] postModel 타입 옵셔널로 변경

--- travelPlan/Source/Feature/Feed/ViewModel/Post/PostCellViewModel.swift
+++ travelPlan/Source/Feature/Feed/ViewModel/Post/PostCellViewModel.swift
@@ -9,18 +9,18 @@ import Foundation
 
 final class PostCellViewModel {
   // MARK: - Properties
-  private let postModel: PostModel
+  private let postModel: PostModel?
   
-  var headerModel: PostHeaderProfileAndInfoView.Model {
-      postModel.header
+  var headerModel: PostHeaderProfileAndInfoView.Model? {
+    postModel?.header
   }
   
-  var contentAreaModel: PostContentAreaModel {
-    postModel.content
+  var contentAreaModel: PostContentAreaModel? {
+    postModel?.content
   }
   
-  var footerModel: PostFooterModel {
-    postModel.footer
+  var footerModel: PostFooterModel? {
+    postModel?.footer
   }
   
   // MARK: - Default data(When fetched data invalid)
@@ -37,7 +37,7 @@ final class PostCellViewModel {
   }
   
   // MARK: - Initialization
-  init(postModel: PostModel) {
+  init(postModel: PostModel?) {
     self.postModel = postModel
   }
 }
@@ -47,12 +47,12 @@ extension PostCellViewModel {
   
   // MARK: - Check model is valid data
   func isValidatedHeaderModel() -> Bool {
-    let subInfoModel = headerModel.subInfo
     let manager = DateValidationManager()
-    guard subInfoModel.isValidatedDuration(),
-          subInfoModel.isValidatedUsername(),
-          subInfoModel.isValidatedYearMonthDayRange(
-            fromDateValidationManager: manager)
+    guard 
+      let subInfoModel = headerModel?.subInfo,
+      subInfoModel.isValidatedDuration(),
+      subInfoModel.isValidatedUsername(),
+      subInfoModel.isValidatedYearMonthDayRange(fromDateValidationManager: manager)
     else {
       return false
     }
@@ -60,7 +60,9 @@ extension PostCellViewModel {
   }
   
   func isValidatedContentAreaModel() -> Bool {
-    guard contentAreaModel.isValidatedThumbnailImages() else {
+    guard
+      let contentAreaModel,
+      contentAreaModel.isValidatedThumbnailImages() else {
       return false
     }
     return true
@@ -68,6 +70,7 @@ extension PostCellViewModel {
   
   func isValidatedFooterModel() -> Bool {
     guard
+      let footerModel,
       footerModel.isValidatedCommentCount() ||
       footerModel.isValidatedHeartCount()
     else {
