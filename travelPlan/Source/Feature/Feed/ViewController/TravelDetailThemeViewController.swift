a324940bb4aa9cba71a4b5dc6431b3846d50dfa4
shcommit <happysh_s2@naver.com>
2023-10-03 00:03:21 +0900

    [Feat] 여행 후기를 보여주는 postView 프로퍼티, vm, adapter 추가 및 collectionview layout itemsize결정될 때 publihser 추가

--- travelPlan/Source/Feature/Feed/ViewController/TravelDetailThemeViewController.swift
+++ travelPlan/Source/Feature/Feed/ViewController/TravelDetailThemeViewController.swift
@@ -5,4 +5,50 @@
 //  Created by 양승현 on 10/2/23.
 //
 
-import Foundation
+import UIKit
+import Combine
+
+final class TravelDetailThemeViewController: UIViewController {
+  // MARK: - Properties
+  private let postView = PostCollectionView()
+  
+  var itemSizeSetNotifier: AnyPublisher<Void, Never> {
+    postView.itemSizeSetNotifier
+  }
+  
+  private var viewModel: PostViewModel!
+  
+  private var postViewAdapter: PostViewAdapter!
+  
+  private var isViewModelNil: Bool {
+    viewModel == nil
+  }
+  
+  override func loadView() {
+    view = postView
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+  
+  // MARK: - Lifecycle
+  init(with filterInfo: FeedPostSearchFilterInfo) {
+    super.init(nibName: nil, bundle: nil)
+    viewModel = PostViewModel(filterInfo: filterInfo)
+    postViewAdapter = PostViewAdapter(dataSource: viewModel, collectionView: postView)
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
+}
+
+
+// MARK: - Private Helpers
+extension TravelDetailThemeViewController {
+  private func configureUI() {
+    view.translatesAutoresizingMaskIntoConstraints = false
+  }
+}
