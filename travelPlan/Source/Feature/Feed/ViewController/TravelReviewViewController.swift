de012501467d79ca8ed2708c2276e5a78152c3ca
shcommit <happysh_s2@naver.com>
2023-10-02 11:53:16 +0900

    [Feat] TravelReviewViewController 생성 및 구현해야할 사항 임시 이미지로 대체

--- travelPlan/Source/Feature/Feed/ViewController/TravelReviewViewController.swift
+++ travelPlan/Source/Feature/Feed/ViewController/TravelReviewViewController.swift
@@ -1,8 +1,45 @@
 //
-//  TravelReviewViewControllerswift.swift
+//  TravelReviewViewController.swift
 //  travelPlan
 //
 //  Created by 양승현 on 10/2/23.
 //
 
-import Foundation
+import UIKit
+
+final class TravelReviewViewController: UIViewController {
+  
+  // MARK: - Lifecycle
+  init() {
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    configureUI()
+    temp()
+  }
+  
+  // MARK: - Private helper
+  private func configureUI() {
+    view.backgroundColor = .white
+    setupBackBarButtonItem()
+  }
+  private func temp() {
+    _=UIImageView(image: UIImage(named: "tempTravelReviewPage")).set {
+      $0.translatesAutoresizingMaskIntoConstraints = false
+      $0.contentMode = .scaleAspectFit
+      view.addSubview($0)
+      NSLayoutConstraint.activate([
+        $0.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        $0.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+        $0.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        $0.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
+    }
+  }
+}
