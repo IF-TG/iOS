cdf5b9e3ee7e4c3a3ea55414673be8e49f0037ef
shcommit <happysh_s2@naver.com>
2023-10-03 21:48:27 +0900

    [Feat] FavoriteDetailViewController 생성

---
+++ travelPlan/Source/Feature/Favorite/FavoriteList/FavoriteDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FavoriteDetailViewController.swift
+//  travelPlan
+//
+//  Created by 양승현 on 10/3/23.
+//
+
+import UIKit
+
+final class FavoriteDetailViewController: UIViewController {
+  // MARK: - Properties
+  
+  // MARK: - Lifecycle
+  init() {
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
