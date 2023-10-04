7d2ca1174f1edaa7d16132476d5115d30aaf2710
shcommit <happysh_s2@naver.com>
2023-10-04 19:46:47 +0900

    [Feat] post 저장된 label 추가 및 intrinsicContentSize 추가

--- travelPlan/Source/Feature/Favorite/View/FavoriteDetailCategoryAreaView.swift
+++ travelPlan/Source/Feature/Favorite/View/FavoriteDetailCategoryAreaView.swift
@@ -1,5 +1,5 @@
 //
-//  FavoriteDetailCategoryView.swift
+//  FavoriteDetailCategoryAreaView.swift
 //  travelPlan
 //
 //  Created by 양승현 on 10/4/23.
@@ -14,7 +14,6 @@ final class FavoriteDetailCategoryAreaView: UIView {
       enum Spacing {
         static let leading: CGFloat = 16
         static let top = leading
-        static let bottom = leading
       }
     }
     
@@ -24,18 +23,41 @@ final class FavoriteDetailCategoryAreaView: UIView {
         static let leading: CGFloat = 8
         static let top: CGFloat = 16
         static let trailing: CGFloat = top
-        static let bottom: CGFloat = top
+      }
+    }
+    
+    enum StoredTotalPostLabel {
+      static let textColor: UIColor = .YG.gray3
+      static let textSize: CGFloat = 13
+      static let textWeight: CGFloat = 500
+      enum Spacing {
+        static let top: CGFloat = 16
+        static let leaidng: CGFloat = 20
+        static let trailing = leaidng
       }
     }
   }
   
   // MARK: - Properties
-  private var travelReviewLabel = PrimaryColorToneRoundLabel(currentState: .selected).set {
+  private let travelReviewLabel = PrimaryColorToneRoundLabel(currentState: .selected).set {
     $0.text = "여행 리뷰"
   }
   
-  private var travelLocationLabel = PrimaryColorToneRoundLabel().set {
+  private let travelLocationLabel = PrimaryColorToneRoundLabel().set {
     $0.text = "장소"
+  }
+  
+  private var storedPostCount: Int = 0
+  
+  private lazy var storedTotalPostLabel: UILabel = UILabel(frame: .zero).set {
+    typealias Const = Constant.StoredTotalPostLabel
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    $0.numberOfLines = 1
+    $0.textAlignment = .natural
+    $0.textColor = Const.textColor
+    $0.font = UIFont.systemFont(ofSize: Const.textSize, weight: .init(Const.textWeight))
+    $0.text = "찜한 글 " + storedPostCount.zeroPaddingString + "개"
+    $0.sizeToFit()
   }
   
   var travelReviewTapHandler: (() -> Void)? {
@@ -44,6 +66,12 @@ final class FavoriteDetailCategoryAreaView: UIView {
   
   var travelLocationTapHandler: (() -> Void)? {
     travelLocationLabel.tapHandler
+  }
+  
+  override var intrinsicContentSize: CGSize {
+    let width = super.intrinsicContentSize.width
+    let height = 98.0
+    return .init(width: width, height: height)
   }
   
   // MARK: - Lifecycle
@@ -73,7 +101,8 @@ extension FavoriteDetailCategoryAreaView: LayoutSupport {
   func addSubviews() {
     _=[
       travelReviewLabel,
-      travelLocationLabel
+      travelLocationLabel,
+      storedTotalPostLabel
     ].map {
       addSubview($0)
     }
@@ -82,7 +111,8 @@ extension FavoriteDetailCategoryAreaView: LayoutSupport {
   func setConstraints() {
     _=[
       travelReviewLabelConstraints,
-      travelLocationLabelConstraints
+      travelLocationLabelConstraints,
+      storedTotalPostLabelConstraints
     ].map {
       NSLayoutConstraint.activate($0)
     }
@@ -92,20 +122,30 @@ extension FavoriteDetailCategoryAreaView: LayoutSupport {
 // MARK: - LayoutSupport Constraints
 private extension FavoriteDetailCategoryAreaView {
   var travelReviewLabelConstraints: [NSLayoutConstraint] {
-    typealias Spacing = Constant.TravelReviewLabel.Spacing
+    typealias Const = Constant.TravelReviewLabel
+    typealias Spacing = Const.Spacing
     return [
       travelReviewLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Spacing.leading),
       travelReviewLabel.topAnchor.constraint(equalTo: topAnchor, constant: Spacing.top),
-      travelReviewLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -Spacing.bottom)]
+      travelReviewLabel.heightAnchor.constraint(equalToConstant: Const.height)]
   }
   
   var travelLocationLabelConstraints: [NSLayoutConstraint] {
-    typealias Spacing = Constant.TravelLocationLabel.Spacing
+    typealias Const = Constant.TravelLocationLabel
+    typealias Spacing = Const.Spacing
     return [
       travelLocationLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Spacing.leading),
       travelLocationLabel.topAnchor.constraint(equalTo: topAnchor, constant: Spacing.top),
-      travelLocationLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -Spacing.bottom),
-      travelLocationLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: Spacing.trailing)]
+      travelLocationLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Spacing.trailing),
+      travelLocationLabel.heightAnchor.constraint(equalToConstant: Const.height)]
   }
   
+  var storedTotalPostLabelConstraints: [NSLayoutConstraint] {
+    typealias Const = Constant.StoredTotalPostLabel
+    typealias Spacing = Const.Spacing
+    return [
+      storedTotalPostLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Spacing.leaidng),
+      storedTotalPostLabel.topAnchor.constraint(equalTo: travelReviewLabel.bottomAnchor, constant: Spacing.top),
+      storedTotalPostLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Spacing.trailing)]
+  }
 }
