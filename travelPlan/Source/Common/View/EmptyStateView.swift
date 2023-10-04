4e133cf05e48d5ec52f4e43aa6d197597b0f6653
shcommit <happysh_s2@naver.com>
2023-10-04 20:35:50 +0900

    [Feat] UseageType 추가

--- travelPlan/Source/Common/View/EmptyStateView.swift
+++ travelPlan/Source/Common/View/EmptyStateView.swift
@@ -8,5 +8,11 @@
 import UIKit
 
 final class EmptyStateView: UIView {
-  
+  enum UseageType {
+    case emptyNotifiation
+    case disabledNotification
+    case emptyTravelPost
+    case emptyTravelLocation
+    case custom(imagePath: String, title: String, content: String)
+  }
 }
