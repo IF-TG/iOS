8f8007c57982469a4c3c5a2e399b28f6c873245d
shcommit <happysh_s2@naver.com>
2023-10-05 22:59:27 +0900

    [Feat] ContentView 추가 및 컨텐츠가 비어있을 경우 emptyStateView 보여지는 공통로직 추가: )

--- travelPlan/Source/Common/View/EmptyStateBasedContentViewController.swift
+++ travelPlan/Source/Common/View/EmptyStateBasedContentViewController.swift
@@ -5,4 +5,83 @@
 //  Created by 양승현 on 10/5/23.
 //
 
-import Foundation
+import UIKit
+import Combine
+
+protocol EmptyStateBasedContentViewCheckable: AnyObject {
+  var itemState: PassthroughSubject<Bool, Never> { get }
+}
+
+class EmptyStateBasedContentViewController: UIViewController {
+  // MARK: - Properties
+  private let contentView: UIView & EmptyStateBasedContentViewCheckable
+  
+  private let emptyStateView: UIView = .init(frame: .zero)
+  
+  private var subscription: AnyCancellable?
+  
+  // MARK: - Lifecycle
+  init(contentView: UIView & EmptyStateBasedContentViewCheckable) {
+    self.contentView = contentView
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+  
+  override func loadView() {
+    view = contentView
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+    bind()
+  }
+}
+
+// MARK: - Private Helpers
+private extension EmptyStateBasedContentViewController {
+  func configureUI() {
+    view.backgroundColor = .white
+    setupUI()
+  }
+  
+  func bind() {
+    subscription = contentView
+      .itemState
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] in
+        self?.emptyStateView.isHidden = $0 ? false : true
+      }
+  }
+}
+
+// MARK: - LayoutSupport
+extension EmptyStateBasedContentViewController: LayoutSupport {
+  func addSubviews() {
+    _=[
+      emptyStateView
+    ].map {
+      view.addSubview($0)
+    }
+  }
+  
+  func setConstraints() {
+    _=[
+      emptyStateViewConstraints
+    ].map {
+      NSLayoutConstraint.activate($0)
+    }
+  }
+}
+
+// MARK: - LayoutSupport Constraints
+private extension EmptyStateBasedContentViewController {
+  var emptyStateViewConstraints: [NSLayoutConstraint] {
+    return [
+      emptyStateView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      emptyStateView.centerXAnchor.constraint(equalTo: view.centerXAnchor)]
+  }
+}
