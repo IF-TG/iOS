1b9c316d86f508d787685889c0847b1864d060b9
seokhyun <letushyun@gmail.com>
2023-10-06 15:23:25 +0900

    [Rename] 공통뷰를 위해 프로토콜 네이밍을 변경

---
+++ travelPlan/Source/Feature/Search/Protocol/TitleWithButtonHeaderViewDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  TitleWithButtonHeaderViewDelegate.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 2023/08/15.
+//
+
+import Foundation
+
+protocol TitleWithButtonHeaderViewDelegate: AnyObject {
+  func didTaplookingMoreButton(in section: Int)
+}
