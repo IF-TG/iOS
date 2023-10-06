a4f4173f87e4ff5a307611c67de45421be0b461f
seokhyun <letushyun@gmail.com>
2023-10-07 06:14:52 +0900

    [Rename] 공통 뷰에서 제거하기 위해 개별 네이밍으로 변경

---
+++ travelPlan/Source/Feature/Search/View/SearchCampingCell.swift
@@ -0,0 +1,124 @@
+//
+//  SearchCampingCell.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 2023/06/01.
+//
+
+import UIKit
+import SnapKit
+import Combine
+
+class SearchCampingCell: UICollectionViewCell {
+  typealias Input = TravelDestinationCellViewModel.Input
+  typealias ErrorType = TravelDestinationCellViewModel.ErrorType
+  typealias State = TravelDestinationCellViewModel.State
+  
+  // MARK: - Properties
+  static var id: String {
+    return String(describing: self)
+  }
+  
+  private var viewModel: TravelDestinationCellViewModel? {
+    didSet {
+      bind()
+    }
+  }
+  
+  private lazy var containerView: BaseDestinationView<LeftAlignThreeLabelsView> 
+  = .init(centerView: LeftAlignThreeLabelsView()).set {
+    $0.delegate = self
+  }
+  
+  private lazy var input = Input()
+  private var subscriptions = Set<AnyCancellable>()
+  
+  // MARK: - LifeCycle
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    containerView.clearButtonSelectedState()
+    containerView.clearThumbnailImage()
+    subscriptions.removeAll()
+  }
+}
+
+// MARK: - ViewBindCase
+extension SearchCampingCell: ViewBindCase {
+  func bind() {
+    guard let viewModel = self.viewModel else { return }
+    
+    let output = viewModel.transform(input)
+    output
+      .receive(on: RunLoop.main)
+      .sink { [weak self] completion in
+        switch completion {
+        case .finished:
+          print("DEBUG: finished SearchFamousSpotCell")
+        case .failure(let error):
+          self?.handleError(error)
+        }
+      } receiveValue: { [weak self] in
+        self?.render($0)
+      }
+      .store(in: &subscriptions)
+  }
+  
+  func render(_ state: State) {
+    switch state {
+    case .changeButtonColor:
+      containerView.toggleStarButtonState()
+    case .none: break
+    }
+  }
+  
+  func handleError(_ error: ErrorType) {
+    switch error {
+    case .fatalError: 
+      print("DEBUG: fatalError occurred")
+    case .networkError:
+      print("DEBUG: networkError occurred")
+    case .unexpected:
+      print("DEBUG: unexpected occurred")
+    }
+  }
+}
+
+// MARK: - StarButtonDelegate
+extension SearchCampingCell: StarButtonDelegate {
+  func didTapStarButton(_ button: UIButton) {
+    input.didTapStarButton.send()
+  }
+}
+
+// MARK: - Helpers
+extension SearchCampingCell {
+  func configure(with viewModel: TravelDestinationCellViewModel) {
+    self.viewModel = viewModel
+    
+    containerView.configure(centerModel: viewModel.contentModel)
+    containerView.configure(imageURL: viewModel.imageURLString,
+                            isSelectedButton: viewModel.isSelectedButton)
+  }
+}
+
+// MARK: - LayoutSupport
+extension SearchCampingCell: LayoutSupport {
+  func addSubviews() {
+    contentView.addSubview(containerView)
+  }
+  
+  func setConstraints() {
+    containerView.snp.makeConstraints {
+      $0.edges.equalTo(contentView)
+    }
+  }
+}
