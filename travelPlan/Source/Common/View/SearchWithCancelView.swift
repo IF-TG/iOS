247e47696467f3c4f0ce7025fbe641f4dc0a05dc
shcommit <happysh_s2@naver.com>
2023-10-07 15:31:06 +0900

    [Feat] 글자수 초과시 초과 레이블 추가. 초과 레이블 애니메이션 도입

--- travelPlan/Source/Common/View/SearchWithCancelView.swift
+++ travelPlan/Source/Common/View/SearchWithCancelView.swift
@@ -9,11 +9,29 @@ import UIKit
 import Combine
 
 final class SearchWithCancelView: UIView {
+  enum EditingState {
+    case origin
+    case good
+    case excess
+    
+    var borderColor: CGColor {
+      var color: UIColor
+      switch self {
+      case .origin:
+        color = .yg.gray0
+      case .good:
+        color = .yg.primary
+      case .excess:
+        color = .yg.red2
+      }
+      return color.cgColor
+    }
+  }
+  
   enum Constant {
     static let height: CGFloat = TextField.font.lineHeight + TextField.Spacing.top + TextField.Spacing.bottom
     static let radius: CGFloat = 2
     static let borderWidth: CGFloat = 0.5
-    static let borderColor: UIColor = .yg.gray0
     
     enum TextField {
       static let font = UIFont(pretendard: .medium, size: 16)!
@@ -51,12 +69,41 @@ final class SearchWithCancelView: UIView {
     $0.setImage(UIImage(named: Const.iconName), for: .normal)
   }
   
+  private let textExcessAlertLabel = UILabel(frame: .zero).set {
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    $0.text = "입력 가능한 글자수를 초과했습니다."
+    $0.font = .init(pretendard: .medium, size: 12)
+    $0.textColor = .yg.red2
+    $0.alpha = 0
+  }
+  
+  private var isTextExcessAlertExecuted = false
+  
   var changed: AnyPublisher<String, Never> {
     textField.changed
   }
   
   var text: String? {
     textField.text
+  }
+  
+  var editingState: EditingState = .origin {
+    didSet {
+      if editingState != .excess {
+        // TODO: - 애니메이션 실행 스택을 취소하는 새로운 애니메이션 도입해야합니다.
+        isTextExcessAlertExecuted = false
+        textExcessAlertLabel.isHidden = true
+        textExcessAlertLabel.alpha = 0
+        textExcessAlertLabel.transform = .identity
+      }
+      UIView.animate(
+        withDuration: 0.2,
+        delay: 0,
+        options: .curveEaseOut
+      ) {
+        self.layer.borderColor = self.editingState.borderColor
+      }
+    }
   }
   
   override var intrinsicContentSize: CGSize {
@@ -79,12 +126,39 @@ final class SearchWithCancelView: UIView {
   }
 }
 
-  // MARK: - Private Helpers
+// MARK: - Helpers
+extension SearchWithCancelView {
+  func showTextExcessAlertLabel() {
+    guard !isTextExcessAlertExecuted else { return }
+    isTextExcessAlertExecuted = true
+    textExcessAlertLabel.isHidden = false
+    let targetY = textExcessAlertLabel.font.lineHeight
+    UIView.transition(
+      with: textExcessAlertLabel,
+      duration: 0.3,
+      options: [.curveEaseIn],
+      animations: {
+        self.textExcessAlertLabel.alpha = 1
+        self.textExcessAlertLabel.transform = .init(translationX: 0, y: -targetY)
+      }, completion: { _ in
+        UIView.animate(
+          withDuration: 0.3,
+          delay: 1.3,
+          options: .curveEaseOut,
+          animations: {
+            self.textExcessAlertLabel.alpha = 0
+            self.textExcessAlertLabel.transform = .identity
+          })
+      })
+  }
+}
+
+// MARK: - Private Helpers
 private extension SearchWithCancelView {
   func configureUI() {
     typealias Const = Constant
     translatesAutoresizingMaskIntoConstraints = false
-    layer.borderColor = Const.borderColor.cgColor
+    layer.borderColor = editingState.borderColor
     layer.borderWidth = Const.borderWidth
     layer.cornerRadius = Const.radius
     setupUI()
@@ -104,7 +178,8 @@ extension SearchWithCancelView: LayoutSupport {
   func addSubviews() {
     _=[
       textField,
-      cancelButton
+      cancelButton,
+      textExcessAlertLabel
     ].map {
       addSubview($0)
     }
@@ -113,7 +188,8 @@ extension SearchWithCancelView: LayoutSupport {
   func setConstraints() {
     _=[
       textFieldConstraints,
-      cancelButtonConstraints
+      cancelButtonConstraints,
+      textExcessAlertLabelConstraints
     ].map {
       NSLayoutConstraint.activate($0)
     }
@@ -138,5 +214,11 @@ private extension SearchWithCancelView {
       cancelButton.heightAnchor.constraint(equalToConstant: Const.size.height),
       cancelButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Spacing.trailing),
       cancelButton.centerYAnchor.constraint(equalTo: centerYAnchor)]
+  }
+  
+  var textExcessAlertLabelConstraints: [NSLayoutConstraint] {
+    return [
+      textExcessAlertLabel.topAnchor.constraint(equalTo: topAnchor),
+      textExcessAlertLabel.trailingAnchor.constraint(equalTo: trailingAnchor)]
   }
 }
