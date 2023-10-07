a5e52be890a9e14391182c0950369db4304b1398
shcommit <happysh_s2@naver.com>
2023-10-08 01:12:31 +0900

    [Feat] 임시 목 파일 생성

---
+++ travelPlan/Source/Feature/Favorite/Mock/MockFavoriteUseCase.swift
@@ -0,0 +1,20 @@
+//
+//  MockFavoriteUseCase.swift
+//  travelPlan
+//
+//  Created by 양승현 on 2023/05/22.
+//
+
+import UIKit
+
+struct MockFavoriteUseCase {
+  private let profileImageURLs = ["tempThumbnail7", "tempThumbnail13"]
+  
+  lazy var favoriteHeader: FavoriteHeaderDirectoryEntity = .init(
+    categoryCount: 2,
+    imageURLs: profileImageURLs)
+  
+  lazy var favoriteDirectories: [FavoriteDirectoryEntity] = [
+    .init(id: 0, title: "분위기 있는 카페", innerItemCount: 0, imageURL: profileImageURLs[0]),
+    .init(id: 1, title: "벌써 10월이야? 단풍 각", innerItemCount: 1, imageURL: profileImageURLs[1])]
+}
