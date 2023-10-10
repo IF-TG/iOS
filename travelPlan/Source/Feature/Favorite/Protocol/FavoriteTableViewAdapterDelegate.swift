79981b3c1fdaacb1ce89091b5d8b8d590f16a2bd
shcommit <happysh_s2@naver.com>
2023-10-11 03:13:15 +0900

    [Feat] 델리게이트에 전달하는 title 옵셔널 부여 추후 에러 알림창으로 대응

--- travelPlan/Source/Feature/Favorite/Protocol/FavoriteTableViewAdapterDelegate.swift
+++ travelPlan/Source/Feature/Favorite/Protocol/FavoriteTableViewAdapterDelegate.swift
@@ -9,6 +9,6 @@ import UIKit
 
 protocol FavoriteTableViewAdapterDelegate: AnyObject {
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
-  func tableViewCell(_ cell: FavoriteTableViewCell, didTapEditModeTitle title: String)
+  func tableViewCell(_ cell: FavoriteTableViewCell, didTapEditModeTitle title: String?)
   func tableViewCell(_ cell: FavoriteTableViewCell, didtapDeleteButton: UIButton)
 }
