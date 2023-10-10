9023c0314e142155ccdeb5844846b7e90a29d022
shcommit <happysh_s2@naver.com>
2023-10-11 03:09:45 +0900

    [Style] DirectoryLabel -> EditModeTitleLabel로 변경

--- travelPlan/Source/Feature/Favorite/Protocol/FavoriteTableViewCellDelegate.swift
+++ travelPlan/Source/Feature/Favorite/Protocol/FavoriteTableViewCellDelegate.swift
@@ -8,6 +8,6 @@
 import UIKit
 
 protocol FavoriteTableViewCellDelegate: AnyObject {
-  func favoriteTableViewCell(_ cell: FavoriteTableViewCell, touchUpDirectoryLabel: UILabel)
+  func favoriteTableViewCell(_ cell: FavoriteTableViewCell, touchUpEditModeTitleLabel: UILabel)
   func favoriteTableViewCell(_ cell: FavoriteTableViewCell, touchUpDeleteButton: UIButton)
 }
