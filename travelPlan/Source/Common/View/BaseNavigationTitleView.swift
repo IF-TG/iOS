2bfe8f9959f71217a8d6c724a20a445dbf0bbf0d
seokhyun <letushyun@gmail.com>
2023-10-12 00:29:21 +0900

    [Feat] BaseNavigationTitleView 추가
    init시점에 type을 지정해서 사용할 수 있습니다. default의 경우, String만 지정해주면 됩니다. custom의 경우, UIView를 서브클래싱한 객체를 연관값으로 넣어주면 됩니다.
    적용하고 싶은 ViewController에서 "navigationItem.titleView = BaseNavigationTitleView객체" 를 주입해주면 됩니다.
    titleView에 적용하면 시스템이 leftBarButtonItem과 rightBarButtonItem으로부터 자동으로 레이아웃을 잡아줍니다.

---
+++ travelPlan/Source/Common/View/BaseNavigationTitleView.swift
@@ -0,0 +1,69 @@
+//
+//  BaseNavigationTitleView.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 10/12/23.
+//
+
+import UIKit
+import SnapKit
+
+/// 적용하고 싶은 ViewControler에서 navigationItem.titleView에 해당 객체를 대입하면 됩니다.
+/// titleView에 적용하면 시스템이 leftBarButtonItem과 rightBarButtonItem으로부터 자동으로 레이아웃을 잡아줍니다.
+class BaseNavigationTitleView: UIView {
+  enum Constant {
+    enum DefaultLabel {
+      static let fontSize: CGFloat = 18
+    }
+  }
+  
+  enum `Type` {
+    case `default`(title: String)
+    case custom(customView: UIView)
+    
+    fileprivate var titleView: UIView {
+      switch self {
+      case let .default(title):
+        return UILabel().set {
+          $0.text = title
+          $0.font = .init(pretendard: .semiBold, size: Constant.DefaultLabel.fontSize)
+          $0.textColor = .yg.gray7
+        }
+      case let .custom(customView):
+        return customView
+      }
+    }
+  }
+  
+  // MARK: - Properties
+  private let contentView: UIView
+  
+  // MARK: - LifeCycle
+  convenience init(type: `Type`) {
+    self.init(frame: .zero, type: type)
+  }
+  
+  init(frame: CGRect, type: `Type`) {
+    self.contentView = type.titleView
+    super.init(frame: frame)
+    setupUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: - LayoutSupport
+extension BaseNavigationTitleView: LayoutSupport {
+  func addSubviews() {
+    addSubview(contentView)
+  }
+  
+  func setConstraints() {
+    contentView.snp.makeConstraints {
+      $0.center.equalToSuperview()
+      $0.size.equalToSuperview()
+    }
+  }
+}
