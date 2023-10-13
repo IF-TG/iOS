eef72f022280b8b0026323e473c7f08bdd66b2d5
shcommit <happysh_s2@naver.com>
2023-10-14 02:17:04 +0900

    [Feat] makeURL(with:) 함수 구현

--- travelPlan/Source/Network/Requestable.swift
+++ travelPlan/Source/Network/Requestable.swift
@@ -5,7 +5,6 @@
 //  Created by 양승현 on 10/12/23.
 //
 
-import Foundation
 import Alamofire
 
 protocol Requestable {
@@ -14,8 +13,18 @@ protocol Requestable {
   var host: String { get }
   var method: HTTPMethod { get }
   var prefixPath: String { get }
-  var suffixPath: ResponseType { get }
-  var queryParams: Parameters? { get }
-  var bodyParams: Parameters? { get }
+  var queryParams: Encodable? { get }
+  var bodyParams: Encodable? { get }
   var headers: HTTPHeaders? { get }
+}
+
+extension Requestable {
+  func makeURL(with responseType: ResponseType) throws -> URL {
+    let baseURL = "\(scheme)://\(host)" + prefixPath + responseType.path
+    guard var components = URLComponents(string: baseURL) else {
+      throw Error.invalidURL(url: baseURL)
+    }
+    components.queryItems = queryParams?.makeQueryItems()
+    return try components.asURL()
+  }
 }
