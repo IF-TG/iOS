0672c425d82f1e9ec0668e0ebe4b8266a743706c
shcommit <happysh_s2@naver.com>
2023-10-14 17:49:53 +0900

    [Feat] requestType 추가. makeRequest 매개변수 타입 제거

--- travelPlan/Source/Network/Requestable.swift
+++ travelPlan/Source/Network/Requestable.swift
@@ -14,16 +14,16 @@ protocol Requestable {
   var host: String { get }
   var method: HTTPMethod { get }
   var prefixPath: String { get }
-  
   /// get일땐 queryItems에 부착, post일때 httpbody에 추가.
   var parameters: Params? { get }
+  var requestType: RequestType { get }
   var headers: HTTPHeaders? { get }
   var interceptor: RequestInterceptor? { get }
 }
 
 extension Requestable where Params: Encodable {
-  func makeRequest(with responseType: ResponseType) throws -> DataRequest {
-    let baseURL = "\(scheme)://\(host)" + prefixPath + responseType.path
+  func makeRequest() throws -> DataRequest {
+    let baseURL = "\(scheme)://\(host)" + prefixPath + requestType.path
     guard method == .post else {
       return AF.request(baseURL, method: method, parameters: parameters, interceptor: interceptor)
     }
