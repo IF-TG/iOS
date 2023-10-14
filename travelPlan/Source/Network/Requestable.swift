a62a4d3a610a2614e6a58ade2f2e0ea2d400f469
shcommit <happysh_s2@naver.com>
2023-10-14 17:02:02 +0900

    [Feat] Parameter타입 수정

--- travelPlan/Source/Network/Requestable.swift
+++ travelPlan/Source/Network/Requestable.swift
@@ -9,18 +9,19 @@ import Alamofire
 
 protocol Requestable {
   typealias Error = AFError
-  associatedtype Params: Encodable
+  associatedtype Params
   var scheme: String { get }
   var host: String { get }
   var method: HTTPMethod { get }
   var prefixPath: String { get }
-  /// get일땐 queryItems에 부착, post일때 httpbody에 추가. AF는
+  
+  /// get일땐 queryItems에 부착, post일때 httpbody에 추가.
   var parameters: Params? { get }
   var headers: HTTPHeaders? { get }
   var interceptor: RequestInterceptor? { get }
 }
 
-extension Requestable {
+extension Requestable where Params: Encodable {
   func makeRequest(with responseType: ResponseType) throws -> DataRequest {
     let baseURL = "\(scheme)://\(host)" + prefixPath + responseType.path
     guard method == .post else {
