0a196a53c3c98a93dbbf309f312ee5f360a79746
seokhyun <letushyun@gmail.com>
2023-10-14 20:25:25 +0900

    [Feat] SectionBackgroundView Layout 적용

--- travelPlan/Source/Feature/Search/View/SearchMoreDetailLayoutManager.swift
+++ travelPlan/Source/Feature/Search/View/SearchMoreDetailLayoutManager.swift
@@ -22,7 +22,7 @@ class SearchMoreDetailLayoutManager: CompositionalLayoutCreatable {
 
 // MARK: - Private Helpers
 extension SearchMoreDetailLayoutManager {
-  enum Constants {
+  enum Constant {
     enum Item {
       static let fractionWidth: CGFloat = 1
       static let fractionHeight: CGFloat = 1
@@ -36,36 +36,50 @@ extension SearchMoreDetailLayoutManager {
     }
     enum Section {
       enum ContentInsets {
-        static let top: CGFloat = 16
+        static let top: CGFloat = headerViewSectionViewSpacing + sectionViewCellSpacing
       }
     }
     enum Header {
       static let fractionWidth: CGFloat = 1
-      static let fractionHeight: CGFloat = 0.275
+      static let height: CGFloat = 200
     }
+    enum SectionBackgroundView {
+      enum ContentInsets {
+        static let top: CGFloat = Constant.Header.height + headerViewSectionViewSpacing
+      }
+    }
+    static let headerViewSectionViewSpacing: CGFloat = 16
+    static let sectionViewCellSpacing: CGFloat = 8
   }
   
   private func makeSectionLayout() -> NSCollectionLayoutSection {
-    let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(Constants.Item.fractionWidth),
-                                          heightDimension: .fractionalHeight(Constants.Item.fractionHeight))
+    let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(Constant.Item.fractionWidth),
+                                          heightDimension: .fractionalHeight(Constant.Item.fractionHeight))
     let item = NSCollectionLayoutItem(layoutSize: itemSize)
     
-    let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(Constants.Group.fractionWidth),
-                                           heightDimension: .absolute(Constants.Group.height))
+    let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(Constant.Group.fractionWidth),
+                                           heightDimension: .absolute(Constant.Group.height))
     let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
     group.contentInsets = .init(top: .zero,
-                                leading: Constants.Group.ContentInsets.leading,
+                                leading: Constant.Group.ContentInsets.leading,
                                 bottom: .zero,
                                 trailing: .zero)
     
     let section = NSCollectionLayoutSection(group: group)
-    section.contentInsets = .init(top: Constants.Section.ContentInsets.top,
+    section.contentInsets = .init(top: Constant.Section.ContentInsets.top,
                                   leading: .zero,
                                   bottom: .zero,
                                   trailing: .zero)
-
-    let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(Constants.Header.fractionWidth),
-                                            heightDimension: .fractionalHeight(Constants.Header.fractionHeight))
+    let sectionBackgroundView = NSCollectionLayoutDecorationItem
+      .background(elementKind: InnerRoundRectReusableView.id)
+    sectionBackgroundView.contentInsets = .init(top: Constant.SectionBackgroundView.ContentInsets.top,
+                                                leading: .zero,
+                                                bottom: .zero,
+                                                trailing: .zero)
+    section.decorationItems = [sectionBackgroundView]
+    
+    let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(Constant.Header.fractionWidth),
+                                            heightDimension: .absolute(Constant.Header.height))
     let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
       layoutSize: headerSize,
       elementKind: UICollectionView.elementKindSectionHeader,
