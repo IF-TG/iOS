f1eaaf407ffb7e613d3f1050f48eb8ce8ae6dfc2
shcommit <happysh_s2@naver.com>
2023-10-14 17:06:35 +0900

    [Feat] Endpoint 추가

--- travelPlan/Source/Network/Endpoint.swift
+++ travelPlan/Source/Network/Endpoint.swift
@@ -5,4 +5,32 @@
 //  Created by 양승현 on 10/11/23.
 //
 
-import Foundation
+import Alamofire
+
+final class Endpoint<ResponseDTO>: NetworkInteractionable where ResponseDTO: Decodable {
+  typealias Params = Encodable?
+  
+  var scheme: String
+  var host: String
+  var method: HTTPMethod
+  var prefixPath: String
+  var parameters: Params?
+  var headers: HTTPHeaders?
+  var interceptor: RequestInterceptor?
+  
+  init(
+    scheme: String = "https",
+    host: String = "호스트 미정",
+    method: HTTPMethod = .get,
+    prefixPath: String = "미정",
+    headers: HTTPHeaders? = ["Content-Type": "application/json"],
+    interceptor: RequestInterceptor? = nil
+  ) {
+    self.scheme = scheme
+    self.host = host
+    self.method = method
+    self.prefixPath = prefixPath
+    self.headers = headers
+    self.interceptor = interceptor
+  }
+}
