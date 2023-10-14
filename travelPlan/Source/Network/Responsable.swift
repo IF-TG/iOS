103ac6f8969dc194d91ae4d38b791864a416e1d3
shcommit <happysh_s2@naver.com>
2023-10-14 17:07:30 +0900

    [Style] 주석 추가

---
+++ travelPlan/Source/Network/Responsable.swift
@@ -0,0 +1,13 @@
+//
+//  Responsable.swift
+//  travelPlan
+//
+//  Created by 양승현 on 10/14/23.
+//
+
+import Foundation
+
+protocol Responsable {
+  /// AF 요청 후 응답받을 반환 타입
+  associatedtype ResponseDTO
+}
