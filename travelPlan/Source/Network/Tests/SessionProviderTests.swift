6f7152aa349a8f3c856a3cb8cefd1b45e8683154
shcommit <happysh_s2@naver.com>
2023-10-16 02:44:33 +0900

    [Feat] External class 추가

--- travelPlan/Source/Network/Tests/SessionProviderTests.swift
+++ travelPlan/Source/Network/Tests/SessionProviderTests.swift
@@ -5,17 +5,36 @@
 //  Created by 양승현 on 10/16/23.
 //
 
+import Combine
+import Alamofire
 import XCTest
 @testable import travelPlan
 
 final class SessionProviderTests: XCTestCase {
+  var sut: Endpoint<UserNameResponseModel>!
+  var mockRequestModel = UserNameRequestModel(name: "iosDeveloper:]", id: 1334)
+  let mockSession: Session!
+  var expectation: XCTestExpectation!
 
   override func setUp() {
     super.setUp()
+    sut = Endpoint(
+      scheme: "https",
+      host: "test.com",
+      method: .get,
+      prefixPath: "/user",
+      parameters: mockRequestModel,
+      requestType: .custom("name-update"))
+    MockUrlProtocol.requestHandler = { _ in
+      return ((HTTPURLResponse(), Data()))
+    }
+    expectation = expectation(description: "finish")
   }
   
   override func tearDown() {
     super.tearDown()
+    sut = nil
+    expectation = nil
   }
 
 }
