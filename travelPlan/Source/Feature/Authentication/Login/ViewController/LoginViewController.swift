56be7feda3b4cdaa617035da0b8623d4b96e8240
seokhyun <letushyun@gmail.com>
2023-10-16 01:17:07 +0900

    [Feat] 온보딩 동영상 및 StartView 추가

--- travelPlan/Source/Feature/Authentication/Login/ViewController/LoginViewController.swift
+++ travelPlan/Source/Feature/Authentication/Login/ViewController/LoginViewController.swift
@@ -7,22 +7,30 @@
 
 import UIKit
 import Combine
+import AVFoundation
 
 class LoginViewController: UIViewController {
   // MARK: - Properteis
   var vm: LoginViewModel!
   let appear = PassthroughSubject<Void, ErrorType>()
   let viewLoad = PassthroughSubject<Void, ErrorType>()
-  var subscription = Set<AnyCancellable>()
+  private var subscriptions = Set<AnyCancellable>()
   weak var coordinator: LoginCoordinatorDelegate?
+  
+  private var player: AVPlayer!
+  private var playerLayer: AVPlayerLayer!
+  
+  private let loginStartView = LoginStartView()
   
   // MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    setupPlayer()
+    setupUI()
     bind()
-    viewLoad.send()
-    view.backgroundColor = .brown
+    setupStyles()
     
+    viewLoad.send()
   }
   
   private override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -67,7 +75,7 @@ extension LoginViewController: ViewBindCase {
       }
     } receiveValue: { [weak self] in
       self?.render($0)
-    }.store(in: &subscription)
+    }.store(in: &subscriptions)
     
   }
   
@@ -88,6 +96,55 @@ extension LoginViewController: ViewBindCase {
       print("none")
     case .unexpectedError:
       print("unexpectedError")
+    }
+  }
+}
+
+// MARK: - Private Helpers
+extension LoginViewController {
+  private func setupStyles() {
+    view.backgroundColor = .white
+  }
+  
+  private func setupPlayer() {
+    guard let url = Bundle.main.url(forResource: "onboarding-video", withExtension: "mp4") else { return }
+    self.player = AVPlayer(playerItem: AVPlayerItem(url: url))
+    self.playerLayer = AVPlayerLayer(player: player)
+    
+    playerLayer.frame = view.bounds
+    playerLayer.videoGravity = .resizeAspectFill
+    view.layer.addSublayer(playerLayer)
+    
+    NotificationCenter.default.publisher(
+      for: AVPlayerItem.didPlayToEndTimeNotification,
+      object: self.player.currentItem
+    )
+    .sink { [weak self] _ in
+      // CMTime.zero: 비디오의 시작점
+      self?.player.seek(to: CMTime.zero, completionHandler: { isSeekingCompelete in
+        if isSeekingCompelete {
+          self?.player.play()
+        }
+      })
+    }
+    .store(in: &subscriptions)
+    
+    player.play()
+  }
+}
+
+// MARK: - LayoutSupport
+extension LoginViewController: LayoutSupport {
+  func addSubviews() {
+    view.addSubview(loginStartView)
+  }
+  
+  func setConstraints() {
+    loginStartView.snp.makeConstraints {
+      $0.centerX.equalToSuperview()
+      $0.bottom.equalToSuperview().inset(60)
+      $0.width.equalTo(70)
+      $0.height.equalTo(125)
     }
   }
 }
