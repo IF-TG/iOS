ffa19c29afb81576b863c95f1286e852b966bd01
shcommit <happysh_s2@naver.com>
2023-10-16 14:12:49 +0900

    [Test] SessionProvider를 통해 요청 후 응답된 값이 200..299이외의 값인 404를 반환할 때 에러 반환되는지 체크

--- travelPlan/Source/Network/Tests/SessionProviderTests.swift
+++ travelPlan/Source/Network/Tests/SessionProviderTests.swift
@@ -11,6 +11,8 @@ import XCTest
 @testable import travelPlan
 
 final class SessionProviderTests: XCTestCase {
+  let mockUserNameRequestDTO = UserNameRequestDTO(name: "iosDeveloper:]", id: 1334)
+  var mockUserEndpoint: MockUserEndpoint!
   var sut: Sessionable! = nil
   var mockSession: Session!
   var expectation: XCTestExpectation!
@@ -19,6 +21,7 @@ final class SessionProviderTests: XCTestCase {
   override func setUp() {
     super.setUp()
     mockSession = MockSession.default
+    mockUserEndpoint = MockUserEndpoint.shared
     sut = SessionProvider(session: MockSession.default)
     expectation = expectation(description: "finish")
   }
@@ -26,6 +29,7 @@ final class SessionProviderTests: XCTestCase {
   override func tearDown() {
     super.tearDown()
     sut = nil
+    mockUserEndpoint = nil
     expectation = nil
     mockSession = nil
     subscription = nil
@@ -35,8 +39,6 @@ final class SessionProviderTests: XCTestCase {
 extension SessionProviderTests {
   func testUploadUserName_SessionProvider로요청후응답할때ResponseDTO체크_ShouldReturnEqual() {
     // Arrange
-    var mockUserNameRequestDTO = UserNameRequestDTO(name: "iosDeveloper:]", id: 1334)
-    var mockUserEndpoint = MockUserEndpoint.shared
     let userNameUploadEndpoint = mockUserEndpoint.uploadUserName(with: mockUserNameRequestDTO)
     let responseJSONString = """
       {
@@ -50,10 +52,7 @@ extension SessionProviderTests {
     
     // Act
     subscription = sut.request(endpoint: userNameUploadEndpoint)
-      .eraseToAnyPublisher()
-      .catch {_ in 
-        Just(UserNameResponseDTO(status: "False"))
-      }
+      .catch {_ in Just(UserNameResponseDTO(status: "False")) }
       .sink { [unowned self] responseDTO in
         
         // Assert
@@ -61,5 +60,35 @@ extension SessionProviderTests {
         expectation.fulfill()
       }
     wait(for: [expectation], timeout: 10)
+  }
+  
+  func testUploadUserName_SessionProvider로요청후응답할때StatusCode404일때_ShouldReturnFailure() {
+    // Arrange
+    let userNameUploadEndpoint = mockUserEndpoint.uploadUserName(with: mockUserNameRequestDTO)
+    MockUrlProtocol.requestHandler = { _ in
+      let mockUrl = URL(string: "https://test.com...")!
+      let httpResponse = HTTPURLResponse(url: mockUrl, statusCode: 404, httpVersion: nil, headerFields: nil)!
+      return ((httpResponse, Data()))
+    }
+    
+    // Act
+    subscription = sut.request(endpoint: userNameUploadEndpoint)
+      .sink(receiveCompletion: { [unowned self] completion in
+        
+        // Assert
+        switch completion {
+        case .finished:
+          XCTAssert(false, "Reqeust의 결과로 Failure를 반환해야하는데 subscription의 completion이 반환됨")
+        case .failure(let error):
+          XCTAssertEqual(
+            error.responseCode!, 404,
+            " Request의 결과로 서버에서 요청 데이터를 찾을 수 없다는 결과 404가 반환되야하는데 다른 statusCode가 반환됨")
+        }
+        expectation.fulfill()
+      }, receiveValue: { [unowned self] _ in
+        XCTAssert(false, "Request의 결과로 Failure를 반환해야하는데 이상한 response DTO 값이 반환됨.")
+        expectation.fulfill()
+      })
+    wait(for: [expectation], timeout: 7)
   }
 }
