be353c0675dc1f19c06d8b65fe3d0b3078990a26
shcommit <happysh_s2@naver.com>
2023-10-16 13:49:29 +0900

    [Test] SessionProvider로 UserEndpoint -> request 요청후 값 받는것까지 테스트

--- travelPlan/Source/Network/Tests/SessionProviderTests.swift
+++ travelPlan/Source/Network/Tests/SessionProviderTests.swift
@@ -5,29 +5,21 @@
 //  Created by 양승현 on 10/16/23.
 //
 
-import Combine
 import Alamofire
+import Combine
 import XCTest
 @testable import travelPlan
 
 final class SessionProviderTests: XCTestCase {
-  var sut: Endpoint<UserNameResponseModel>!
-  var mockRequestModel = UserNameRequestModel(name: "iosDeveloper:]", id: 1334)
-  let mockSession: Session!
+  var sut: Sessionable! = nil
+  var mockSession: Session!
   var expectation: XCTestExpectation!
+  var subscription: AnyCancellable!
 
   override func setUp() {
     super.setUp()
-    sut = Endpoint(
-      scheme: "https",
-      host: "test.com",
-      method: .get,
-      prefixPath: "/user",
-      parameters: mockRequestModel,
-      requestType: .custom("name-update"))
-    MockUrlProtocol.requestHandler = { _ in
-      return ((HTTPURLResponse(), Data()))
-    }
+    mockSession = MockSession.default
+    sut = SessionProvider(session: MockSession.default)
     expectation = expectation(description: "finish")
   }
   
@@ -35,6 +27,39 @@ final class SessionProviderTests: XCTestCase {
     super.tearDown()
     sut = nil
     expectation = nil
+    mockSession = nil
+    subscription = nil
   }
+}
 
+extension SessionProviderTests {
+  func testUploadUserName_SessionProvider로요청후응답할때ResponseDTO체크_ShouldReturnEqual() {
+    // Arrange
+    var mockUserNameRequestDTO = UserNameRequestDTO(name: "iosDeveloper:]", id: 1334)
+    var mockUserEndpoint = MockUserEndpoint.shared
+    let userNameUploadEndpoint = mockUserEndpoint.uploadUserName(with: mockUserNameRequestDTO)
+    let responseJSONString = """
+      {
+        "status": "OK"
+      }
+      """
+    MockUrlProtocol.requestHandler = { _ in
+      let responseData = responseJSONString.data(using: .utf8)!
+      return ((HTTPURLResponse(), responseData))
+    }
+    
+    // Act
+    subscription = sut.request(endpoint: userNameUploadEndpoint)
+      .eraseToAnyPublisher()
+      .catch {_ in 
+        Just(UserNameResponseDTO(status: "False"))
+      }
+      .sink { [unowned self] responseDTO in
+        
+        // Assert
+        XCTAssertEqual(responseDTO.status, "OK")
+        expectation.fulfill()
+      }
+    wait(for: [expectation], timeout: 10)
+  }
 }
