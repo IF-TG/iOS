2e0da5bcc571d0a4161f80ff218151427e267eb8
shcommit <happysh_s2@naver.com>
2023-10-16 19:48:01 +0900

    [Style] PostViewAdapterDataSource 생성 및 로직 이동

---
+++ travelPlan/Source/Common/Protocol/PostViewAdapterDataSource.swift
@@ -0,0 +1,15 @@
+//
+//  PostViewAdapterDataSource.swift
+//  travelPlan
+//
+//  Created by 양승현 on 10/16/23.
+//
+
+protocol PostViewAdapterDataSource: AnyObject {
+  var numberOfItems: Int { get }
+  var travelTheme: TravelMainThemeType { get }
+  var travelTrend: TravelOrderType { get }
+  
+  func postViewCellItem(at index: Int) -> PostModel
+  func contentText(at index: Int) -> String
+}
