99e5b4214dc152642da15a191f09f45275638545
shcommit <happysh_s2@naver.com>
2023-10-17 16:33:45 +0900

    [Feat] 불필요한 validator코드 제거. 상위 객체에서 해야함

--- travelPlan/Source/Common/ViewModel/PostCellViewModel.swift
+++ travelPlan/Source/Common/ViewModel/PostCellViewModel.swift
@@ -22,57 +22,9 @@ final class PostCellViewModel {
   var footerModel: PostFooterInfo? {
     postModel?.footer
   }
-  
-  // MARK: - Default data(When fetched data invalid)
- 
-  var defaultContentAreaModel: PostContentInfo {
-    PostContentInfo()
-  }
-  
-  var defaultFooterModel: PostFooterInfo {
-    PostFooterInfo()
-  }
-  
+
   // MARK: - Initialization
   init(postModel: PostInfo?) {
     self.postModel = postModel
-  }
-}
-
-// MARK: - Public Helpers
-extension PostCellViewModel {
-  
-  // MARK: - Check model is valid data
-  func isValidatedHeaderModel() -> Bool {
-    let manager = DateValidationManager()
-    guard 
-      let contentInfo = headerModel?.contentInfo,
-      contentInfo.bottomViewInfo.isValidatedDuration(),
-      contentInfo.bottomViewInfo.isValidatedUsername(),
-      contentInfo.bottomViewInfo.isValidatedYearMonthDayRange(fromDateValidationManager: manager)
-    else {
-      return false
-    }
-    return true
-  }
-  
-  func isValidatedContentAreaModel() -> Bool {
-    guard
-      let contentAreaModel,
-      contentAreaModel.isValidatedThumbnailImages() else {
-      return false
-    }
-    return true
-  }
-  
-  func isValidatedFooterModel() -> Bool {
-    guard
-      let footerModel,
-      footerModel.isValidatedCommentCount() ||
-      footerModel.isValidatedHeartCount()
-    else {
-      return false
-    }
-    return true
   }
 }
