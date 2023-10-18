0fcbb3bacf5a4ef953ab00d3ebc16dc20893f138
shcommit <happysh_s2@naver.com>
2023-10-18 15:32:17 +0900

    [Feat] ShadowInfo 구조체 추가

---
+++ travelPlan/Source/Common/Model/ShadowInfo.swift
@@ -0,0 +1,15 @@
+//
+//  ShadowInfo.swift
+//  travelPlan
+//
+//  Created by 양승현 on 10/18/23.
+//
+
+import UIKit
+
+struct ShadowInfo {
+  let color: UIColor
+  let opaciry: Float
+  let radius: CGFloat
+  let offset: CGSize
+}
