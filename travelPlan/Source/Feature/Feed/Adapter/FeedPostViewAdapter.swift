e4c69e78d9f8c24d650a794fb9d8be1a5f0a8c8e
shcommit <happysh_s2@naver.com>
2023-10-22 21:48:26 +0900

    [Feat] FeedPostViewAdapter 추가 및 header 추가

---
+++ travelPlan/Source/Feature/Feed/Adapter/FeedPostViewAdapter.swift
@@ -0,0 +1,40 @@
+//
+//  FeedPostViewAdapter.swift
+//  travelPlan
+//
+//  Created by 양승현 on 10/22/23.
+//
+
+import UIKit
+
+final class FeedPostViewAdapter: PostViewAdapter {
+  
+  private weak var feedDataSource: FeedPostViewAdapterDataSource?
+  
+  init(dataSource: FeedPostViewAdapterDataSource? = nil, collectionView: PostCollectionView?) {
+    feedDataSource = dataSource
+    super.init(dataSource: dataSource, collectionView: collectionView)
+  }
+}
+
+// MARK: - UICollectionViewDelegate
+extension FeedPostViewAdapter {
+  func collectionView(
+    _ collectionView: UICollectionView,
+    viewForSupplementaryElementOfKind kind: String,
+    at indexPath: IndexPath
+  ) -> UICollectionReusableView {
+    if kind == UICollectionView.elementKindSectionHeader , indexPath.section == 0 {
+      guard
+        let dataSource = feedDataSource,
+        let header = collectionView.dequeueReusableSupplementaryView(
+        ofKind: UICollectionView.elementKindSectionHeader,
+        withReuseIdentifier: PostSortingAreaView.id,
+        for: indexPath
+      ) as? PostSortingAreaView else { return .init(frame: .zero) }
+      header.configure(with: dataSource.headerItem)
+      return header
+    }
+    return .init(frame: .zero)
+  }
+}
