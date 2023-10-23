a752e8bb0a04efaf59a26ea8bd4a46aa0a8d3a76
shcommit <happysh_s2@naver.com>
2023-10-23 18:32:00 +0900

    [Feat] FeedPostViewModel 추가 및 FeedPostViewModelable 적용

--- travelPlan/Source/Feature/Feed/ViewModel/FeedPostViewModel.swift
+++ travelPlan/Source/Feature/Feed/ViewModel/FeedPostViewModel.swift
@@ -12,19 +12,38 @@ class FeedPostViewModel: PostViewModel {
   // MARK: - Properties
   private let filterInfo: FeedPostSearchFilterInfo
   
+  private let postUseCase: PostUseCase
+  
   // MARK: - Lifecycle
   init(filterInfo: FeedPostSearchFilterInfo, postUseCase: PostUseCase) {
+    self.postUseCase = postUseCase
     self.filterInfo = filterInfo
     super.init(postUseCase: postUseCase)
   }
 }
 
+// MARK: - FeedPostViewModelable
 extension FeedPostViewModel: FeedPostViewModelable {
   func transform(_ input: Input) -> AnyPublisher<State, Never> {
-    return .init(Just(.none))
+    return Publishers.MergeMany([
+      viewDidLoadStream(input),
+      postsStream()]
+    ).eraseToAnyPublisher()
   }
 }
 
+private extension FeedPostViewModel {
+  func viewDidLoadStream(_ input: Input) -> Output {
+    return input.viewDidLoad
+      .map {_ in return .none }
+      .eraseToAnyPublisher()
+  }
+  func postsStream() -> Output {
+    return $posts.map { _ in .updatePosts }.eraseToAnyPublisher()
+  }
+}
+
+// MARK: - FeedPostViewAdapterDataSource
 extension FeedPostViewModel: FeedPostViewAdapterDataSource {
   var headerItem: TravelCategorySortingType {
     return .detailCategory(filterInfo.travelTheme)
