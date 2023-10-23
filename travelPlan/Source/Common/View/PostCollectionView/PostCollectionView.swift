54b47a4131280dc567dae500e25df4527371c6b4
shcommit <happysh_s2@naver.com>
2023-10-23 18:24:17 +0900

    [Feat] makeLayout()함수의 첫번째 섹션 커스텀 해서 layout 반환할 수 있게 추가

--- travelPlan/Source/Common/View/PostCollectionView/PostCollectionView.swift
+++ travelPlan/Source/Common/View/PostCollectionView/PostCollectionView.swift
@@ -27,7 +27,7 @@ class PostCollectionView: UICollectionView {
   
   init(frame: CGRect) {
     super.init(frame: frame, collectionViewLayout: .init())
-    collectionViewLayout = makePostLayout()
+    collectionViewLayout = makeLayout()
     configureUI()
   }
   
@@ -43,17 +43,17 @@ class PostCollectionView: UICollectionView {
   required init?(coder: NSCoder) {
     super.init(coder: coder)
     configureUI()
-    collectionViewLayout = makePostLayout()
+    collectionViewLayout = makeLayout()
   }
 }
 
 // MARK: - Public Helpers
 extension PostCollectionView {
-  func makePostLayout() -> UICollectionViewLayout {
+  func makeLayout(withCustomSection customSection : NSCollectionLayoutSection? = nil) -> UICollectionViewLayout {
     return UICollectionViewCompositionalLayout { [weak self] (sectionIndex, _) -> NSCollectionLayoutSection? in
       switch sectionIndex {
       case 0:
-        return self?.tempSection
+        return customSection ?? self?.tempSection
       case 1:
         return self?.postSection
       default:
