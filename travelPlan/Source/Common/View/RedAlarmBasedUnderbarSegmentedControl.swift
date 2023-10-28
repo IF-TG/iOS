b229d5362bfd15fea28e4f11236b9e04f4bfa789
shcommit <happysh_s2@naver.com>
2023-10-29 00:15:28 +0900

    [Feat] redIcon 추가. 사용자가 클릭시 redIcon 사라지도록 하는 기능 추가

--- travelPlan/Source/Common/View/RedAlarmBasedUnderbarSegmentedControl.swift
+++ travelPlan/Source/Common/View/RedAlarmBasedUnderbarSegmentedControl.swift
@@ -14,10 +14,33 @@ struct UnderbarInfo {
 }
 
 class RedAlarmBasedUnderbarSegmentedControl: UISegmentedControl {
+  enum Constant {
+    static let redIconWidth: CGFloat = 5
+  }
   // MARK: - Properties
   private lazy var underbar = makeUnderbar()
   
   private lazy var underbarWidth: CGFloat? = bounds.size.width / CGFloat(numberOfSegments)
+  
+  private lazy var redIcons: [UIView] = {
+    return (0..<numberOfSegments).map { _ in
+      return UIView(frame: .zero).set {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.layer.cornerRadius = Constant.redIconWidth/2
+        $0.layer.backgroundColor = UIColor.red.cgColor
+        $0.alpha = 1
+      }
+    }
+  }()
+  
+  override var selectedSegmentIndex: Int {
+    didSet {
+      print(selectedSegmentIndex)
+      hideSpecificRedIcon(with: selectedSegmentIndex)
+      // TODO: - selectedSegmentedIndex를 변경하는 뷰컨트롤러측에서
+      //          사용자가 특정 segmented view를 확인했음을 뷰모델이나 저장소 등에 갱신해야합니다.
+    }
+  }
   
   private var underbarInfo: UnderbarInfo
   
@@ -45,6 +68,7 @@ class RedAlarmBasedUnderbarSegmentedControl: UISegmentedControl {
     super.layoutSubviews()
     if !isFirstSettingDone {
       isFirstSettingDone.toggle()
+      setRedIconInSegmentedViews()
       setUnderbarMovableBackgroundLayer()
       layer.cornerRadius = 0
       layer.masksToBounds = false
@@ -58,7 +82,17 @@ class RedAlarmBasedUnderbarSegmentedControl: UISegmentedControl {
 
 // MARK: - Helpers
 extension RedAlarmBasedUnderbarSegmentedControl {
+  func showSpecificRedIcon(with index: Int) {
+    UIView.animate(withDuration: 0.24, animations: {
+      self.redIcons[index].alpha = 1
+    })
+  }
   
+  func hideSpecificRedIcon(with index: Int) {
+    UIView.animate(withDuration: 0.24, animations: {
+      self.redIcons[index].alpha = 0
+    })
+  }
 }
 
 // MARK: - Private Helpers
@@ -82,6 +116,35 @@ private extension RedAlarmBasedUnderbarSegmentedControl {
       height: underbarInfo.height)
     backgroundLayer.backgroundColor = underbarInfo.backgroundColor.cgColor
     layer.addSublayer(backgroundLayer)
+  }
+  
+  func setRedIconInSegmentedViews() {
+    let titles = (0..<numberOfSegments).map {
+      titleForSegment(at: $0)
+    }
+    let segmentedTitleLabels = subviews
+      .compactMap { subview in
+        subview.subviews.compactMap { $0 as? UILabel }
+      }
+      .flatMap { $0 }
+      .sorted {
+        guard
+          let idx1 = titles.firstIndex(of: $0.text ?? ""),
+          let idx2 = titles.firstIndex(of: $1.text ?? "")
+        else { return false }
+        return idx1 < idx2
+      }
+    
+    segmentedTitleLabels.enumerated().forEach {
+      let redIcon = redIcons[$0]
+      $1.addSubview(redIcon)
+      NSLayoutConstraint.activate([
+        redIcon.widthAnchor.constraint(equalToConstant: Constant.redIconWidth),
+        redIcon.heightAnchor.constraint(equalToConstant: Constant.redIconWidth),
+        redIcon.topAnchor.constraint(equalTo: $1.topAnchor),
+        redIcon.trailingAnchor.constraint(equalTo: $1.trailingAnchor, constant: Constant.redIconWidth*2)])
+      
+    }
   }
   
   func makeUnderbar() -> UIView {
