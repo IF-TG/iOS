39444864672a3bf6faaa296c6e48857fb3bda83e
seokhyun <letushyun@gmail.com>
2023-10-28 15:20:31 +0900

    [Feat] 복잡한 코드 함수화

--- travelPlan/Source/Feature/Authentication/Login/ViewController/LoginViewController.swift
+++ travelPlan/Source/Feature/Authentication/Login/ViewController/LoginViewController.swift
@@ -38,47 +38,19 @@ class LoginViewController: UIViewController {
     $0.text = "설레는 여행의 내딛음"
   }
   
-  private let loginDescriptionLabel = UILabel().set {
+  private lazy var loginDescriptionLabel = UILabel().set {
     $0.text = "설레는 여행의 내딛음,\n여행을 가다"
     $0.font = .init(pretendard: .medium, size: 20)
     $0.textColor = .yg.littleWhite
     $0.numberOfLines = 0
     
-    var paragraphStyle = NSMutableParagraphStyle()
-    paragraphStyle.lineHeightMultiple = 1.2
-    
-    let attributedString = NSMutableAttributedString(
-      string: $0.text!,
-      attributes: [.paragraphStyle: paragraphStyle]
-    )
-    
-    let firstRange = ($0.text! as NSString).range(of: "설레는 여행")
-    let firstAttrs: [NSAttributedString.Key: Any] = [
-      .font: UIFont(pretendard: .bold, size: 24) ?? .systemFont(ofSize: 24),
-      .foregroundColor: UIColor.yg.littleWhite
-    ]
-    attributedString.addAttributes(firstAttrs, range: firstRange)
-    
-    let primaryAttrs: [NSAttributedString.Key: Any] = [
-      .font: UIFont(pretendard: .bold, size: 24) ?? .systemFont(ofSize: 24),
-      .foregroundColor: UIColor.yg.primary
-    ]
-    
-    if let secondRange = ($0.text! as NSString).range(of: "여행을").validation {
-      let newRange = NSRange(location: secondRange.location, length: 2)
-      attributedString.addAttributes(primaryAttrs, range: newRange)
-    }
-    
-    let thirdRange = ($0.text! as NSString).range(of: "가다")
-    attributedString.addAttributes(primaryAttrs, range: thirdRange)
-    
-    $0.attributedText = attributedString
+    setLoginDescriptionLabelAttributedText($0)
   }
   
   private let loginView = UIView().set {
     $0.backgroundColor = .clear
   }
-  
+
   // MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -164,6 +136,38 @@ extension LoginViewController: ViewBindCase {
 
 // MARK: - Private Helpers
 extension LoginViewController {
+  private func setLoginDescriptionLabelAttributedText(_ label: UILabel) {
+    var paragraphStyle = NSMutableParagraphStyle()
+    paragraphStyle.lineHeightMultiple = 1.2
+    
+    let attributedString = NSMutableAttributedString(
+      string: label.text!,
+      attributes: [.paragraphStyle: paragraphStyle]
+    )
+    
+    let firstRange = (label.text! as NSString).range(of: "설레는 여행")
+    let firstAttrs: [NSAttributedString.Key: Any] = [
+      .font: UIFont(pretendard: .bold, size: 24) ?? .systemFont(ofSize: 24),
+      .foregroundColor: UIColor.yg.littleWhite
+    ]
+    attributedString.addAttributes(firstAttrs, range: firstRange)
+    
+    let primaryAttrs: [NSAttributedString.Key: Any] = [
+      .font: UIFont(pretendard: .bold, size: 24) ?? .systemFont(ofSize: 24),
+      .foregroundColor: UIColor.yg.primary
+    ]
+    
+    if let secondRange = (label.text! as NSString).range(of: "여행을").validation {
+      let newRange = NSRange(location: secondRange.location, length: 2)
+      attributedString.addAttributes(primaryAttrs, range: newRange)
+    }
+    
+    let thirdRange = (label.text! as NSString).range(of: "가다")
+    attributedString.addAttributes(primaryAttrs, range: thirdRange)
+    
+    label.attributedText = attributedString
+  }
+  
   private func setupLoginUI() {
     loginStartView.completionHandler = { [weak self] in
       UIView.animate(withDuration: 1, animations: {
@@ -174,38 +178,44 @@ extension LoginViewController {
         self?.loginStartView.removeFromSuperview()
         
         guard let self else { return }
-        
-        self.loginView.addSubview(self.loginDescriptionLabel)
-        
-        self.airplaneLogo.snp.remakeConstraints {
-          $0.top.equalTo(self.view.safeAreaLayoutGuide).inset(20)
-          $0.leading.equalTo(self.loginDescriptionLabel.snp.trailing).offset(-20)
-          $0.width.equalTo(48)
-          $0.height.equalTo(30)
-        }
-        
-        self.loginDescriptionLabel.snp.makeConstraints {
-          $0.leading.equalToSuperview().inset(25)
-          $0.top.equalTo(self.airplaneLogo.snp.bottom).offset(8)
-        }
-        
-        self.yeogaLogo.snp.remakeConstraints {
-          $0.leading.equalToSuperview().inset(25)
-          $0.top.equalTo(self.loginDescriptionLabel.snp.bottom).offset(20)
-          $0.width.equalTo(125)
-          $0.height.equalTo(47.5)
-        }
-        UIView.animate(withDuration: 0.5, animations: {
-          self.firstDescriptionLabel.alpha = 0
-        }) { _ in
-          self.firstDescriptionLabel.snp.removeConstraints()
-          self.firstDescriptionLabel.removeFromSuperview()
-        }
+        self.setupTopComponents()
+        self.disappearFirstDescriptionLabel()
         
         UIView.animate(withDuration: 1) {
           self.loginView.layoutIfNeeded()
         }
       }
+    }
+  }
+  
+  private func setupTopComponents() {
+    self.loginView.addSubview(self.loginDescriptionLabel)
+    self.loginDescriptionLabel.snp.makeConstraints {
+      $0.leading.equalToSuperview().inset(25)
+      $0.top.equalTo(self.airplaneLogo.snp.bottom).offset(8)
+    }
+    
+    self.airplaneLogo.snp.remakeConstraints {
+      $0.top.equalTo(self.view.safeAreaLayoutGuide).inset(20)
+      $0.leading.equalTo(self.loginDescriptionLabel.snp.trailing).offset(-20)
+      $0.width.equalTo(48)
+      $0.height.equalTo(30)
+    }
+    self.yeogaLogo.snp.remakeConstraints {
+      $0.leading.equalToSuperview().inset(25)
+      $0.top.equalTo(self.loginDescriptionLabel.snp.bottom).offset(20)
+      $0.width.equalTo(125)
+      $0.height.equalTo(47.5)
+    }
+  }
+  
+  private func disappearFirstDescriptionLabel() {
+    UIView.animate(withDuration: 0.5,
+                   animations: {
+      self.firstDescriptionLabel.alpha = 0
+    }) { _ in
+      self.firstDescriptionLabel.snp.removeConstraints()
+      self.firstDescriptionLabel.removeFromSuperview()
     }
   }
   
