b78f3296c94de553adf7992ec558c22427f1be2f
seokhyun <letushyun@gmail.com>
2023-10-28 16:53:14 +0900

    [Rebuild] LoginView 클래스를 정의해서 VC로부터 분리

--- travelPlan/Source/Feature/Authentication/Login/ViewController/LoginViewController.swift
+++ travelPlan/Source/Feature/Authentication/Login/ViewController/LoginViewController.swift
@@ -20,43 +20,14 @@ class LoginViewController: UIViewController {
   private var player: AVPlayer!
   private var playerLayer: AVPlayerLayer!
   
-  private let loginStartView = LoginStartView()
+  private let loginView = LoginView()
   
-  private let yeogaLogo: UIImageView = .init().set {
-    $0.image = UIImage(named: "yeoga-logo")?.withRenderingMode(.alwaysTemplate)
-    $0.tintColor = .yg.littleWhite
-  }
-  private let airplaneLogo: UIImageView = .init().set {
-    $0.image = UIImage(named: "airplane-logo")?.withRenderingMode(.alwaysTemplate)
-    $0.contentMode = .scaleToFill
-    $0.tintColor = .yg.primary
-  }
   
-  private let firstDescriptionLabel = UILabel().set {
-    $0.font = .init(pretendard: .medium, size: 16)
-    $0.textColor = .yg.littleWhite
-    $0.text = "설레는 여행의 내딛음"
-  }
-  
-  private lazy var loginDescriptionLabel = UILabel().set {
-    $0.text = "설레는 여행의 내딛음,\n여행을 가다"
-    $0.font = .init(pretendard: .medium, size: 20)
-    $0.textColor = .yg.littleWhite
-    $0.numberOfLines = 0
-    
-    setLoginDescriptionLabelAttributedText($0)
-  }
-  
-  private let loginView = UIView().set {
-    $0.backgroundColor = .clear
-  }
-
   // MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
     setupPlayer()
     setupUI()
-    setupLoginUI()
     setupStyles()
     bind()
     viewLoad.send()
@@ -136,89 +107,6 @@ extension LoginViewController: ViewBindCase {
 
 // MARK: - Private Helpers
 extension LoginViewController {
-  private func setLoginDescriptionLabelAttributedText(_ label: UILabel) {
-    var paragraphStyle = NSMutableParagraphStyle()
-    paragraphStyle.lineHeightMultiple = 1.2
-    
-    let attributedString = NSMutableAttributedString(
-      string: label.text!,
-      attributes: [.paragraphStyle: paragraphStyle]
-    )
-    
-    let firstRange = (label.text! as NSString).range(of: "설레는 여행")
-    let firstAttrs: [NSAttributedString.Key: Any] = [
-      .font: UIFont(pretendard: .bold, size: 24) ?? .systemFont(ofSize: 24),
-      .foregroundColor: UIColor.yg.littleWhite
-    ]
-    attributedString.addAttributes(firstAttrs, range: firstRange)
-    
-    let primaryAttrs: [NSAttributedString.Key: Any] = [
-      .font: UIFont(pretendard: .bold, size: 24) ?? .systemFont(ofSize: 24),
-      .foregroundColor: UIColor.yg.primary
-    ]
-    
-    if let secondRange = (label.text! as NSString).range(of: "여행을").validation {
-      let newRange = NSRange(location: secondRange.location, length: 2)
-      attributedString.addAttributes(primaryAttrs, range: newRange)
-    }
-    
-    let thirdRange = (label.text! as NSString).range(of: "가다")
-    attributedString.addAttributes(primaryAttrs, range: thirdRange)
-    
-    label.attributedText = attributedString
-  }
-  
-  private func setupLoginUI() {
-    loginStartView.completionHandler = { [weak self] in
-      UIView.animate(withDuration: 1, animations: {
-        self?.loginView.backgroundColor = .black.withAlphaComponent(0.24)
-        self?.loginStartView.alpha = 0
-      }) { _ in
-        self?.loginStartView.snp.removeConstraints()
-        self?.loginStartView.removeFromSuperview()
-        
-        guard let self else { return }
-        self.setupTopComponents()
-        self.disappearFirstDescriptionLabel()
-        
-        UIView.animate(withDuration: 1) {
-          self.loginView.layoutIfNeeded()
-        }
-      }
-    }
-  }
-  
-  private func setupTopComponents() {
-    self.loginView.addSubview(self.loginDescriptionLabel)
-    self.loginDescriptionLabel.snp.makeConstraints {
-      $0.leading.equalToSuperview().inset(25)
-      $0.top.equalTo(self.airplaneLogo.snp.bottom).offset(8)
-    }
-    
-    self.airplaneLogo.snp.remakeConstraints {
-      $0.top.equalTo(self.view.safeAreaLayoutGuide).inset(20)
-      $0.leading.equalTo(self.loginDescriptionLabel.snp.trailing).offset(-20)
-      $0.width.equalTo(48)
-      $0.height.equalTo(30)
-    }
-    self.yeogaLogo.snp.remakeConstraints {
-      $0.leading.equalToSuperview().inset(25)
-      $0.top.equalTo(self.loginDescriptionLabel.snp.bottom).offset(20)
-      $0.width.equalTo(125)
-      $0.height.equalTo(47.5)
-    }
-  }
-  
-  private func disappearFirstDescriptionLabel() {
-    UIView.animate(withDuration: 0.5,
-                   animations: {
-      self.firstDescriptionLabel.alpha = 0
-    }) { _ in
-      self.firstDescriptionLabel.snp.removeConstraints()
-      self.firstDescriptionLabel.removeFromSuperview()
-    }
-  }
-  
   private func setupStyles() {
     navigationController?.navigationBar.isHidden = true
     view.backgroundColor = .white
@@ -255,42 +143,11 @@ extension LoginViewController {
 extension LoginViewController: LayoutSupport {
   func addSubviews() {
     view.addSubview(loginView)
-    loginView.addSubview(loginStartView)
-    loginView.addSubview(airplaneLogo)
-    loginView.addSubview(yeogaLogo)
-    loginView.addSubview(firstDescriptionLabel)
   }
   
   func setConstraints() {
     loginView.snp.makeConstraints {
       $0.edges.equalToSuperview()
-    }
-    
-    loginStartView.snp.makeConstraints {
-      $0.centerX.equalToSuperview()
-      $0.bottom.equalToSuperview().inset(60)
-      $0.width.equalTo(70)
-      $0.height.equalTo(LoginStartView.Constant.CircleView.size * 2.5 +
-                        LoginStartView.Constant.CircleView.Spacing.leadingTrailingBottom)
-    }
-    
-    airplaneLogo.snp.makeConstraints {
-      $0.top.equalTo(view.safeAreaLayoutGuide).offset(50)
-      $0.width.equalTo(48)
-      $0.height.equalTo(30)
-      $0.trailing.equalTo(yeogaLogo).offset(8)
-    }
-    
-    yeogaLogo.snp.makeConstraints {
-      $0.centerX.equalToSuperview()
-      $0.top.equalTo(airplaneLogo.snp.bottom).offset(10)
-      $0.width.equalTo(125)
-      $0.height.equalTo(47.5)
-    }
-    
-    firstDescriptionLabel.snp.makeConstraints {
-      $0.centerX.equalToSuperview()
-      $0.top.equalTo(yeogaLogo.snp.bottom).offset(15)
     }
   }
 }
