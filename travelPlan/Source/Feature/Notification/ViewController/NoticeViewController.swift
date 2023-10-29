6640c93dd87bf01cb2bf5912cec94e8eef044df7
shcommit <happysh_s2@naver.com>
2023-10-30 02:32:09 +0900

    [Feat] tableView 생성 및 NoticeViewAdapterDelegate 채택

--- travelPlan/Source/Feature/Notification/ViewController/NoticeViewController.swift
+++ travelPlan/Source/Feature/Notification/ViewController/NoticeViewController.swift
@@ -5,4 +5,50 @@
 //  Created by 양승현 on 10/29/23.
 //
 
-import Foundation
+import UIKit
+
+final class NoticeViewController: UIViewController {
+  // MARK: - Properties
+  private let tableView = UITableView().set {
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    $0.estimatedRowHeight = 91
+    $0.rowHeight = UITableView.automaticDimension
+    $0.register(NoticeCell.self, forCellReuseIdentifier: NoticeCell.id)
+    $0.separatorStyle = .none
+  }
+  
+  private var adapter: NoticeViewAdapter?
+  
+  private let viewModel: NoticeViewAdapterDataSource
+    
+  // MARK: - Lifecycle
+  init(viewModel: NoticeViewAdapterDataSource) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+    adapter = NoticeViewAdapter(dataSource: viewModel, tableView: tableView)
+    adapter?.delegate = self
+  }
+  
+  required init?(coder: NSCoder) {
+    nil
+  }
+  
+  override func loadView() {
+    view = tableView
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
+// MARK: - NoticeViewAdapterDelegate
+extension NoticeViewController: NoticeViewAdapterDelegate {
+  func noticeViewAdapter(
+    _ noticeViewAdapter: NoticeViewAdapter,
+    didSelectRowAt indexPath: IndexPath,
+    isExpended: Bool
+  ) {
+    // TODO: - input전송
+  }
+}
