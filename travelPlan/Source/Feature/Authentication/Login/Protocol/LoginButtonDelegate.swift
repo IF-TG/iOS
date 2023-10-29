c92f6cce04d7755ad060f4cd47eefb90f6479449
seokhyun <letushyun@gmail.com>
2023-10-30 06:16:01 +0900

    [Feat] 로그인 버튼 delegate 추가

---
+++ travelPlan/Source/Feature/Authentication/Login/Protocol/LoginButtonDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  LoginButtonDelegate.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 10/30/23.
+//
+
+import UIKit
+
+protocol LoginButtonDelegate: AnyObject {
+  func loginButton(_ button: UIButton)
+}
