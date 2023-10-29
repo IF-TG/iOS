5e9377d4ab7981e89a08691f7629333b2d1a6ac0
shcommit <happysh_s2@naver.com>
2023-10-30 00:01:07 +0900

    [Add] NoticeViewAdapter 클래스 및 관련 dataSource, delegate protocol 생성

---
+++ travelPlan/Source/Feature/Notification/Protocol/NoticeViewAdapterDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  NoticeViewAdapterDelegate.swift
+//  travelPlan
+//
+//  Created by 양승현 on 10/30/23.
+//
+
+import Foundation
---
+++ travelPlan/Source/Feature/Notification/ViewModel/NoticeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NoticeViewModel.swift
+//  travelPlan
+//
+//  Created by 양승현 on 10/29/23.
+//
+
+import Foundation
