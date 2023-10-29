7b10ad2259fc89a9232e19c39cba0f357fca2163
shcommit <happysh_s2@naver.com>
2023-10-30 01:50:11 +0900

    [Feat] NotificationRepository 준수

--- travelPlan/Source/Data/Repositories/DefaultNotificationRepository.swift
+++ travelPlan/Source/Data/Repositories/DefaultNotificationRepository.swift
@@ -5,4 +5,37 @@
 //  Created by 양승현 on 10/30/23.
 //
 
-import Foundation
+import Combine
+
+final class DefaultNotificationRepository {
+  // MARK: - Properties
+  private let service : Sessionable
+  
+  private var subscription: AnyCancellable?
+  
+  // MARK: - Lifecycle
+  init(service: Sessionable) {
+    self.service = service
+  }
+}
+
+// MARK: - NotificationRepository
+extension DefaultNotificationRepository: NotificationRepository {
+  func fetchNotices() -> Future<[NoticeEntity], Never> {
+    let noticeEndpoint = NotificationAPIEndpoints.default.fetchNotices()
+    return .init { [weak self] promise in
+      self?.subscription = self?.service
+        .request(endpoint: noticeEndpoint)
+        .sink { completion in
+          switch completion {
+          case .finished:
+            return
+          case .failure(let error):
+            print("DEBUG: \(error.localizedDescription)")
+          }
+        } receiveValue: { responseDTO in
+          promise(.success(responseDTO.map { $0.toDomain }))
+        }
+    }
+  }
+}
