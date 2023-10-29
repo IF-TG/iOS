a807174394433bd334be1de5177395a79114edf6
shcommit <happysh_s2@naver.com>
2023-10-30 00:50:26 +0900

    [Feat] noticeUseCase 추가 및 NoticeViewAdapterDataSource 준수

--- travelPlan/Source/Feature/Notification/ViewModel/NoticeViewModel.swift
+++ travelPlan/Source/Feature/Notification/ViewModel/NoticeViewModel.swift
@@ -5,4 +5,45 @@
 //  Created by 양승현 on 10/29/23.
 //
 
+import Combine
 import Foundation
+
+final class NoticeViewModel {
+  // MARK: - Dependencies
+  private let noticeUseCase: NoticeUseCase
+  
+  // MARK: - Properties
+  private var notices: [NoticeCellInfo] = []
+  
+  private var subscriptions = Set<AnyCancellable>()
+  
+  // MARK: - Lifecycle
+  init(noticeUseCase: NoticeUseCase) {
+    self.noticeUseCase = noticeUseCase
+    bind()
+  }
+}
+
+// MARK: - Private Helpers
+private extension NoticeViewModel {
+  func bind() {
+    noticeUseCase.noticeEntities
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] noticeEntities in
+        self?.notices = noticeEntities.map {
+          .init(title: $0.title, date: $0.date, details: $0.details, isExpended: false)
+        }
+      }.store(in: &subscriptions)
+  }
+}
+
+// MARK: - NoticeViewAdapterDataSource
+extension NoticeViewModel: NoticeViewAdapterDataSource {
+  var numberOfItems: Int {
+    notices.count
+  }
+  
+  func getItem(_ indexPath: IndexPath) -> NoticeCellInfo {
+    return notices[indexPath.row]
+  }
+}
