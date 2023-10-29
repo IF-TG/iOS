66b1d7516e41bd35bda054f0592ac0021848bc2c
shcommit <happysh_s2@naver.com>
2023-10-29 22:57:49 +0900

    [Feat] Subview들 layout 지정 및 클릭시 확장기능 추가

--- travelPlan/Source/Feature/Notification/View/NoticeCell.swift
+++ travelPlan/Source/Feature/Notification/View/NoticeCell.swift
@@ -7,6 +7,13 @@
 
 import UIKit
 
+struct NoticeCellInfo {
+  let title: String
+  let date: String
+  let details: String
+  var isExpended: Bool
+}
+
 final class NoticeCell: UITableViewCell {
   static let id = String(describing: NoticeCell.self)
   
@@ -31,14 +38,16 @@ final class NoticeCell: UITableViewCell {
       static let fontColor = UIColor.yg.gray3
       enum Spacing {
         static let leading: CGFloat = 12
-        static let bottom: CGFloat = 15
       }
     }
     enum CellDivider {
       static let color = UIColor.yg.gray0
-      static let height = 1
+      static let height: CGFloat = 1
+      enum Spacing {
+        static let top: CGFloat = 14
+      }
     }
-    enum ExpendableLabel {
+    enum DetailsLabel {
       static let inset = UIEdgeInsets(top: 20, left: 11, bottom: 20, right: 11)
       static let font = UIFont(pretendard: .regular, size: 15)
       static let attributes: [NSAttributedString.Key: Any] = {
@@ -77,16 +86,147 @@ final class NoticeCell: UITableViewCell {
     $0.backgroundColor = Const.color
   }
   
-  private let expendableLabel = BasePaddingLabel(padding: Constant.ExpendableLabel.inset).set {
-    typealias Const = Constant.ExpendableLabel
+  private let detailsLabel = BasePaddingLabel(padding: Constant.DetailsLabel.inset).set {
+    typealias Const = Constant.DetailsLabel
     $0.translatesAutoresizingMaskIntoConstraints = false
     $0.font = Const.font
     $0.numberOfLines = 0
   }
   
+  private var notExpendedConstraints: [NSLayoutConstraint] = []
+  
+  private var expendedConstriants: [NSLayoutConstraint] = []
+  
   var isExpended = false {
     didSet {
-      
+      updateVisibleLayout()
     }
+  }
+  
+  // MARK: - Lifecycle
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    configureUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    nil
+  }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    configure(with: nil)
+  }
+}
+
+// MARK: - Helpers
+extension NoticeCell {
+  func configure(with info: NoticeCellInfo?) {
+    titleLabel.text = info?.title
+    dateLabel.text = info?.date
+    detailsLabel.text = info?.details
+    isExpended = info?.isExpended ?? false
+    detailsLabel.alpha = CGFloat(isExpended.toInt)
+  }
+}
+
+// MARK: - Private Helpers
+private extension NoticeCell {
+  func configureUI() {
+    setupUI()
+    selectionStyle = .none
+  }
+  
+  func updateVisibleLayout() {
+    if isExpended {
+      NSLayoutConstraint.deactivate(notExpendedConstraints)
+      NSLayoutConstraint.activate(expendedConstriants)
+    } else {
+      NSLayoutConstraint.deactivate(expendedConstriants)
+      NSLayoutConstraint.activate(notExpendedConstraints)
+    }
+    detailsLabel.alpha = CGFloat(isExpended.toInt)
+  }
+}
+
+// MARK: - LayoutSupport
+extension NoticeCell: LayoutSupport {
+  func addSubviews() {
+    _=[
+      titleLabel,
+      chevronIcon,
+      dateLabel,
+      cellDivider,
+      detailsLabel
+    ].map {
+      contentView.addSubview($0)
+    }
+  }
+  
+  func setConstraints() {
+    let sharedConstraints: [NSLayoutConstraint] = [
+      titleLabelConstraints,
+      dateLabelConstraints,
+      chevronIconConstraints,
+      cellDividerConstraints,
+      detailsLabelConstraints
+    ].flatMap { $0 }
+    
+    let cellDividerBottomConstraint = cellDivider.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+    cellDividerBottomConstraint.priority = .defaultHigh
+    
+    let detailsLabelBottomConstraint = detailsLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+    detailsLabelBottomConstraint.priority = .defaultHigh
+    
+    notExpendedConstraints = [cellDividerBottomConstraint]
+    expendedConstriants = [detailsLabelBottomConstraint]
+    
+    NSLayoutConstraint.activate([sharedConstraints + notExpendedConstraints].flatMap { $0 })
+  }
+}
+
+// MARK: - LayoutSupport Constraints
+private extension NoticeCell {
+  var titleLabelConstraints: [NSLayoutConstraint] {
+    typealias Spacing = Constant.TitleLabel.Spacing
+    return [
+      titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Spacing.leading),
+      titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Spacing.top),
+      titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -Spacing.trailing)]
+  }
+  
+  var dateLabelConstraints: [NSLayoutConstraint] {
+    typealias Spacing = Constant.DateLabel.Spacing
+    return [
+      dateLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Spacing.leading),
+      dateLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
+      dateLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor)]
+  }
+  
+  var chevronIconConstraints: [NSLayoutConstraint] {
+    typealias Const = Constant.ChevronIcon
+    typealias Spacing = Const.Spacing
+    return [
+      chevronIcon.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -Spacing.trailing),
+      chevronIcon.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+      chevronIcon.widthAnchor.constraint(equalToConstant: Const.size.width),
+      chevronIcon.heightAnchor.constraint(equalToConstant: Const.size.height)]
+  }
+  
+  var cellDividerConstraints: [NSLayoutConstraint] {
+    typealias Const = Constant.CellDivider
+    typealias Spacing = Const.Spacing
+    return [
+      cellDivider.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+      cellDivider.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: Spacing.top),
+      cellDivider.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+      cellDivider.heightAnchor.constraint(equalToConstant: Const.height)]
+  }
+  
+  var detailsLabelConstraints: [NSLayoutConstraint] {
+    return [
+      detailsLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+      detailsLabel.topAnchor.constraint(equalTo: cellDivider.bottomAnchor),
+      detailsLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)]
   }
 }
