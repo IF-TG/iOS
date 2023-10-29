9cf87d3b147d4ee627ad1b1719a5922fc55c810d
shcommit <happysh_s2@naver.com>
2023-10-30 01:51:26 +0900

    [Feat] NoticeUseCase 준수

--- travelPlan/Source/Domain/UseCases/DefaultNoticeUseCase.swift
+++ travelPlan/Source/Domain/UseCases/DefaultNoticeUseCase.swift
@@ -9,9 +9,24 @@ import Combine
 import Foundation
 
 final class DefaultNoticeUseCase: NoticeUseCase {
+  // MARK: - Dependencies
+  private let notificationRepository: NotificationRepository
+  
+  // MARK: - Properties
   var noticeEntities: CurrentValueSubject<[NoticeEntity], Never> = .init([])
   
+  private var subscriptions = Set<AnyCancellable>()
+  
+  // MARK: - Lifecycle
+  init(notificationRepository: NotificationRepository) {
+    self.notificationRepository = notificationRepository
+  }
+  
   func fetchNotices() {
-    
+    notificationRepository
+      .fetchNotices()
+      .sink { [weak self] noticeEntities in
+        self?.noticeEntities.send(noticeEntities)
+      }.store(in: &subscriptions)
   }
 }
