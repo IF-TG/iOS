8658b8590b285738e48459cc907feb36997a3bf1
shcommit <happysh_s2@naver.com>
2023-11-02 00:06:31 +0900

    [Feat] notification info, type 추가

---
+++ travelPlan/Source/Feature/Notification/View/NotificationIconType.swift
@@ -0,0 +1,47 @@
+//
+//  NotificationIconType.swift
+//  travelPlan
+//
+//  Created by 양승현 on 11/1/23.
+//
+
+import Foundation
+
+enum NotificationIconType {
+  case comment(postTitle: String)
+  case commentReply(postTitle: String)
+  case heart(postTitle: String)
+  
+  var path: String {
+    switch self {
+    case .comment:
+      return "message-dots-circle"
+    case .commentReply:
+      return "corner-down-right"
+    case .heart:
+      return "unselectedHeart"
+    }
+  }
+  
+  var postTitle: String {
+    switch self {
+    case .comment(postTitle: let title):
+      return title
+    case .commentReply(postTitle: let title):
+      return title
+    case .heart(postTitle: let title):
+      return title
+    }
+  }
+  
+  var suffixWords: String {
+    switch self {
+    case .comment(postTitle: let title):
+      return "님이 " + title + " 글에 댓글을 남겼어요."
+    case .commentReply(postTitle: let title):
+      return "님이 " + title + " 글에 답글을 남겼어요."
+    case .heart(postTitle: let title):
+      return "님이 " + title + " 글에 좋아요를 남겼어요."
+    }
+  }
+}
---
+++ travelPlan/Source/Feature/Notification/View/NotificationInfo.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationInfo.swift
+//  travelPlan
+//
+//  Created by 양승현 on 11/1/23.
+//
+
+import Foundation
+
+struct NotificationInfo {
+  let userName: String
+  let details: String
+  let duration: String
+  let imagePath: NotificationIconType
+}
