c54be06210f03dde101d7bb3a01610aa98a064e3
shcommit <happysh_s2@naver.com>
2023-11-03 03:31:18 +0900

    [Feat] 메뉴는 스택뷰 + 스크롤뷰로 구성해봤습니다.

--- travelPlan/Source/Feature/Profile/ViewController/ProfileViewController.swift
+++ travelPlan/Source/Feature/Profile/ViewController/ProfileViewController.swift
@@ -12,6 +12,18 @@ class ProfileViewController: UIViewController {
     enum TopSheetView {
       static let height: CGFloat = 253
     }
+    static let SettingLabelInset: UIEdgeInsets = .init(top: 8, left: 20, bottom: 8, right: 20)
+    static let versionLabelTrailing: CGFloat = 30
+    static let settingStackViewRadius: CGFloat = 10
+    static let stackViewSpacing: CGFloat = 8
+    enum ScrollView {
+      enum Spacing {
+        static let leading: CGFloat = 20
+        static let trailing = leading
+        static let top: CGFloat = 168
+        static let bottom: CGFloat = 5
+      }
+    }
   }
   
   // MARK: - Properties
@@ -19,9 +31,20 @@ class ProfileViewController: UIViewController {
   
   private let topSheetView = ProfileTopSheetView()
   
-  private let settingStackViews: [UIStackView] = []
+  private let scrollView = UIScrollView().set {
+    $0.translatesAutoresizingMaskIntoConstraints = false
+  }
   
-  private let scrollView = UIScrollView()
+  private lazy var settingStackViews: [UIStackView] = (0..<4).map { _ in
+    return UIStackView(frame: .zero).set {
+      $0.translatesAutoresizingMaskIntoConstraints = false
+      $0.backgroundColor = .white
+      $0.layer.cornerRadius = Constant.settingStackViewRadius
+      $0.axis = .vertical
+      $0.alignment = .fill
+      $0.distribution = .fill
+    }
+  }
   
   private var isAnimated = false
    
@@ -56,7 +79,12 @@ class ProfileViewController: UIViewController {
 // MARK: - Private Helpers
 private extension ProfileViewController {
   func configureUI() {
-    view.backgroundColor = .white
+    view.backgroundColor = .yg.gray00Background
+    let settingLabels = makeSettingLabels()
+    setStackView(index: 0, from: (0...1).map { settingLabels[$0] })
+    setStackView(index: 1, from: (2...5).map { settingLabels[$0] })
+    setStackView(index: 2, from: (6...8).map { settingLabels[$0] })
+    setStackView(index: 3, from: (9...9).map { settingLabels[$0] })
     setupUI()
   }
   
@@ -75,24 +103,103 @@ private extension ProfileViewController {
         self.topSheetView.showAnimation()
       })
   }
+  
+  func setStackView(index: Int, from subviews: [UIView]) {
+    _=subviews.map {
+      settingStackViews[index].addArrangedSubview($0)
+    }
+  }
+  
+  func makeSettingLabels() -> [UILabel] {
+    let dataSource = SettingType.allCases
+    let inset = Constant.SettingLabelInset
+    return dataSource.map { settingType in
+      return BasePaddingLabel(
+        padding: inset,
+        fontType: settingType.fontType,
+        lineHeight: settingType.lineHeight
+      ).set {
+        $0.text = settingType.rawValue
+        $0.textColor = settingType.fontColor
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapSettingLabel))
+        $0.addGestureRecognizer(tap)
+        if settingType == .versionInformation {
+          let versionLabel = makeVersionLabel(from: settingType)
+          appendVersionLabel(from: $0, subview: versionLabel)
+        }
+      }
+    }
+  }
+  
+  func makeVersionLabel(from settingType: SettingType) -> UILabel {
+    return BaseLabel(fontType: settingType.fontType, lineHeight: settingType.lineHeight).set {
+      $0.text = "1.0.0"
+      $0.textColor = settingType.fontColor
+      $0.isUserInteractionEnabled  = false
+    }
+  }
+  
+  func appendVersionLabel(from superView: UIView, subview: UIView) {
+    superView.addSubview(subview)
+    NSLayoutConstraint.activate([
+      subview.centerYAnchor.constraint(equalTo: superView.centerYAnchor),
+      subview.trailingAnchor.constraint(
+        equalTo: superView.trailingAnchor,
+        constant: Constant.versionLabelTrailing)])
+  }
+}
+
+// MARK: - Actions
+private extension ProfileViewController {
+  @objc func didTapSettingLabel(_ sender: UILabel) {
+    print(sender.text ?? "")
+    // TODO: - 터치할때 그래이세그로
+  }
 }
 
 // MARK: - LayoutSupport
 extension ProfileViewController: LayoutSupport {
   func addSubviews() {
     _=[
-      topSheetView
+      topSheetView,
+      scrollView
     ].map {
       view.addSubview($0)
+    }
+    _=settingStackViews.map {
+      scrollView.addSubview($0)
     }
   }
   
   func setConstraints() {
     _=[
-      topSheetViewConstraints
+      topSheetViewConstraints,
+      scrollViewConstraints
     ].map {
       NSLayoutConstraint.activate($0)
     }
+    
+    let topSpacing = Constant.stackViewSpacing
+    let sc = scrollView.contentLayoutGuide
+    NSLayoutConstraint.activate([
+      settingStackViews[0].topAnchor.constraint(equalTo: sc.topAnchor),
+      settingStackViews[1].topAnchor.constraint(
+        equalTo: settingStackViews[0].bottomAnchor,
+        constant: topSpacing),
+      settingStackViews[2].topAnchor.constraint(
+        equalTo: settingStackViews[1].bottomAnchor,
+        constant: topSpacing),
+      settingStackViews[3].topAnchor.constraint(
+        equalTo: settingStackViews[2].bottomAnchor, 
+        constant: topSpacing),
+      settingStackViews[3].bottomAnchor.constraint(equalTo: sc.bottomAnchor)])
+    
+    NSLayoutConstraint.activate(
+      settingStackViews.map {[
+        $0.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+        $0.leadingAnchor.constraint(equalTo: sc.leadingAnchor),
+        $0.trailingAnchor.constraint(equalTo: sc.trailingAnchor)
+      ]}.flatMap { $0 })
   }
 }
 
@@ -105,5 +212,22 @@ private extension ProfileViewController {
       topSheetView.topAnchor.constraint(equalTo: view.topAnchor),
       topSheetView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
       topSheetView.heightAnchor.constraint(equalToConstant: Const.height)]
+  }
+  
+  var scrollViewConstraints: [NSLayoutConstraint] {
+    typealias Spacing = Constant.ScrollView.Spacing
+    return [
+      scrollView.leadingAnchor.constraint(
+        equalTo: view.leadingAnchor,
+        constant: Spacing.leading),
+      scrollView.topAnchor.constraint(
+        equalTo: view.safeAreaLayoutGuide.topAnchor,
+        constant: Spacing.top),
+      scrollView.trailingAnchor.constraint(
+        equalTo: view.trailingAnchor,
+        constant: -Spacing.trailing),
+      scrollView.bottomAnchor.constraint(
+        equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+        constant: -Spacing.bottom)]
   }
 }
