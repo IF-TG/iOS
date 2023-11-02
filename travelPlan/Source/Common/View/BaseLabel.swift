c94ae0f9b617dc44e09aea9ade598c0ad5dd39db
shcommit <happysh_s2@naver.com>
2023-11-02 18:11:34 +0900

    [Feat] BaseLabel 구현

--- travelPlan/Source/Common/View/BaseLabel.swift
+++ travelPlan/Source/Common/View/BaseLabel.swift
@@ -5,4 +5,63 @@
 //  Created by 양승현 on 11/2/23.
 //
 
-import Foundation
+import UIKit
+
+final class BaseLabel: UILabel {
+  // MARK: - Properties
+  private var fontType: UIFont.Pretendard
+  
+  private var lineHeight: CGFloat?
+  
+  override var text: String? {
+    didSet {
+      setPretendardFont()
+    }
+  }
+  
+  // MARK: - Lifecycle
+  init(frame: CGRect, fontType: UIFont.Pretendard, lineHeight: CGFloat? = nil) {
+    self.lineHeight = lineHeight
+    self.fontType = fontType
+    super.init(frame: frame)
+  }
+  
+  convenience init(fontType: UIFont.Pretendard, lineHeight: CGFloat? = nil) {
+    self.init(frame: .zero, fontType: fontType, lineHeight: lineHeight)
+    translatesAutoresizingMaskIntoConstraints = false
+  }
+  
+  required init?(coder: NSCoder) {
+    nil
+  }
+}
+
+// MARK: - Helpers
+extension BaseLabel {
+  /// 강조를하기전에 기본적으로 text에 텍스트가 있어야 합니다.
+  func setHighlight(with info: HighlightFontInfo) {
+    guard let text else { return }
+    attributedText = NSMutableAttributedString(string: text).set {
+      $0.setHighlightedPretendard(with: info)
+    }
+  }
+  
+  func setHighlights(with list: HighlightFontInfo...) {
+    guard let text else { return }
+    attributedText = NSMutableAttributedString(string: text).set { mutableString in
+      list.forEach {
+        mutableString.setHighlightedPretendard(with: $0)
+      }
+    }
+  }
+}
+
+// MARK: - Private Helpers
+private extension BaseLabel {
+  func setPretendardFont() {
+    guard let text else { return }
+    attributedText = NSMutableAttributedString(string: text).set {
+      $0.setPretendard(with: fontType, lineHeight: lineHeight)
+    }
+  }
+}
