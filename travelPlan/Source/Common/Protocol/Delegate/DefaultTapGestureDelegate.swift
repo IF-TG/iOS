14b0c03ec9489c43376f4c03991c4fb4da5709ca
seokhyun <letushyun@gmail.com>
2023-11-05 18:14:02 +0900

    [Style] TapGestureDelegate를 Common 디렉토리로 변경

---
+++ travelPlan/Source/Common/Protocol/Delegate/DefaultTapGestureDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  DefaultTapGestureDelegate.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 11/5/23.
+//
+
+import UIKit.UIView
+
+protocol DefaultTapGestureDelegate: AnyObject {
+  func tapGestureRecognizer(in view: UIView)
+}
