19e0a94bc5966e5fa76d332ac73bf8597b250063
seokhyun <letushyun@gmail.com>
2023-11-05 18:46:41 +0900

    [Feat] 리뷰 후기화면의 하단 뷰인 ReviewWritingBottomView 추가

--- travelPlan/Source/Feature/Feed/View/ReviewWriting/ReviewWritingBottomView.swift
+++ travelPlan/Source/Feature/Feed/View/ReviewWriting/ReviewWritingBottomView.swift
@@ -6,6 +6,94 @@
 //
 
 import UIKit
+import SnapKit
 
 final class ReviewWritingBottomView: UIView {
+  // MARK: - Properties
+  private lazy var planView: ReviewWritingPlanView = .init().set {
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapPlanView))
+    $0.addGestureRecognizer(tapGesture)
+  }
+  
+  private let cameraButton: UIButton = .init(type: .system).set {
+    $0.setImage(.init(named: "camera")?.withRenderingMode(.alwaysOriginal), for: .normal)
+    $0.backgroundColor = .yg.primary.withAlphaComponent(0.1)
+    $0.layer.cornerRadius = 16
+    if #available(iOS 15.0, *) {
+      $0.configuration?.imagePadding = 8
+    } else {
+      $0.imageEdgeInsets = .init(top: 8, left: 8, bottom: 8, right: 8)
+    }
+  }
+  
+  private let alertCircleImageView: UIImageView = .init().set {
+    $0.image = .init(named: "alert-circle")?.withRenderingMode(.alwaysOriginal)
+  }
+  
+  private let cameraWarningLabel: UILabel = .init().set {
+    $0.text = "사진을 1개 이상 등록해주세요."
+    $0.textColor = .yg.gray1
+    $0.font = .init(pretendard: .regular_400(fontSize: 14))
+  }
+  
+  weak var delegate: DefaultTapGestureDelegate?
+  // MARK: - LifeCycle
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupUI()
+    setupStyles()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: - Private Helpers
+extension ReviewWritingBottomView {
+  private func setupStyles() {
+    backgroundColor = .white
+  }
+}
+
+// MARK: - LayoutSupport
+extension ReviewWritingBottomView: LayoutSupport {
+  func addSubviews() {
+    addSubview(planView)
+    addSubview(cameraButton)
+    addSubview(alertCircleImageView)
+    addSubview(cameraWarningLabel)
+  }
+  
+  func setConstraints() {
+    planView.snp.makeConstraints {
+      $0.top.equalToSuperview().inset(7)
+      $0.trailing.equalToSuperview().inset(24)
+      $0.height.equalTo(planView.intrinsicContentSize.height)
+    }
+    
+    cameraButton.snp.makeConstraints {
+      $0.top.equalTo(planView.snp.bottom).offset(9)
+      $0.leading.equalToSuperview().inset(20)
+      $0.size.equalTo(44)
+    }
+    
+    alertCircleImageView.snp.makeConstraints {
+      $0.leading.equalTo(cameraButton.snp.trailing).offset(8)
+      $0.centerY.equalTo(cameraButton)
+      $0.size.equalTo(20)
+    }
+    
+    cameraWarningLabel.snp.makeConstraints {
+      $0.leading.equalTo(alertCircleImageView.snp.trailing).offset(4)
+      $0.centerY.equalTo(alertCircleImageView)
+    }
+  }
+}
+
+// MARK: - Actions
+private extension ReviewWritingBottomView {
+  @objc func didTapPlanView() {
+    delegate?.tapGestureRecognizer(in: planView)
+  }
 }
