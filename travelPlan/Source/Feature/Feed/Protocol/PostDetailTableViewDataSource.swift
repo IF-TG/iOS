8319a1cc2c4546a8926af459b22f0ddebc8e3e1c
shcommit <happysh_s2@naver.com>
2023-11-07 19:09:58 +0900

    [Feat] PostDetailTableViewDataSource 포스트 이미지, 내용에 대한 item 추가

---
+++ travelPlan/Source/Feature/Feed/Protocol/PostDetailTableViewDataSource.swift
@@ -0,0 +1,15 @@
+//
+//  PostDetailTableViewDataSource.swift
+//  travelPlan
+//
+//  Created by 양승현 on 11/7/23.
+//
+
+import Foundation
+
+protocol PostDetailTableViewDataSource: AnyObject {
+  var numberOfSections: Int { get }
+  
+  func numberOfItems(in section: Int) -> Int
+  func postContentItem(at indexPath: IndexPath) -> PostDetailContentInfo
+}
