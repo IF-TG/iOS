3693676b560cd02b3fce70921d502dd79a790a1e
shcommit <happysh_s2@naver.com>
2023-11-07 19:24:11 +0900

    [Feat] PostDetailTableViewDelegate 추가

---
+++ travelPlan/Source/Feature/Feed/Protocol/PostDetailTableViewDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  PostDetailTableViewDelegate.swift
+//  travelPlan
+//
+//  Created by 양승현 on 11/7/23.
+//
+
+import UIKit
+
+protocol PostDetailTableViewDelegate: AnyObject {
+  func scrollViewDidScroll(_ scrollView: UIScrollView)
+}
