a37d9fb0d6f116e3d2a2e1acfaff9a102daff9f9
shcommit <happysh_s2@naver.com>
2023-11-09 01:47:53 +0900

    [Add] BaseCommentView 파일 생성 및 PostDetailCommentHeader생성

---
+++ travelPlan/Source/Feature/Feed/View/BaseCommentView.swift
@@ -0,0 +1,8 @@
+//
+//  BaseCommentView.swift
+//  travelPlan
+//
+//  Created by 양승현 on 11/9/23.
+//
+
+import Foundation
---
+++ travelPlan/Source/Feature/Feed/View/PostDetailCommentHeader.swift
@@ -0,0 +1,8 @@
+//
+//  PostDetailCommentHeader.swift
+//  travelPlan
+//
+//  Created by 양승현 on 11/9/23.
+//
+
+import Foundation
