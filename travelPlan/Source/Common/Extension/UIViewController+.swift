abc02ae6beef878b8ce48c30f0d376271c75877e
shcommit <happysh_s2@naver.com>
2023-11-11 02:06:51 +0900

    [Common] 탭바 보여지고, 사라지는 함수 추가.with animation

--- travelPlan/Source/Common/Extension/UIViewController+.swift
+++ travelPlan/Source/Common/Extension/UIViewController+.swift
@@ -7,9 +7,26 @@
 
 import UIKit
 
+// MARK: - Presentation
 extension UIViewController {
   func presentBottomSheet(_ viewController: UIViewController, _ completion: (() -> Void)? = nil) {
     viewController.modalPresentationStyle = .overFullScreen
     present(viewController, animated: false, completion: completion)
+  }
+}
+
+// MARK: - Animation
+extension UIViewController {
+  func setTabBarVisible(_ hidden: Bool, animated: Bool = true, duration: TimeInterval = 0.27) {
+    if animated, var frame = tabBarController?.tabBar.frame {
+      frame.origin.y = CGFloat(hidden.toInt) * frame.size.height
+      UIView.animate(withDuration: duration, animations: {
+        self.tabBarController?.tabBar.frame = frame
+      }, completion: { _ in
+        self.tabBarController?.tabBar.isHidden = hidden
+      })
+      return
+    }
+    self.tabBarController?.tabBar.isHidden = hidden
   }
 }
