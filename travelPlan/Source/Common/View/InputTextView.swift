1b43e458946662abc37a55993c9bba3a47be735e
shcommit <happysh_s2@naver.com>
2023-11-11 20:39:03 +0900

    [Common] Input text view 구현. placeholder 기능 추가.

--- travelPlan/Source/Common/View/InputTextView.swift
+++ travelPlan/Source/Common/View/InputTextView.swift
@@ -5,4 +5,119 @@
 //  Created by 양승현 on 11/11/23.
 //
 
-import Foundation
+import UIKit
+
+class InputTextView: UITextView {
+  // MARK: - Properties
+  public var placeholder = BaseLabel(fontType: .medium_500(fontSize: 15)).set {
+    $0.textColor = .yg.gray1
+    $0.text = "텍스트를 입력해주세요."
+  }
+  
+  public var isPlaceholderHiddenWhenTextBeginEditing: Bool = false
+  
+  private let placeholderInfo: PlaceholderInfo
+  
+  private var placeholderText: String? {
+    didSet {
+      placeholder.text = placeholderText
+    }
+  }
+  
+  var length: Int {
+    text.count
+  }
+  
+  // MARK: - Lifecycle
+  init(
+    frame: CGRect,
+    textContainer: NSTextContainer?,
+    placeholderInfo: PlaceholderInfo
+  ) {
+    self.placeholderInfo = placeholderInfo
+    super.init(frame: frame, textContainer: textContainer)
+  }
+  
+  convenience init(textContainer: NSTextContainer?, placeholderInfo: PlaceholderInfo = .init()) {
+    self.init(frame: .zero, textContainer: textContainer, placeholderInfo: placeholderInfo)
+    translatesAutoresizingMaskIntoConstraints = false
+  }
+  
+  convenience init() {
+    self.init(frame: .zero, textContainer: nil, placeholderInfo: .init())
+    translatesAutoresizingMaskIntoConstraints = false
+  }
+  
+  required init?(coder: NSCoder) {
+    nil
+  }
+  // MARK: - Helpers
+  
+  // MARK: - Private Helpers
+  func baseConfigureUI() {
+    setupUI()
+  }
+}
+
+// MARK: - LayoutSupport
+extension InputTextView: LayoutSupport {
+  func addSubviews() {
+    addSubview(placeholder)
+  }
+  
+  func setConstraints() {
+    NSLayoutConstraint.activate(placeholderConsriants)
+  }
+  
+  private var placeholderConsriants: [NSLayoutConstraint] {
+    let inset = placeholderInfo.inset
+    let position = placeholderInfo.position
+    var constraints = [
+      placeholder.leadingAnchor.constraint(equalTo: leadingAnchor, constant: inset.left),
+      placeholder.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor, constant: -inset.right)]
+    switch position {
+    case .top:
+      return constraints + [placeholder.topAnchor.constraint(equalTo: topAnchor, constant: inset.top)]
+    case .centerY:
+      return constraints + [placeholder.centerYAnchor.constraint(equalTo: centerYAnchor)]
+    }
+  }
+}
+
+// MARK: - UITextViewDelegate
+extension InputTextView: UITextViewDelegate {
+  func textViewDidChange(_ textView: UITextView) {
+    guard let text = textView.text else { return }
+    placeholder.isHidden = text.isEmpty ? false : true
+  }
+  
+  func textViewDidBeginEditing(_ textView: UITextView) {
+    if isPlaceholderHiddenWhenTextBeginEditing {
+      placeholder.isHidden = true
+    }
+  }
+  
+  func textViewDidEndEditing(_ textView: UITextView) {
+    if textView.text == nil || length < 1 {
+      placeholder.isHidden = falsetravelPlan/Source/Common/View/InputTextView.swift
+    }
+  }
+}
+
+// MARK: - Utils
+extension InputTextView {
+  struct PlaceholderInfo {
+    let position: Position
+    let inset: UIEdgeInsets
+    
+    init(position: Position = .centerY, inset: UIEdgeInsets = .zero) {
+      self.position = position
+      self.inset = inset
+    }
+    
+    enum Position {
+      case top
+      case centerY
+    }
+  }
+}
