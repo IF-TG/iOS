c1665f81d20cf173b2a3ef669db82a3db627d2c9
shcommit <happysh_s2@naver.com>
2023-11-11 22:26:29 +0900

    [Feat] PostCommetInputAccessory 구현

--- travelPlan/Source/Feature/Feed/View/PostCommentInputAccessory.swift
+++ travelPlan/Source/Feature/Feed/View/PostCommentInputAccessory.swift
@@ -1,5 +1,5 @@
 //
-//  CommentInputAccessory.swift
+//  PostCommentInputAccessory.swift
 //  travelPlan
 //
 //  Created by 양승현 on 11/11/23.
@@ -7,6 +7,36 @@
 
 import UIKit
 
-final class CommentInputAccessory: BaseProfileAreaView {
-
+final class PostCommentInputAccessory: BaseProfileAreaView {
+  // MARK: - Properties
+  private let commentInputView = CommentInputView()
+  
+  weak var commentInputViewDelegate: CommentInputViewDelegate? {
+    get {
+      commentInputView.commentDelegate
+    } set {
+      commentInputView.commentDelegate = newValue
+    }
+  }
+  
+  weak var textViewDelegate: UITextViewDelegate? {
+    get {
+      commentInputView.delegate
+    } set {
+      commentInputView.delegate = newValue
+    }
+  }
+  
+  // MARK: - Lifecycle
+  init() {
+    super.init(
+      frame: .zero,
+      contentView: commentInputView,
+      contentViewSpacing: .init(top: 0, left: 10, bottom: 0, right: 0),
+      profileLayoutInfo: .medium(.bottom))
+  }
+  
+  required init?(coder: NSCoder) {
+    nil
+  }
 }
