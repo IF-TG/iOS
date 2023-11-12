57f8d540f21e01988f9d8e3544af07f3fff76809
shcommit <happysh_s2@naver.com>
2023-11-13 03:39:01 +0900

    [Feat] Icon과 CountLabel이 있는 stackView 구현했습니다.

---
+++ travelPlan/Source/Common/View/IconWithCountLabelStackView.swift
@@ -0,0 +1,65 @@
+//
+//  IconWithCountLabelStackView.swift
+//  travelPlan
+//
+//  Created by 양승현 on 11/13/23.
+//
+
+import UIKit
+
+class IconWithCountLabelStackView: UIStackView {
+  public let icon: UIImageView
+  
+  public let countLabel: BaseLabel
+  
+  init(frame: CGRect, iconInfo: IconInfo, countInfo: CountLabelInfo) {
+    icon = UIImageView(image: UIImage(named: iconInfo.iconPath)).set {
+      $0.contentMode = .scaleToFill
+    }
+    countLabel = BaseLabel(fontType: countInfo.fontType, lineHeight: countInfo.lineHeight)
+    super.init(frame: frame)
+    configureUI()
+  }
+  
+  convenience init(
+    iconInfo: IconInfo,
+    countInfo: CountLabelInfo
+  ) {
+    self.init(frame: .zero, iconInfo: iconInfo, countInfo: countInfo)
+    translatesAutoresizingMaskIntoConstraints = false
+  }
+  
+  required init(coder: NSCoder) {
+    fatalError()
+  }
+}
+
+// MARK: - Helpers
+extension IconWithCountLabelStackView {
+  func setCountLabel(text: String?) {
+    countLabel.text = text
+  }
+}
+
+// MARK: - Private Helpers
+extension IconWithCountLabelStackView {
+  private func configureUI() {
+    axis = .horizontal
+    spacing = 5
+    distribution = .equalSpacing
+    alignment = .leading
+  }
+}
+
+// MARK: - Utils
+extension IconWithCountLabelStackView {
+  struct IconInfo {
+    let size: CGSize
+    let iconPath: String
+  }
+  
+  struct CountLabelInfo {
+    let fontType: UIFont.Pretendard
+    let lineHeight: CGFloat
+  }
+}
