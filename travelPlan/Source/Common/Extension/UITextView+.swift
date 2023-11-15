d5ecf3a893bd23d87acb15976ca77e64fa01b67c
shcommit <happysh_s2@naver.com>
2023-11-16 00:26:14 +0900

    [Feat] Long press gesture 추가

--- travelPlan/Source/Common/Extension/UITextView+.swift
+++ travelPlan/Source/Common/Extension/UITextView+.swift
@@ -8,16 +8,16 @@
 import UIKit
 
 extension UITextView {
-  func setDraggingGestureToCopyInClipboard() {
+  func setTapGestureForDraggingToCopyInClipboard() {
     isUserInteractionEnabled = true
     isEditable = false
     let gesture = UIPanGestureRecognizer(
       target: self,
-      action: #selector(handleDragging))
+      action: #selector(handleTapGestureDragging))
     addGestureRecognizer(gesture)
   }
   
-  @objc private func handleDragging(_ gesture: UIPanGestureRecognizer) {
+  @objc private func handleTapGestureDragging(_ gesture: UIPanGestureRecognizer) {
     if gesture.state == .ended {
       guard let selectedText else { return }
       UIPasteboard.general.string = selectedText
@@ -27,6 +27,19 @@ extension UITextView {
   var selectedText: String? {
     guard let range = selectedTextRange else { return nil }
     return text(in: range)
+  }
+  
+  func setLongGestureForDraggingToCopyInClipboard() {
+    isUserInteractionEnabled = true
+    isEditable = false
+    let gesture = UILongPressGestureRecognizer(
+      target: self,
+      action: #selector(handleLongGestureDragging))
+  }
+  
+  @objc private func handleLongGestureDragging(_ gesture: UILongPressGestureRecognizer) {
+    guard gesture.state == .ended, let range = selectedTextRange, let text = text(in: range) else { return }
+    UIPasteboard.general.string = text
   }
 }
 
