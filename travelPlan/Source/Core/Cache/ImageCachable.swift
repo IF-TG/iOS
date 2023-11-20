539c49a0e0e414c0d1bbca7c28b0b74cf364b9e7
shcommit <happysh_s2@naver.com>
2023-11-20 11:13:52 +0900

    [Feat] MainActor말고 어트리뷰트 추가

--- travelPlan/Source/Core/Cache/ImageCachable.swift
+++ travelPlan/Source/Core/Cache/ImageCachable.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 protocol ImageCachable: Actor {
+  @MainActor
   func image(for url: String) -> UIImage?
-  func insertImage(_ image: UIImage, forKey url: String)
+  @MainActor
+  func insert(_ image: UIImage, forKey url: String)
+  @MainActor
   func removeImage(for url: String)
+  @MainActor
   func removeAllImages()
+  
+  @MainActor
   subscript(_ url: String) -> UIImage? { get set }
 }
