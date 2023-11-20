018e82a1dfae8d1efa5cf72f544876584df28a27
shcommit <happysh_s2@naver.com>
2023-11-20 11:03:04 +0900

    [Feat] ImageCachable 행동 추가

--- travelPlan/Source/Core/Cache/ImageCachable.swift
+++ travelPlan/Source/Core/Cache/ImageCachable.swift
@@ -5,4 +5,12 @@
 //  Created by 양승현 on 11/20/23.
 //
 
-import Foundation
+import UIKit
+
+protocol ImageCachable: Actor {
+  func image(for url: String) -> UIImage?
+  func insertImage(_ image: UIImage, forKey url: String)
+  func removeImage(for url: String)
+  func removeAllImages()
+  subscript(_ url: String) -> UIImage? { get set }
+}
