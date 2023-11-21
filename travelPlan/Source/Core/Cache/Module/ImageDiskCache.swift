03ae29bff238acc45d09f8d8060a236851a71b7f
shcommit <happysh_s2@naver.com>
2023-11-21 19:59:52 +0900

    [Feat] 파일에 write하려면 확장자명이있어야하고 그전에 해당 경로의 디렉터리를 create해야함. 관련 기능 추가

--- travelPlan/Source/Core/Cache/Module/ImageDiskCache.swift
+++ travelPlan/Source/Core/Cache/Module/ImageDiskCache.swift
@@ -13,46 +13,56 @@ class ImageDiskCache {
   }
   
   private var folderURL: URL? {
-    /// 디렉터리가 없을 경우 자동으로 생성
+    /// 디렉터리 안만들면 이 경로 생성x
     return FileManager.default.urls(
       for: .cachesDirectory,
       in: .userDomainMask
     )[0].appendingPathComponent(".cache")
+  }
+  
+  init() {
+    createCacheDirectory()
   }
 }
 
 // MARK: - Helpers
 extension ImageDiskCache {
   /// 경로 생성
-  func imageURL(for key: String) -> URL? {
-    return folderURL?.appendingPathComponent(key)
+  func imageFileURL(for key: String) -> URL? {
+    return folderURL?.appendingPathComponent("\(key).txt")
   }
   
   func loadImage(with key: String) -> UIImage? {
     guard
-      let imageURL = imageURL(for: key),
+      let imageURL = imageFileURL(for: key),
       storage.fileExists(atPath: imageURL.path)
     else { return nil }
     return UIImage.init(contentsOfFile: imageURL.path)
   }
   
   func save(_ image: UIImage, with key: String) {
-    guard let imageURL = imageURL(for: key) else {
+    guard let imageURL = imageFileURL(for: key) else {
       return
     }
     
     if !hasExistFile(atPath: imageURL.path) {
+      print("파일 생성")
       storage.createFile(atPath: imageURL.path, contents: nil, attributes: nil)
     }
+    
     guard let data = image.jpegData(compressionQuality: 1) else {
       print("DEBUG: Image 데이터가 존재하지 않습니다.")
       return
     }
-    try? data.write(to: imageURL)
+    do {
+      try data.write(to: imageURL, options: .atomic)
+    } catch {
+      print("DEBUG: 파일 write 실패 \(error.localizedDescription)")
+    }
   }
   
   func removeImage(with key: String) -> Bool {
-    guard let imageURL = imageURL(for: key) else {
+    guard let imageURL = imageFileURL(for: key) else {
       print("DEBUG: Image 경로가 없습니다")
       return true
     }
@@ -69,7 +79,7 @@ extension ImageDiskCache {
   }
   
   func hasExistFile(atKey key: String) -> Bool {
-    guard let path = folderURL?.appendingPathComponent(key).path else {
+    guard let path = imageFileURL(for: key)?.path else {
       return false
     }
     return hasExistFile(atPath: path)
@@ -84,10 +94,31 @@ extension ImageDiskCache {
     do {
       let imageFiles = try storage.contentsOfDirectory(atPath: folderURL.path)
       for imageFile in imageFiles {
+        print("삭제할 파일", imageFile)
         try? storage.removeItem(at: folderURL.appendingPathExtension(imageFile))
       }
     } catch {
       print("DEBUG: ImageDiskCache 디렉터리의 폴더 중 에러 발생: \(error.localizedDescription)")
+    }
+  }
+  
+  private func createCacheDirectory() {
+    do {
+      let cacheDirectoryURL = try FileManager.default.url(
+        for: .cachesDirectory,
+        in: .userDomainMask,
+        appropriateFor: nil,
+        create: true
+      ).appendingPathComponent(".cache")
+      
+      if !FileManager.default.fileExists(atPath: cacheDirectoryURL.path) {
+        try FileManager.default.createDirectory(
+          at: cacheDirectoryURL,
+          withIntermediateDirectories: true,
+          attributes: nil)
+      }
+    } catch {
+      print("DEBUG: 디렉터리 생성 실패: \(error.localizedDescription)")
     }
   }
 }
