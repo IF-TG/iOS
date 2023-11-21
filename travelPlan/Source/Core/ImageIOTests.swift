b9ba036f4cad0663707ec971d682d95748ee976b
shcommit <happysh_s2@naver.com>
2023-11-22 03:03:44 +0900

    [Test] 원본 이미지와 다운샘플링된 이미지 바이너리 크기 비교 테스트

--- travelPlan/Source/Core/ImageIOTests.swift
+++ travelPlan/Source/Core/ImageIOTests.swift
@@ -6,30 +6,39 @@
 //
 
 import XCTest
+@testable import travelPlan
 
 final class ImageIOTests: XCTestCase {
+  let sut = ImageIO()
+}
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+extension ImageIOTests {
+  func testImageIO_CompareDownsampledImageWithOriginalImageWhenUsingSetDownsampleFunc_shouldLessthanOrignal() {
+    // Arrange
+    let originalImage = UIImage(named: "tempThumbnail1")!
+    let originalImageData = originalImage.pngData()!
+    let targetResizedSize = CGSize(width: 35, height: 35)
+    let downsampledOptions = ImageIO.DownsampledOptions.init(imagePixelSize: targetResizedSize)
+    
+    // Act
+    let cgImage = sut.setDwonsampled(atImageData: originalImageData, for: downsampledOptions)
+    
+    // Assert
+    XCTAssertNotNil(cgImage, "다운샘플링된 이미지가 nil이 되면 안되는데 nil이 반환됨.")
+    let downsampledData = UIImage(cgImage: cgImage!).pngData()!
+    XCTAssertLessThan(downsampledData.count, originalImageData.count, "다운 샘플링된 이미지가 원본 UIImage 데이터보다 작아야함")
+    debugPrint("원래 image bytes: ", originalImageData)
+    debugPrint("다운 샘플링된 image bytes ", downsampledData)
+  }
+  
+  func testImageIO_ImageDimension_ShouldReturnEqual() {
+    // Arrange
+    let originalImage = UIImage(named: "tempThumbnail1")
+    
+    // Act
+    
+    
+    // Assert
+    XCTAssertFalse(true)
+  }
 }
