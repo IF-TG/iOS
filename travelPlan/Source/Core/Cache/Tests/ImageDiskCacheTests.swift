15ed39f53b4519aed71a95528f7f5f6bd1e7d9f8
shcommit <happysh_s2@naver.com>
2023-11-21 20:17:01 +0900

    [Test] 파일 저장 및 파일이 존재하는지 테스트

--- travelPlan/Source/Core/Cache/Tests/ImageDiskCacheTests.swift
+++ travelPlan/Source/Core/Cache/Tests/ImageDiskCacheTests.swift
@@ -6,30 +6,39 @@
 //
 
 import XCTest
+@testable import travelPlan
 
 final class ImageDiskCacheTests: XCTestCase {
+  var sut: ImageDiskCache!
+  
+  override func setUp() {
+    super.setUp()
+    sut = ImageDiskCache()
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+    sut.removeAllImages()
+    sut = nil
+  }
+}
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+// MARK: - Test
+extension ImageDiskCacheTests {
+  /// 이미지를 저장할 파일 경로를 base64로 인코딩해서 저장하려고하면.. "/" 이런 경로가 들어가는데, 이때 파일경로로 인식될수있고 너무길어서 파일로 write가 안됩니다.
+  /// 굳이라는 생각도있고.. 그런데 서버에서 받아올땐 base64여서 우선 hashValue로 저장하고 꺼내오도록 실험했습니다.
+  func testImageDiskCache_save로이미지저장할경우_ShouldReturnTrue() {
+    // Arrange
+    let image = UIImage(named: "tempProfile1")!
+    let URLPath = "\(image.base64!.hashValue)"
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    // Act
+    sut.save(image, with: URLPath)
+    let filePath = sut.imageFileURL(for: URLPath)?.path
+    let res = sut.hasExistFile(atKey: URLPath)
+    
+    // Assert
+    XCTAssertNotNil(filePath, "이미지 파일 경로 nil")
+    XCTAssertTrue(res, "파일이 존재하지 않습니다.")
+  }
 }
