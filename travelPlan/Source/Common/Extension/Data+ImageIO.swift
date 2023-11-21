57bd2892dd80750923838d98de490f51e02e88c0
shcommit <happysh_s2@naver.com>
2023-11-22 00:58:52 +0900

    [Feat] Data 인 경우 kCGImageSource를 통해 다운샘플링으로 이미지 반환하는 함수 추가

--- travelPlan/Source/Common/Extension/Data+ImageIO.swift
+++ travelPlan/Source/Common/Extension/Data+ImageIO.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import UIKit.UIImage
+import ImageIO
+
+extension Data {
+  func convertToDownsampledImage(for size: CGSize) -> UIImage? {
+    let options = [kCGImageSourceShouldCache: false] as CFDictionary
+    guard let imageSource = CGImageSourceCreateWithData(self as CFData, options) else {
+      return nil
+    }
+    let downsampleOptions = [
+      kCGImageSourceCreateThumbnailFromImageAlways: false,
+      kCGImageSourceCreateThumbnailWithTransform: true,
+      kCGImageSourceShouldCacheImmediately: false,
+      kCGImageSourceThumbnailMaxPixelSize: Swift.max(size.width, size.height)
+    ] as CFDictionary
+    guard let downsampledImage = CGImageSourceCreateThumbnailAtIndex(
+      imageSource, 0, downsampleOptions
+    ) else {
+      return nil
+    }
+    return UIImage(cgImage: downsampledImage)
+  }
+}
