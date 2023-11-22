40d6e07d0be3b0130821fc21c2f5dc7a9b4747bf
shcommit <happysh_s2@naver.com>
2023-11-22 14:53:25 +0900

    [Feat] 섬네일 하나일 경우 BasePostCell 구체타입 추가

--- travelPlan/Source/Common/View/PostCollectionView/PostCellWithOneThumbnail.swift
+++ travelPlan/Source/Common/View/PostCollectionView/PostCellWithOneThumbnail.swift
@@ -1,8 +1,42 @@
 //
-//  PostCell.WithOneImageswift.swift
+//  PostCellWithOneThumbnail.swift
 //  travelPlan
 //
 //  Created by 양승현 on 11/22/23.
 //
 
-import Foundation
+import UIKit
+
+private final class PostCellOneThumbnailView: UIImageView, BasePostCellThumbnailConfigurable {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    translatesAutoresizingMaskIntoConstraints = false
+    contentMode = .scaleAspectFill
+  }
+  
+  required init?(coder: NSCoder) {
+    nil
+  }
+  
+  func setThumbnail(with images: [String]?) {
+    guard let images else {
+      image = nil
+      return
+    }
+    image = UIImage(named: images[0])
+  }
+}
+
+final class PostCellWithOneThumbnail: BasePostCell {
+  static let id = String(describing: PostCellWithOneThumbnail.self)
+  
+  // MARK: - Lifecycle
+  init(frame: CGRect) {
+    let contentView = PostCellOneThumbnailView(frame: .zero)
+    super.init(frame: frame, thumbnailView: contentView)
+  }
+  
+  required init?(coder: NSCoder) {
+    nil
+  }
+}
