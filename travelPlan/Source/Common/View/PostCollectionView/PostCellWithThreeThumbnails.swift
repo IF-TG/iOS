249ea9806cc5b28c731e7213dae5d93dbffd0e45
shcommit <happysh_s2@naver.com>
2023-11-22 15:25:19 +0900

    [Feat] PostCell에서 썸네일 3개일 경우 cell구현

--- travelPlan/Source/Common/View/PostCollectionView/PostCellWithThreeThumbnails.swift
+++ travelPlan/Source/Common/View/PostCollectionView/PostCellWithThreeThumbnails.swift
@@ -1,8 +1,69 @@
 //
-//  PostCellWithThreeThumbnail.swift
+//  PostCellWithThreeThumbnails.swift
 //  travelPlan
 //
 //  Created by 양승현 on 11/22/23.
 //
 
-import Foundation
+import UIKit
+
+final class PostCellWithThreeThumbnails: BasePostCell {
+  static let id = String(describing: PostCellWithTwoThumbnails.self)
+  
+  // MARK: - Nested
+  private final class PostThreeThumbnailsView: UIStackView {
+    private var imageViews: [UIImageView] = []
+    init() {
+      super.init(frame: .zero)
+      axis = .horizontal
+      spacing = 1
+      distribution = .equalSpacing
+      alignment = .fill
+      imageViews = (0...2).map { _ -> UIImageView in
+        return UIImageView(frame: .zero).set {
+          $0.contentMode = .scaleAspectFill
+        }
+      }
+      imageViews.forEach { addArrangedSubview($0) }
+    }
+    
+    required init(coder: NSCoder) {
+      fatalError()
+    }
+    
+    func configureThumbnail(with images: [String]?) {
+      guard let images else {
+        imageViews.forEach { $0.image = nil }
+        return
+      }
+      imageViews.enumerated().forEach {
+        $1.image = UIImage(named: images[$0])
+      }
+    }
+  }
+  
+  // MARK: - Properties
+  private var thumbnailView: PostThreeThumbnailsView?
+  
+  // MARK: - Lifecycle
+  init(frame: CGRect) {
+    let contentView = PostThreeThumbnailsView()
+    self.thumbnailView = contentView
+    super.init(frame: frame, thumbnailView: contentView)
+  }
+  
+  required init?(coder: NSCoder) {
+    nil
+  }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    thumbnailView?.configureThumbnail(with: nil)
+  }
+  
+  // MARK: - Helpers
+  override func configure(with post: PostInfo?) {
+    super.configure(with: post)
+    thumbnailView?.configureThumbnail(with: post?.content.thumbnailURLs)
+  }
+}
