f8f95dd39b768908112b1b4f216801a27b9422db
shcommit <happysh_s2@naver.com>
2023-11-22 15:00:03 +0900

    [Style] Nested로 컨텐트 뷰 변경

--- travelPlan/Source/Common/View/PostCollectionView/PostCellWithOneThumbnail.swift
+++ travelPlan/Source/Common/View/PostCollectionView/PostCellWithOneThumbnail.swift
@@ -7,32 +7,33 @@
 
 import UIKit
 
-private final class PostCellOneThumbnailView: UIImageView, BasePostCellThumbnailConfigurable {
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    translatesAutoresizingMaskIntoConstraints = false
-    contentMode = .scaleAspectFill
-  }
-  
-  required init?(coder: NSCoder) {
-    nil
-  }
-  
-  func setThumbnail(with images: [String]?) {
-    guard let images else {
-      image = nil
-      return
-    }
-    image = UIImage(named: images[0])
-  }
-}
-
 final class PostCellWithOneThumbnail: BasePostCell {
+  // MARK: - Nested
+  private final class PostOneThumbnailView: UIImageView, BasePostCellThumbnailConfigurable {
+    override init(frame: CGRect) {
+      super.init(frame: frame)
+      translatesAutoresizingMaskIntoConstraints = false
+      contentMode = .scaleAspectFill
+    }
+    
+    required init?(coder: NSCoder) {
+      nil
+    }
+    
+    func setThumbnail(with images: [String]?) {
+      guard let images else {
+        image = nil
+        return
+      }
+      image = UIImage(named: images[0])
+    }
+  }
+  
   static let id = String(describing: PostCellWithOneThumbnail.self)
   
   // MARK: - Lifecycle
   init(frame: CGRect) {
-    let contentView = PostCellOneThumbnailView(frame: .zero)
+    let contentView = PostOneThumbnailView(frame: .zero)
     super.init(frame: frame, thumbnailView: contentView)
   }
   
