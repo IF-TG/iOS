1df8ea19a3e382528e3580a7281877effd6ede45
shcommit <happysh_s2@naver.com>
2023-11-23 02:51:56 +0900

    [Feat] fillEqually로 스택뷰 길이에 맞게 서브뷰들 같게 설정하도록 타입 변경

--- travelPlan/Source/Common/Extension/UIStackView+Helpers.swift
+++ travelPlan/Source/Common/Extension/UIStackView+Helpers.swift
@@ -12,7 +12,6 @@ extension UIStackView {
   func configureDefaultPostThumbnail(with axis: NSLayoutConstraint.Axis) {
     self.axis = axis
     spacing = 1
-    distribution = .equalSpacing
-    alignment = .fill
+    distribution = .fillEqually
   }
 }
