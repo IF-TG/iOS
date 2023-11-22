94796222d3f8daa1c1dbeddd76532bda686447f4
shcommit <happysh_s2@naver.com>
2023-11-22 16:08:23 +0900

    [Style] configureDefaultPostThumbnial(with:) 주석 추가

--- travelPlan/Source/Common/Extension/UIStackView+Helpers.swift
+++ travelPlan/Source/Common/Extension/UIStackView+Helpers.swift
@@ -8,6 +8,7 @@
 import UIKit.UIStackView
 
 extension UIStackView {
+  /// Feed 탭의 PostCell에 thumbnail이미지를 arranged subview로하는 stackview의 스택 뷰 공통 설정 값입니다.
   func configureDefaultPostThumbnail(with axis: NSLayoutConstraint.Axis) {
     self.axis = axis
     spacing = 1
