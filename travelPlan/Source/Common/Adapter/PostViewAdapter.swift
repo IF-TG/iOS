e579c404e3b51cb50540d15a4bd465b22f574d1f
shcommit <happysh_s2@naver.com>
2023-11-23 02:50:41 +0900

    [Feat] 1~5개일 떄 post cell's configure(with:) 호출 및 마지막 cell인지 검사로직 호출

--- travelPlan/Source/Common/Adapter/PostViewAdapter.swift
+++ travelPlan/Source/Common/Adapter/PostViewAdapter.swift
@@ -43,19 +43,47 @@ extension PostViewAdapter: UICollectionViewDataSource {
     _ collectionView: UICollectionView,
     cellForItemAt indexPath: IndexPath
   ) -> UICollectionViewCell {
-    if indexPath.section == 1 {
-      guard
-        let cell = collectionView.dequeueReusableCell(
-          withReuseIdentifier: BasePostCell.id,
-          for: indexPath
-        ) as? BasePostCell,
-        let dataSource = dataSource
-      else {
+    if indexPath.section == 1, let thumbnails = dataSource?.numberOfThumbnailsInPost(at: indexPath.row) {
+      guard let postItem = dataSource?.postItem(at: indexPath.row) else { return .init() }
+      switch thumbnails {
+      case 1:
+        guard let cell = collectionView.dequeueReusableCell(
+          withReuseIdentifier: PostCellWithOneThumbnail.id, for: indexPath
+        ) as? PostCellWithOneThumbnail else { return .init() }
+        cell.configure(with: postItem)
+        checkLastCell(cell, indexPath: indexPath)
+        return cell
+      case 2:
+        guard let cell = collectionView.dequeueReusableCell(
+          withReuseIdentifier: PostCellWithTwoThumbnails.id, for: indexPath
+        ) as? PostCellWithTwoThumbnails else { return .init(frame: .zero) }
+        cell.configure(with: postItem)
+        checkLastCell(cell, indexPath: indexPath)
+        return cell
+      case 3:
+        guard let cell = collectionView.dequeueReusableCell(
+          withReuseIdentifier: PostCellWithThreeThumbnails.id, for: indexPath
+        ) as? PostCellWithThreeThumbnails else { return .init(frame: .zero) }
+        cell.configure(with: postItem)
+        checkLastCell(cell, indexPath: indexPath)
+        return cell
+      case 4:
+        guard let cell = collectionView.dequeueReusableCell(
+          withReuseIdentifier: PostCellWithFourThumbnails.id, for: indexPath
+        ) as? PostCellWithFourThumbnails else { return .init(frame: .zero) }
+        cell.configure(with: postItem)
+        checkLastCell(cell, indexPath: indexPath)
+        return cell
+      case 5:
+        guard let cell = collectionView.dequeueReusableCell(
+          withReuseIdentifier: PostCellWithFiveThumbnails.id, for: indexPath
+        ) as? PostCellWithFiveThumbnails else { return .init(frame: .zero) }
+        cell.configure(with: postItem)
+        checkLastCell(cell, indexPath: indexPath)
+        return cell
+      default:
         return .init(frame: .zero)
       }
-      cell.configure(with: dataSource.postItem(at: indexPath.row))
-      checkLastCell(cell, indexPath: indexPath)
-      return cell
     }
     return .init(frame: .zero)
   }
@@ -63,7 +91,7 @@ extension PostViewAdapter: UICollectionViewDataSource {
 
 // MARK: - Private helper
 private extension PostViewAdapter {
-  func checkLastCell(_ cell: BasePostCell, indexPath: IndexPath) {
+  func checkLastCell(_ cell: UICollectionViewCell & PostCellEdgeDividable, indexPath: IndexPath) {
     if let numberOfItems = dataSource?.numberOfItems, indexPath.item == numberOfItems - 1 {
       cell.hideCellDivider()
     }
