b3be251c28ea15bf3fa2e1d0a866479e29fe894e
shcommit <happysh_s2@naver.com>
2023-11-27 13:16:06 +0900

    [Feat] index, section 추가

--- travelPlan/Source/Feature/Profile/ViewController/ProfileViewController+SettingType.swift
+++ travelPlan/Source/Feature/Profile/ViewController/ProfileViewController+SettingType.swift
@@ -5,21 +5,25 @@
 //  Created by 양승현 on 11/3/23.
 //
 
-import UIKit
+import UIKit.UIColor
 
 @frozen enum SettingType: String, CaseIterable {
+  // index 0
   case accountSetting = "계정 관리"
   case myInformation = "내 정보"
   
+  // index = 2
   case activitySetting = "활동 관리"
   case myPosts = "내가 작성한 후기 글"
   case myActivity = "내 활동"
   case blockList = "차단 목록"
   
+  // index = 6
   case service = "서비스"
   case operationGuide = "이용안내"
   case customerService = "고객센터"
   
+  // index = 0
   case versionInformation = "버전정보"
   
   var fontType: UIFont.Pretendard {
@@ -30,6 +34,40 @@ import UIKit
       return .regular_400(fontSize: 13)
     default:
       return .medium_500(fontSize: 14)
+    }
+  }
+  
+  var index: Int {
+    switch self {
+    case .accountSetting: return 0
+    case .myInformation: return 1
+    case .activitySetting: return 2
+    case .myPosts: return 3
+    case .myActivity: return 4
+    case .blockList: return 5
+    case .service: return 6
+    case .operationGuide: return 7
+    case .customerService: return 8
+    case .versionInformation: return 9
+    }
+  }
+  
+  var section: Int {
+    switch self {
+    case .accountSetting,
+         .myInformation:
+      return 0
+    case .activitySetting,
+         .myPosts,
+         .myActivity,
+         .blockList:
+      return 1
+    case .service,
+         .operationGuide,
+          .customerService:
+      return 2
+    case .versionInformation:
+      return 3
     }
   }
   
