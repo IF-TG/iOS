b3d71e37e3906e42737f6a6a8ada7f60f45c1e15
shcommit <happysh_s2@naver.com>
2023-11-27 23:53:48 +0900

    [Add] 세팅마이 프로필 뷰, 내 정보 뷰 컨트롤러 생성

---
+++ travelPlan/Source/Feature/Profile/View/SettingMyProfileView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingMyProfileView.swift
+//  travelPlan
+//
+//  Created by 양승현 on 11/27/23.
+//
+
+import Foundation
---
+++ travelPlan/Source/Feature/Profile/ViewController/MyInformationViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MyInformationViewController.swift
+//  travelPlan
+//
+//  Created by 양승현 on 11/27/23.
+//
+
+import Foundation
