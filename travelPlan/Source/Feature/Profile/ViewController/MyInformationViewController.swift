3ab5bde109f7d9581de5bd676606e237c312cf7e
shcommit <happysh_s2@naver.com>
2023-11-28 23:04:02 +0900

    [Feat] subviews 추가 및 레이아웃

--- travelPlan/Source/Feature/Profile/ViewController/MyInformationViewController.swift
+++ travelPlan/Source/Feature/Profile/ViewController/MyInformationViewController.swift
@@ -5,4 +5,109 @@
 //  Created by 양승현 on 11/27/23.
 //
 
-import Foundation
+import UIKit
+
+final class MyInformationViewController: UIViewController {
+  // MARK: - Properties
+  private let profileImageView = SettingMyProfileView()
+  
+  private let nicknameLabel = BaseLabel(fontType: .semiBold_600(fontSize: 18), lineHeight: 38).set {
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    $0.text = "닉네임"
+  }
+  
+  private lazy var inputNoticeLabel = BaseLabel(fontType: .medium_500(fontSize: 14)).set {
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    $0.textAlignment = .right
+  }
+  
+  private let inputTextField = SettingUserNameTextField().set {
+    $0.placeholder = "닉네임을 입력해주세요"
+  }
+  
+  private let connectedSNSLabel = BaseLabel(fontType: .semiBold_600(fontSize: 18), lineHeight: 38).set {
+    $0.translatesAutoresizingMaskIntoConstraints = false
+  }
+  
+  private let connectedSNSStackView = ConnectedSNSStackView()
+  
+  private let logoutLabel = BaseLabel(fontType: .regular_400(fontSize: 14), lineHeight: 46).set {
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    $0.text = "로그아웃"
+    $0.textColor = .yg.gray3
+  }
+  
+  private var isLogoutBottomLineDrawn = false
+  
+  private let withdrawalLabel = BaseLabel(fontType: .regular_400(fontSize: 14), lineHeight: 46).set {
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    $0.textColor = .yg.red2
+    $0.text = "회원탈퇴"
+  }
+  
+  // MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .yg.gray00Background
+    setupUI()
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    if !isLogoutBottomLineDrawn {
+      isLogoutBottomLineDrawn.toggle()
+      let rightLayer = CALayer()
+      rightLayer.frame = CGRect(x: 0, y: 0, width: logoutLabel.bounds.width, height: logoutLabel.bounds.height-1)
+      rightLayer.backgroundColor = UIColor.yg.gray3.cgColor
+      logoutLabel.layer.addSublayer(rightLayer)
+    }
+  }
+}
+
+// MARK: - LayoutSupport
+extension MyInformationViewController: LayoutSupport {
+  func addSubviews() {
+    [profileImageView,
+     nicknameLabel,
+     inputNoticeLabel,
+     inputTextField,
+     connectedSNSLabel,
+     connectedSNSStackView,
+     logoutLabel,
+     withdrawalLabel
+    ].forEach {
+      view.addSubview($0)
+    }
+  }
+  
+  func setConstraints() {
+    NSLayoutConstraint.activate([
+      profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+      
+      nicknameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 32),
+      nicknameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+      
+      inputNoticeLabel.centerYAnchor.constraint(equalTo: nicknameLabel.centerYAnchor),
+      inputNoticeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+      
+      inputTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+      inputTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+      inputTextField.topAnchor.constraint(equalTo: nicknameLabel.bottomAnchor, constant: 4),
+      inputTextField.heightAnchor.constraint(equalToConstant: 46),
+      
+      connectedSNSLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      connectedSNSLabel.topAnchor.constraint(equalTo: inputTextField.bottomAnchor, constant: 16),
+      
+      connectedSNSStackView.topAnchor.constraint(equalTo: connectedSNSLabel.bottomAnchor, constant: 17),
+      connectedSNSStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      connectedSNSStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 71.5),
+      connectedSNSStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -71.5),
+      
+      logoutLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+      logoutLabel.topAnchor.constraint(equalTo: connectedSNSStackView.bottomAnchor, constant: 17),
+      
+      withdrawalLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+      withdrawalLabel.topAnchor.constraint(equalTo: logoutLabel.bottomAnchor)])
+  }
+}
