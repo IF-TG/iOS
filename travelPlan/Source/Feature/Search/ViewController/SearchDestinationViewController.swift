9d57e4a38dd625b3af5881a7d96a12700fb72d16
seokhyun <letushyun@gmail.com>
2023-11-29 00:29:35 +0900

    [Feat] collectionView 추가

--- travelPlan/Source/Feature/Search/ViewController/SearchDestinationViewController.swift
+++ travelPlan/Source/Feature/Search/ViewController/SearchDestinationViewController.swift
@@ -6,6 +6,16 @@
 //
 
 import UIKit
+import SnapKit
+
+enum DestinationType {
+  case cultureFacility
+  case touristAttraction
+  case leports
+  case restaurant
+  case shopping
+  case festival
+}
 
 class SearchDestinationViewController: UIViewController {
   // MARK: - Properties
@@ -13,18 +23,42 @@ class SearchDestinationViewController: UIViewController {
     $0.setImage(.init(named: "emptyStar-border-white"), for: .normal)
     $0.addTarget(self, action: #selector(didTapStarButton(_:)), for: .touchUpInside)
   }
-  
   private lazy var shareButton = UIButton().set {
     // TODO: - 색상을 image에서 흰색으로 바꿔야합니다.(작동 되는지 확인해보기)
-    $0.setImage(.init(named: "feedShare")?.withRenderingMode(.alwaysTemplate), for: .normal)
-    $0.tintColor = .white
+    let image = resizeImage(image: .init(named: "feedShare"),
+                            size: .init(width: 24, height: 24))?
+      .withRenderingMode(.alwaysTemplate)
+    $0.setImage(image, for: .normal)
+    $0.imageView?.tintColor = .white
     $0.addTarget(self, action: #selector(didTapShareButton(_:)), for: .touchUpInside)
   }
-  
+  private let type: DestinationType
+  private let layout = UICollectionViewFlowLayout().set {
+    $0.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+  }
+  private lazy var collectionView = UICollectionView(
+    frame: .zero,
+    collectionViewLayout: .init()
+  ).set {
+    $0.backgroundColor = .yg.littleWhite
+    $0.register(SearchDestinationTitleCell.self, forCellWithReuseIdentifier: SearchDestinationTitleCell.id)
+    $0.dataSource = self
+  }
   // MARK: - LifeCycle
+  init(type: DestinationType) {
+    self.type = type
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     setupNavigationBar()
+    setupStyles()
+    setupUI()
   }
 }
 
@@ -32,8 +66,24 @@ class SearchDestinationViewController: UIViewController {
 extension SearchDestinationViewController {
   private func setupNavigationBar() {
     setupDefaultBackBarButtonItem(tintColor: .white)
-    navigationItem.leftBarButtonItems = [UIBarButtonItem(customView: starButton),
-                                         UIBarButtonItem(customView: shareButton)]
+    navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: shareButton),
+                                          UIBarButtonItem(customView: starButton)]
+  }
+  
+  private func setupStyles() {
+    view.backgroundColor = .black
+  }
+  
+  private func resizeImage(image: UIImage?, size: CGSize) -> UIImage? {
+    guard let image = image else { return nil }
+    
+    let newSize = CGSize(width: size.width, height: size.height)
+    UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+    image.draw(in: CGRect(origin: .zero, size: newSize))
+    let newImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    
+    return newImage ?? image
   }
 }
 
@@ -47,3 +97,43 @@ private extension SearchDestinationViewController {
     print("Share Button 클릭")
   }
 }
+
+// MARK: - LayoutSupport
+extension SearchDestinationViewController: LayoutSupport {
+  func addSubviews() {
+    view.addSubview(collectionView)
+  }
+  
+  func setConstraints() {
+    collectionView.snp.makeConstraints {
+      $0.edges.equalTo(view.safeAreaLayoutGuide)
+    }
+  }
+}
+
+// MARK: - UICollectionViewDataSource
+extension SearchDestinationViewController: UICollectionViewDataSource {
+  func collectionView(
+    _ collectionView: UICollectionView,
+    numberOfItemsInSection section: Int
+  ) -> Int {
+    return 1
+  }
+  
+  func collectionView(
+    _ collectionView: UICollectionView,
+    cellForItemAt indexPath: IndexPath
+  ) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(
+      withReuseIdentifier: SearchDestinationTitleCell.id,
+      for: indexPath
+    ) as? SearchDestinationTitleCell else { return .init() }
+    
+    return cell
+  }
+}
+
+// MARK: - UICollectionViewDelegate
+//extension SearchDestinationViewController: UICollectionViewDelegateFlowLayout {
+//
+//}
