bb6f36e5fda882688660a19f87b5f92976e05d90
shcommit <happysh_s2@naver.com>
2023-11-29 14:43:18 +0900

    [Feat] 메일 기본문구 추가 및 MFMailComposeViewController를 통해 메일 전송기능 추가

--- travelPlan/Source/Feature/Profile/ViewController/CustomerServiceViewController.swift
+++ travelPlan/Source/Feature/Profile/ViewController/CustomerServiceViewController.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
+import Combine
+import MessageUI
 
 final class CustomerServiceViewController: BaseSettingViewController {
-  
   // MARK: - Properties
   private lazy var mailServiceView = IconWithLabelStackView(
     iconInfo: .init(size: .init(width: 24, height: 24), iconPath: "mail-icon"),
@@ -29,14 +30,68 @@ final class CustomerServiceViewController: BaseSettingViewController {
     $0.backgroundColor = .white
   }
   
+  private var contentViewSubscription: AnyCancellable?
+  
   // MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
     setupUI()
-    
+    handleContentView()
   }
 }
 
+// MARK: - Private Helpers
+private extension CustomerServiceViewController {
+  
+  func handleContentView() {
+    contentViewSubscription = contentView
+      .tapGesture
+      .filter { MFMailComposeViewController.canSendMail() }
+      .sink { [weak self] _ in
+        let mail = MFMailComposeViewController()
+        mail.mailComposeDelegate = self
+        mail.setSubject("여가팀에게 문의 및 개선사항을 남겨주세요.")
+        mail.setToRecipients(["shyeoga2023@gmail.com"])
+        mail.setMessageBody(
+          self?.defaultMeessage ?? "문의사항이나 개선사항을 작성해주세요.",
+          isHTML: true)
+        self?.present(mail, animated: true)
+      }
+
+  }
+  
+  private var defaultMeessage: String {
+    """
+    <p style="font-family: Arial, sans-serif; color: #333; background-color: #f0f0f0; padding: 20px; margin: 0; white-space: pre-wrap;">
+    안녕하세요. "<span style="font-weight: bold; color: #1BA0EB;">여행을 가다"</span> 여가팀입니다.
+    <br>
+    - 문의사항이나 버그, 새선사항 등은 아래에 작성해주세요.
+    
+    
+    ⭐ 문의 관련 스크린샷을 첨부하시면 더욱 빠른 확인이 가능합니다.
+    <br><br>
+    -------------------
+    <span style="font-weight: bold; color: #3366cc;">Device Model : \(UIDevice.modelName)</span><br>
+    <span style="font-weight: bold; color: #3366cc;">Device OS :  \(UIDevice.current.systemVersion)</span><br>
+    <span style="font-weight: bold; color: #3366cc;">App Version : \(Bundle.main.currentAppVersion)</span><br>
+    -------------------
+    </p>
+    """
+  }
+}
+
+extension CustomerServiceViewController: MFMailComposeViewControllerDelegate {
+  func mailComposeController(
+    _ controller: MFMailComposeViewController,
+    didFinishWith result: MFMailComposeResult,
+    error: Error?
+  ) {
+    self.dismiss(animated: true, completion: nil)
+  }
+
+}
+
+// MARK: - LayoutSupport
 extension CustomerServiceViewController: LayoutSupport {
   func addSubviews() {
     view.addSubview(contentView)
