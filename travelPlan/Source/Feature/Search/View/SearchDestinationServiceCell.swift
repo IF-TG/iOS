2e2ccb75b57b7c6fdc1c5c6e6b703127bfb0030b
seokhyun <letushyun@gmail.com>
2023-11-29 16:43:02 +0900

    [Feat] SearchDestinationServiceCell 및 View 추가

---
+++ travelPlan/Source/Feature/Search/View/SearchDestinationServiceCell.swift
@@ -0,0 +1,8 @@
+//
+//  SearchDestinationServiceCell.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 11/29/23.
+//
+
+import Foundation
---
+++ travelPlan/Source/Feature/Search/View/SearchDestinationServiceView.swift
@@ -0,0 +1,8 @@
+//
+//  SearchDestinationServiceView.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 11/29/23.
+//
+
+import Foundation
