e6d60d9589895aafcdd58e41252fa22795978ecf
seokhyun <letushyun@gmail.com>
2023-11-29 23:01:51 +0900

    [Add] temp data 추가

--- travelPlan/Source/Feature/Search/Model/SearchCampingModel.swift
+++ travelPlan/Source/Feature/Search/Model/SearchCampingModel.swift
@@ -19,16 +19,16 @@ struct SearchCampingModel {
 // MARK: - Mock
 extension SearchCampingModel {
   static var mockModels: [SearchCampingModel] = [
-    .init(id: 1, imagePath: "성심당",
+    .init(id: 1, imagePath: "seoungsimdang",
           place: "성심당",
           location: "대전",
           category: "베이커리",
           isSelectedButton: false),
-    .init(id: 1, imagePath: "오씨칼국수",
+    .init(id: 1, imagePath: "ockalguksoo",
           place: "오씨칼국수", location: "대전", category: "음식점", isSelectedButton: false),
-    .init(id: 1, imagePath: "온천집",
+    .init(id: 1, imagePath: "oncheonjeep",
           place: "온천집", location: "대전", category: "음식점", isSelectedButton: false),
-    .init(id: 1, imagePath: "태평소국밥",
+    .init(id: 1, imagePath: "taepyeongsokookbob",
           place: "태평소국밥", location: "대전", category: "음식점", isSelectedButton: false),
     .init(id: 1, imagePath: "tempThumbnail12",
           place: "관광 장소명", location: "부산", category: "관광 카테고리", isSelectedButton: false),
--- travelPlan/Source/Feature/Search/Model/SearchDetailHeaderInfo.swift
+++ travelPlan/Source/Feature/Search/Model/SearchDetailHeaderInfo.swift
@@ -16,6 +16,6 @@ struct SearchDetailHeaderInfo {
 extension SearchDetailHeaderInfo {
   static let festivalMock = SearchDetailHeaderInfo(title: "베스트 축제 🎡", imageURL: "festival-more")
   //  static let campingMock = SearchDetailHeaderInfo(title: "야영, 레포츠 어떠세요? 🏕️", imageURL: "camping-more")
-  static let campingMock = SearchDetailHeaderInfo(title: "대전 맛집", imageURL: "성심당")
+  static let campingMock = SearchDetailHeaderInfo(title: "대전 맛집", imageURL: "seoungsimdang")
   static let topTenMock = SearchDetailHeaderInfo(title: "여행지 TOP 10 🌟", imageURL: "topten-more")
 }
