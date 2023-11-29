591c5c1000b33165a80e9adeaa6eae233ace4631
seokhyun <letushyun@gmail.com>
2023-11-29 19:13:21 +0900

    [Feat] ServiceCell 및 view 화면 일부 구현

--- travelPlan/Source/Feature/Search/View/SearchDestinationServiceCell.swift
+++ travelPlan/Source/Feature/Search/View/SearchDestinationServiceCell.swift
@@ -5,4 +5,43 @@
 //  Created by SeokHyun on 11/29/23.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class SearchDestinationServiceCell: UICollectionViewCell {
+  // MARK: - Properties
+  private let stackView = UIStackView().set {
+    $0.axis = .horizontal
+    $0.alignment = .center
+    $0.distribution = .fillEqually
+    $0.spacing = 20
+  }
+  
+  private let titleLabel = UILabel().set {
+    $0.text = "제목"
+    $0.font = .init(pretendard: .medium_500(fontSize: 12))
+    $0.textColor = .yg.gray6
+  }
+  
+  // MARK: - LifeCycle
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: - LayoutSupport
+extension SearchDestinationServiceCell: LayoutSupport {
+  func addSubviews() {
+    contentView.addSubview(stackView)
+  }
+  
+  func setConstraints() {
+    stackView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+  }
+}
--- travelPlan/Source/Feature/Search/View/SearchDestinationServiceView.swift
+++ travelPlan/Source/Feature/Search/View/SearchDestinationServiceView.swift
@@ -5,4 +5,58 @@
 //  Created by SeokHyun on 11/29/23.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class SearchDestinationServiceView: UIView {
+  // MARK: - Properties
+  private let titleLabel = UILabel().set {
+    $0.text = "제목"
+    $0.font = .init(pretendard: .medium_500(fontSize: 12))
+    $0.textColor = .yg.gray6
+  }
+  
+  private let imageView = UIImageView().set {
+    $0.contentMode = .scaleAspectFit
+  }
+  
+  // MARK: - LifeCycle
+  convenience init(title: String, image: UIImage) {
+    self.init(frame: .zero, title: title, image: image)
+  }
+  
+  init(frame: CGRect, title: String, image: UIImage) {
+    self.titleLabel.text = title
+    self.imageView.image = image
+    super.init(frame: frame)
+    setupUI()
+    setupStyles()
+    
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: - LayoutSupport
+extension SearchDestinationServiceView: LayoutSupport {
+  func addSubviews() {
+    addSubview(imageView)
+    addSubview(titleLabel)
+  }
+  
+  func setConstraints() {
+    imageView.snp.makeConstraints {
+      $0.top.equalToSuperview().inset(16)
+      
+    }
+  }
+}
+
+// MARK: - Private Helpers
+extension SearchDestinationServiceView {
+  private func setupStyles() {
+    backgroundColor = .gray
+  }
+}
