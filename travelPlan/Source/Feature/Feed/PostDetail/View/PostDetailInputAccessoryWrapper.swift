cf4df95ae7f66b5b8a879a097b6b24df0a0f2dc3
shcommit <happysh_s2@naver.com>
2023-11-30 12:53:25 +0900

    [Feat] 임시로 사용자 설정 화면과 맞춤. 이미지 경로 변경

--- travelPlan/Source/Feature/Feed/PostDetail/View/PostDetailInputAccessoryWrapper.swift
+++ travelPlan/Source/Feature/Feed/PostDetail/View/PostDetailInputAccessoryWrapper.swift
@@ -26,7 +26,7 @@ final class PostDetailInputAccessoryWrapper: UIView {
     backgroundColor = .white
     contentView.inputDelegate = self
     contentView.baseDelegate = self
-    configure(with: "tempProfile1")
+    configure(with: "tempProfile3")
   }
   
   override var intrinsicContentSize: CGSize {
