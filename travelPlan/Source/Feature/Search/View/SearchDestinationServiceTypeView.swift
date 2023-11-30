1b62f52546f8f22e3041c4ec710e1c00bfe41637
seokhyun <letushyun@gmail.com>
2023-11-30 10:21:14 +0900

    [Add] 아이콘 이미지 추가

---
+++ travelPlan/Source/Feature/Search/View/SearchDestinationServiceTypeView.swift
@@ -0,0 +1,12 @@
+//
+//  SearchDestinationServiceStackView.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 11/30/23.
+//
+
+import UIKit
+
+final class SearchDestinationServiceStackView: UIStackView {
+  
+}
