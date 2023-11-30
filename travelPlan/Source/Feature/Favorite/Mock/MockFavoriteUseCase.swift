97ead3502e88577268d917194382a91c919c8696
shcommit <happysh_s2@naver.com>
2023-11-30 13:34:58 +0900

    [Feat] 목데이터 제거

--- travelPlan/Source/Feature/Favorite/Mock/MockFavoriteUseCase.swift
+++ travelPlan/Source/Feature/Favorite/Mock/MockFavoriteUseCase.swift
@@ -16,25 +16,5 @@ struct MockFavoriteUseCase {
   
   lazy var favoriteDirectories: [FavoriteDirectoryEntity] = [
     .init(id: 0, title: "분위기 있는 카페", innerItemCount: 0, imageURL: profileImageURLs[0]),
-    .init(id: 1, title: "벌써 10월이야? 단풍 명소~", innerItemCount: 1, imageURL: profileImageURLs[1]),
-    .init(id: 0, title: "분위기 있는 카페", innerItemCount: 0, imageURL: profileImageURLs[0]),
-    .init(id: 1, title: "벌써 10월이야? 단풍 명소~", innerItemCount: 1, imageURL: profileImageURLs[1]),
-    .init(id: 0, title: "분위기 있는 카페", innerItemCount: 0, imageURL: profileImageURLs[0]),
-    .init(id: 1, title: "벌써 10월이야? 단풍 명소~", innerItemCount: 1, imageURL: profileImageURLs[1]),
-    .init(id: 0, title: "분위기 있는 카페", innerItemCount: 0, imageURL: profileImageURLs[0]),
-    .init(id: 1, title: "벌써 10월이야? 단풍 명소~", innerItemCount: 1, imageURL: profileImageURLs[1]),
-    .init(id: 0, title: "분위기 있는 카페", innerItemCount: 0, imageURL: profileImageURLs[0]),
-    .init(id: 1, title: "벌써 10월이야? 단풍 명소~", innerItemCount: 1, imageURL: profileImageURLs[1]),
-    .init(id: 0, title: "분위기 있는 카페", innerItemCount: 0, imageURL: profileImageURLs[0]),
-    .init(id: 1, title: "벌써 10월이야? 단풍 명소~", innerItemCount: 1, imageURL: profileImageURLs[1]),
-    .init(id: 0, title: "분위기 있는 카페", innerItemCount: 0, imageURL: profileImageURLs[0]),
-    .init(id: 1, title: "벌써 10월이야? 단풍 명소~", innerItemCount: 1, imageURL: profileImageURLs[1]),
-    .init(id: 0, title: "분위기 있는 카페", innerItemCount: 0, imageURL: profileImageURLs[0]),
-    .init(id: 1, title: "벌써 10월이야? 단풍 명소~", innerItemCount: 1, imageURL: profileImageURLs[1]),
-    .init(id: 0, title: "분위기 있는 카페", innerItemCount: 0, imageURL: profileImageURLs[0]),
-    .init(id: 1, title: "벌써 10월이야? 단풍 명소~", innerItemCount: 1, imageURL: profileImageURLs[1]),
-    .init(id: 0, title: "분위기 있는 카페", innerItemCount: 0, imageURL: profileImageURLs[0]),
-    .init(id: 1, title: "벌써 10월이야? 단풍 명소~", innerItemCount: 1, imageURL: profileImageURLs[1]),
-    .init(id: 0, title: "분위기 있는 카페", innerItemCount: 0, imageURL: profileImageURLs[0]),
     .init(id: 1, title: "벌써 10월이야? 단풍 명소~", innerItemCount: 1, imageURL: profileImageURLs[1])]
 }
