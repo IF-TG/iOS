046af2d782bd56a76bc3a61f6f621cb3d0a29dc7
shcommit <happysh_s2@naver.com>
2023-12-21 00:50:09 +0900

    [Feat] PostViewAdapterDelegate에 포스트 클릭시 및 포스트 하트, 커멘트, 공유, 옵션 클릭시 델리게이트 함수 추가

--- travelPlan/Source/Feature/Feed/Protocol/PostView/PostViewAdapterDelegate.swift
+++ travelPlan/Source/Feature/Feed/Protocol/PostView/PostViewAdapterDelegate.swift
@@ -5,8 +5,12 @@
 //  Created by 양승현 on 11/8/23.
 //
 
-import Foundation
+import UIKit
 
 protocol PostViewAdapterDelegate: AnyObject {
   func didTapPost(with postId: Int)
+  func didTapHeart(in cell: UICollectionViewCell)
+  func didTapComment(in cell: UICollectionViewCell)
+  func didTapShare(in cell: UICollectionViewCell)
+  func didTapOption(in cell: UICollectionViewCell)
 }
