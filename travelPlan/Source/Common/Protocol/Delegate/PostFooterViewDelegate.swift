a59b23b8a032410d5fc4cb080aff40b0d6013188
shcommit <happysh_s2@naver.com>
2023-12-21 00:41:53 +0900

    [Feat] PostFooterViewDelegate 추가

---
+++ travelPlan/Source/Common/Protocol/Delegate/PostFooterViewDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  PostFooterViewDelegate.swift
+//  travelPlan
+//
+//  Created by 양승현 on 12/21/23.
+//
+
+import Foundation
+
+protocol PostFooterViewDelegate: AnyObject {
+  func didTapHeart()
+  func didTapComment()
+  func didTapShare()
+}
