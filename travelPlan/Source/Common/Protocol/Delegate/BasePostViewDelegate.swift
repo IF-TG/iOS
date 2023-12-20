94ab6103d5049f0f696c7b2743c269a3eb5f5d6b
shcommit <happysh_s2@naver.com>
2023-12-21 00:46:03 +0900

    [Feat] FooterViewDelegate 기반으로한 BasePostViewDelegate 추가

---
+++ travelPlan/Source/Common/Protocol/Delegate/BasePostViewDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  BasePostViewDelegate.swift
+//  travelPlan
+//
+//  Created by 양승현 on 12/21/23.
+//
+
+import Foundation
+
+protocol BasePostViewDelegate: PostFooterViewDelegate {
+  func didTapOptionButton()
+}
