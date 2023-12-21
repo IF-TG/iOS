94de129c36b457e3d98da35b0d55139e940e8200
shcommit <happysh_s2@naver.com>
2023-12-22 02:15:32 +0900

    [Feat] 포스트 share 매개변수 추가(title, image)

--- travelPlan/Source/Feature/Feed/Protocol/PostView/FeedPostViewControllerDelegate.swift
+++ travelPlan/Source/Feature/Feed/Protocol/PostView/FeedPostViewControllerDelegate.swift
@@ -5,11 +5,11 @@
 //  Created by 양승현 on 12/20/23.
 //
 
-import Foundation
+import UIKit
 
 protocol FeedPostViewControllerDelegate: AnyObject {
   func didTapPost(with postId: Int)
   func didTapComment(with postId: Int)
-  func didTapShare(with postId: Int)
+  func didTapShare(with postId: Int, title: String?, contentImage: UIImage)
   func didTapOption(with postId: Int)
 }
