91f7d401bb7f5b2362842e202acedaa46a2472fa
shcommit <happysh_s2@naver.com>
2023-12-28 00:40:22 +0900

    [Feat] metadata 프로퍼티 추가 및 TODO 추가

--- travelPlan/Source/Common/Utils/PostActivityItemSource.swift
+++ travelPlan/Source/Common/Utils/PostActivityItemSource.swift
@@ -12,18 +12,38 @@ final class PostActivityItemSource: NSObject, UIActivityItemSource {
   // MARK: - Properties
   private let image: UIImage
   private let title: String
+  private let postId: Int
+  private var metadata: LPLinkMetadata?
   
-  init(image: UIImage, title: String) {
+  init(image: UIImage, title: String, postId: Int) {
     self.image = image
     self.title = title
+    self.postId = postId
+    guard let icon = UIImage(named: "AppIcon") else { 
+      super.init()
+      return
+    }
+    metadata = {
+      $0.iconProvider = NSItemProvider(object: icon)
+      $0.title = title
+      if let deepLinkURL = URL(string: "yeoga://post/\(postId)") {
+        $0.originalURL = deepLinkURL
+      }
+      return $0
+    }(LPLinkMetadata())
     
     super.init()
   }
   
-  func activityViewControllerPlaceholderItem(_ activityViewController: UIActivityViewController) -> Any {
+  func activityViewControllerPlaceholderItem(
+    _ activityViewController: UIActivityViewController
+  ) -> Any {
     return title
   }
   
+  /// 링크로할 때 이미지가 보이지 않음..
+  /// 이미지로. 할때 링크가 보이지 않음..ㅋㅋ
+  // TODO: - 공유 버튼 작아서 잘 안눌림. 여백 포함으로 키우기
   func activityViewController(
     _ activityViewController: UIActivityViewController,
     itemForActivityType activityType: UIActivity.ActivityType?
@@ -37,12 +57,6 @@ final class PostActivityItemSource: NSObject, UIActivityItemSource {
   func activityViewControllerLinkMetadata(
     _ activityViewController: UIActivityViewController
   ) -> LPLinkMetadata? {
-    guard let icon = UIImage(named: "AppIcon") else { return .init() }
-    return {
-      $0.iconProvider = NSItemProvider(object: icon)
-      $0.title = title
-      $0.originalURL = URL(fileURLWithPath: "Yeoga.com")
-      return $0
-    }(LPLinkMetadata())
+    return metadata
   }
 }
