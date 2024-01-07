77761c1fdd641ea1da03911520f306963497a954
seokhyun <letushyun@gmail.com>
2024-01-07 17:51:24 +0900

    [Feat] PhotoService 구현체 추가
    PHCachingImageManager에게 PHAsset을 요청해서 image를 반환 받습니다.

--- travelPlan/Source/Feature/Album/Service/ReviewWritePhotoService.swift
+++ travelPlan/Source/Feature/Album/Service/ReviewWritePhotoService.swift
@@ -5,4 +5,47 @@
 //  Created by SeokHyun on 1/6/24.
 //
 
-final class ReviewWritePhotoService
+import Photos
+import UIKit
+
+final class ReviewWritePhotoService: PhotoService {
+  // MARK: - Properties
+  private let imageManager = PHCachingImageManager()
+  
+  // MARK: - Helpers
+  func convertAlbumToPHAssets(
+    album: PHFetchResult<PHAsset>,
+    completion: @escaping ([PHAsset]) -> Void
+  ) {
+    var assets = [PHAsset]()
+    defer { completion(assets) }
+    
+    guard 0 < album.count else { return }
+    album.enumerateObjects { asset, _, _ in
+      assets.append(asset)
+    }
+  }
+  
+  func fetchImage(
+    asset: PHAsset,
+    size: CGSize,
+    contentMode: PHImageContentMode,
+    completion: @escaping (UIImage) -> Void
+  ) {
+    let options = PHImageRequestOptions().set {
+      $0.deliveryMode = .highQualityFormat
+    }
+    
+    imageManager.requestImage(
+      for: asset,
+      targetSize: size,
+      contentMode: contentMode,
+      options: options) { image, _ in
+        guard let image else { return }
+        
+        completion(image)
+      }
+  }
+  
+  
+}
