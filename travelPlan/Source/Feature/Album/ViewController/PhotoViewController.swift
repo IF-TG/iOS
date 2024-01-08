5b0cb10209877a5130b00e91d5f400293c0c10c9
seokhyun <letushyun@gmail.com>
2024-01-09 05:29:58 +0900

    [Feat] 사진 선택 시, 순서 변경하는 알고리즘 구현

--- travelPlan/Source/Feature/Album/ViewController/PhotoViewController.swift
+++ travelPlan/Source/Feature/Album/ViewController/PhotoViewController.swift
@@ -10,17 +10,31 @@ import SnapKit
 import Photos
 
 final class PhotoViewController: UIViewController {
+  // MARK: - Nested
+  private enum Const {
+    static let numberOfColumns = 3.0
+    static let cellSpace = 1.0
+    static let length = (UIScreen.main.bounds.size.width - cellSpace * (numberOfColumns - 1)) / numberOfColumns
+    static let cellSize = CGSize(width: length, height: length)
+    static let scale = UIScreen.main.scale
+  }
   
   // MARK: - Properties
-  private let albumService: AlbumService = YeogaAlbumService()
-  private var albums = [PHFetchResult<PHAsset>]()
-  
-  private lazy var collectionView: UICollectionView = {
-    let cv = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
-    cv.delegate = self
-    cv.dataSource = self
-    return cv
-  }()
+  private var dataSource = [PhotoCellInfo]()
+  private lazy var collectionView: UICollectionView = .init(
+    frame: .zero,
+    collectionViewLayout: UICollectionViewFlowLayout().set {
+      $0.minimumLineSpacing = 1
+      $0.minimumInteritemSpacing = 0
+      $0.itemSize = Const.cellSize
+    }
+  ).set {
+    $0.register(PhotoCell.self, forCellWithReuseIdentifier: PhotoCell.id)
+    $0.backgroundColor = .clear
+    $0.showsHorizontalScrollIndicator = false
+    $0.delegate = self
+    $0.dataSource = self
+  }
   
   private lazy var cancelButton: BaseNavigationLeftButton = .init().set {
     $0.addTarget(self, action: #selector(didTapCancelButton(_:)), for: .touchUpInside)
@@ -37,13 +51,50 @@ final class PhotoViewController: UIViewController {
     self.addGestureRecognizer(from: $0, action: #selector(didTapTitleView))
   }
   
+  private var currentAlbumIndex = 0 {
+    didSet { loadImages() }
+  }
+  private let albumService: AlbumService = ReviewWriteAlbumService()
+  private let photoService: PhotoService = ReviewWritePhotoService()
+  private var albums = [PHFetchResult<PHAsset>]()
+  // index: order-1
+  // element: indexPath.item
+  private var selectedIndexArray = [Int]()
+  
   // MARK: - LifeCycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    setupUI()
     setupNavigationBar()
     loadAlbums(completion: { [weak self] in
       self?.loadImages()
     })
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    tabBarController?.tabBar.isHidden = true
+    (tabBarController as? MainTabBarController)?.hideShadowLayer()
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    tabBarController?.tabBar.isHidden = false
+    (tabBarController as? MainTabBarController)?.showShadowLayer()
+  }
+}
+
+// MARK: - LayoutSupport
+extension PhotoViewController: LayoutSupport {
+  func addSubviews() {
+    view.addSubview(collectionView)
+  }
+  
+  func setConstraints() {
+    collectionView.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide)
+      $0.leading.trailing.bottom.equalToSuperview()
+    }
   }
 }
 
@@ -53,24 +104,69 @@ extension PhotoViewController: UICollectionViewDataSource {
     _ collectionView: UICollectionView,
     numberOfItemsInSection section: Int
   ) -> Int {
-    1
+    dataSource.count
   }
   
   func collectionView(
     _ collectionView: UICollectionView,
     cellForItemAt indexPath: IndexPath
   ) -> UICollectionViewCell {
-    return .init()
+    guard let cell = collectionView.dequeueReusableCell(
+      withReuseIdentifier: PhotoCell.id,
+      for: indexPath
+    ) as? PhotoCell else { return .init() }
+    
+    let imageInfo = dataSource[indexPath.item]
+    let asset = imageInfo.asset
+    let imageSize = CGSize(width: Const.cellSize.width * Const.scale,
+                           height: Const.cellSize.height * Const.scale)
+    photoService.fetchImage(
+      asset: asset,
+      size: imageSize,
+      contentMode: .aspectFit) { [weak cell] image in
+        let cellInfo = PhotoCellInfo(asset: asset, image: image, selectedOrder: imageInfo.selectedOrder)
+        cell?.configure(with: cellInfo)
+      }
+    return cell
   }
 }
 
 // MARK: - UICollectionViewDelegateFlowLayout
 extension PhotoViewController: UICollectionViewDelegateFlowLayout {
-  
+  func collectionView(
+    _ collectionView: UICollectionView,
+    didSelectItemAt indexPath: IndexPath
+  ) {
+    let cellInfo = dataSource[indexPath.item]
+    let newIndexPaths: [IndexPath]
+    if case .selected = cellInfo.selectedOrder {
+      dataSource[indexPath.item].selectedOrder = .none
+      selectedIndexArray.removeAll(where: { $0 == indexPath.item })
+      
+      for (order, index) in selectedIndexArray.enumerated() {
+        let order = order + 1
+        let prev = dataSource[index]
+        dataSource[index] = PhotoCellInfo(asset: prev.asset, image: prev.image, selectedOrder: .selected(order))
+      }
+      newIndexPaths = [indexPath] + selectedIndexArray.map { IndexPath(item: $0, section: 0) }
+    } else {
+      selectedIndexArray.append(indexPath.item)
+      dataSource[indexPath.item].selectedOrder = .selected(selectedIndexArray.count)
+      newIndexPaths = selectedIndexArray.map { IndexPath(item: $0, section: .zero) }
+    }
+    
+    update(indexPaths: newIndexPaths)
+  }
 }
 
 // MARK: - Private Helpers
 extension PhotoViewController {
+  private func update(indexPaths: [IndexPath]) {
+    collectionView.performBatchUpdates {
+      collectionView.reloadItems(at: indexPaths)
+    }
+  }
+  
   private func loadAlbums(completion: @escaping () -> Void) {
     albumService.getAlbums(mediaType: .image) { [weak self] albumInfos in
       self?.albums = albumInfos.map(\.album)
@@ -87,6 +183,14 @@ extension PhotoViewController {
   private func addGestureRecognizer(from view: UIView, action: Selector?) {
     let tapGesture = UITapGestureRecognizer(target: self, action: action)
     view.addGestureRecognizer(tapGesture)
+  }
+  
+  private func loadImages() {
+    let album = albums[currentAlbumIndex]
+    photoService.convertAlbumToPHAssets(album: album) { [weak self] assets in
+      self?.dataSource = assets.map { PhotoCellInfo(asset: $0, image: nil, selectedOrder: .none) }
+      self?.collectionView.reloadData()
+    }
   }
 }
 
