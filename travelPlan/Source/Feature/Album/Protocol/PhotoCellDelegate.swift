e61d5756f662ad7a99bc1400c9c408307b0ef366
seokhyun <letushyun@gmail.com>
2024-01-23 19:25:41 +0900

    [Feat] PhotoCellDelegate 추가

---
+++ travelPlan/Source/Feature/Album/Protocol/PhotoCellDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCellDelegate.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 1/9/24.
+//
+
+import UIKit.UICollectionViewCell
+
+protocol PhotoCellDelegate: AnyObject {
+  func touchBegan(_ cell: UICollectionViewCell, quadrant: PhotoCell.Quadrant)
+}
