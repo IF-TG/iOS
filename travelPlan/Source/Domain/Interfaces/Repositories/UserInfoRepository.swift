2c215646070d5eaccc4c14ee0edd9d5258adf198
shcommit <happysh_s2@naver.com>
2024-02-27 21:38:29 +0900

    [Feat] isDuplicatedName(with:) 추가 및 주석 추가

--- travelPlan/Source/Domain/Interfaces/Repositories/UserInfoRepository.swift
+++ travelPlan/Source/Domain/Interfaces/Repositories/UserInfoRepository.swift
@@ -5,4 +5,9 @@
 //  Created by 양승현 on 2/27/24.
 //
 
-import Foundation
+import Combine
+
+/// 사용자 정보 CRUD 관련 레포지토리
+protocol UserInfoRepository {
+  func isDuplicatedName(with name: String) -> Future<Bool, Never> 
+}
