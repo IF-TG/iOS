83f7818560289c88da261c949c7a42373ad94a54
shcommit <happysh_s2@naver.com>
2024-02-27 21:44:52 +0900

    [Feat] UserInfoRepository 준수 및 isDuplicatedName(with:) 구현

--- travelPlan/Source/Data/Repositories/DefaultUserInfoRepository.swift
+++ travelPlan/Source/Data/Repositories/DefaultUserInfoRepository.swift
@@ -5,4 +5,38 @@
 //  Created by 양승현 on 2/27/24.
 //
 
-import Foundation
+import Combine
+
+final class DefaultUserInfoRepository {
+  // MARK: - Properties
+  private let service: Sessionable
+  
+  private var subscription: AnyCancellable?
+  
+  // MARK: - Lifecycle
+  init(service: Sessionable) {
+    self.service = service
+  }
+}
+
+// MARK: - UserInfoRepository
+extension DefaultUserInfoRepository: UserInfoRepository {
+  func isDuplicatedName(with name: String) -> Future<Bool, Never> {
+    let reqeustDTO = UserNicknameRequestDTO(nickname: name)
+    let endpoint = UserInfoAPIEndpoint.default.isDuplicatedNickname(with: reqeustDTO)
+    return .init { [weak self] promise in
+      self?.subscription = self?.service
+        .request(endpoint: endpoint)
+        .sink { completion in
+          switch completion {
+          case .finished:
+            return
+          case .failure(let error):
+            print("DEBUG: isDuplicatedName(with:) 파싱할때 에러 발생\n  \(error.localizedDescription)")
+          }
+        } receiveValue: { responseDTO in
+          promise(.success(responseDTO.result))
+        }
+    }
+  }
+}
