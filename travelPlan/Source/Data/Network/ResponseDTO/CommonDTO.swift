290f24f42f61cf72495bfc81c741c5128f8a98f1
shcommit <happysh_s2@naver.com>
2024-02-27 21:11:14 +0900

    [Feat] CommonDTO 객체 구현

--- travelPlan/Source/Data/Network/ResponseDTO/CommonDTO.swift
+++ travelPlan/Source/Data/Network/ResponseDTO/CommonDTO.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+// result에 실제 서버에서 응답하는 데이터가 담깁니다. 그래서 CommonDTO를 사용할 때 T의 구체 타입을 명시해야 합니다.
+struct CommonDTO<T: Decodable>: Decodable {
+  let result: T
+  let status: String
+  let statusCode: String
+  let message: String
+}
