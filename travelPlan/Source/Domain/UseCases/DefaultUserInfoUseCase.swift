641e7ad5ac7ed1df0df8e1ca34f6f44aab63e821
shcommit <happysh_s2@naver.com>
2024-02-27 22:05:36 +0900

    [Feat] UserInfoUseCase 준수 및 isDuplicatedName(with:) 구현

--- travelPlan/Source/Domain/UseCases/DefaultUserInfoUseCase.swift
+++ travelPlan/Source/Domain/UseCases/DefaultUserInfoUseCase.swift
@@ -5,4 +5,25 @@
 //  Created by 양승현 on 2/27/24.
 //
 
-import Foundation
+import Combine
+
+final class DefaultUserInfoUseCase: UserInfoUseCase {
+  // MARK: - Dependencies
+  private let userInfoRepository: UserInfoRepository
+  
+  // MARK: - Properties
+  @Published var isDuplicatedName: Bool?
+  
+  var subscription: AnyCancellable?
+  
+  // MARK: - Lifecycle
+  init(userInfoRepository: UserInfoRepository) {
+    self.userInfoRepository = userInfoRepository
+  }
+  
+  func isDuplicatedName(with name: String) {
+    subscription = userInfoRepository.isDuplicatedName(with: name).sink { [weak self] in
+      self?.isDuplicatedName = $0
+    }
+  }
+}
