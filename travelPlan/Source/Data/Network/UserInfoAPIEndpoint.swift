20106992519b20dc37f45f06d31fcdcf59f9f122
shcommit <happysh_s2@naver.com>
2024-02-28 22:29:09 +0900

    [Feat] 싱글톤 대신 static func로 변경

--- travelPlan/Source/Data/Network/UserInfoAPIEndpoint.swift
+++ travelPlan/Source/Data/Network/UserInfoAPIEndpoint.swift
@@ -8,11 +8,8 @@
 import Foundation
 
 struct UserInfoAPIEndpoint {
-  private init() {}
-  static let `default` = UserInfoAPIEndpoint()
-  
   /// 사용자의 이름이 중복인지 확인할 때 사용합니다.
-  func isDuplicatedNickname(
+  static func isDuplicatedNickname(
     with requestDTO: UserNicknameRequestDTO
   ) -> Endpoint<CommonDTO<Bool>> {
     return Endpoint<CommonDTO<Bool>>(
