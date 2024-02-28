8d04e6dc61b5dc6331ac2aceae100ed7ed87b0b3
shcommit <happysh_s2@naver.com>
2024-02-28 22:47:30 +0900

    [Feat] MyInformationViewModel 생성 및 Input, State 구현

---
+++ travelPlan/Source/Feature/Profile/ViewModel/MyInformationViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  MyInformationViewModel.swift
+//  travelPlan
+//
+//  Created by 양승현 on 2/28/24.
+//
+
+import Foundation
+import Combine
+
+struct MyInformationViewModel {
+  struct Input {
+    let isDuplicatedUserName: PassthroughSubject<String, Never>
+  }
+  
+  enum State {
+    case none
+  }
+}
