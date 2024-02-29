98514055a9f2e88007b704aa3e4d58815d2d711a
shcommit <happysh_s2@naver.com>
2024-03-01 00:44:13 +0900

    [Feat] 여러 에러들을 묶는 MainError 구현

--- travelPlan/Source/Common/Error/MainError.swift
+++ travelPlan/Source/Common/Error/MainError.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum MainError: Swift.Error {
+  case general(String)
+  case networkError(NetworkError)
+  case referenceError(ReferenceError)
+}
+
+// MARK: - CustomStringConvertible
+extension MainError: CustomStringConvertible {
+  var description: String {
+    switch self {
+    case .general(let reason):
+      return "DEBUG: General error occured: \(reason)"
+    case .networkError(let networkError):
+      return "DEBUG: Network error occured: \(networkError.localizedDescription)"
+    case .referenceError(let referenceError):
+      return "DEBUG: Reference error occured: \(referenceError.localizedDescription)"
+    }
+  }
+}
