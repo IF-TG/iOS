2c6a7201681c75037ae9684fc3c826b9f0c108ce
shcommit <happysh_s2@naver.com>
2024-03-01 02:36:06 +0900

    [Fix] 계속 유닛테스트 실패했는데 이유는 subscription을 Future 클로저 내부에 새로 할당하고 캡쳐한게 원인..

--- travelPlan/Source/Data/Repositories/DefaultUserInfoRepository.swift
+++ travelPlan/Source/Data/Repositories/DefaultUserInfoRepository.swift
@@ -11,7 +11,7 @@ final class DefaultUserInfoRepository {
   // MARK: - Properties
   private let service: Sessionable
   
-  private var subscription = Set<AnyCancellable>()
+  private var subscriptions = Set<AnyCancellable>()
   
   // MARK: - Lifecycle
   init(service: Sessionable) {
@@ -24,35 +24,8 @@ extension DefaultUserInfoRepository: UserInfoRepository {
   func isDuplicatedName(with name: String) -> Future<Bool, MainError> {
     let reqeustDTO = UserNicknameRequestDTO(nickname: name)
     let endpoint = UserInfoAPIEndpoint.isDuplicatedNickname(with: reqeustDTO)
-    return .init { [weak self] promise in
-      guard var subscription = self?.subscription else {
-        promise(.failure(.referenceError(.weakSelfError)))
-        return
-      }
-      self?.service.request(endpoint: endpoint)
-        .sink { completion in
-          switch completion {
-          case .finished:
-            return
-          case .failure(let error):
-            promise(.failure(.networkError(error)))
-          }
-        } receiveValue: { responseDTO in
-          promise(.success(responseDTO.result))
-        }.store(in: &subscription)
-    }
-  }
-  
-  func updateUserNickname(with name: String) -> Future<Bool, MainError> {
-    //TODO: - 로그인 중인 사용자의 userId를 키체인이나 유저디폴츠에서 가져와야 합니다.
-    let requestDTO = UserNicknamePatchRequestDTO(nickname: name, userId: 000)
-    let endpoint = UserInfoAPIEndpoint.updateUserNickname(with: requestDTO)
-    return .init { [weak self] promise in
-      guard var subscription = self?.subscription else {
-        promise(.failure(.referenceError(.weakSelfError)))
-        return
-      }
-      self?.service.request(endpoint: endpoint)
+    return .init { [unowned self] promise in
+      service.request(endpoint: endpoint)
         .mapError { MainError.networkError($0) }
         .sink { completion in
           switch completion {
@@ -63,7 +36,27 @@ extension DefaultUserInfoRepository: UserInfoRepository {
           }
         } receiveValue: { responseDTO in
           promise(.success(responseDTO.result))
-        }.store(in: &subscription)
+        }.store(in: &subscriptions)
+    }
+  }
+  
+  func updateUserNickname(with name: String) -> Future<Bool, MainError> {
+    //TODO: - 로그인 중인 사용자의 userId를 키체인이나 유저디폴츠에서 가져와야 합니다.
+    let requestDTO = UserNicknamePatchRequestDTO(nickname: name, userId: 000)
+    let endpoint = UserInfoAPIEndpoint.updateUserNickname(with: requestDTO)
+    return .init { [unowned self] promise in
+      service.request(endpoint: endpoint)
+        .mapError { MainError.networkError($0) }
+        .sink { completion in
+          switch completion {
+          case .finished:
+            return
+          case .failure(let error):
+            promise(.failure(error))
+          }
+        } receiveValue: { responseDTO in
+          promise(.success(responseDTO.result))
+        }.store(in: &subscriptions)
     }
   }
 }
