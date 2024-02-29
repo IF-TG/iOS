f29b3f5dff75ec6277e7bff07feb6e27cd3bac5f
shcommit <happysh_s2@naver.com>
2024-02-29 23:27:26 +0900

    [Feat] UserInfoRepository에 updateUserNickname(with:) 추가

--- travelPlan/Source/Domain/Interfaces/Repositories/UserInfoRepository.swift
+++ travelPlan/Source/Domain/Interfaces/Repositories/UserInfoRepository.swift
@@ -10,4 +10,5 @@ import Combine
 /// 사용자 정보 CRUD 관련 레포지토리
 protocol UserInfoRepository {
   func isDuplicatedName(with name: String) -> Future<Bool, Never> 
+  func updateUserNickname(with name: String) -> Future<Bool, Never>
 }
