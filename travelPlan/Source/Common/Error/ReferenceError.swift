2614240542312fdbcfc0c8f4243dc5db8db522ca
shcommit <happysh_s2@naver.com>
2024-03-01 00:29:45 +0900

    [Feat] ReferenceError weak, unowned 그밖의 경우로 3개 케이스 구성

--- travelPlan/Source/Common/Error/ReferenceError.swift
+++ travelPlan/Source/Common/Error/ReferenceError.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum ReferenceError: Swift.Error {
+  case weakSelfError
+  case unownedSelfError
+  case other(String)
+}
+
+// MARK: - LocalizedError
+extension ReferenceError: LocalizedError {
+  var errorDescription: String? {
+    switch self {
+    case .weakSelfError:
+      return NSLocalizedString("DEBUG: Weak self reference error occurred", comment: "")
+    case .unownedSelfError:
+      return NSLocalizedString("DEBUG: Unowned self reference error occurred", comment: "")
+    case .other(let description):
+      return "DEBUG: Unexpected reference error occured: \(description)"
+    }
+  }
+}
