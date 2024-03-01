83174099538f973972419931b7b027bcb3fe1f65
seokhyun <letushyun@gmail.com>
2024-03-02 02:33:12 +0900

    [Feat] OAuthType 추가

---
+++ travelPlan/Source/Common/Enum/OAuthType.swift
@@ -0,0 +1,15 @@
+//
+//  OAuthType.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 3/2/24.
+//
+
+import Foundation
+
+enum OAuthType: String {
+  case apple
+  case kakao
+  case naver
+  case google
+}
