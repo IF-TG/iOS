7067b92463da016b19c03ed18bab728930e1541e
shcommit <happysh_s2@naver.com>
2024-03-03 03:18:20 +0900

    [Test] EndpointTest. 새롭게 반영된 Parameters테스트. 두개의 param인 경우 AbsoluteURL, httpbody 값 잘 담겼는지 검증

--- travelPlan/Source/Core/Network/Tests/EndpointTests.swift
+++ travelPlan/Source/Core/Network/Tests/EndpointTests.swift
@@ -99,6 +99,7 @@ extension EndpointTests {
     wait(for: [expectation], timeout: 10)
   }
   
+  /// ex) body Parameter :  { profile: String }  + query Parameter : { userId :  long  } 이 두개가 한 요청에 url에 담겨야 할 경우에 대한 테스트입니다.
   func testEndPoint_makeRequest할때_두개의Parameters가_사용될_경우_AbsoluteURL이_잘_만들어지는지_shouldReturnEqaul() {
     // Arrange
     // 이 객체가 url의 query param에 담겨야 합니다.
@@ -130,5 +131,40 @@ extension EndpointTests {
     XCTAssertNotNil(dataRequest?.convertible.urlRequest, "DataRequest의 urlRequest를 반환해야하는데 nil반환")
     XCTAssertEqual(
       dataRequest?.convertible.urlRequest?.url, targetURL)
+  }
+  
+  /// ex) body Parameter :  { profile: String }  + query Parameter : { userId :  long  } 이 두개가 한 요청에 url에 담겨야 할 경우에 대한 테스트입니다.
+  func testEndPoint_makeRequest할때_두개의Parameters가_사용될_경우_bodyParam이_httpBody에_잘_추가되는지_shouldReturnEqual() {
+    // Arrange
+    // 이 객체가 url의 query param에 담겨야 합니다.
+    struct TempQueryParameterReqeustDTO: Encodable {
+      // 만약 사용자 액세스 토큰을 HTTP header말고 query param에 같이 보내야 한다면?
+      let accessToken: String
+    }
+    
+    let mockReqeustQueryParamDTO = TempQueryParameterReqeustDTO(accessToken: "ab1@2")
+    mockRequestModel = UserNameRequestDTO(name: "nice", id: 777)
+    let expectedJsonString = "id=\(mockRequestModel.id)&name=\(mockRequestModel.name)"
+    sut = Endpoint(
+      scheme: "http",
+      host: "test.com",
+      method: .post,
+      parameters: [.query(mockReqeustQueryParamDTO), .body(mockRequestModel)],
+      requestType: .custom("user/name-update"))
+    var dataRequest: DataRequest?
+    
+    // Act
+    DispatchQueue.global().async { [unowned self] in
+      dataRequest = try? sut.makeRequest(from: mockSession)
+      expectation.fulfill()
+    }
+    wait(for: [expectation], timeout: 10)
+    
+    // Assert
+    XCTAssertNotNil(dataRequest, "DataRequest를 반환해야하는데 nil반환")
+    XCTAssertNotNil(dataRequest?.convertible.urlRequest?.httpBody, "httpBody에 값이 담겨야 하는데 nil 반환")
+    let testData = dataRequest!.convertible.urlRequest!.httpBody!
+    let testString = String(data: testData, encoding: .utf8) ?? "no"
+    XCTAssertEqual(testString, expectedJsonString)
   }
 }
