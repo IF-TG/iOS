f1d5c905738b05c0253872dde6d8e9938484700b
seokhyun <letushyun@gmail.com>
2024-03-03 20:31:32 +0900

    [Feat] DefaultLoginRepository 구현
    인가코드를 백엔드로 요청하고, access token과 refresh token을 받아오는 로직 구현

--- travelPlan/Source/Data/Repositories/DefaultLoginRepository.swift
+++ travelPlan/Source/Data/Repositories/DefaultLoginRepository.swift
@@ -7,9 +7,39 @@
 
 import Combine
 
-final class DefaultLoginRepository: LoginRepository {
-  func fetchAuthToken(authCode: String, identityToken: String) -> AnyPublisher<AuthToken, Never> {
-    let requestDTO = LoginRequestDTO(authCode: authCode, identityToken: identityToken)
-    
+final class DefaultLoginRepository {
+  // MARK: - Properties
+  private let session: Sessionable
+  private var subscriptions = Set<AnyCancellable>()
+  private let keyChainManager: KeyChainManager
+  
+  // MARK: - LifeCycle
+  init(session: Sessionable, keyChainManager: KeyChainManager) {
+    self.session = session
+    self.keyChainManager = keyChainManager
+  }
+}
+
+// MARK: - LoginRepository
+extension DefaultLoginRepository: LoginRepository {
+  func fetchAuthToken(authCode: String, identityToken: String) -> AnyPublisher<Bool, Never> {
+    return Future { promise in
+      let requestDTO = LoginRequestDTO(authCode: authCode, identityToken: identityToken)
+      let endpoint = LoginAPIEndPoints.getAppleAuthToken(requestDTO: requestDTO)
+      self.session
+        .request(endpoint: endpoint)
+        .sink { completion in
+          if case let .failure(error) = completion {
+            print("DEBUG: \(error.localizedDescription)")
+            promise(.success(false))
+          }
+        } receiveValue: { [weak self] response in
+          guard let self = self else { return }
+          keyChainManager.addToken(response.accessToken, forKey: .accessToken)
+          keyChainManager.addToken(response.refreshToken, forKey: .refreshToken)
+          promise(.success(true))
+        }
+        .store(in: &self.subscriptions)
+    }.eraseToAnyPublisher()
   }
 }
