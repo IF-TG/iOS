670285209ded72e6f6521fc3e8f660a88cf9e028
shcommit <happysh_s2@naver.com>
2024-03-06 02:22:50 +0900

    [Feat] TravelRegoin에 toDTO() 추가

--- travelPlan/Source/Domain/Entities/TravelCategory/TravelRegion.swift
+++ travelPlan/Source/Domain/Entities/TravelCategory/TravelRegion.swift
@@ -17,12 +17,54 @@ enum TravelRegion: String, CaseIterable {
   case ulsan = "울산"
   case sejong = "세종"
   case gyeonggido = "경기도"
-  case chungcheongukdo = "충청북도"
+  case chungcheongbukdo = "충청북도"
   case chungcheongnamdo = "충청남도"
   case jeollabukdo = "전라북도"
-  case jeollanam_do = "전라남도"
+  case jeollanamdo = "전라남도"
   case gyeongsangbukdo = "경상북도"
   case gyeongsangnamdo = "경상남도"
   case gangwonSpecialSelfGoverningProvince = "강원특별자치도"
   case jejuSpecialSelfGoverningProvince = "제주특별자치도"
+}
+
+// MARK: - Mappings toDTO
+extension TravelRegion {
+  func toDTO() -> String {
+    return switch self {
+    case .seoul:
+      "SEOUL"
+    case .busan:
+      "BUSAN"
+    case .incheon:
+      "INCHEON"
+    case .daegu:
+      "DAEGU"
+    case .gwangju:
+      "GWANGJU"
+    case .daejeon:
+      "DAEJEON"
+    case .ulsan:
+      "ULSAN"
+    case .sejong:
+      "SEJONG"
+    case .gyeonggido:
+      "GYEONGGI"
+    case .chungcheongbukdo:
+      "CHUNGBUK"
+    case .chungcheongnamdo:
+      "CHUNGNAM"
+    case .jeollabukdo:
+      "JEONBUK"
+    case .jeollanamdo:
+      "JEONNAM"
+    case .gyeongsangbukdo:
+      "GYEONGBUK"
+    case .gyeongsangnamdo:
+      "GYEONGNAM"
+    case .gangwonSpecialSelfGoverningProvince:
+      "GANGWON"
+    case .jejuSpecialSelfGoverningProvince:
+      "JEJU"
+    }
+  }
 }
