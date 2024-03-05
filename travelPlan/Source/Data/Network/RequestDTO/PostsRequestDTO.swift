32b0867a14643451d11a257fa1a396c33275d22e
shcommit <happysh_s2@naver.com>
2024-03-06 04:08:18 +0900

    [Feat] PostsRequestDTO 구현

--- travelPlan/Source/Data/Network/RequestDTO/PostsRequestDTO.swift
+++ travelPlan/Source/Data/Network/RequestDTO/PostsRequestDTO.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct PostsRequestDTO: Encodable {
+  let page: Int32
+  let perPage: Int32
+  let orderMethod: String
+  let mainCategory: String
+  let subCategory: String
+  let userId: Int64
+}
