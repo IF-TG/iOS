8d18d181e8f18fa25f1c9cbbe684517f2bdcf6c7
shcommit <happysh_s2@naver.com>
2024-03-06 04:05:35 +0900

    [Feat] PostContainerResponseDTO 구현

--- travelPlan/Source/Data/Network/ResponseDTO/PostContainerResponseDTO.swift
+++ travelPlan/Source/Data/Network/ResponseDTO/PostContainerResponseDTO.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct PostContainerResponseDTO: Decodable {
+  let post: PostResponseDTO
+  let thumbnails: [String]
+  
+  enum CodingKeys: String, CodingKey {
+    case post
+    case thumbnails = "thumbnailUri"
+  }
+  
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    self.post = try container.decode(PostResponseDTO.self, forKey: .post)
+    self.thumbnails = try container.decode([String].self, forKey: .thumbnails)
+  }
+}
