1102d86c5f29ae50ff965f85c09d039c4c04401e
shcommit <happysh_s2@naver.com>
2024-03-06 03:04:19 +0900

    [Feat] TravelTheme에 RawRepresentation 추가

--- travelPlan/Source/Domain/Entities/TravelCategory/TravelTheme.swift
+++ travelPlan/Source/Domain/Entities/TravelCategory/TravelTheme.swift
@@ -16,6 +16,28 @@ import Foundation
   case festivals = "축제"
 }
 
+// MARK: - RawRepresentable
+extension TravelTheme: RawRepresentable {
+  init?(rawValue: String) {
+    switch rawValue {
+    case "REST":
+      self = .relaxation
+    case "SHOPPING":
+      self = .shopping
+    case "CAMPING_GLAMPING":
+      self = .campingGlamping
+    case "ADVENTURE":
+      self = .adventure
+    case "LOCAL_EXPERIENCE":
+      self = .local
+    case "FESTIVAL":
+      self = .festivals
+    default:
+        return nil
+    }
+  }
+}
+
 // MARK: - Mappings toDTO
 extension TravelTheme {
   func toDTO() -> String {
