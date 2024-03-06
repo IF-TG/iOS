61d14b700c4c4c373351622114b0d2551bf69d8f
shcommit <happysh_s2@naver.com>
2024-03-07 02:11:15 +0900

    [Fix] travelRegion의 RawRepresentable, toDTO() 제거. TravelRegionMapper가 역할 담당

--- travelPlan/Source/Data/Network/DataMapper/TravelRegionMapper.swift
+++ travelPlan/Source/Data/Network/DataMapper/TravelRegionMapper.swift
@@ -6,3 +6,85 @@
 //
 
 import Foundation
+
+struct TravelRegionMapper {
+  static func fromDTO(_ dto: String) -> TravelRegion? {
+    return switch dto {
+    case "SEOUL":
+        .seoul
+    case "BUSAN":
+        .busan
+    case "INCHEON":
+        .incheon
+    case "DAEGU":
+        .daegu
+    case "GWANGJU":
+        .gwangju
+    case "DAEJEON":
+        .daejeon
+    case "ULSAN":
+        .ulsan
+    case "SEJONG":
+        .sejong
+    case "GYEONGGI":
+        .gyeonggido
+    case "CHUNGBUK":
+        .chungcheongbukdo
+    case "CHUNGNAM":
+        .chungcheongnamdo
+    case "JEONBUK":
+        .jeollabukdo
+    case "JEONNAM":
+        .jeollanamdo
+    case "GYEONGBUK":
+        .gyeongsangbukdo
+    case "GYEONGNAM":
+        .gyeongsangnamdo
+    case "GANGWON":
+        .gangwonSpecialSelfGoverningProvince
+    case "JEJU":
+        .jejuSpecialSelfGoverningProvince
+    default:
+      nil
+    }
+  }
+  
+  static func toDTO(_ theme: TravelRegion) -> String {
+    return switch theme {
+    case .seoul:
+      "SEOUL"
+    case .busan:
+      "BUSAN"
+    case .incheon:
+      "INCHEON"
+    case .daegu:
+      "DAEGU"
+    case .gwangju:
+      "GWANGJU"
+    case .daejeon:
+      "DAEJEON"
+    case .ulsan:
+      "ULSAN"
+    case .sejong:
+      "SEJONG"
+    case .gyeonggido:
+      "GYEONGGI"
+    case .chungcheongbukdo:
+      "CHUNGBUK"
+    case .chungcheongnamdo:
+      "CHUNGNAM"
+    case .jeollabukdo:
+      "JEONBUK"
+    case .jeollanamdo:
+      "JEONNAM"
+    case .gyeongsangbukdo:
+      "GYEONGBUK"
+    case .gyeongsangnamdo:
+      "GYEONGNAM"
+    case .gangwonSpecialSelfGoverningProvince:
+      "GANGWON"
+    case .jejuSpecialSelfGoverningProvince:
+      "JEJU"
+    }
+  }
+}
