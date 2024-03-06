81a662df680e483b7ea9a03f865c1981f469bcbb
shcommit <happysh_s2@naver.com>
2024-03-07 02:20:06 +0900

    [Fix] TravelMainThemeType에서 toDTO()제거. TravelMainThemeTypeMapper.toDTO가 담당

--- travelPlan/Source/Data/Network/DataMapper/TravelMainThemeTypeMapper.swift
+++ travelPlan/Source/Data/Network/DataMapper/TravelMainThemeTypeMapper.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct TravelMainThemeTypeMapper {
+  static func toDTO(_ requestValue: TravelMainThemeType) -> String {
+    return switch requestValue {
+    case .all:
+      ""
+    case .season(let season):
+      SeasonMapper.toDTO(season)
+    case .region(let region):
+      TravelRegionMapper.toDTO(region)
+    case .travelTheme(let theme):
+      TravelThemeMapper.toDTO(theme)
+    case .partner(let partner):
+      TravelPartnerMapper.toDTO(partner)
+    case .categoryDevelop:
+      ""
+    }
+  }
+}
