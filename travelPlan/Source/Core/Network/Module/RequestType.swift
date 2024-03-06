b8f6fcf37d4b368f5b7396cff245bbbf894a719e
shcommit <happysh_s2@naver.com>
2024-03-07 02:50:18 +0900

    [Feat] RequestType에 Post enum 추가 및 path 추가.

--- travelPlan/Source/Core/Network/Module/RequestType.swift
+++ travelPlan/Source/Core/Network/Module/RequestType.swift
@@ -11,6 +11,7 @@ enum RequestType {
   case none
   /// 사용자 이름 중복 체크와 사용자 이름 업데이트 두 개의 로직에서 사용중
   case userNameDuplicateCheck
+  case post(Post)
   case custom(String)
   
   var path: String {
@@ -19,8 +20,24 @@ enum RequestType {
       return ""
     case .userNameDuplicateCheck:
       return "nickname"
+    case .post(let post):
+      return post.path
     case .custom(let requestPath):
       return requestPath
+    }
+  }
+}
+
+// MARK: - Nested
+extension RequestType {
+  enum Post {
+    case postsFetch
+    
+    var path: String {
+      switch self {
+      case .postsFetch:
+         "posts"
+      }
     }
   }
 }
