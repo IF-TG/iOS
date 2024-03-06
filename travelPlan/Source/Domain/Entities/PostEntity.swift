c832d584a322059e42b7c2e80b5d46af041e8646
shcommit <happysh_s2@naver.com>
2024-03-07 02:43:45 +0900

    [Feat] PostEntity 구현

--- travelPlan/Source/Domain/Entities/PostEntity.swift
+++ travelPlan/Source/Domain/Entities/PostEntity.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+struct PostEntity {
+  let liked: Bool
+  let sort: Bool
+  let post: Post
+  let author: Author
+}
+
+// MARK: - Nested
+extension PostEntity {
+  struct Post {
+    let postID: Int64
+    let title: String
+    let thumbnails: [Thumbnail]
+    let content: String
+    let likes: Int32
+    let comments: Int32
+    let location: Location
+    let createAt: String
+    let tripDate: TripDate
+  }
+  
+  struct Thumbnail {
+    let imageUri: String
+    let sort: Int32
+  }
+  
+  struct Author {
+    let profileUri: String
+    let nickname: String
+  }
+  
+  struct TripDate {
+    let start: String
+    let end: String
+  }
+  
+  struct Category {
+    let themes: [TravelTheme]
+    let regions: [TravelRegion]
+    let seasons: [Season]
+    let partners: [TravelPartner]
+  }
+  
+  struct Location {
+    let x: Double
+    let y: Double
+  }
+}
