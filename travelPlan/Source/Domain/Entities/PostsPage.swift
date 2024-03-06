b04aebf291cd7dd4a845e38433de98db23bb8671
shcommit <happysh_s2@naver.com>
2024-03-07 03:04:44 +0900

    [Feat] PostsPage 추가

--- travelPlan/Source/Domain/Entities/PostsPage.swift
+++ travelPlan/Source/Domain/Entities/PostsPage.swift
@@ -1,8 +1,23 @@
 //
-//  PostPage.swift
+//  PostsPage.swift
 //  travelPlan
 //
 //  Created by 양승현 on 3/7/24.
 //
 
 import Foundation
+
+struct PostsPage {
+  let page: Int
+  let perPage: Int
+  let category: Category
+  let posts: [Post]
+}
+
+// MARK: - Nested
+extension PostsPage {
+  struct Category {
+    let mainTheme: TravelMainThemeType
+    let orderBy: TravelOrderType
+  }
+}
