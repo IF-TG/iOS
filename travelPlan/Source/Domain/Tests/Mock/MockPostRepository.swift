641631db788115d5f04e21450ba992dab6f4dcba
shcommit <happysh_s2@naver.com>
2024-03-08 03:39:10 +0900

    [Feat] MockPostRepository 구현 및 fetchPosts(with:) 정의, mockData 파싱하도록 구현

---
+++ travelPlan/Source/Domain/Tests/Mock/MockPostRepository.swift
@@ -0,0 +1,43 @@
+//
+//  MockPostRepository.swift
+//  travelPlan
+//
+//  Created by 양승현 on 3/8/24.
+//
+
+import Combine
+import Foundation
+
+final class MockPostRepository: PostRepository {
+  let mockService = SessionProvider(session: MockSession.default)
+  var subscription = Set<AnyCancellable>()
+  
+  typealias Endpoint = PostAPIEndpoint
+  
+  func fetchPosts(with page: PostsPage) -> Future<[PostContainer], MainError> {
+    let requestDTO = PostsRequestDTO(
+      page: 0,
+      perPage: 20,
+      orderMethod: "임시",
+      mainCategory: "임시",
+      subCategory: "임시",
+      userId: 3)
+    let endpoint = Endpoint.fetchPosts(with: requestDTO)
+    
+    MockUrlProtocol.requestHandler = { _ in
+      let mockData = MockUrlProtocol.loadMockData(with: .postContainerResponse)
+      return ((HTTPURLResponse(), mockData))
+    }
+    
+    return .init { [unowned self] promise in
+      mockService.request(endpoint: endpoint)
+        .sink { completion in
+          if case .failure(let error) = completion {
+            promise(.failure(MainError.networkError(error)))
+          }
+        } receiveValue: { responseDTO in
+          promise(.success(responseDTO.result.map { $0.toDomain() }))
+        }.store(in: &subscription)
+    }
+  }
+}
