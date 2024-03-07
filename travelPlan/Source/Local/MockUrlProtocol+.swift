5d0c97cb0e515f2608b6bacc27d00d3d1f51f36e
shcommit <happysh_s2@naver.com>
2024-03-08 03:32:54 +0900

    [Feat] MockUrlProtocol extension에 postContainerResponse case 추가 및 path구현

--- travelPlan/Source/Local/MockUrlProtocol+.swift
+++ travelPlan/Source/Local/MockUrlProtocol+.swift
@@ -10,9 +10,13 @@ import Foundation
 extension MockUrlProtocol {
   enum MockResponseDTOType {
     case notice
+    case postContainerResponse
     
     var filePath: String {
-      let dict: [Self: String] = [.notice: "mock_response_notice.json."]
+      let dict: [Self: String] = [
+        .notice: "mock_response_notice.json.",
+        .postContainerResponse: "mock_postContainerResponse"
+      ]
       return dict[self]!
     }
   }
