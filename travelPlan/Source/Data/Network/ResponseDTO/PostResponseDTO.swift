93b6210c8eb581bd35850eec0551968c1f27ff55
shcommit <happysh_s2@naver.com>
2024-03-08 06:15:14 +0900

    [Fix] PostResponseDTO 결국 돌고돌아.. 내부 모델링해서 받는거보다 toDomain으로 받기로,,수정

--- travelPlan/Source/Data/Network/ResponseDTO/PostResponseDTO.swift
+++ travelPlan/Source/Data/Network/ResponseDTO/PostResponseDTO.swift
@@ -9,72 +9,77 @@ import Foundation
 
 struct PostResponseDTO: Decodable {
   // MARK: - Properties
-  let author: Author
-  let category: Category
-  let postDetail: PostDetail
+  let postID: Int64
+  let postImages: [PostImage]
+  let title: String
+  let content: String
+  let likes: Int32
+  let comments: Int32
+  let mapX: Double
+  let mapY: Double
+  let createAt: String
+  let profile: String
+  let nickname: String
+  let startDate: String
+  let endDate: String
+  let themes: [String]
+  let regions: [String]
+  let seasons: [String]
+  let partners: [String]
   let liked: Bool
   
   enum CodingKeys: String, CodingKey {
-    case author
-    case category
-    case postDetail
+    case postID = "postId"
+    case postImages = "postImgUri"
+    case title
+    case content
+    case likes = "likeNum"
+    case comments = "commentNum"
+    case location
+    case createAt
+    case profile = "profileImgUri"
+    case nickname
+    case startDate
+    case endDate
+    case themes
+    case regions
+    case seasons
+    case partners = "companions"
     case liked
+    case mapX
+    case mapY
   }
   
   init(from decoder: Decoder) throws {
     let container = try decoder.container(keyedBy: CodingKeys.self)
-    self.author = try container.decode(PostResponseDTO.Author.self, forKey: .author)
-    self.category = try container.decode(PostResponseDTO.Category.self, forKey: .category)
-    self.postDetail = try container.decode(PostResponseDTO.PostDetail.self, forKey: .postDetail)
+    self.postID = try container.decode(Int64.self, forKey: .postID)
+    self.postImages = try container.decode([PostImage].self, forKey: .postImages)
+    self.title = try container.decode(String.self, forKey: .title)
+    self.content = try container.decode(String.self, forKey: .content)
+    self.likes = try container.decode(Int32.self, forKey: .likes)
+    self.comments = try container.decode(Int32.self, forKey: .comments)
+    self.mapX = try container.decode(Double.self, forKey: .mapX)
+    self.mapY = try container.decode(Double.self, forKey: .mapY)
+    self.createAt = try container.decode(String.self, forKey: .createAt)
+    self.profile = try container.decode(String.self, forKey: .profile)
+    self.nickname = try container.decode(String.self, forKey: .nickname)
+    self.startDate = try container.decode(String.self, forKey: .startDate)
+    self.endDate = try container.decode(String.self, forKey: .endDate)
+    self.themes = try container.decode([String].self, forKey: .themes)
+    self.regions = try container.decode([String].self, forKey: .regions)
+    self.seasons = try container.decode([String].self, forKey: .seasons)
+    self.partners = try container.decode([String].self, forKey: .partners)
     self.liked = try container.decode(Bool.self, forKey: .liked)
   }
 }
 
 // MARK: - Nested
 extension PostResponseDTO {
-  struct PostDetail: Decodable {
-    let postID: Int64
-    let postImages: [PostImage]
-    let title: String
-    let content: String
-    let likes: Int32
-    let comments: Int32
-    let location: Location
-    let createAt: String
-    let tripDate: TripDate
-    
-    enum CodingKeys: String, CodingKey {
-      case postImages
-      case postID = "postId"
-      case title
-      case content
-      case likes = "likeNum"
-      case comments = "commentNum"
-      case location
-      case createAt
-      case tripDate
-    }
-    
-    func toDomain() -> Post.PostDetail {
-      return Post.PostDetail(
-        postID: postID,
-        title: title,
-        postImages: postImages.map{ $0.toDomain() },
-        content: content,
-        likes: likes,
-        comments: comments,
-        location: location.toDomain(),
-        createAt: createAt,
-        tripDate: tripDate.toDomain())
-    }
-
-  }
-  
   struct PostImage: Decodable {
     let image: String
     let sort: Int32
     
-    private enum CodingKeys: String, CodingKey {
+    enum CodingKeys: String, CodingKey {
       case image = "img"
       case sort
     }
@@ -83,60 +88,34 @@ extension PostResponseDTO {
       return .init(imageUri: image, sort: sort)
     }
   }
-  
-  struct Author: Decodable {
-    let profile: String
-    let nickname: String
-    
-    private enum CodingKeys: String, CodingKey {
-      case profile = "profileImgUri"
-      case nickname = "nickname"
-    }
-    
-    func toDomain() -> Post.Author {
-      .init(profileUri: profile, nickname: nickname)
-    }
+}
+
+
+
+// MARK: - Mappings DTO
+extension PostResponseDTO {
+  func toDomain() -> Post.PostDetail {
+    return Post.PostDetail(
+      postID: postID,
+      title: title,
+      postImages: postImages.map { $0.toDomain() },
+      content: content,
+      likes: likes,
+      comments: comments,
+      location: toDomain(),
+      createAt: createAt,
+      tripDate: toDomain())
   }
   
-  struct TripDate: Decodable {
-    let start: String
-    let end: String
-    
-    private enum CodingKeys: String, CodingKey {
-      case start = "startDate"
-      case end = "endDate"
-    }
-    
-    func toDomain() -> Post.TripDate {
-      return .init(start: start, end: end)
-    }
+  func toDomain() -> Post.Author {
+    .init(profileUri: profile, nickname: nickname)
   }
   
-  struct Category: Decodable {
-    let themes: [String]
-    let regions: [String]
-    let seasons: [String]
-    let partners: [String]
-    
-    enum CodingKeys: String, CodingKey {
-      case themes
-      case regions
-      case seasons
-      case partners = "companions"
-    }
+  func toDomain() -> Post.TripDate {
+    return .init(start: startDate, end: endDate)
   }
   
-  struct Location: Decodable {
-    let x: Double
-    let y: Double
-    
-    private enum CodingKeys: String, CodingKey {
-      case x = "mapX"
-      case y = "mapY"
-    }
-    
-    func toDomain() -> Post.Location {
-      return .init(x: x, y: y)
-    }
+  func toDomain() -> Post.Location {
+    return .init(x: mapX, y: mapY)
   }
 }
