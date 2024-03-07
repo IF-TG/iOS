88b52644015a7a336af9b27e681e4c6fcdcd0b77
shcommit <happysh_s2@naver.com>
2024-03-08 02:48:10 +0900

    [Feat] DefaultPostUseCase에 PostUseCase준수 및 fetchPosts(with:) 구현

--- travelPlan/Source/Domain/UseCases/DefaultPostUseCase.swift
+++ travelPlan/Source/Domain/UseCases/DefaultPostUseCase.swift
@@ -5,4 +5,30 @@
 //  Created by 양승현 on 3/8/24.
 //
 
-import Foundation
+import Combine
+
+final class DefaultPostUseCase: PostUseCase {
+  // MARK: - Dependencies
+  private let postRepository: PostRepository
+  
+  // MARK: - Properties
+  var postContainers = PassthroughSubject<[PostContainer], MainError>()
+  
+  private var subscriptions = Set<AnyCancellable>()
+  
+  // MARK: - Lifecycle
+  init(postRepository: PostRepository) {
+    self.postRepository = postRepository
+  }
+  
+  func fetchPosts(with page: PostsPage) {
+    postRepository.fetchPosts(with: page)
+      .sink { [weak self] completion in
+        if case .failure(let error) = completion {
+          self?.postContainers.send(completion: .failure(error))
+        }
+      } receiveValue: { [weak self] result in
+        self?.postContainers.send(result)
+      }.store(in: &subscriptions)
+  }
+}
