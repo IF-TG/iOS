c7d01ac09bc6a8293f9c240fef98796739a5fb10
shcommit <happysh_s2@naver.com>
2024-03-11 02:24:37 +0900

    [Feat] UIViewController+Indicator에 인디케이터 시작, 중지 추가

--- travelPlan/Source/Common/Extension/UIViewController+Indicator.swift
+++ travelPlan/Source/Common/Extension/UIViewController+Indicator.swift
@@ -5,4 +5,33 @@
 //  Created by 양승현 on 3/11/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+  private static var activityIndicatorTag: Int { return 999 }
+  private static var overlayViewTag: Int { return 998 }
+  
+  func startIndicator() {
+    let indicator = UIActivityIndicatorView(style: .large)
+    indicator.center = view.center
+    indicator.tag = UIViewController.activityIndicatorTag
+    view.addSubview(indicator)
+    indicator.startAnimating()
+    
+    let overlayView = UIView(frame: view.bounds)
+    overlayView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+    overlayView.tag = UIViewController.overlayViewTag
+    view.addSubview(overlayView)
+  }
+  
+  func stopIndicator() {
+    if let indicator = view.subviews
+      .filter({ $0.tag == UIViewController.activityIndicatorTag }).first as? UIActivityIndicatorView {
+      indicator.stopAnimating()
+      indicator.removeFromSuperview()
+    }
+    if let overlayView = view.subviews.filter({ $0.tag == UIViewController.overlayViewTag }).first {
+      overlayView.removeFromSuperview()
+    }
+  }
+}
