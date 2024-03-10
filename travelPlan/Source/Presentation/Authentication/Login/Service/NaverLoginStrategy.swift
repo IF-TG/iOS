116190f1a20fd917c34a59477ab853dc75221a65
seokhyun <letushyun@gmail.com>
2024-03-10 17:48:37 +0900

    [Comment] NavarLoginStrategy 잠시 주석처리

---
+++ travelPlan/Source/Presentation/Authentication/Login/Service/NaverLoginStrategy.swift
@@ -0,0 +1,17 @@
+//
+//  NaverLoginStrategy.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 3/10/24.
+//
+
+import Foundation
+import Combine
+
+//final class NaverLoginStrategy: LoginStrategy {
+//  var resultPublisher: PassthroughSubject<AuthenticationResponseValue, AuthenticationServiceError>
+//  
+//  func login() {
+//    <#code#>
+//  }
+//}
