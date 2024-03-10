29e92227ddffc4756e0e9b7373a8e0b2f84b91e0
shcommit <happysh_s2@naver.com>
2024-03-11 02:11:14 +0900

    [Feat] MyInformationVM에 프로필 선택시 데이터 vm에 저장 및 프로필 편집 됬는지 관련 프로퍼티 제거.

--- travelPlan/Source/Presentation/Profile/ViewModel/MyInformationViewModel.swift
+++ travelPlan/Source/Presentation/Profile/ViewModel/MyInformationViewModel.swift
@@ -10,12 +10,9 @@ import Combine
 
 final class MyInformationViewModel {
   struct Input {
-    struct UserEditableElement {
-      let nickname: String?
-      let profileImage: String?
-    }
     let isNicknameDuplicated: PassthroughSubject<String, MainError> = .init()
-    let tapStoreButton: PassthroughSubject<UserEditableElement, MainError> = .init()
+    let selectProfile: PassthroughSubject<String?, MainError> = .init()
+    let tapStoreButton: PassthroughSubject<Void, MainError> = .init()
   }
   
   enum State {
@@ -31,7 +28,8 @@ final class MyInformationViewModel {
   private let userInfoUseCase: UserInfoUseCase
   
   // MARK: - Properties
-  private var hasProfileChanged = false
+  private var editedUserProfileImage: String?
+  private var editedUserNickname: String?
   private var changedNameAvailable = false
   private var isProcessingBothNameAndProfile = false
   
@@ -54,10 +52,12 @@ extension MyInformationViewModel: MyInformationViewModelable {
   func transform(_ input: Input) -> Output {
     let isDuplicatedUserName = isDuplicatedUserNameStream(input: input)
     let checkDuplicatedUserName = checkDuplicatedUserNameStream()
+    let selectProfile = selectProfileStream(input: input)
     
     return Publishers.MergeMany([
       isDuplicatedUserName,
       checkDuplicatedUserName,
+      selectProfileStream(input: input),
       hasNicknameUpdatedStream(),
       hasProfileUpdatedStream(),
       hasBothNameAndProfileUpdatedStream()]
@@ -67,8 +67,16 @@ extension MyInformationViewModel: MyInformationViewModelable {
 
 // MARK: - Private Helpers
 private extension MyInformationViewModel {
+  func selectProfileStream(input: Input) -> Output {
+    return input.selectProfile.map { [weak self] base64Image -> State in
+      self?.editedUserProfileImage = base64Image
+      return .none
+    }.eraseToAnyPublisher()
+  }
+  
   func isDuplicatedUserNameStream(input: Input) -> Output {
     return input.isNicknameDuplicated.map { [weak self] nickname in
+      self?.editedUserNickname = nickname
       self?.userInfoUseCase.checkIfNicknameDuplicate(with: nickname)
       return .networkProcessing
     }.eraseToAnyPublisher()
@@ -77,27 +85,30 @@ private extension MyInformationViewModel {
   func checkDuplicatedUserNameStream() -> Output {
     userInfoUseCase.isNicknameDuplicated.map { [weak self] isDuplicatedUserName -> State in
       self?.changedNameAvailable = isDuplicatedUserName
+      if !isDuplicatedUserName {
+        self?.editedUserNickname = nil
+      }
       return isDuplicatedUserName ? .duplicatedNickname : .availableNickname
     }.eraseToAnyPublisher()
   }
   
   func tapStoreButtonStream(input: Input) -> Output {
     return input.tapStoreButton
-      .map { [weak self] (nickname, image) -> State in
-        if self?.changedNameAvailable == self?.hasProfileChanged {
+      .map { [weak self] _ -> State in
+        if self?.changedNameAvailable == true, self?.editedUserProfileImage != nil {
           self?.isProcessingBothNameAndProfile = true
         }
-        if self?.changedNameAvailable == true, let nickname = nickname {
+        if self?.changedNameAvailable == true, let nickname = self?.editedUserNickname {
           self?.userInfoUseCase.updateNickname(with: nickname)
           self?.changedNameAvailable = false
         }
-        if self?.hasProfileChanged == true, let image = image {
+        if let image = self?.editedUserProfileImage {
           // TODO: - 프로필 처음 저장 여부 파악해야합니다. 두번쨰이상일경우 update 호출해야합니다.
           /// userDefaults에 사용자의 프로필이 서버에 저장되어있는지 최초 확인해야합니다.
           /// 최초로 저장되어있다면, 그 다음부터는 update를 통해서만 (delete -> save) 서버에 추가해야한다고 합니다.
           /// 맨 처음 가입해서 들어올떄 자동으로 최초 한번 기본이미지 저장하는게 편할것 같습니다..
           self?.userInfoUseCase.updateProfile(with: image)
-          self?.hasProfileChanged = false
+          self?.editedUserProfileImage = nil
         }
         return .networkProcessing
       }.eraseToAnyPublisher()
@@ -134,7 +145,7 @@ private extension MyInformationViewModel {
     return bothNameAndProfileUpdatedPublisher
       .map { [weak self] (updatedNameResult, updatedProfileResult) -> State in
         self?.isProcessingBothNameAndProfile = false
-        if updatedNameResult == updatedNameResult {
+        if updatedNameResult == updatedProfileResult {
           return .correctionSaved
         }
         return .correctionNotSaved
