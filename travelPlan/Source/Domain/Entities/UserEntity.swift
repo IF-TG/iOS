2529c6181634ba3fc72d6568b88b2179ee54f14d
shcommit <happysh_s2@naver.com>
2024-03-12 01:46:19 +0900

    [Feat] UserEntity 생성 및 추가

---
+++ travelPlan/Source/Domain/Entities/UserEntity.swift
@@ -0,0 +1,18 @@
+//
+//  UserEntity.swift
+//  travelPlan
+//
+//  Created by 양승현 on 3/12/24.
+//
+
+import Foundation
+
+struct UserEntity {
+  let id: String
+  let nickname: String
+  let profileUrl: String?
+  /// 프로필을 변경하기 전에 최초 프로필이 저장되어 있는지 여부를 파악해야합니다.
+  /// 프로필이 저장되어 있지 않은 경우 UserInfoUseCase -> saveProfile.
+  /// 프로필이 최초 한번 서버에 저장된 이후 UserInfoUseCase -> updateProfile
+  let isSavedProfile: Bool
+}
