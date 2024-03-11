7c84848c58250627d8f4d46a60bab4b5ee43a70b
shcommit <happysh_s2@naver.com>
2024-03-12 03:07:31 +0900

    [Feat] UserEntity 추가

--- travelPlan/Source/Domain/Entities/UserEntity.swift
+++ travelPlan/Source/Domain/Entities/UserEntity.swift
@@ -9,10 +9,10 @@ import Foundation
 
 struct UserEntity {
   let id: String
-  let nickname: String
-  let profileUrl: String?
+  var nickname: String
+  var profileURL: String?
   /// 프로필을 변경하기 전에 최초 프로필이 저장되어 있는지 여부를 파악해야합니다.
   /// 프로필이 저장되어 있지 않은 경우 UserInfoUseCase -> saveProfile.
   /// 프로필이 최초 한번 서버에 저장된 이후 UserInfoUseCase -> updateProfile
-  let isSavedProfile: Bool
+  var isSavedProfile: Bool
 }
