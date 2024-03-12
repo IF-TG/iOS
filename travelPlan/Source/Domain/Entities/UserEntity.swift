a69c06535f0bd30589bf65ce5fdf7683bfccd39e
shcommit <happysh_s2@naver.com>
2024-03-13 00:55:12 +0900

    [Style] UserEntity에 isSavedProfile -> InServer 단어 추가

--- travelPlan/Source/Domain/Entities/UserEntity.swift
+++ travelPlan/Source/Domain/Entities/UserEntity.swift
@@ -14,5 +14,5 @@ struct UserEntity {
   /// 프로필을 변경하기 전에 최초 프로필이 저장되어 있는지 여부를 파악해야합니다.
   /// 프로필이 저장되어 있지 않은 경우 UserInfoUseCase -> saveProfile.
   /// 프로필이 최초 한번 서버에 저장된 이후 UserInfoUseCase -> updateProfile
-  var isSavedProfile: Bool
+  var isSavedProfileInServer: Bool
 }
