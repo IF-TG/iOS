054db82ef446fab88de89ca3344f34c6d7603f2d
seokhyun <letushyun@gmail.com>
2024-03-13 05:05:11 +0900

    [Style] 네이버 관련 DTO 잠시 주석처리

---
+++ travelPlan/Source/Data/Network/ResponseDTO/NaverLoginFirstResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  NaverLoginFirstResponseDTO.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 3/11/24.
+//
+
+//import Foundation
+//
+//struct NaverLoginFirstResponseDTO: Decodable {
+//  let redirectURL: String
+//}
