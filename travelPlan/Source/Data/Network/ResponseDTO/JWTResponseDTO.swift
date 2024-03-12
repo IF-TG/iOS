826f4199d1504c2deb4d7db0b40c3c0c5fb22520
seokhyun <letushyun@gmail.com>
2024-03-13 04:55:25 +0900

    [Feat] JWTDTO 추가

---
+++ travelPlan/Source/Data/Network/ResponseDTO/JWTResponseDTO.swift
@@ -0,0 +1,15 @@
+//
+//  JWTResponseDTO.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 3/13/24.
+//
+
+import Foundation
+
+struct JWTResponseDTO: Decodable {
+  let accessToken: String
+  let refreshToken: String
+  let accessTokenExpiresIn: String
+  let refreshTokenExpiresIn: String
+}
