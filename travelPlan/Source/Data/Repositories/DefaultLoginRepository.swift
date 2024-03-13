e960c67bf98b247f8641b376f60da9c52be86aae
seokhyun <letushyun@gmail.com>
2024-03-13 12:17:44 +0900

    [Feat] 불필요 코드 정리 및 네이버 로직 주석처리

--- travelPlan/Source/Data/Repositories/DefaultLoginRepository.swift
+++ travelPlan/Source/Data/Repositories/DefaultLoginRepository.swift
@@ -7,14 +7,16 @@
 
 import Combine
 
-final class DefaultLoginRepository {
+final class DefaultLoginRepository: LoginRepository {
   // MARK: - Properties
   private var subscriptions = Set<AnyCancellable>()
-  private let authService: AuthenticationService
-//  private let 
+  let authService: AuthenticationService
+  let loginResponseStorage: LoginResponseStorage
+
   // MARK: - LifeCycle
-  init(authService: AuthenticationService) {
+  init(authService: AuthenticationService, loginResponseStorage: LoginResponseStorage) {
     self.authService = authService
+    self.loginResponseStorage = loginResponseStorage
   }
   
   deinit {
@@ -23,14 +25,14 @@ final class DefaultLoginRepository {
 }
 
 // MARK: - LoginRepository
-extension DefaultLoginRepository: LoginRepository {
+extension DefaultLoginRepository {
   func performLogin(type: OAuthType) -> AnyPublisher<Bool, MainError> {
     switch type {
     case .apple:
       authService.setLoginStrategy(AppleLoginStrategy())
       authService.performLogin()
         .map { [weak self] jwtDTO in
-          // TODO: - Bool 타입을 반환해야합니다.
+          // TODO: - keychainStorage에서 Bool 타입을 반환해야합니다.
           KeychainManager.shared.add(
             key: KeychainKey.accessToken.rawValue,
             value: jwtDTO.accessToken.data(using: .utf8)
@@ -41,59 +43,23 @@ extension DefaultLoginRepository: LoginRepository {
           )
         }
         .eraseToAnyPublisher()
-//        .sink { completion in
-//          if case let .failure(error) = completion {
-//            print("auth error: \(error)")
-//          }
-//        } receiveValue: { [weak self] jwtDTO in
-//          // TODO: - keychainService에 jwtDTO를 넣어서 로직을 실행하고 성공여부를 반환해야합니다.
-//          KeychainManager.shared.add(
-//            key: KeychainKey.accessToken.rawValue,
-//            value: jwtDTO.accessToken.data(using: .utf8)
-//          )
-//          KeychainManager.shared.add(
-//            key: KeychainKey.refreshToken.rawValue,
-//            value: jwtDTO.refreshToken.data(using: .utf8)
-//          )
-//        }.store(in: &subscriptions)
     }
   }
   
   // 1. ios->back, back->ios
   // 2. ios->naver, naver->ios
   // 3. ios->back, back->ios
-  func performNaverLogin() {
-    authService.setLoginStrategy(NaverLoginStrategy())
-    authService
-      .performLogin()
-      .sink { completion in
-        if case let .failure(error) = completion {
-          print("auth error: \(error)")
-        }
-      } receiveValue: { responseValue in
-        
-      }
-      .store(in: &subscriptions)
-  }
-  
-  func fetchAuthToken(authCode: String, identityToken: String) -> AnyPublisher<Bool, MainError> {
-    return Future<Bool, MainError> { promise in
-      let requestDTO = LoginRequestDTO(authCode: authCode, identityToken: identityToken)
-      let endpoint = LoginAPIEndPoints.getAppleAuthToken(requestDTO: requestDTO)
-      self.session
-        .request(endpoint: endpoint)
-        .map { $0.result }
-        .sink { completion in
-          if case let .failure(error) = completion {
-            print("DEBUG: \(error.localizedDescription)")
-            promise(.failure(.networkError(error)))
-          }
-        } receiveValue: { responseDTO in
-          
-          )
-          promise(.success(true))
-        }
-        .store(in: &self.subscriptions)
-    }.eraseToAnyPublisher()
-  }
+//  func performNaverLogin() {
+//    authService.setLoginStrategy(NaverLoginStrategy())
+//    authService
+//      .performLogin()
+//      .sink { completion in
+//        if case let .failure(error) = completion {
+//          print("auth error: \(error)")
+//        }
+//      } receiveValue: { responseValue in
+//        
+//      }
+//      .store(in: &subscriptions)
+//  }
 }
