328c8d76cf3c93af7ea58fdfe2752b529ccb03da
seokhyun <letushyun@gmail.com>
2024-03-14 15:46:22 +0900

    [Add] UserDefaultsLoginResultStorage 추가

---
+++ travelPlan/Source/Data/PersistentStorages/LoginResultStorage/UserDefaultsLoginResultStorage.swift
@@ -0,0 +1,14 @@
+//
+//  UserDefaultsLoginResultStorage.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 3/14/24.
+//
+
+import Foundation
+
+final class UserDefaultsLoginResultStorage: LoginResultStorage {
+  func save() -> Bool {
+    <#code#>
+  }
+}
