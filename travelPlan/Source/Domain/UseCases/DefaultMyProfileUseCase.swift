d614af951199bb4965f24b666b65967ae8e09120
shcommit <happysh_s2@naver.com>
2024-03-19 02:21:47 +0900

    [Rebuild] DefaultMyProfileUseCase's 함수 반환타입 AnyPublihser로 변경.

--- travelPlan/Source/Domain/UseCases/DefaultMyProfileUseCase.swift
+++ travelPlan/Source/Domain/UseCases/DefaultMyProfileUseCase.swift
@@ -8,22 +8,26 @@
 import Foundation
 import Combine
 
+// MARK: - Publisher extension
+private extension Publisher {
+  func mapMyProfileUseCaseError<E>(
+    _ transform: @escaping (Self.Failure) -> E
+  ) -> Publishers.MapError<Self, Error> {
+    return self.mapError { error -> Error in
+      if let connectionError = error.asAFError?.mapConnectionError {
+        return MyProfileRepositoryError.networkError(connectionError)
+      }
+      return MyProfileRepositoryError.unknown(description: error.localizedDescription)
+    }
+  }
+}
+
 final class DefaultMyProfileUseCase: MyProfileUseCase {
   // MARK: - Dependencies
   private let myProfileRepository: MyProfileRepository
   
   // MARK: - Properties
-  var isNicknameDuplicated = PassthroughSubject<Bool, Error>()
-  
-  var isNicknameUpdated = PassthroughSubject<Bool, Error>()
-  
-  var isProfileUpdated = PassthroughSubject<Bool, Error>()
-  
-  var isProfileSaved = PassthroughSubject<Bool, Error>()
-  
-  var isProfileDeleted = PassthroughSubject<Bool, Error>()
-  
-  var fetchedProfile = PassthroughSubject<ProfileImageEntity, Error>()
+  var subscription: AnyCancellable?
   
   var isProfileSavedInServer: Bool {
     myProfileRepository.isProfileSavedInServer
@@ -36,78 +40,39 @@ final class DefaultMyProfileUseCase: MyProfileUseCase {
     self.myProfileRepository = myProfileRepository
   }
   
-  func checkIfNicknameDuplicate(with name: String) {
-    myProfileRepository.checkIfUserNicknameDuplicate(with: name)
-      .sink { [weak self] completion in
-        switch completion {
-        case .finished:
-          break
-        case .failure(let error):
-          self?.isNicknameDuplicated.send(completion: .failure(error))
-        }
-      } receiveValue: { [weak self] result in
-        self?.isNicknameDuplicated.send(result)
-      }.store(in: &subscriptions)
+  func checkIfNicknameDuplicate(with name: String) -> AnyPublisher<Bool, any Error> {
+    return myProfileRepository.checkIfUserNicknameDuplicate(with: name)
+      .mapError { $0 }
+      .eraseToAnyPublisher()
   }
   
-  func updateNickname(with name: String) {
-    myProfileRepository.updateUserNickname(with: name)
-      .sink { [weak self] completion in
-        switch completion {
-        case .finished:
-          return
-        case .failure(let error):
-          self?.isNicknameUpdated.send(completion: .failure(error))
-        }
-      } receiveValue: { [weak self] result in
-        self?.isNicknameUpdated.send(result)
-      }.store(in: &subscriptions)
+  func updateNickname(with name: String) -> AnyPublisher<Bool, Error> {
+    return myProfileRepository.updateUserNickname(with: name)
+      .mapError { $0 }
+      .eraseToAnyPublisher()
   }
   
-  func updateProfile(with base64String: String) {
-    myProfileRepository.updateProfile(with: base64String)
-      .receive(on: DispatchQueue.main)
-      .sink { [weak self] completion in
-        if case .failure(let error) = completion {
-          self?.isProfileUpdated.send(completion: .failure(error))
-        }
-      } receiveValue: { [weak self] result in
-        self?.isProfileUpdated.send(result)
-      }.store(in: &subscriptions)
+  func updateProfile(with base64String: String) -> AnyPublisher<Bool, Error> {
+    return myProfileRepository.updateProfile(with: base64String)
+      .mapError { $0 }
+      .eraseToAnyPublisher()
   }
   
-  func saveProfile(with base64String: String) {
-    myProfileRepository.saveProfile(with: base64String)
-      .receive(on: DispatchQueue.main)
-      .sink { [weak self] completion in
-        if case .failure(let error) = completion {
-          self?.isProfileSaved.send(completion: .failure(error))
-        }
-      } receiveValue: { [weak self] result in
-        self?.isProfileSaved.send(result)
-      }.store(in: &subscriptions)
+  func saveProfile(with base64String: String) -> AnyPublisher<Bool, Error> {
+    return myProfileRepository.saveProfile(with: base64String)
+      .mapError { $0 }
+      .eraseToAnyPublisher()
   }
   
-  func deleteProfile() {
+  func deleteProfile() -> AnyPublisher<Bool, Error> {
     myProfileRepository.deleteProfile()
-      .receive(on: DispatchQueue.main)
-      .sink { [weak self] completion in
-        if case .failure(let error) = completion {
-          self?.isProfileDeleted.send(completion: .failure(error))
-        }
-      } receiveValue: { [weak self] result in
-        self?.isProfileDeleted.send(result)
-      }.store(in: &subscriptions)
+      .mapError { $0 }
+      .eraseToAnyPublisher()
   }
   
-  func fetchProfile() {
+  func fetchProfile() -> AnyPublisher<ProfileImageEntity, any Error> {
     myProfileRepository.fetchProfile()
-      .receive(on: DispatchQueue.main)
-      .sink { [weak self] completion in
-        guard case .failure(let error) = completion else { return }
-        self?.fetchedProfile.send(completion: .failure(error))
-      } receiveValue: { [weak self] entity in
-        self?.fetchedProfile.send(entity)
-      }.store(in: &subscriptions)
+      .mapError { $0 }
+      .eraseToAnyPublisher()
   }
 }
