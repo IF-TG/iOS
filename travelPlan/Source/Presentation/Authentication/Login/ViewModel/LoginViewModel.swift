2d87dbbca44a38dcb7663c1f9960a731135154c3
shcommit <happysh_s2@naver.com>
2024-03-19 02:39:28 +0900

    [Style] 머지? 에러 제거

--- travelPlan/Source/Presentation/Authentication/Login/ViewModel/LoginViewModel.swift
+++ travelPlan/Source/Presentation/Authentication/Login/ViewModel/LoginViewModel.swift
@@ -55,28 +55,17 @@ private extension LoginViewModel {
       .didTapLoginButton
       .flatMap { [weak self] oauthType in
         guard let self = self else { return Just(State.none).eraseToAnyPublisher() }
-        
-<<<<<<< HEAD
-        return self.loginUseCase
-          .execute(requestValue: .init(
-            loginType: .apple,
-            authorizationCode: responseValue.authorizationCode,
-            identityToken: responseValue.identityToken))
-          .map { isLoggedIn in
-            // MARK: - 로그인이 완료될 경우 사용자의 정보(아이디, 이름, 프로필이 서버에 저장됬는지 여부, 프로필 이미지)를
-            // 받은 후에 UserDefaultsManager.setUser에 저장 후 메인 화면으로 넘어와야 합니다.
-            // 프로필 최초 저장은 프로필 url이 있는 경우 true로 설정해야합니다.
-            return isLoggedIn ? State.presentFeed : State.none
-=======
         return self.loginUseCase.execute(type: oauthType)
           .receive(on: RunLoop.main)
           .map { isSavedTokenInKeychain in
+            // MARK: - 로그인이 완료될 경우 사용자의 정보(아이디, 이름, 프로필이 서버에 저장됬는지 여부, 프로필 이미지)를
+            // 받은 후에 UserDefaultsManager.setUser에 저장 후 메인 화면으로 넘어와야 합니다.
+            // 프로필 최초 저장은 프로필 url이 있는 경우 true로 설정해야합니다.
             if isSavedTokenInKeychain {
               return .presentFeed
             } else {
               return .none
             }
->>>>>>> 02c74f799dc34ed1ff76ee1c0b7fef6351713e9e
           }
           .catch { error in
             print("error: \(error.localizedDescription)")
