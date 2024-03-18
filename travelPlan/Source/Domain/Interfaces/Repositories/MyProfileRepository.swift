e600bca57301a5090536052234eee2dae37ac318
shcommit <happysh_s2@naver.com>
2024-03-18 11:08:33 +0900

    [Rebuild] MyProfuileRepository에 Error 타입 추가

--- travelPlan/Source/Domain/Interfaces/Repositories/MyProfileRepository.swift
+++ travelPlan/Source/Domain/Interfaces/Repositories/MyProfileRepository.swift
@@ -5,15 +5,21 @@
 //  Created by 양승현 on 2/27/24.
 //
 
+import Foundation
 import Combine
+
+enum MyProfileRepositoryError: LocalizedError {
+  case invaildUserId
+  case networkError(ConnectionError)
+}
 
 /// 사용자 정보 CRUD 관련 레포지토리
 protocol MyProfileRepository {
-  func checkIfUserNicknameDuplicate(with name: String) -> Future<Bool, MainError>
-  func updateUserNickname(with name: String) -> Future<Bool, MainError>
-  func updateProfile(with profile: String) -> Future<Bool, MainError>
-  func saveProfile(with profile: String) -> Future<Bool, MainError>
-  func deleteProfile() -> Future<Bool, MainError>
-  func fetchProfile() -> Future<ProfileImageEntity, MainError>
+  func checkIfUserNicknameDuplicate(with name: String) -> Future<Bool, MyProfileRepositoryError>
+  func updateUserNickname(with name: String) -> Future<Bool, MyProfileRepositoryError>
+  func updateProfile(with profile: String) -> Future<Bool, MyProfileRepositoryError>
+  func saveProfile(with profile: String) -> Future<Bool, MyProfileRepositoryError>
+  func deleteProfile() -> Future<Bool, MyProfileRepositoryError>
+  func fetchProfile() -> Future<ProfileImageEntity, MyProfileRepositoryError>
   var isProfileSavedInServer: Bool { get }
 }
