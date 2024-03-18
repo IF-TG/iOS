1fa45a6c45954af75ca3a42ac05f178d72d67934
shcommit <happysh_s2@naver.com>
2024-03-18 11:19:40 +0900

    [Feat] ConnectionError에 AFError -> ConnectionError map함수 추가

--- travelPlan/Source/Common/Error/ConnectionError.swift
+++ travelPlan/Source/Common/Error/ConnectionError.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Alamofire
 
 enum ConnectionError: LocalizedError {
   case invalidInternetConnection
@@ -17,6 +18,27 @@ enum ConnectionError: LocalizedError {
       NSLocalizedString("인터넷 연결이 유효하지 않습니다. 네트워크 설정을 확인하세요.", comment: "")
     case .unavailableServer:
       NSLocalizedString("서버에 연결할 수 없습니다. 나중에 다시 시도하세요.", comment: "")
+    }
+  }
+}
+
+// MARK: - Helpers
+extension AFError {
+  var mapConnectionError: ConnectionError? {
+    switch self {
+    case .sessionTaskFailed(let error as URLError):
+      if error.code == .notConnectedToInternet {
+        return .invalidInternetConnection
+      }
+    case .responseValidationFailed(let reason):
+      switch reason {
+      case .unacceptableStatusCode(code: 404...599):
+        return .unavailableServer
+      default:
+        return nil
+      }
+    default:
+      return nil
     }
   }
 }
