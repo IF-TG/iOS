8d75d9a2fffcf0486f7853067e34b91ba12f44f8
shcommit <happysh_s2@naver.com>
2024-03-18 12:19:13 +0900

    [Style] MockUserInfo -> MockMyProfile 로 키워드 변경

--- travelPlan/Source/Domain/Tests/Mock/MockMyProfileRepository.swift
+++ travelPlan/Source/Domain/Tests/Mock/MockMyProfileRepository.swift
@@ -10,21 +10,21 @@ import Combine
 final class MockMyProfileRepository: MyProfileRepository {
   var isProfileSavedInServer: Bool = false
   
-  func updateUserNickname(with name: String) -> Future<Bool, MainError> {
+  func updateUserNickname(with name: String) -> Future<Bool, MyProfileRepositoryError> {
     return .init { promise in
       promise(.success(name == "어려운건 정복해나가는 맛이 있는거지"))
     }
   }
   
   /// 만약 사용자가 "토익은 어려워"라는 닉네임을 입력했을 때 가정
-  func checkIfUserNicknameDuplicate(with name: String) -> Future<Bool, MainError> {
+  func checkIfUserNicknameDuplicate(with name: String) -> Future<Bool, MyProfileRepositoryError> {
     if name == "토익은 어려워" {
       return .init { promise in promise(.success(true)) }
     }
     return .init { $0(.success(false))}
   }
   
-  func updateProfile(with profile: String) -> Future<Bool, MainError> {
+  func updateProfile(with profile: String) -> Future<Bool, MyProfileRepositoryError> {
     return .init { promise in
       if profile == "base64인코딩된데이터" {
         promise(.success(true))
@@ -33,17 +33,17 @@ final class MockMyProfileRepository: MyProfileRepository {
     }
   }
   
-  func saveProfile(with profile: String) -> Future<Bool, MainError> {
+  func saveProfile(with profile: String) -> Future<Bool, MyProfileRepositoryError> {
     return .init { promise in
       promise(.success(profile == "성장해나가자보자구!!!"))
     }
   }
   
-  func deleteProfile() -> Future<Bool, MainError> {
+  func deleteProfile() -> Future<Bool, MyProfileRepositoryError> {
     return .init { $0(.success(true)) }
   }
   
-  func fetchProfile() -> Future<ProfileImageEntity, MainError> {
+  func fetchProfile() -> Future<ProfileImageEntity, MyProfileRepositoryError> {
     return .init { $0(.success(ProfileImageEntity(image: "hi"))) }
   }
 }
