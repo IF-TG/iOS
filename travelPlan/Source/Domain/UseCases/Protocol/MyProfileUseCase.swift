7605e23d51d86901159285df26b21c27cf868756
shcommit <happysh_s2@naver.com>
2024-03-19 02:21:11 +0900

    [Feat] MyProfileUseCaseError unknown 타입 추가

--- travelPlan/Source/Domain/UseCases/Protocol/MyProfileUseCase.swift
+++ travelPlan/Source/Domain/UseCases/Protocol/MyProfileUseCase.swift
@@ -11,13 +11,16 @@ import Foundation
 enum MyProfileUseCaseError: LocalizedError {
   case invalidUserId
   case networkError(ConnectionError)
+  case unknown(String)
   
   var errorDescription: String? {
     switch self {
     case .invalidUserId:
-      NSLocalizedString("로그인 정보가 유효하지 않습니다.", comment: "")
+      NSLocalizedString("로그인한 사용자의 정보가 유효하지 않습니다.", comment: "")
     case .networkError(let connectionError):
       connectionError.errorDescription
+    case .unknown(let errorDescription):
+      NSLocalizedString(errorDescription, comment: "")
     }
   }
 }
