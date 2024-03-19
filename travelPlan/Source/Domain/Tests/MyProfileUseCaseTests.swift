e2f7a4fb1609bfd95c27fb72d477e35d8022398e
shcommit <happysh_s2@naver.com>
2024-03-19 11:56:43 +0900

    [Test] MyProfileUseCase tests 새로 반영된 protocol 기반으로 테스트 반영

--- travelPlan/Source/Domain/Tests/MyProfileUseCaseTests.swift
+++ travelPlan/Source/Domain/Tests/MyProfileUseCaseTests.swift
@@ -32,17 +32,20 @@ final class MyProfileUseCaseTests: XCTestCase {
   
   // MARK: - Tests
   func testMyProfileUseCase_checkIfNicknameDuplicate함수를통해_사용자의이름이중복됬는지여부를확인할때_ShouldRetrunTrue() {
-    subscription = sut.isNicknameDuplicated.sink { _ in
-    } receiveValue: { [unowned self] requestedValue in
-      // Assert
-      XCTAssertTrue(requestedValue, "isDuplicatedName 변수 반환값이 true여야 하지만 false 반환")
-      expectation.fulfill()
-    }
+    // Arrange
+    var testResultValue = false
     
     // Act
-    sut.checkIfNicknameDuplicate(with: "토익은 어려워")
-    
+    subscription = sut.checkIfNicknameDuplicate(with: "토익은 어려워")
+      .sink { _ in
+      } receiveValue: { [unowned self] requestedValue in
+        testResultValue = requestedValue
+        expectation.fulfill()
+      }
     wait(for: [expectation], timeout: 5)
+    
+    // Assert
+    XCTAssertTrue(testResultValue, "checkIfNicknameDuplicate 함수 반환 퍼블리셔의 Output이 true여야 하지만 false 반환")
   }
   
   func testMyProfileUseCase_updateNickname함수를통해_이름업데이트할때_이미_다른사용자가_사용중인이름이라면_ShouldReturnFalse() {
@@ -50,18 +53,17 @@ final class MyProfileUseCaseTests: XCTestCase {
     var testResultValue = true
     
     // Act
-    subscription = sut.isNicknameUpdated.sink { _ in
+    subscription = sut.updateNickname(with: "토익은정말어려워").sink { _ in
     } receiveValue: { [unowned self] in
       testResultValue = $0
       expectation.fulfill()
     }
-    sut.updateNickname(with: "토익은정말어려워")
     wait(for: [expectation], timeout: 5)
     
     // Assert
     XCTAssertFalse(
       testResultValue,
-      "isNicknameUpdated 반환 값이 true여야 하지만 false반환")
+      "updateNickname 함수 반환 퍼블리셔 Output이 true여야 하지만 false반환")
   }
   
   func testMyProfileUseCase_updateNickname함수를통해_이름업데이트할때_성공적으로_변경됬다면_ShouldReturnTrue() {
@@ -69,115 +71,109 @@ final class MyProfileUseCaseTests: XCTestCase {
     var testResultValue = false
     
     // Act
-    subscription = sut.isNicknameUpdated.sink { _ in
+    subscription = sut.updateNickname(with: "어려운건 정복해나가는 맛이 있는거지").sink { _ in
     } receiveValue: { [unowned self] in
       testResultValue = $0
       expectation.fulfill()
     }
-    sut.updateNickname(with: "어려운건 정복해나가는 맛이 있는거지")
     wait(for: [expectation], timeout: 3.1)
     
     // Assert
     XCTAssertTrue(
       testResultValue,
-      "isNicknameUpdated 반환 값이 true여야 하지만 false반환")
+      "updateNickname(with:) 함수 반환 퍼블리셔 Output이 true여야 하지만 false반환")
   }
   
-  func testMyProfileUseCase_updateProfile함수를통해_프로필업데이트성공적일때_isProfileUpdated프로퍼티가_ShouldReturnEqual() {
+  func testMyProfileUseCase_updateProfile함수를통해_프로필업데이트성공적일때_ShouldReturnEqual() {
     // Arrange
     var resultValue = false
     let expectedValue = true
     
     // Act
-    subscription = sut.isProfileUpdated.sink { _ in
+    subscription = sut.updateProfile(with: "base64인코딩된데이터").sink { _ in
     } receiveValue: { [unowned self] in
       resultValue = $0
       expectation.fulfill()
     }
-    sut.updateProfile(with: "base64인코딩된데이터")
     wait(for: [expectation], timeout: 3.00001)
     
     // Assert
     XCTAssertEqual(
       resultValue,
       expectedValue,
-      "updateProfile()함수를통해 서버에 호출한 결과로 isProfileUdpated프로퍼티가 true가 반환되야하는데 false반환됨.")
+      "updateProfile()함수를통해 서버에 호출한 후 반환 퍼블리셔의 Output이 true가 반환되야하는데 false반환됨.")
   }
   
-  func testMyProfileUseCase_updateProfile함수를통해_프로필업데이트가실패했을때_isProfileUpdated프로퍼티가_ShouldReturnFalse() {
+  func testMyProfileUseCase_updateProfile함수를통해_프로필업데이트가실패했을때_ShouldReturnFalse() {
     // Arrange
     var resultValue = true
     
     // Act
-    subscription = sut.isProfileUpdated.sink { _ in
+    subscription = sut.updateProfile(with: "무슨이유에서인지실패..").sink { _ in
     } receiveValue: { [unowned self] in
       resultValue = $0
       expectation.fulfill()
     }
-    sut.updateProfile(with: "무슨이유에서인지실패..")
     wait(for: [expectation], timeout: 3.00001)
     
     // Assert
     XCTAssertFalse(
       resultValue,
-      "updateProfile()함수를통해 서버에 호출한 결과로 isProfileUdpated프로퍼티가 false가 반환되야하는데 true반환됨.")
+      "updateProfile()함수를통해 서버에 호출한 결과로 반환 퍼블리셔의 Output value가 false가 반환되야하는데 true반환됨.")
   }
   
-  func testMyProfileUseCase_saveProfile함수를통해_프로필저장이_성공했을때_isProfileSaved프로퍼티_ShouldReturnTrue() {
+  func testMyProfileUseCase_saveProfile함수를통해_프로필저장이_성공했을때_ShouldReturnTrue() {
     // Arrange
     var resultValue = false
     
     // Act
-    subscription = sut.isProfileSaved.sink { _ in
+    subscription = sut.saveProfile(with: "성장해나가자보자구!!!").sink { _ in
     } receiveValue: { [unowned self] in
       resultValue = $0
       expectation.fulfill()
     }
-    sut.saveProfile(with: "성장해나가자보자구!!!")
     wait(for: [expectation], timeout: 4)
     
     // Assert
     XCTAssertTrue(
       resultValue,
-      "saveProfile()함수를통해 서버에 호출한 결과로 isProfileSaved프로퍼티가 true가 반환되야하는데 false반환됨.")
+      "saveProfile()함수를통해 서버에 호출한 결과로 반환 퍼블리셔의 Output value가 true가 반환되야하는데 false반환됨.")
   }
   
-  func testMyProfileUseCase_saveProfile함수를통해_프로필저장이_실패했을때_isProfileSaved프로퍼티_ShouldReturnFalse() {
+  func testMyProfileUseCase_saveProfile함수를통해_프로필저장이_실패했을때_ShouldReturnFalse() {
     // Arrange
     var resultValue = true
     
     // Act
-    subscription = sut.isProfileSaved.sink { _ in
+    subscription = sut.saveProfile(with: "!!!!").sink { _ in
     } receiveValue: { [unowned self] in
       resultValue = $0
       expectation.fulfill()
     }
-    sut.saveProfile(with: "!!!!")
     wait(for: [expectation], timeout: 4)
     
     // Assert
     XCTAssertFalse(
       resultValue,
-      "saveProfile()함수를통해 서버에 호출한 결과로 isProfileSaved프로퍼티가 false가 반환되야하는데 true반환됨.")
+      "saveProfile()함수를통해 서버에 호출한 결과로 반환 퍼블리셔의 Output value가 false가 반환되야하는데 true반환됨.")
   }
   
-  func testMyProfileUseCase_deleteProfile함수를통해_프로필삭제했을때_isProfileDeleted프로퍼티_ShouldReturnTrue() {
+  func testMyProfileUseCase_deleteProfile함수를통해_프로필삭제했을때_ShouldReturnTrue() {
     // Arrange
     var resultValue = false
     
     // Act
-    subscription = sut.isProfileDeleted.sink { _ in
+    subscription = sut.deleteProfile().sink { _ in
     } receiveValue: { [unowned self] in
       resultValue = $0
       expectation.fulfill()
     }
-    sut.deleteProfile()
     wait(for: [expectation], timeout: 4)
     
     // Assert
     XCTAssertTrue(
       resultValue,
-      "deleteProfile()함수를통해 서버에 호출한 결과로 isProfileDeleted프로퍼티가 false가 반환되야하는데 true반환됨.")
+      "deleteProfile()함수를통해 서버에 호출한 결과로 반환 퍼블리셔의 Output value가 false가 반환되야하는데 true반환됨.")
   }
   
   func testMyProfileUseCase_fetchProfile함수를통해_사용자의프로필을받아올때_fetchedProfile프로퍼티_반환값이예상값과일치하는지_ShouldReturnEqual() {
@@ -186,12 +182,11 @@ final class MyProfileUseCaseTests: XCTestCase {
     var requestedProfileEntity = ProfileImageEntity(image: "")
     
     // Act
-    subscription = sut.fetchedProfile.sink { _ in
+    subscription = sut.fetchProfile().sink { _ in
     } receiveValue: { [unowned self] requestedValue in
       requestedProfileEntity = requestedValue
       expectation.fulfill()
     }
-    sut.fetchProfile()
     wait(for: [expectation], timeout: 5)
     
     // Assert
