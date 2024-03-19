13a23f3d7d5ab34c47d105f7547b8a2d681d480f
shcommit <happysh_s2@naver.com>
2024-03-19 13:09:37 +0900

    [Feat] MockUserStorage 구현

---
+++ travelPlan/Source/Domain/Tests/Mock/MockUserStorage.swift
@@ -0,0 +1,44 @@
+//
+//  MockUserStorage.swift
+//  travelPlan
+//
+//  Created by 양승현 on 3/19/24.
+//
+
+import Foundation
+
+final class MockUserStorage: UserStorage {
+  var nickname: String? {
+    "짱구"
+  }
+  
+  var profileURL: String? {
+    "bieiofhwnkslkh39f2i"
+  }
+  
+  var isSavedProfileInServer: Bool {
+    false
+  }
+  
+  var id: Int64? {
+    1
+  }
+  
+  var user: UserEntity? {
+    .init(id: 1, nickname: "짱구", isSavedProfileInServer: false)
+  }
+  
+  func setUser(with userInfo: UserEntity) { }
+  
+  func updateNickname(with nickname: String) -> Bool {
+    return true
+  }
+  
+  func updateProfileURL(with url: String) -> Bool {
+    return true
+  }
+  
+  func deleteProfile() -> Bool {
+    return true
+  }
+}
