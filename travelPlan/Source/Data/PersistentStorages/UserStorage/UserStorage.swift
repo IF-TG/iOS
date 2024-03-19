b3c722488e2e1129e161ae8608ea2e174103dbaa
shcommit <happysh_s2@naver.com>
2024-03-19 12:29:45 +0900

    [Feat] UserStorage 기능 선언

--- travelPlan/Source/Data/PersistentStorages/UserStorage/UserStorage.swift
+++ travelPlan/Source/Data/PersistentStorages/UserStorage/UserStorage.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+protocol UserStorage {
+  var nickname: String? { get }
+  var profileURL: String? { get }
+  var isSavedProfileInServer: Bool { get }
+  var id: Int64? { get }
+  var user: UserEntity? { get }
+  
+  func setUser(with userInfo: UserEntity)
+  
+  @discardableResult
+  func updateNickname(with nickname: String) -> Bool
+  
+  @discardableResult
+  func updateProfileURL(with url: String) -> Bool
+  
+  @discardableResult
+  func deleteProfile() -> Bool
+}
