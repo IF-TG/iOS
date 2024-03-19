e01b07ad76517d5c9761bcd8004e149e463030d7
shcommit <happysh_s2@naver.com>
2024-03-19 12:51:11 +0900

    [Feat] DefaultLoggedInUserRepository 구현

--- travelPlan/Source/Data/Repositories/DefaultLoggedInUserRepository.swift
+++ travelPlan/Source/Data/Repositories/DefaultLoggedInUserRepository.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+final class DefaultLoggedInUserRepository: LoggedInUserRepository {
+  // MARK: - Dependencies
+  let storage: UserStorage
+  
+  // MARK: - Properties
+  var nickname: String? {
+    storage.nickname
+  }
+  
+  var profileURL: String?  {
+    storage.profileURL
+  }
+  
+  var isSavedProfileInServer: Bool {
+    storage.isSavedProfileInServer
+  }
+  
+  var id: Int64? {
+    storage.id
+  }
+  
+  var user: UserEntity? {
+    storage.user
+  }
+  
+  // MARK: - Lifecycle
+  init(storage: UserStorage) {
+    self.storage = storage
+  }
+  
+  // MARK: - Helpers
+  func setUser(with userInfo: UserEntity) {
+    storage.setUser(with: userInfo)
+  }
+  
+  func updateNickname(with nickname: String) -> Bool {
+    storage.updateNickname(with: nickname)
+  }
+  
+  func updateProfileURL(with url: String) -> Bool {
+    storage.updateProfileURL(with: url)
+  }
+  
+  func deleteProfile() -> Bool {
+    storage.deleteProfile()
+  }
+}
