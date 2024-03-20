b7bf3e21b449da1be9c56f85dab03f5b8c8dd25e
shcommit <happysh_s2@naver.com>
2024-03-20 15:41:56 +0900

    [Rebuild] DefaultPostUseCase fetchPosts 새로 반영된 반환타입에 맞게 리빌딩

--- travelPlan/Source/Domain/UseCases/DefaultPostUseCase.swift
+++ travelPlan/Source/Domain/UseCases/DefaultPostUseCase.swift
@@ -12,18 +12,17 @@ final class DefaultPostUseCase: PostUseCase {
   // MARK: - Dependencies
   private let postRepository: PostRepository
   
-  // MARK: - Properties
-  private var subscriptions = Set<AnyCancellable>()
-  
   // MARK: - Lifecycle
   init(postRepository: PostRepository) {
     self.postRepository = postRepository
   }
   
-  func fetchPosts(with page: PostsPage) -> AnyPublisher<[PostContainer], any Error> {
-    postRepository.fetchPosts(with: page)
-      .subscribe(on: DispatchQueue.global(qos: .userInteractive))
-      .mapError { $0 }
-      .eraseToAnyPublisher()
+  func fetchPosts(with page: PostFetchRequestValue) -> AnyPublisher<[PostContainer], any Error> {
+    return postRepository.fetchPosts(
+      page: page.page,
+      perPage: page.perPage,
+      category: page.category)
+    .subscribe(on: DispatchQueue.global(qos: .userInteractive))
+    .eraseToAnyPublisher()
   }
 }
