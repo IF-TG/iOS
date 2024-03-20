4e175ab749ce6c061cba1a24fd940746ffa49096
shcommit <happysh_s2@naver.com>
2024-03-21 02:06:03 +0900

    [Feat] PostViewSection에 count 추가

--- travelPlan/Source/Presentation/Post/Model/PostViewSection.swift
+++ travelPlan/Source/Presentation/Post/Model/PostViewSection.swift
@@ -8,11 +8,15 @@
 import Foundation
 
 // 오랜만에 0, 1, 2로되어있는 CollectionViewDataSource를 보니 까먹었다 코드보며 기억이나서 타입화 했습니다.
-@frozen enum PostViewSection: Int {
+@frozen enum PostViewSection: Int, CaseIterable {
   /// 오더타입, 메인테마 section
   case category = 0
   /// 포스트 section
   case post = 1
   /// refresh section
   case bottomRefresh = 2
+  
+  static var count: Int {
+    return allCases.count
+  }
 }
