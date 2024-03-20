1396dfb2f4de186d2652edc910456984ae0c5395
shcommit <happysh_s2@naver.com>
2024-03-21 02:03:20 +0900

    [Feat] PostViewSection 추가

---
+++ travelPlan/Source/Presentation/Post/Model/PostViewSection.swift
@@ -0,0 +1,18 @@
+//
+//  PostViewSection.swift
+//  travelPlan
+//
+//  Created by 양승현 on 3/21/24.
+//
+
+import Foundation
+
+// 오랜만에 0, 1, 2로되어있는 CollectionViewDataSource를 보니 까먹었다 코드보며 기억이나서 타입화 했습니다.
+@frozen enum PostViewSection: Int {
+  /// 오더타입, 메인테마 section
+  case category = 0
+  /// 포스트 section
+  case post = 1
+  /// refresh section
+  case bottomRefresh = 2
+}
