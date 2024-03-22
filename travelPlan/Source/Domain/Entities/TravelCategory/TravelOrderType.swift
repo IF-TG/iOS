7433a819b10d6bdb10da6cd3235912834e989acf
shcommit <happysh_s2@naver.com>
2024-03-22 16:55:35 +0900

    [Feat] TravelOrderType에 RawRepresentable 추가

--- travelPlan/Source/Domain/Entities/TravelCategory/TravelOrderType.swift
+++ travelPlan/Source/Domain/Entities/TravelCategory/TravelOrderType.swift
@@ -7,7 +7,18 @@
 
 import Foundation
 
-@frozen enum TravelOrderType: String, CaseIterable, Equatable {
+@frozen enum TravelOrderType: String, CaseIterable, Equatable, RawRepresentable {
   case newest = "최신순"
   case popularity = "인기순"
+  
+  init?(rawValue: String) {
+    switch rawValue {
+    case TravelOrderType.newest.rawValue:
+      self = .newest
+    case TravelOrderType.popularity.rawValue:
+      self = .popularity
+    default:
+      return nil
+    }
+  }
 }
