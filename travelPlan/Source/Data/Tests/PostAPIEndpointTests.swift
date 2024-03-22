f34225c70923fe19375946e87527f5ecbefca079
shcommit <happysh_s2@naver.com>
2024-03-23 03:41:38 +0900

    [Test] PostAPIEndpoint에서 MainTheme-travelTheme's local 타입 requestDTO요청시 AbsoluteURL이 정확히 반영되었는지 검증

--- travelPlan/Source/Data/Tests/PostAPIEndpointTests.swift
+++ travelPlan/Source/Data/Tests/PostAPIEndpointTests.swift
@@ -112,6 +112,23 @@ extension PostAPIEndpointTests {
       "PostAPIEndpoint의 fetchPosts()에서 DataRequest의 urlRequest를 반환해야하는데 nil 반환")
     XCTAssertEqual(dataRequest?.convertible.urlRequest?.url, expectedURL)
   }
+  
+  func testPostAPIEndpiont_MainTheme_Theme_LOCAL_EXPERIENCE타입_RequestDTO요청시_AbsoluteURL이_정확히반영되는지_ShouldReturnTrue() {
+    // Arrange
+    let mockCategory = PostCategory(mainTheme: .travelTheme(.local), orderBy: .newest)
+    let mockRequestDTO = makePostsReqeustDTO(with: mockCategory)
+    let expectedURL = URL(string: "\(baseURL)?mainCategory=THEME&orderMethod=RECENT_ORDER&page=\(mockPage)&perPage=\(mockPerPage)&subCategory=LOCAL_EXPERIENCE&userId=\(mockUserId)")
+    let endpoint = sut.fetchPosts(with: mockRequestDTO)
+    
+    // Act
+    makeRequest(fromFetchPosts: endpoint)
+    
+    // Assert
+    XCTAssertNotNil(
+      dataRequest?.convertible.urlRequest,
+      "PostAPIEndpoint의 fetchPosts()에서 DataRequest의 urlRequest를 반환해야하는데 nil 반환")
+    XCTAssertEqual(dataRequest?.convertible.urlRequest?.url, expectedURL)
+  }
 }
 
 // MARK: - Private Helpers
