1dda6293c0f2e303ee27ad3f8cf633be50214342
shcommit <happysh_s2@naver.com>
2024-03-22 20:05:44 +0900

    [Feat] PostOrderCategoryBottomSheet 구현 및 Delegate 선언

--- travelPlan/Source/Presentation/Feed/ViewController/BottomSheet/PostOrderCategoryBottomSheet.swift
+++ travelPlan/Source/Presentation/Feed/ViewController/BottomSheet/PostOrderCategoryBottomSheet.swift
@@ -5,4 +5,26 @@
 //  Created by 양승현 on 3/22/24.
 //
 
-import Foundation
+import UIKit
+
+protocol PostOrderCategoryBottomSheetDelegate: BasePostCategoryBottomSheetDelegate
+where Category == TravelOrderType {}
+
+final class PostOrderCategoryBottomSheet: BasePostCategoryBottomSheet {
+  init() {
+    super.init(bottomSheetMode: .couldBeFull, titles: TravelOrderType.toKoreanList)
+  }
+  
+  required init?(coder: NSCoder) { nil }
+  
+  weak var delegate: (any PostOrderCategoryBottomSheetDelegate)?
+  
+  override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
+    super.dismiss(animated: flag, completion: completion)
+    guard
+      let selectedTitle,
+      let selectedOrderType = TravelOrderType(rawValue: selectedTitle)
+    else { return }
+    delegate?.notifySelectedCategory(selectedOrderType)
+  }
+}
