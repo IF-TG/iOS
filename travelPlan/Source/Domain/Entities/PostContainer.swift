71c06868935d0f8ee43a225ea660b977d8dcd922
shcommit <happysh_s2@naver.com>
2024-03-23 01:14:19 +0900

    [Feat] PostContainer는 필요했었다,,

---
+++ travelPlan/Source/Domain/Entities/PostContainer.swift
@@ -0,0 +1,14 @@
+//
+//  PostContainer.swift
+//  travelPlan
+//
+//  Created by 양승현 on 3/23/24.
+//
+
+import Foundation
+
+struct PostContainer {
+  let post: Post
+  let thumbnail: PostThumbnails
+  let totalPosts: Int64
+}
