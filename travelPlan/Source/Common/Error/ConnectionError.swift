3c5a21205ea939c3fb29965a0844765150b08586
shcommit <happysh_s2@naver.com>
2024-03-23 01:21:16 +0900

    [Common] ConnectionError?에 서버에서 준 데이터가 오류가 있는 경우 추가했습니다.

--- travelPlan/Source/Common/Error/ConnectionError.swift
+++ travelPlan/Source/Common/Error/ConnectionError.swift
@@ -11,6 +11,8 @@ import Alamofire
 enum ConnectionError: LocalizedError {
   case invalidInternetConnection
   case unavailableServer
+  /// response시점에서 서버에서 주어야하는 데이터가 nil인 경우..
+  case missingRequiredData
   
   var errorDescription: String? {
     switch self {
@@ -18,6 +20,8 @@ enum ConnectionError: LocalizedError {
       NSLocalizedString("인터넷 연결이 유효하지 않습니다. 네트워크 설정을 확인하세요.", comment: "")
     case .unavailableServer:
       NSLocalizedString("서버에 연결할 수 없습니다. 나중에 다시 시도하세요.", comment: "")
+    case .missingRequiredData:
+      NSLocalizedString("서버에서 필요한 데이터가 누락되었습니다.", comment: "")
     }
   }
 }
