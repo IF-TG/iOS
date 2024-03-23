f467387687b3e6db432395d677387899bec2ac9b
shcommit <happysh_s2@naver.com>
2024-03-23 21:48:11 +0900

    [Feat] PostCommentAPIEndpoint 생성

---
+++ travelPlan/Source/Data/Network/PostCommentAPIEndpoint.swift
@@ -0,0 +1,12 @@
+//
+//  PostCommentAPIEndpoint.swift
+//  travelPlan
+//
+//  Created by 양승현 on 3/23/24.
+//
+
+import Foundation
+
+struct PostCommentAPIEndpoint {
+  static func sendComment(with requestDTO: PostCommentSendingRequestDTO)
+}
