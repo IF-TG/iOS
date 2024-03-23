7428e8e73ab7b18e54aaaaa25a7bd926737166d3
shcommit <happysh_s2@naver.com>
2024-03-23 23:14:01 +0900

    [Feat] PostCommentUseCase 생성 및 sendComment(postId:comment:) 선언

---
+++ travelPlan/Source/Domain/UseCases/Protocol/PostCommentUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  PostCommentUseCase.swift
+//  travelPlan
+//
+//  Created by 양승현 on 3/23/24.
+//
+
+import Foundation
+import Combine
+
+protocol PostCommentUseCase {
+  func sendComment(postId: Int64, comment: String) -> AnyPublisher<PostCommentEntity, Error>
+}
