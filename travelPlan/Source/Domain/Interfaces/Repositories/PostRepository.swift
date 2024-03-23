a8673c31d2b22e714698c9aef5b803222ad72fba
shcommit <happysh_s2@naver.com>
2024-03-23 18:25:55 +0900

    [Feat] PostRepository.fetchComments(page:perPage:postId:) 정의

--- travelPlan/Source/Domain/Interfaces/Repositories/PostRepository.swift
+++ travelPlan/Source/Domain/Interfaces/Repositories/PostRepository.swift
@@ -9,4 +9,10 @@ import Combine
 
 protocol PostRepository {
   func fetchPosts(with page: PostsPage) -> Future<[PostContainer], MainError>
+  
+  func fetchComments(
+    page: Int32,
+    perPage: Int32,
+    postId: Int64
+  ) -> Future<[PostCommentContainerEntity], Error>
 }
