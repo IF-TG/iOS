f14278d92daa709c777c5d4a464287cc7b0c1b2c
shcommit <happysh_s2@naver.com>
2024-03-23 23:18:39 +0900

    [Feat] 잘못  제거한  PostUseCase 파일 및 코드  복구

---
+++ travelPlan/Source/Domain/UseCases/Protocol/PostUseCase.swift
@@ -0,0 +1,25 @@
+//
+//  PostUseCase.swift
+//  travelPlan
+//
+//  Created by 양승현 on 3/6/24.
+//
+
+import Combine
+
+struct PostCommentsReqeustValue {
+  let page: Int32
+  let perPage: Int32
+  let postId: Int64
+}
+
+protocol PostUseCase {
+  var postContainers: PassthroughSubject<[PostContainer], MainError> { get }
+  
+  func fetchPosts(with page: PostsPage)
+  
+  func fetchComments(
+    with requestValue: PostCommentsReqeustValue
+  ) -> AnyPublisher<PostCommentContainerEntity, any Error>
+  
+}
