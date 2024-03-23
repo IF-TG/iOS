53323d3bcc5dc435eb3f4832e8ee32259a52b820
shcommit <happysh_s2@naver.com>
2024-03-23 18:44:19 +0900

    [Feat] PostUseCase protocol에 PostCommentsReqeustValue, fetchComments(with:) 추가

--- travelPlan/Source/Domain/UseCases/Protocol/PostUseCase.swift
+++ travelPlan/Source/Domain/UseCases/Protocol/PostUseCase.swift
@@ -7,8 +7,18 @@
 
 import Combine
 
+struct PostCommentsReqeustValue {
+  let page: Int32
+  let perPage: Int32
+  let postId: Int64
+}
+
 protocol PostUseCase {
   var postContainers: PassthroughSubject<[PostContainer], MainError> { get }
   
   func fetchPosts(with page: PostsPage)
+  
+  func fetchComments(
+    with requestValue: PostCommentsReqeustValue
+  ) -> AnyPublisher<PostCommentContainerEntity, Error>
 }
