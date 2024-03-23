9f253bfc9aa7fe742423dde394b0c42dc31596b1
shcommit <happysh_s2@naver.com>
2024-03-23 21:54:16 +0900

    [Feat] PotCommentAPIENdpoint.sendComment(with:) 구현

--- travelPlan/Source/Data/Network/PostCommentAPIEndpoint.swift
+++ travelPlan/Source/Data/Network/PostCommentAPIEndpoint.swift
@@ -8,5 +8,14 @@
 import Foundation
 
 struct PostCommentAPIEndpoint {
-  static func sendComment(with requestDTO: PostCommentSendingRequestDTO)
+  static func sendComment(
+    with requestDTO: PostCommentSendingRequestDTO
+  ) -> Endpoint<CommonDTO<PostCommentResponseDTO>> {
+    return Endpoint(
+      scheme: "http",
+      host: "localhost:8080",
+      method: .post,
+      parameters: [.body(requestDTO)],
+      requestType: .postComment(.send))
+  }
 }
