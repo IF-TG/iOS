05b69130a4fd63705a860d6fe7d1f0fc7f458bc6
shcommit <happysh_s2@naver.com>
2024-03-23 22:35:33 +0900

    [Feat] ConnectionError 수정. 클라이언트 error, 알수없는 error추가

--- travelPlan/Source/Common/Error/ConnectionError.swift
+++ travelPlan/Source/Common/Error/ConnectionError.swift
@@ -6,40 +6,63 @@
 //
 
 import Foundation
+import Combine
 import Alamofire
 
+/// 레포지토리에서 AFError가 발생될 경우
+/// mapError 안에서 AFError -> ConnectionError로 특정 조건에 의한 경우만 변환하거나 아니면 옵셔널을 반환한다면,
+/// 이미 그 클로저 내부 타입은 다른 타입의 error를 방출할 수 없습니다.... 그래서 asConnectionError의 반환타입을 nil로했다가 제거했습니다.
 enum ConnectionError: LocalizedError {
   case invalidInternetConnection
   case unavailableServer
+  case unexpectedError(error: Error)
+  case clientError
   
-  var errorDescription: String? {
+  var errorDescription: String {
     switch self {
     case .invalidInternetConnection:
       NSLocalizedString("인터넷 연결이 유효하지 않습니다. 네트워크 설정을 확인하세요.", comment: "")
     case .unavailableServer:
       NSLocalizedString("서버에 연결할 수 없습니다. 나중에 다시 시도하세요.", comment: "")
+    case .clientError:
+      NSLocalizedString("클라이언트에서 에러가 발생했습니다.", comment: "")
+    case .unexpectedError(let error):
+      NSLocalizedString("알 수 없는 에러가 발생됬습니다.\n:\(error.localizedDescription)", comment: "")
     }
   }
 }
 
-// MARK: - Helpers
+// MARK: - AFError extension
 extension AFError {
-  var mapConnectionError: ConnectionError? {
+  var asConnectionError: ConnectionError {
     switch self {
     case .sessionTaskFailed(let error as URLError):
       if error.code == .notConnectedToInternet {
         return .invalidInternetConnection
       }
-      return nil
+      return .unexpectedError(error: self)
     case .responseValidationFailed(let reason):
       switch reason {
-      case .unacceptableStatusCode(code: 404...599):
+      case .unacceptableStatusCode(code: 500...599):
         return .unavailableServer
+      case .unacceptableStatusCode(code: 404...499):
+        return .clientError
       default:
-        return nil
+        return .unexpectedError(error: self)
       }
     default:
-      return nil
+      return .unexpectedError(error: self)
+    }
+  }
+}
+
+// MARK: - Publisher extension
+extension Publisher where Self.Failure == AFError {
+  func mapConnectionError<E: Error>(
+    _ transform: @escaping (Self.Failure) -> E
+  ) -> Publishers.MapError<Self, Error>  {
+    return self.mapError { error in
+      error.asConnectionError
     }
   }
 }
