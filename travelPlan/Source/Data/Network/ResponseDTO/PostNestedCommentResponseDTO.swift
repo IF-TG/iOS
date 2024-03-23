fe433d00224f7cd70c6f679d1f189a981b0cef0b
shcommit <happysh_s2@naver.com>
2024-03-23 17:42:18 +0900

    [Feat] Decdoable 타입 준수 init(from:)구현 및 toDomain() 추가

--- travelPlan/Source/Data/Network/ResponseDTO/PostNestedCommentResponseDTO.swift
+++ travelPlan/Source/Data/Network/ResponseDTO/PostNestedCommentResponseDTO.swift
@@ -8,22 +8,47 @@
 import Foundation
 
 struct PostNestedCommentResponseDTO: Decodable {
-  let replyId: Int64
+  let nestedCommentId: Int64
   let userProfileURL: String
   let nickname: String
   let timestamp: String
   let comment: String
-  let likes: Int32
-  let isLiked: Bool
+  let hearts: Int32
+  let isOnHeart: Bool
   
   enum CodingKeys: String, CodingKey {
-    case replyId = "nestedCommentId"
+    case nestedCommentId
     case userProfileURL = "profileImgUri"
     case nickname
     // FIXME: - 경완이한테 알려주고 추후 경완이가 api 개선하면 그에 따라 바꾸기
     case timestamp = "crateAt"
     case comment
-    case likes = "likeNum"
-    case isLiked = "liked"
+    case hearts = "likeNum"
+    case isOnHeart = "liked"
+  }
+  
+  init(from decoder: any Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    self.nestedCommentId = try container.decode(Int64.self, forKey: .nestedCommentId)
+    self.userProfileURL = try container.decode(String.self, forKey: .userProfileURL)
+    self.nickname = try container.decode(String.self, forKey: .nickname)
+    self.timestamp = try container.decode(String.self, forKey: .timestamp)
+    self.comment = try container.decode(String.self, forKey: .comment)
+    self.hearts = try container.decode(Int32.self, forKey: .hearts)
+    self.isOnHeart = try container.decode(Bool.self, forKey: .isOnHeart)
+  }
+}
+
+// MARK: - Mappings to Domain
+extension PostNestedCommentResponseDTO {
+  func toDomain() -> PostNestedCommentEntity {
+    return PostNestedCommentEntity(
+      nestedCommentId: nestedCommentId,
+      userProfileURL: userProfileURL,
+      nickname: nickname,
+      timestamp: timestamp,
+      comment: comment,
+      hearts: hearts,
+      isOnHeart: isOnHeart)
   }
 }
