ed92542f096fe10028feeab799324be2417b69a9
shcommit <happysh_s2@naver.com>
2024-03-24 16:29:33 +0900

    [Feat] PostCommentUpdateResponseDTO 생성 및 구현

---
+++ travelPlan/Source/Data/Network/ResponseDTO/PostCommentUpdateResponseDTO.swift
@@ -0,0 +1,13 @@
+//
+//  PostCommentUpdateResponseDTO.swift
+//  travelPlan
+//
+//  Created by 양승현 on 3/24/24.
+//
+
+import Foundation
+
+struct PostCommentUpdateResponseDTO: Decodable {
+  let commentId: Int64
+  let comment: String
+}
