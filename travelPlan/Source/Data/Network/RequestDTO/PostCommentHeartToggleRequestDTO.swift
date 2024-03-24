afb9f372a1ea1851b23172ab0ddba83ff7cc8555
shcommit <happysh_s2@naver.com>
2024-03-24 20:10:58 +0900

    [Feat] PostCommentHeartToggleRequestDTO 생성 및 구현

---
+++ travelPlan/Source/Data/Network/RequestDTO/PostCommentHeartToggleRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  PostCommentHeartToggleRequestDTO.swift
+//  travelPlan
+//
+//  Created by 양승현 on 3/24/24.
+//
+
+import Foundation
+
+struct PostCommentHeartToggleRequestDTO: Encodable {
+  let id: Int64
+}
