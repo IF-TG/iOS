412bdfc1b54dccfbd213981c86009b15841eddd3
shcommit <happysh_s2@naver.com>
2024-03-24 17:29:16 +0900

    [Feat] UpdatedCommentEntity 구현

---
+++ travelPlan/Source/Domain/Entities/UpdatedCommentEntity.swift
@@ -0,0 +1,13 @@
+//
+//  UpdatedCommentEntity.swift
+//  travelPlan
+//
+//  Created by 양승현 on 3/24/24.
+//
+
+import Foundation
+
+struct UpdatedCommentEntity {
+  let commentId: Int64
+  let comment: String
+}
