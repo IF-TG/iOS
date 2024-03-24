5e9b038634b4febfa3e166015b812b3131f4fdc6
shcommit <happysh_s2@naver.com>
2024-03-24 19:30:26 +0900

    [Feat] PostCommentAPIEndpoint에 fetchComments 함수 추가

--- travelPlan/Source/Data/Network/PostCommentAPIEndpoint.swift
+++ travelPlan/Source/Data/Network/PostCommentAPIEndpoint.swift
@@ -40,4 +40,15 @@ struct PostCommentAPIEndpoint {
       parameters: [.query(requestDTO)],
       requestType: .postComment(.delete))
   }
+  
+  static func fetchComments(
+    with requestDTO: PostCommentsRequestDTO
+  ) -> Endpoint<CommonDTO<[PostCommentResponseDTO]>> {
+    return Endpoint(
+      scheme: "http",
+      host: "localhost:8080",
+      method: .get,
+      parameters: [.query(requestDTO)],
+      requestType: .postComment(.fetchComments))
+  }
 }
