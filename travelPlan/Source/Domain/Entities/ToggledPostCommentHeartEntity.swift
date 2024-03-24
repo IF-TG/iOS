e1b40722ef53cadc5f8337bc053c9584f9f3ee33
shcommit <happysh_s2@naver.com>
2024-03-24 20:25:18 +0900

    [Feat] ToggledPostCommentHeartEntity 생성 및 객체 정의

---
+++ travelPlan/Source/Domain/Entities/ToggledPostCommentHeartEntity.swift
@@ -0,0 +1,13 @@
+//
+//  ToggledPostCommentHeartEntity.swift
+//  travelPlan
+//
+//  Created by 양승현 on 3/24/24.
+//
+
+import Foundation
+
+struct ToggledPostCommentHeartEntity {
+  let id: Int64
+  let isOnHeart: Bool
+}
