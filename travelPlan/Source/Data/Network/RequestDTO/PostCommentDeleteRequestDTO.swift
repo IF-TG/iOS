93849abe2cf0b46dc6bca8ff7429a972e49f9e22
shcommit <happysh_s2@naver.com>
2024-03-24 18:31:04 +0900

    [Feat] PostCommentDeleteRequestDTO 생성 및 구현

---
+++ travelPlan/Source/Data/Network/RequestDTO/PostCommentDeleteRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  PostCommentDeleteRequestDTO.swift
+//  travelPlan
+//
+//  Created by 양승현 on 3/24/24.
+//
+
+import Foundation
+
+struct PostCommentDeleteRequestDTO: Encodable {
+  let commentId: Int64
+}
