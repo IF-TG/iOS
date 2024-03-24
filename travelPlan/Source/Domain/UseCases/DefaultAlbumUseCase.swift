f927f4200304379e0b89cc5995826f162f2c61be
seokhyun <letushyun@gmail.com>
2024-03-25 04:38:30 +0900

    [Feat] AlbumUseCase 최종 구현

--- travelPlan/Source/Domain/UseCases/DefaultAlbumUseCase.swift
+++ travelPlan/Source/Domain/UseCases/DefaultAlbumUseCase.swift
@@ -6,9 +6,50 @@
 //
 
 import Foundation
+import Combine
+import Photos
 
-final class DefaultAlbumUseCase: AlbumUseCase {
-  func getAlbums(mediaType: MediaType) -> AnyPublisher<[AlbumInfo], Never> {
+final class DefaultAlbumUseCase {
+  // MARK: - Properties
+  private let sortDescriptors = [
+    NSSortDescriptor(key: "creationDate", ascending: false),
+    NSSortDescriptor(key: "modificationDate", ascending: false)
+  ]
+  
+  private var photoEntity = [PhotoEntity]()
+  private var selectedIndexArray = [Int]()
+}
+
+// MARK: - AlbumUseCase
+extension DefaultAlbumUseCase: AlbumUseCase {
+  var maxSelectedImageCount: Int {
+    return 20
+  }
+  
+  func getAssets() -> [PHAsset] {
+    let fetchOptions = PHFetchOptions().set {
+      $0.predicate = NSPredicate(
+        format: "mediaType == %d",
+        PHAssetMediaType.image.rawValue
+      )
+      $0.sortDescriptors = self.sortDescriptors
+      $0.includeAssetSourceTypes = .typeUserLibrary
+    }
+    let fetchResult = PHAsset.fetchAssets(with: fetchOptions)
+    let assets = convertAlbumToPHAssets(fetchResult: fetchResult)
     
+    return assets
+  }
+}
+
+// MARK: - Private Helpers
+extension DefaultAlbumUseCase {
+  private func convertAlbumToPHAssets(fetchResult: PHFetchResult<PHAsset>) -> [PHAsset] {
+    var assets = [PHAsset]()
+    fetchResult.enumerateObjects { asset, _, _ in
+      assets.append(asset)
+    }
+    
+    return assets
   }
 }
--- travelPlan/Source/Domain/UseCases/Protocol/AlbumUseCase.swift
+++ travelPlan/Source/Domain/UseCases/Protocol/AlbumUseCase.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 import Photos
-//import Combine
 
 protocol AlbumUseCase {
-  func getPhotos(mediaType: MediaType) -> [PHAsset]
+  func getAssets() -> [PHAsset]
+  var maxSelectedImageCount: Int { get }
 }
