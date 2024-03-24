a092bfea90e7980b3feb27900b88173ac1a115d0
shcommit <happysh_s2@naver.com>
2024-03-24 17:29:54 +0900

    [Style] UpdatedCommentEntity -> UpdatedPostCommentEntity로 네이밍 변경

--- travelPlan/Source/Domain/Entities/UpdatedPostCommentEntity.swift
+++ travelPlan/Source/Domain/Entities/UpdatedPostCommentEntity.swift
@@ -1,5 +1,5 @@
 //
-//  UpdatedCommentEntity.swift
+//  UpdatedPostCommentEntity.swift
 //  travelPlan
 //
 //  Created by 양승현 on 3/24/24.
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct UpdatedCommentEntity {
+struct UpdatedPostCommentEntity {
   let commentId: Int64
   let comment: String
 }
