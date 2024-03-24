72587cf4ca7c53591141559b410d2543aa9d2a02
shcommit <happysh_s2@naver.com>
2024-03-24 17:25:37 +0900

    [Test] PostCommentAPIEndpointTests에 updateComment시 absoluteurl이 정확한지 테스트코드 추가

--- travelPlan/Source/Data/Network/Tests/PostCommentAPIEndpointTests.swift
+++ travelPlan/Source/Data/Network/Tests/PostCommentAPIEndpointTests.swift
@@ -43,7 +43,29 @@ final class PostCommentAPIEndpointTests: XCTestCase {
     // Assert
     XCTAssertNotNil(
       dataRequest?.convertible.urlRequest,
-      "PostAPIEndpoint의 fetchPosts()에서 DataRequest의 urlRequest를 반환해야하는데 nil 반환")
+      "PostAPIEndpoint의 sendComment()에서 DataRequest의 urlRequest를 반환해야하는데 nil 반환")
+    XCTAssertEqual(dataRequest?.convertible.urlRequest?.url, expectedURL)
+  }
+  
+  func testPostCommentAPIEndpoint_updateComment함수호출시_DataRequest과AbsoluteURL이_정확한지_ShouldReturnEqual() {
+    // Arrange
+    let expectedURL = URL(string: "http://localhost:8080/comment")
+    let mockRequestDTO = PostCommentUpdateRequestDTO(commentId: 1, comment: "testtest")
+    
+    let endpoint = sut.updateComment(with: mockRequestDTO)
+    var dataRequest: DataRequest?
+    
+    // Act
+    DispatchQueue.global(qos: .background).async { [unowned self] in
+      dataRequest = try? endpoint.makeRequest(from: mockSession)
+      expectation.fulfill()
+    }
+    wait(for: [expectation], timeout: 10)
+    
+    // Assert
+    XCTAssertNotNil(
+      dataRequest?.convertible.urlRequest,
+      "PostAPIEndpoint의 updateComment()에서 DataRequest의 urlRequest를 반환해야하는데 nil 반환")
     XCTAssertEqual(dataRequest?.convertible.urlRequest?.url, expectedURL)
   }
 }
