4d66b434644841e5d05431989e89a18b529142ff
shcommit <happysh_s2@naver.com>
2024-03-27 02:58:09 +0900

    [Feat] PostNestedCommentRepository 생성 및 sendNestedComment(commentId:comment:) 선언

---
+++ travelPlan/Source/Domain/Interfaces/Repositories/PostNestedCommentRepository.swift
@@ -0,0 +1,12 @@
+//
+//  PostNestedCommentRepository.swift
+//  travelPlan
+//
+//  Created by 양승현 on 3/27/24.
+//
+
+import Combine
+
+protocol PostNestedCommentRepository {
+  func sendNestedComment(commentId: Int64, comment: String) -> Future<PostNestedCommentEntity, Error>
+}
