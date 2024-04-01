fef381695bf233287eb5ceefa57227ea73b4b913
shcommit <happysh_s2@naver.com>
2024-04-01 18:53:26 +0900

    [Style] PostNestedCommentRepository에 toggleCommentHeart 매개변수 확실하게 변경

--- travelPlan/Source/Domain/Interfaces/Repositories/PostNestedCommentRepository.swift
+++ travelPlan/Source/Domain/Interfaces/Repositories/PostNestedCommentRepository.swift
@@ -11,5 +11,5 @@ protocol PostNestedCommentRepository {
   func sendNestedComment(commentId: Int64, comment: String) -> Future<PostNestedCommentEntity, Error>
   func updateNestedComment(nestedCommentId: Int64, comment: String) -> Future<Bool, Error>
   func deleteNestedComment(nestedCommentId: Int64) -> Future<Bool, Error>
-  func toggleCommentHeart(commentId: Int64) -> Future<ToggledPostCommentHeartEntity, Error>
+  func toggleCommentHeart(nestedCommentId: Int64) -> Future<ToggledPostCommentHeartEntity, Error>
 }
