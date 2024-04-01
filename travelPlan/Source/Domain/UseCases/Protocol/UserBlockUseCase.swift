5fe383eb92d93bcb34560ec545e48bf38b5bd37f
shcommit <happysh_s2@naver.com>
2024-04-01 22:05:28 +0900

    [Feat] UserBlockUseCase 생성 및  blockUser(with:) 선언

---
+++ travelPlan/Source/Domain/UseCases/Protocol/UserBlockUseCase.swift
@@ -0,0 +1,12 @@
+//
+//  UserBlockUseCase.swift
+//  travelPlan
+//
+//  Created by 양승현 on 4/1/24.
+//
+
+import Combine
+
+protocol UserBlockUseCase {
+  func blockUser(with userId: Int64) -> AnyPublisher<BlockedUserEntity, Error>
+}
