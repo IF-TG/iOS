1b5cfe6b30b56c112ca454905a8e0cfeba42e99c
shcommit <happysh_s2@naver.com>
2024-04-01 21:47:30 +0900

    [Feat] BlockedUserEntity 생성 및 구현

---
+++ travelPlan/Source/Domain/Entities/BlockedUserEntity.swift
@@ -0,0 +1,13 @@
+//
+//  BlockedUserEntity.swift
+//  travelPlan
+//
+//  Created by 양승현 on 4/1/24.
+//
+
+import Foundation
+
+struct BlockedUserEntity {
+  let userId: Int64
+  let isBlocked: Bool
+}
