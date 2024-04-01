18e6e4c565c4eb0aa98760772166e4a9d99abf6b
shcommit <happysh_s2@naver.com>
2024-04-01 21:55:50 +0900

    [Feat] UserBlockAPIEndpoint생성 및 blockUser(with:) endpoint반환 함수 구현

---
+++ travelPlan/Source/Data/Network/UserBlockAPIEndpoint.swift
@@ -0,0 +1,21 @@
+//
+//  UserBlockAPIEndpoint.swift
+//  travelPlan
+//
+//  Created by 양승현 on 4/1/24.
+//
+
+import Foundation
+
+struct UserBlockAPIEndpoint {
+  static func blockUser(
+    with requestDTO: UserBlockRequestDTO
+  ) -> Endpoint<CommonDTO<UserBlockResponseDTO>> {
+    return Endpoint(
+      scheme: "http",
+      host: "localhost:8080",
+      method: .post,
+      parameters: [.body(requestDTO)],
+      requestType: .userBlock(.userBlock))
+  }
+}
