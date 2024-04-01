8f0f19949f0837da8d3d9ac2a22540700ee9a1ad
shcommit <happysh_s2@naver.com>
2024-04-01 22:32:31 +0900

    [Feat] BlockedUserProfileResponseDTO 구현 및 toDomain() 구현

---
+++ travelPlan/Source/Data/Network/ResponseDTO/BlockedUserProfileResponseDTO.swift
@@ -0,0 +1,25 @@
+//
+//  BlockedUserProfileResponseDTO.swift
+//  travelPlan
+//
+//  Created by 양승현 on 4/1/24.
+//
+
+import Foundation
+
+struct BlockedUserProfileResponseDTO: Decodable {
+  let nickname: String
+  let thumbnailURL: String
+  
+  enum CodingKeys: String, CodingKey {
+    case nickname
+    case thumbnailURL = "thumbnailUrl"
+  }
+}
+
+// MARK: - Mappings to Domain
+extension BlockedUserProfileResponseDTO {
+  func toDomain() -> BlockedUserProfileEntity {
+    return .init(nickname: nickname, thumbnailURL: thumbnailURL)
+  }
+}
