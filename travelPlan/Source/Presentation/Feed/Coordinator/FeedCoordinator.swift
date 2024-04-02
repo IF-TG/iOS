0be49248591728b9bd8483c0f526ce204af0ae94
shcommit <happysh_s2@naver.com>
2024-04-02 21:56:22 +0900

    [Feat] FeedCoordinator에서 포스트 상세 화면 갈 때 PostDetailCoordinator이용하도록 로직 변경

--- travelPlan/Source/Presentation/Feed/Coordinator/FeedCoordinator.swift
+++ travelPlan/Source/Presentation/Feed/Coordinator/FeedCoordinator.swift
@@ -71,9 +71,8 @@ final class FeedCoordinator: FlowCoordinator {
 // MARK: - FeedPostCoordinatorDelegate
 extension FeedCoordinator: FeedPostCoordinatorDelegate {
   func showDetailPost(post: Post, category: PostCategory) {
-    let postUseCase = DefaultPostUseCase(postRepository: MockPostRepository())
-    let postDetailViewModel = PostDetailViewModel(post: post, category: category, postUseCase: postUseCase)
-    presenter?.pushViewController(PostDetailViewController(viewModel: postDetailViewModel), animated: true)
+    let childCoordinator = PostDetailCoordinator(presenter: presenter, post: post, category: category)
+    addChild(with: childCoordinator)
   }
 }
 
