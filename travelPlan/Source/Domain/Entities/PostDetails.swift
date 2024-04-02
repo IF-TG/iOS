f1b2b12fdab201eeff61c523dcdd85ce2c26f5f7
shcommit <happysh_s2@naver.com>
2024-04-02 18:37:24 +0900

    [Feat] PostDetails 생성 및 구현

---
+++ travelPlan/Source/Domain/Entities/PostDetails.swift
@@ -0,0 +1,15 @@
+//
+//  PostDetails.swift
+//  travelPlan
+//
+//  Created by 양승현 on 4/2/24.
+//
+
+import Foundation
+
+struct PostDetails {
+  let detail: Post.Detail<[PostDetailContentType]>
+  let author: Post.Author
+  let isFavorite: Bool
+  let category: PostCategory
+}
