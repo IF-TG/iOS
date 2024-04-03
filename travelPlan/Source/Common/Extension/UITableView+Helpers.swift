40fdfd7e9d5394562d25584db1604d969ac5870c
shcommit <happysh_s2@naver.com>
2024-04-03 19:28:53 +0900

    [Common] UITableView extension에 section(for:) 구현. 섹션 헤더푸터 인스턴스로 특정 section Int 반환로직 구현

---
+++ travelPlan/Source/Common/Extension/UITableView+Helpers.swift
@@ -0,0 +1,20 @@
+//
+//  UITableView+Helpers.swift
+//  travelPlan
+//
+//  Created by 양승현 on 4/3/24.
+//
+
+import UIKit
+
+// MARK: - TableView Section Helpers
+extension UITableView {
+  func section(for header: UITableViewHeaderFooterView) -> Int? {
+    let relativePoint = convert(header.frame.origin, to: self)
+    for specificSection in 0..<self.numberOfSections
+    where rectForHeader(inSection: specificSection).isContained(point: relativePoint) {
+      return specificSection
+    }
+    return nil
+  }
+}
