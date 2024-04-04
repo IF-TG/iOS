7fa08f705fb7689e9e577ed49bcb820d5741fa7f
shcommit <happysh_s2@naver.com>
2024-04-04 17:45:35 +0900

    [Feat] DefaultFavoritePostInDirectoryUseCase 생성 및 fetchFavoritePosts구현

---
+++ travelPlan/Source/Domain/UseCases/DefaultFavoritePostInDirectoryUseCase.swift
@@ -0,0 +1,38 @@
+//
+//  DefaultFavoritePostInDirectoryUseCase.swift
+//  travelPlan
+//
+//  Created by 양승현 on 4/4/24.
+//
+
+import Foundation
+import Combine
+
+final class DefaultFavoritePostInDirectoryUseCase {
+  // MARK: - Dependencies
+  private let favoritePostInDirectoryRepository: FavoritePostInDirectoryRepository
+  
+  private let backgroundQueue: DispatchQueue
+  
+  // MARK: - Lifecycle
+  init(
+    favoritePostInDirectoryRepository: FavoritePostInDirectoryRepository,
+    backgroundQueue: DispatchQueue = .global(qos: .default)
+  ) {
+    self.favoritePostInDirectoryRepository = favoritePostInDirectoryRepository
+    self.backgroundQueue = backgroundQueue
+  }
+}
+
+// MARK: - FavoritePostInDirectoryUseCase
+extension DefaultFavoritePostInDirectoryUseCase: FavoritePostInDirectoryUseCase {
+  func fetchFavoritePosts(
+    name directoryName: String,
+    page: Int32, perPage: Int32
+  ) -> AnyPublisher<[Post], any Error> {
+    return favoritePostInDirectoryRepository
+      .fetchFavoritePosts(name: directoryName, page: page, perPage: perPage)
+      .subscribe(on: backgroundQueue)
+      .eraseToAnyPublisher()
+  }
+}
