aa54465e9047da1010ca801e44e290ce3b7ef272
shcommit <happysh_s2@naver.com>
2024-04-04 11:45:54 +0900

    [Feat] PostDetailCoordinator에서 알림창 액션추가 helper추가, 대댓작성시 취소할경우 취소확인알림창 로직추가 및 알림창액션추가리빌딩

--- travelPlan/Source/Presentation/PostDetail/Coordinator/PostDetailCoordinator.swift
+++ travelPlan/Source/Presentation/PostDetail/Coordinator/PostDetailCoordinator.swift
@@ -10,8 +10,10 @@ import SHCoordinator
 
 protocol PostDetailCoordinatorDelegate: FlowCoordinatorDelegate {
   func showAlertForError(with description: String, completion: (() -> Void)?)
+  func showAnAlertToAskWhetherToCancelWrittingTheReply(completion: ((Bool) -> Void)?)
 }
 
+// MARK: - PostDetailCoordinator
 final class PostDetailCoordinator: FlowCoordinator {
   var parent: FlowCoordinator?
   var child: [FlowCoordinator] = []
@@ -52,13 +54,27 @@ final class PostDetailCoordinator: FlowCoordinator {
   }
 }
 
+// MARK: - PostDetailCoordinatorDelegate
 extension PostDetailCoordinator: PostDetailCoordinatorDelegate {
   func showAlertForError(with description: String, completion: (() -> Void)?) {
     let alert = UIAlertController(title: nil, message: description, preferredStyle: .alert).set {
-      $0.addAction(UIAlertAction(title: "OK", style: .default) { _ in
-        completion?()
-      })
+      $0.addAction(title: "OK", style: .default) { _ in completion?() }
     }
     postDetailViewController.present(alert, animated: true)
+  }
+  
+  func showAnAlertToAskWhetherToCancelWrittingTheReply(completion: ((Bool) -> Void)?) {
+    let alert = UIAlertController(title: "작성 중인 대댓글을 취소하시겠습니까?", message: nil, preferredStyle: .alert).set {
+      $0.addAction(title: "예", style: .default) { _ in completion?(true) }
+      $0.addAction(title: "아니요", style: .cancel) { _ in completion?(false) }
+    }
+    postDetailViewController.present(alert, animated: true)
+  }
+}
+
+// MARK: - Fileprivate Helpers
+fileprivate extension UIAlertController {
+  func addAction(title: String, style: UIAlertAction.Style, handler: ((UIAlertAction) -> Void)?) {
+    self.addAction(UIAlertAction(title: title, style: style, handler: handler))
   }
 }
