d8685a00ade1442c6d05eb3889245bdeb7d974ad
seokhyun <letushyun@gmail.com>
2024-04-05 02:07:45 +0900

    [Feat] AlbumPhotoDetailViewModel 추가 및 일부 구현

--- travelPlan/Source/Presentation/Album/ViewModel/AlbumPhotoDetailViewModel.swift
+++ travelPlan/Source/Presentation/Album/ViewModel/AlbumPhotoDetailViewModel.swift
@@ -6,3 +6,72 @@
 //
 
 import Foundation
+import Combine
+
+protocol AlbumPhotoDetailViewModelable: ViewModelable
+where Input == AlbumPhotoDetailViewModelInput,
+      State == AlbumPhotoDetailViewModelState,
+      Output == AnyPublisher<State, Never> { }
+
+struct AlbumPhotoDetailViewModelInput {
+  let didTapOrderView: PassthroughSubject<Void, Never> = .init()
+  let didTapBackButton: PassthroughSubject<Void, Never> = .init()
+  let didTapCancelButton: PassthroughSubject<Void, Never> = .init()
+}
+
+enum AlbumPhotoDetailViewModelState {
+  case popViewController
+  case setOrder(Int)
+  case cancelOrder
+  case none
+}
+
+final class DefaultAlbumPhotoDetailViewModel {
+  // MARK: - Properties
+  
+  // MARK: - LifeCycle
+  init() {
+    
+  }
+}
+
+// MARK: - AlbumPhotoDetailViewModelable
+extension DefaultAlbumPhotoDetailViewModel: AlbumPhotoDetailViewModelable {
+  func transform(_ input: Input) -> Output {
+    return Publishers.MergeMany(
+      didTapOrderViewStream(input),
+      didTapBackButtonStream(input),
+      didTapCancelButtonStream(input)
+    )
+    .eraseToAnyPublisher()
+  }
+}
+ 
+// MARK: - Private Helpers
+extension DefaultAlbumPhotoDetailViewModel {
+  private func didTapCancelButtonStream(_ input: Input) -> Output {
+    input.didTapCancelButton
+      .map {
+        return State.popViewController
+      }
+      .eraseToAnyPublisher()
+  }
+  
+  private func didTapOrderViewStream(_ input: Input) -> Output {
+    input.didTapOrderView
+      .map {
+        print("didTapOrderView")
+        return State.none
+      }
+      .eraseToAnyPublisher()
+  }
+  
+  private func didTapBackButtonStream(_ input: Input) -> Output {
+    input.didTapBackButton
+      .map {
+        print("didTapBackButton")
+        return State.none
+      }
+      .eraseToAnyPublisher()
+  }
+}
