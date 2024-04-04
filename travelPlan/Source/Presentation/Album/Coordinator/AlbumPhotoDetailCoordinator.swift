90a0e9a8c49652793c50acc2d8b831eedd01f48b
seokhyun <letushyun@gmail.com>
2024-04-05 02:05:39 +0900

    [Feat] AlbumPhotoDetailCoordinator 일부 구현

--- travelPlan/Source/Presentation/Album/Coordinator/AlbumPhotoDetailCoordinator.swift
+++ travelPlan/Source/Presentation/Album/Coordinator/AlbumPhotoDetailCoordinator.swift
@@ -5,4 +5,42 @@
 //  Created by SeokHyun on 4/3/24.
 //
 
-import Foundation
+import SHCoordinator
+import UIKit
+import Photos
+
+protocol AlbumPhotoDetailCoordinatorDelegate: FlowCoordinatorDelegate {
+  
+}
+
+final class AlbumPhotoDetailCoordinator: FlowCoordinator {
+  
+  // MARK: - Properties
+  var parent: FlowCoordinator?
+  var child: [FlowCoordinator] = []
+  var presenter: UINavigationController?
+  private let asset: PHAsset
+  
+  // MARK: - LifeCycle
+  init(presenter: UINavigationController?, asset: PHAsset) {
+    self.presenter = presenter
+    self.asset = asset
+  }
+  
+  func start() {
+    let albumPhotoDetailViewModel = DefaultAlbumPhotoDetailViewModel()
+    let photoService = DefaultPhotoService()
+    let albumPhotoDetailViewController = AlbumPhotoDetailViewController(
+      viewModel: albumPhotoDetailViewModel,
+      photoService: photoService,
+      asset: asset
+    )
+    albumPhotoDetailViewController.coordinator = self
+    presenter?.pushViewController(albumPhotoDetailViewController, animated: true)
+  }
+}
+
+// MARK: - AlbumPhotoDetailCoordinatorDelegate
+extension AlbumPhotoDetailCoordinator: AlbumPhotoDetailCoordinatorDelegate {
+  
+}
