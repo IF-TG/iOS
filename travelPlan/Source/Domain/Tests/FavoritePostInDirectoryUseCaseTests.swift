a4042ae6b09232bf5bfeac6d3e27c0807d072eae
shcommit <happysh_s2@naver.com>
2024-04-05 15:37:52 +0900

    [Test] FavoritePostInDirectoryUseCase에 toggleFavoritePost호출시 원하는 responseDTO 받아지는지 테스트

--- travelPlan/Source/Domain/Tests/FavoritePostInDirectoryUseCaseTests.swift
+++ travelPlan/Source/Domain/Tests/FavoritePostInDirectoryUseCaseTests.swift
@@ -59,4 +59,28 @@ extension FavoritePostInDirectoryUseCaseTests {
     checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "fetchFavoritePosts")
     XCTAssertTrue(result, notReceivedErrorMessage)
   }
+  
+  func test_toggleFavoritePost함수호출시_연관entity를받았는지_ShouldReturnTrue() {
+    // Arrange
+    var result = false
+    var unexpectedError: Error?
+    
+    // Act
+    subscription = sut.toggleFavoritePost(postId: 1, directoryName: "맛집폴더")
+      .sink { [unowned self] completion in
+        if case .failure(let error) = completion {
+          unexpectedError = error
+        }
+        expectation.fulfill()
+      } receiveValue: { [unowned self] entity in
+        print("DEBUG: 값을 성공적으로 받았습니다.\n\n:\(entity)\n\n\n")
+        result = true
+        expectation.fulfill()
+      }
+    wait(for: [expectation], timeout: 3)
+    
+    // Assert
+    checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "toggleFavoritePost")
+    XCTAssertTrue(result, notReceivedErrorMessage)
+  }
 }
