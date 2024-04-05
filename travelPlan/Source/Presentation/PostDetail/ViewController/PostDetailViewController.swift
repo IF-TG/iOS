a2b70ddb6c8c2872329aadf6aabaf22b2d1ad2ee
shcommit <happysh_s2@naver.com>
2024-04-05 12:35:18 +0900

    [Fix] PR반영. PostDetailVC에서 reloadSection 애니메이션 없게 동작처리

--- travelPlan/Source/Presentation/PostDetail/ViewController/PostDetailViewController.swift
+++ travelPlan/Source/Presentation/PostDetail/ViewController/PostDetailViewController.swift
@@ -168,13 +168,11 @@ extension PostDetailViewController: ViewBindCase {
   
   func handleNestedCommentState(_ nestedCommentState: PostDetailNestedCommentState) {
     switch nestedCommentState {
-    case .sentSuccessfully:
+    case .sentSuccessfully(let section):
       inputAccessory.hideKeyboard()
-      /// 이상하게 reloadData하면 잘 됩니다.
-      /// 테이블뷰 리로드 섹션할때 키보드 에니메이션도 동작되서그런건지 section내 특정 row가 위로 샤라락하면서 없어집니다.
-      /// hideKeyboard() 애니메이션이 완료된 시점이후에 reloadSections를 해도 그렇습니다.
-      tableView.reloadData()
-      // tableView.reloadSections(IndexSet(integer: section), with: .fade)
+      UITableView.performWithoutAnimation {
+        tableView.reloadSections(IndexSet(integer: section), with: .none)
+      }
       stopIndicator()
     case .replyCancel:
       inputAccessory.clearCommentInputState()
