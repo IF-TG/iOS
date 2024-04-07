5207c9dd291823c38941aeab5648d75e1ad709b7
shcommit <happysh_s2@naver.com>
2024-04-07 22:22:08 +0900

    [Style] PostDetailVC에서 TableView register 코드 extension파일로 이동

--- travelPlan/Source/Presentation/PostDetail/ViewController/PostDetailViewController.swift
+++ travelPlan/Source/Presentation/PostDetail/ViewController/PostDetailViewController.swift
@@ -241,40 +241,8 @@ private extension PostDetailViewController {
     navigationItem.titleView = naviTitle
     naviTitle.alpha = 0
   }
-  
-  func registerReusableViews() {
-    tableView.register(
-      PostDetailCategoryHeaderView.self,
-      forHeaderFooterViewReuseIdentifier: PostDetailCategoryHeaderView.id)
-    tableView.register(
-      PostDetailTitleCell.self,
-      forCellReuseIdentifier: PostDetailTitleCell.id)
-    tableView.register(
-      PostDetailProfileAreaFooterView.self,
-      forHeaderFooterViewReuseIdentifier: PostDetailProfileAreaFooterView.id)
-    
-    tableView.register(
-      PostDetailContentTextCell.self,
-      forCellReuseIdentifier: PostDetailContentTextCell.id)
-    tableView.register(
-      PostDetailContentImageCell.self,
-      forCellReuseIdentifier: PostDetailContentImageCell.id)
-    tableView.register(
-      PostDetailContentFooterView.self,
-      forHeaderFooterViewReuseIdentifier: PostDetailContentFooterView.id)
-    tableView.register(
-      PostHeartAndShareAreaHeaderView.self,
-      forHeaderFooterViewReuseIdentifier: PostHeartAndShareAreaHeaderView.id)
-    
-    tableView.register(
-      PostDetailCommentHeader.self,
-      forHeaderFooterViewReuseIdentifier: PostDetailCommentHeader.id)
-    tableView.register(
-      PostDetailReplyCell.self,
-      forCellReuseIdentifier: PostDetailReplyCell.id)
-  }
 }
-
+  
 // MARK: - Actions
 extension PostDetailViewController {
   @objc private func didTapTableView() {
