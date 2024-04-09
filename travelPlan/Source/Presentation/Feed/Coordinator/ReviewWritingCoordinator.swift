68a036aff18cbebf1fcf823021c6e40ed7790601
shcommit <happysh_s2@naver.com>
2024-04-10 02:27:49 +0900

    [Fix] ReviweWritingCoordinator 에서 bottomSheet 보여주는 로직 수정:)

--- travelPlan/Source/Presentation/Feed/Coordinator/ReviewWritingCoordinator.swift
+++ travelPlan/Source/Presentation/Feed/Coordinator/ReviewWritingCoordinator.swift
@@ -65,14 +65,7 @@ extension ReviewWritingCoordinator: ReviewWritingCoordinatorDelegate {
   func showCategoryBottomSheet() {
     let bottomSheet = PostReviewWritingCategoryBottomSheet()
     bottomSheet.dismissHandler = { [weak self] in
-      // TODO: - 추가로직 정하기
-      // dismiss후 여행 후기 작성 화면으로 돌아간 로딩 화면과 함께 서버 전송 후 피드로 갈것인지
-      // 바텀시트 시점에서 서버 전송 후 뒤로가기 두 번 할 것인지
-      // 뭔가,, 리뷰작성으로 돌아간 후에 인디케이터 -> 완료되면 완료되었습니다! 알림창 0.7초후 -> 뒤로가는 것도 좋을 것 같습니다.
       print(bottomSheet.selectedCategory)
-      if bottomSheet.hasSelectedAtLeastOneTheme {
-        self?.finish(withAnimated: true)
-      }
     }
     presenter?.presentBottomSheet(bottomSheet)
   }
