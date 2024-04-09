06983fdcbfb988da106c3f8438c0b395759e3e61
shcommit <happysh_s2@naver.com>
2024-04-10 00:17:20 +0900

    [Fix] PrimaryColorToneRoundButton 레이어 board layer width 제거

--- travelPlan/Source/Common/View/PrimaryColorToneRoundButton.swift
+++ travelPlan/Source/Common/View/PrimaryColorToneRoundButton.swift
@@ -81,7 +81,6 @@ extension PrimaryColorToneRoundButton {
     translatesAutoresizingMaskIntoConstraints = false
     layer.cornerRadius = Constant.cornerRadius
     titleLabel?.font = Constant.font
-    layer.borderWidth = 1.5
     setTitleColor(Constant.textColor, for: .normal)
     setTitle("확인", for: .normal)
     addTarget(self, action: #selector(didTapPrimaryColorToneRoundButton), for: .touchUpInside)
