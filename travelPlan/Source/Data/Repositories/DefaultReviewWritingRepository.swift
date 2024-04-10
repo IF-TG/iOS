c97049ede3fe170c40058bc231fa7caa317e3f55
seokhyun <letushyun@gmail.com>
2024-04-11 03:57:48 +0900

    [Add] ReviewWritingRepository 추가

---
+++ travelPlan/Source/Data/Repositories/DefaultReviewWritingRepository.swift
@@ -0,0 +1,20 @@
+//
+//  DefaultReviewWritingRepository.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 4/9/24.
+//
+
+import Foundation
+
+final class DefaultReviewWritingRepository {
+  // MARK: - Properties
+  
+  // MARK: - LifeCycle
+  
+}
+
+// MARK: - ReviewWritingRepository
+extension DefaultReviewWritingRepository: ReviewWritingRepository {
+  
+}
---
+++ travelPlan/Source/Domain/Interfaces/Repositories/ReviewWritingRepository.swift
@@ -0,0 +1,12 @@
+//
+//  ReviewWritingRepository.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 4/9/24.
+//
+
+import Foundation
+
+protocol ReviewWritingRepository {
+  
+}
