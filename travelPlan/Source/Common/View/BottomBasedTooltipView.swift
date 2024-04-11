2182b5db104dbcd3664564e2be3438e285d37d7a
shcommit <happysh_s2@naver.com>
2024-04-12 00:19:53 +0900

    [Feat] 툴팁뷰 tipPath, shapeLayer, drawToolTip 함수 추가

--- travelPlan/Source/Common/View/BottomBasedTooltipView.swift
+++ travelPlan/Source/Common/View/BottomBasedTooltipView.swift
@@ -28,14 +28,17 @@ class BottomBasedTooltipView: UIView {
   
   private var defaultRect: CGRect!
   
+  private let colorInfo: ShadowInfo
+  
   init(
     frame: CGRect,
     toolTipSize: CGSize = .init(width: 20, height: 14),
     tipOffset: CGFloat = 20,
     tipPosition: TipPosition = .middle,
     cornerRadius: CGFloat = 7,
+    colorInfo: ShadowInfo,
     message: String,
-    labelPadding: UIEdgeInsets,
+    labelPadding: UIEdgeInsets = .init(top: 7, left: 7, bottom: 7, right: 7),
     labelFontType: UIFont.Pretendard
   ) {
     self.toolTipSize = toolTipSize
@@ -52,16 +55,13 @@ class BottomBasedTooltipView: UIView {
       $0.numberOfLines = 0
     }
     label.text = message
+    self.colorInfo = colorInfo
     super.init(frame: frame)
   }
   
   override func draw(_ rect: CGRect) {
     super.draw(rect)
-    defaultRect = CGRect(
-      x: rect.minX, y: rect.minY,
-      width: rect.width, height: rect.height-toolTipSize.height)
-    let roundRectBez = UIBezierPath(roundedRect: defaultRect, cornerRadius: cornerRadius)
-
+    drawTooltip(rect)
   }
   
   required init?(coder: NSCoder) { nil }
@@ -82,6 +82,41 @@ private extension BottomBasedTooltipView {
         return defaultRect.maxX-toolTipSize.width-5
       }
       return startX
+    }
+  }
+  
+  func drawTooltip(_ rect: CGRect) {
+    defaultRect = CGRect(
+      x: rect.minX, y: rect.minY,
+      width: rect.width,
+      height: rect.height-toolTipSize.height)
+    let contentRectPath = UIBezierPath(roundedRect: defaultRect, cornerRadius: cornerRadius)
+    let trianglePath = makeTipPath()
+    contentRectPath.append(trianglePath)
+    layer.insertSublayer(makeShapeLayer(contentRectPath.cgPath), at: 0)
+  }
+  
+  func makeTipPath() -> UIBezierPath {
+    let tooltipRect = CGRect(
+      origin: .init(x: getStartX(), y: defaultRect.maxY),
+      size: toolTipSize)
+    return UIBezierPath().set {
+      $0.move(to: CGPoint(x: tooltipRect.minX, y: tooltipRect.minY))
+      $0.addLine(to: CGPoint(x: tooltipRect.midX, y: tooltipRect.maxY))
+      $0.addLine(to: CGPoint(x: tooltipRect.maxX, y: tooltipRect.minY))
+      $0.addLine(to: CGPoint(x: tooltipRect.minX, y: tooltipRect.minY))
+      $0.close()
+    }
+  }
+  
+  func makeShapeLayer(_ path: CGPath) -> CAShapeLayer {
+    return CAShapeLayer().set {
+      $0.path = path
+      $0.fillColor = colorInfo.color.cgColor
+      $0.shadowColor = colorInfo.color.withAlphaComponent(0.4).cgColor
+      $0.shadowOffset = CGSize(width: 0, height: 2)
+      $0.shadowRadius = 7
+      $0.shadowOpacity = 0.7
     }
   }
 }
