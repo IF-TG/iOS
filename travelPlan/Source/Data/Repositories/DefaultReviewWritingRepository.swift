26e489c93ed8f0a10f946b7a466b12244399f504
seokhyun <letushyun@gmail.com>
2024-04-12 18:04:16 +0900

    [Feat] repository 프로토콜 메소드 본체 임시 구현

--- travelPlan/Source/Data/Repositories/DefaultReviewWritingRepository.swift
+++ travelPlan/Source/Data/Repositories/DefaultReviewWritingRepository.swift
@@ -6,15 +6,26 @@
 //
 
 import Foundation
+import Combine
 
 final class DefaultReviewWritingRepository {
+  // MARK: - Dependencies
+  private let service: Sessionable
+  private let backgroundQueue: DispatchQueue
+  
   // MARK: - Properties
+  private var subscriptions = Set<AnyCancellable>()
   
   // MARK: - LifeCycle
-  
+  init(service: Sessionable, backgroundQueue: DispatchQueue = .global(qos: .background)) {
+    self.service = service
+    self.backgroundQueue = backgroundQueue
+  }
 }
 
 // MARK: - ReviewWritingRepository
 extension DefaultReviewWritingRepository: ReviewWritingRepository {
-  
+  func uploadPost() -> AnyPublisher<Bool, Never> {
+    return Just(true).eraseToAnyPublisher()
+  }
 }
--- travelPlan/Source/Domain/Interfaces/Repositories/ReviewWritingRepository.swift
+++ travelPlan/Source/Domain/Interfaces/Repositories/ReviewWritingRepository.swift
@@ -6,7 +6,8 @@
 //
 
 import Foundation
+import Combine
 
 protocol ReviewWritingRepository {
-  
+  func uploadPost() -> AnyPublisher<Bool, Never>
 }
