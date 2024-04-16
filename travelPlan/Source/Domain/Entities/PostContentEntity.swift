00351462af84ca2d706538653cb8c326d86366aa
seokhyun <letushyun@gmail.com>
2024-04-17 06:23:39 +0900

    [Comment] TODO 추가
    TODO: - 서버에서 이미지를 저장할 때 이미지 타입도 같이 저장하기 때문에, iOS에서 서버로 이미지 확장자명을 보내야합니다.

--- travelPlan/Source/Domain/Entities/PostContentEntity.swift
+++ travelPlan/Source/Domain/Entities/PostContentEntity.swift
@@ -9,5 +9,13 @@ import Foundation
 
 enum PostContentEntity {
   case text(String)
+  // TODO: - 서버에서 이미지를 저장할 때 이미지 타입도 같이 저장하기 때문에, iOS에서 서버로 이미지 확장자명을 보내야합니다.
+//  case image(Data, ImageType)
   case image(Data)
 }
+
+//enum ImageType: String {
+//  case jpg
+//  case jpeg
+//  case png
+//}
