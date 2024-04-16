298e7b3104a7c6fbdd73a7ac22080a9049a3da0c
seokhyun <letushyun@gmail.com>
2024-04-17 06:20:07 +0900

    [Feat] 리뷰작성 endponint 구현

--- travelPlan/Source/Data/Network/ReviewWritingEndpoints.swift
+++ travelPlan/Source/Data/Network/ReviewWritingEndpoints.swift
@@ -8,11 +8,23 @@
 import Foundation
 
 struct ReviewWritingEndpoints {
-  static func savePost(with requestDTO: ReviewWritingSaveRequestDTO) -> Endpoint<CommonDTO<Bool>> {
+  static func updatePost(with requestDTO: ReviewWritingUpdateRequestDTO) -> Endpoint<CommonDTO<PostResponseDTO>> {
     return .init(
       scheme: "http",
       host: "localhost:8080",
       method: .post,
+      parameters: [.body(requestDTO)],
+      requestType: .post(.save)
+    )
+  }
+  
+  // FIXME: - fireBase 구현에 따라 Endpoint<Void>로 만들지 말지 결정하기
+  static func savePost(with requestDTO: ReviewWritingSaveRequestDTO)
+  -> Endpoint<ReviewWritingSaveResponseDTO> {
+    return .init(
+      scheme: "http",
+      host: "localhost:8080",
+      method: .put,
       parameters: [.body(requestDTO)],
       requestType: .post(.save)
     )
