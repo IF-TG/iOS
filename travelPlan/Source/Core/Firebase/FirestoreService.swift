539af7fc17a512b544ea4521c2013b4396c821cb
shcommit <happysh_s2@naver.com>
2024-04-19 01:15:04 +0900

    [Feat] FirestoreService에서 request(_:)함수 생성 및 초반부 구현

--- travelPlan/Source/Core/Firebase/FirestoreService.swift
+++ travelPlan/Source/Core/Firebase/FirestoreService.swift
@@ -2,7 +2,29 @@
 //  FirestoreService.swift
 //  travelPlan
 //
-//  Created by 양승현 on 4/18/24.
+//  Created by 양승현 on 4/19/24.
 //
 
 import Foundation
+import Combine
+import FirebaseFirestore
+
+final class FirestoreService {
+  static func request<ResponseDTO>(
+    _ endpoint: any FirestoreEndopint
+  ) -> AnyPublisher<ResponseDTO, Error> {
+    if endpoint.reference is CollectionReference {
+      guard let collectionRef = endpoint.reference as? CollectionReference else {
+        return Fail(error: FirestoreServiceError.collectionNotFound)
+          .eraseToAnyPublisher()
+      }
+      // TODO: - 도큐들 반환하자
+    } else {
+      guard let documentRef = endpoint.reference as? DocumentReference else {
+        return Fail(error: FirestoreServiceError.docuemntNotfound)
+          .eraseToAnyPublisher()
+      }
+      // TODO: - 문서 반환하자.
+    }
+  }
+}
