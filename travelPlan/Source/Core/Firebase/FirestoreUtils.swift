cecd43ccf89dc53f3d939be15a035f7d77bd491a
shcommit <happysh_s2@naver.com>
2024-04-18 18:17:52 +0900

    [Feat] FirestoreUtils protocol 생성 및 기능들 구현

--- travelPlan/Source/Core/Firebase/FirestoreUtils.swift
+++ travelPlan/Source/Core/Firebase/FirestoreUtils.swift
@@ -1,12 +1,25 @@
 //
-//  Utils.swift
+//  FirestoreUtils.swift
 //  travelPlan
 //
 //  Created by 양승현 on 4/18/24.
 //
 
-import Foundation
+import FirebaseFirestore
+import FirebaseAuth
 
-protocol FirestoreUtils {
+protocol FirestoreUtils {}
+
+extension FirestoreUtils {
+  var db: Firestore {
+    return Firestore.firestore()
+  }
   
+  func reference(with type: FirestoreCollectionType) -> CollectionReference {
+    return db.collection(type.rawValue)
+  }
+  
+  var auth: Auth {
+    return Auth.auth()
+  }
 }
