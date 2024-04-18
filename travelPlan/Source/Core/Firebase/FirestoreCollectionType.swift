1539dfc804a40ab02ce9ed6136bfc72a54b040d6
shcommit <happysh_s2@naver.com>
2024-04-18 18:18:32 +0900

    [Feat] FirestoreCollectionType 에 case추가

--- travelPlan/Source/Core/Firebase/FirestoreCollectionType.swift
+++ travelPlan/Source/Core/Firebase/FirestoreCollectionType.swift
@@ -1,19 +1,13 @@
 //
-//  FirestoreRequestType.swift
+//  FirestoreCollectionType.swift
 //  travelPlan
 //
 //  Created by 양승현 on 4/18/24.
 //
 
 import Foundation
-protocol FirestoreConstant { }
 
 @frozen enum FirestoreCollectionType: String {
   case users = "users"
   case posts = "posts"
-}
-
-// MARK: - FirestoreRequestType+UserRequestType
-extension FirestoreRequestType {
-  
 }
