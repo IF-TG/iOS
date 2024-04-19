9687474e4b214575ebe050f0cdeb033feee1cf2d
shcommit <happysh_s2@naver.com>
2024-04-19 11:03:52 +0900

    [Fix] FirestoreService에서 하나의 request에서 responseDTO타입에 따라 단일, 배열 처리하려고했으나 디코딩이 좋지않아서 분리

--- travelPlan/Source/Core/Firebase/FirestoreService.swift
+++ travelPlan/Source/Core/Firebase/FirestoreService.swift
@@ -7,24 +7,17 @@
 
 import Foundation
 import Combine
-import FirebaseFirestore
 
-final class FirestoreService {
-  static func request<ResponseDTO>(
-    _ endpoint: any FirestoreEndopint
-  ) -> AnyPublisher<ResponseDTO, Error> {
-    if endpoint.reference is CollectionReference {
-      guard let collectionRef = endpoint.reference as? CollectionReference else {
-        return Fail(error: FirestoreServiceError.collectionNotFound)
-          .eraseToAnyPublisher()
-      }
-      // TODO: - 도큐들 반환하자
-    } else {
-      guard let documentRef = endpoint.reference as? DocumentReference else {
-        return Fail(error: FirestoreServiceError.docuemntNotfound)
-          .eraseToAnyPublisher()
-      }
-      // TODO: - 문서 반환하자.
-    }
-  }
+protocol FirestoreService {
+  func request<D, E>(endpoint: E) -> AnyPublisher<[D], Error>
+  where D: Decodable,
+        E: FirestoreEndopintable,
+        D == E.ResponseDTO
+  
+  func request<D, E>(endpoint: E) -> AnyPublisher<D, Error>
+  where D: Decodable,
+        E: FirestoreEndopintable,
+        D == E.ResponseDTO
+  
+  func request(endpoint: any FirestoreEndopintable) -> AnyPublisher<Void, Error>
 }
