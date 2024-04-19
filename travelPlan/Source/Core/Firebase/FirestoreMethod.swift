20666397be09fb5114dc2cecf4b1e8b0501f7848
shcommit <happysh_s2@naver.com>
2024-04-19 11:08:43 +0900

    [Feat] FirestoreMethod 정의

---
+++ travelPlan/Source/Core/Firebase/FirestoreMethod.swift
@@ -0,0 +1,16 @@
+//
+//  FirestoreMethod.swift
+//  travelPlan
+//
+//  Created by 양승현 on 4/18/24.
+//
+
+import Foundation
+import FirebaseFirestore
+
+@frozen enum FirestoreMethod {
+  case get
+  case save(Encodable)
+  case delete
+  case update(Encodable)
+}
