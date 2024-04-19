384ac4a3505116e3718d2c9abb1a4c2419942058
shcommit <happysh_s2@naver.com>
2024-04-19 11:09:09 +0900

    [Feat] FirestoreEndopintable 구현

---
+++ travelPlan/Source/Core/Firebase/FirestoreEndpointable.swift
@@ -0,0 +1,31 @@
+//
+//  FirestoreEndpointable.swift
+//  travelPlan
+//
+//  Created by 양승현 on 4/19/24.
+//
+
+import Foundation
+import FirebaseFirestore
+
+protocol FirestoreEndopintable {
+  associatedtype ResponseDTO: Decodable
+  
+  var parameters: (any Encodable)? { get }
+  var method: FirestoreMethod { get }
+  var requestType: FirestoreRequestType { get }
+}
+
+extension FirestoreEndopintable {
+  var firestore: Firestore {
+    Firestore.firestore()
+  }
+  
+  /// DocuemntRef가 존재하지 않는다면 endpoint에서 collection에 대한 reqeust를 이용하는 것으로 간주합니다.
+  var reference: FirestoreReference {
+    guard let documentRef = requestType.documentRef else {
+      return requestType.collectionRef
+    }
+    return documentRef
+  }
+}
