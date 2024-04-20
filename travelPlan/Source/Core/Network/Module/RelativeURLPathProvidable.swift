41ce7e040b5930c68b5cd9ee10873e8ccdc8d5d0
shcommit <happysh_s2@naver.com>
2024-04-20 15:33:06 +0900

    [Feat] Endopint requestType 의존성 낮추기

---
+++ travelPlan/Source/Core/Network/Module/RelativeURLPathProvidable.swift
@@ -0,0 +1,12 @@
+//
+//  RelativeURLPathProvidable.swift
+//  travelPlan
+//
+//  Created by 양승현 on 4/20/24.
+//
+
+import Foundation
+
+protocol RelativeURLPathProvidable {
+  var path: String { get }
+}
