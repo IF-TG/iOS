436aa397456003a20274abf9a08e9026a9a2306b
shcommit <happysh_s2@naver.com>
2024-04-20 17:24:30 +0900

    [Style] TourDestinationDetailCommonEntity 구조화

--- travelPlan/Source/Domain/Entities/TourDestinationDetailCommonEntity.swift
+++ travelPlan/Source/Domain/Entities/TourDestinationDetailCommonEntity.swift
@@ -8,20 +8,36 @@
 import Foundation
 
 struct TourDestinationDetailCommonEntity {
-  /// **기본 정보 조화**
-  let contentId: Int
-  let contentTypeId: Int
-  let telNumber: String
-  let telName: String
-  
-  /// **주소 정보 조회**
-  let address1: String
-  /// 상세 주소
-  let address2: String
+  let id: Id
+  let address: addressInfo
+  let contact: ContactInfo
+  let coordinate: Coordinate
+  let overview: String
+}
+
+extension TourDestinationDetailCommonEntity {
+  /// **기본 정보 조회**
+  struct Id {
+    let contentId: Int
+    let contentTypeId: Int
+  }
   
   /// **좌표 정보 조회**
-  let mapx: Double
-  let mapy: Double
+  struct Coordinate {
+    let mapX: Double
+    let mapY: Double
+  }
   
-  let overview: String
+  /// **컨텐츠 연락 정보**
+  struct ContactInfo {
+    let telNumber: String
+    let telName: String
+  }
+  
+  /// **주소 정보 조회**
+  struct addressInfo {
+    let address1: String
+    /// 상세 주소
+    let address2: String
+  }
 }
