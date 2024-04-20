421ad9db19ce3f2671844741214cbc09d11166f5
shcommit <happysh_s2@naver.com>
2024-04-20 17:25:48 +0900

    [Feat] TourApiDetailCommonResponseDTO에 toDomain() 구현

--- travelPlan/Source/Data/Network/TourAPI/ResponseDTO/TourApiDetailCommonResponseDTO.swift
+++ travelPlan/Source/Data/Network/TourAPI/ResponseDTO/TourApiDetailCommonResponseDTO.swift
@@ -31,3 +31,15 @@ final class TourApiDetailCommonResponseDTO: TourApiBaseResponseDTO, Decodable {
   
   let overview: String
 }
+
+// MARK: - Mappings to Domain
+extension TourApiDetailCommonResponseDTO {
+  func toDomain() -> TourDestinationDetailCommonEntity {
+    .init(
+      id: .init(contentId: contentid, contentTypeId: contenttypeid),
+      address: .init(address1: addr1, address2: addr2),
+      contact: .init(telNumber: tel, telName: telname),
+      coordinate: .init(mapX: mapx, mapY: mapy),
+      overview: overview)
+  }
+}
