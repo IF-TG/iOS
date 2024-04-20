5abc811a29e264037a8eb5b5e3dfa2ba336b7a26
shcommit <happysh_s2@naver.com>
2024-04-20 16:57:22 +0900

    [Style] DestinationAPIEndpoint에 주석 추가

--- travelPlan/Source/Data/Network/TourAPI/DestinationAPIEndpoint.swift
+++ travelPlan/Source/Data/Network/TourAPI/DestinationAPIEndpoint.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+/// 검색 결과 섬네일 화면에서 특정 여행지 상세 화면으로 들어갈 때 연관된 데이터들을 호출할 수 있는 Endpoint입니다..
 struct DestinationAPIEndpoint {
   static func makeDetailCommonEndpoint(
     with requestDTO: TourApiDetailCommonRequestDTO
