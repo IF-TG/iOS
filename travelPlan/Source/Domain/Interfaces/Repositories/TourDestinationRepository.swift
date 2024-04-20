d5d4e6fbd07269ff47812bc8bfc3846f49e91d78
shcommit <happysh_s2@naver.com>
2024-04-20 17:05:46 +0900

    [Feat] TourDestinationRepository 선언 및 fetchTourDestinationDetailCommon(contentId:numOfRows:pageNo:) 선언

---
+++ travelPlan/Source/Domain/Interfaces/Repositories/TourDestinationRepository.swift
@@ -0,0 +1,17 @@
+//
+//  TourDestinationRepository.swift
+//  travelPlan
+//
+//  Created by 양승현 on 4/20/24.
+//
+
+import Foundation
+import Combine
+
+protocol TourDestinationRepository {
+  func fetchTourDestinationDetailCommon(
+    contentId: Int,
+    numOfRows: Int?,
+    pageNo: Int?
+  ) -> Future<TourDestinationDetailCommonEntity, Error>
+}
