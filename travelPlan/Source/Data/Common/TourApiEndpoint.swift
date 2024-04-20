d35a0c68ad792208242b8e552ba6ad93ae4aabf3
shcommit <happysh_s2@naver.com>
2024-04-20 19:39:00 +0900

    [Feat] 불필요한 데이터 주입 삭제

--- travelPlan/Source/Data/Common/TourApiEndpoint.swift
+++ travelPlan/Source/Data/Common/TourApiEndpoint.swift
@@ -8,12 +8,12 @@
 import Alamofire
 
 final class TourApiEndpoint<ResponseDTO>: NetworkInteractionable where ResponseDTO: Decodable {
-  var scheme: String = "http"
+  var scheme: String = "https"
   var host: String = "apis.data.go.kr"
   var method: HTTPMethod = .get
   var parameters: [ParameterType]?
   var requestType: any RelativeURLPathProvidable
-  var headers: HTTPHeaders? = ["Content-Type": "application/json"]
+  var headers: HTTPHeaders?
   var interceptor: (any RequestInterceptor)?
   
   init(
