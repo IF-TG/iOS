d4d84d4d9818ae84fd66eec9408e2618b9bb8f19
shcommit <happysh_s2@naver.com>
2024-04-20 16:01:59 +0900

    [Feat] TourApiEndpoint 구현

---
+++ travelPlan/Source/Data/Common/TourApiEndpoint.swift
@@ -0,0 +1,26 @@
+//
+//  TourApiEndpoint.swift
+//  travelPlan
+//
+//  Created by 양승현 on 4/20/24.
+//
+
+import Alamofire
+
+final class TourApiEndpoint<ResponseDTO>: NetworkInteractionable where ResponseDTO: Decodable {
+  var scheme: String = "http"
+  var host: String = "apis.data.go.kr"
+  var method: HTTPMethod = .get
+  var parameters: [ParameterType]?
+  var requestType: any RelativeURLPathProvidable
+  var headers: HTTPHeaders? = ["Content-Type": "application/json"]
+  var interceptor: (any RequestInterceptor)?
+  
+  init(
+    parameters: ParameterType,
+    requestType: TourApiRequestType
+  ) {
+    self.parameters = [parameters]
+    self.requestType = requestType
+  }
+}
