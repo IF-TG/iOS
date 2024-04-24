24c1ab337b31edb9617689aadc70e011b5c0bdb8
shcommit <happysh_s2@naver.com>
2024-04-24 19:06:48 +0900

    [Feat] TourApiSessionProvider에서 reqeust 내부로직에 XML인경우 로직 추가

--- travelPlan/Source/Data/Common/TourApiSessionProvider.swift
+++ travelPlan/Source/Data/Common/TourApiSessionProvider.swift
@@ -10,24 +10,30 @@ import Combine
 import Foundation
 
 final class TourApiSessionProvider {
-  let session: Session
+  // MARK: - Properties
+  private let session: Session
+  
+  private let xmlParsingService: XMLParsingServiceProtocol
+  
+  private var subscriptions = Set<AnyCancellable?>()
+  
   private let timeout: Double
-  init(session: Session = .default, timeout: Double = 30) {
+  
+  // MARK: - Lifecycle
+  init(
+    session: Session = .default,
+    xmlParsingService: XMLParsingServiceProtocol,
+    timeout: Double = 30
+  ) {
     self.session = session
     self.timeout = timeout
+    self.xmlParsingService = xmlParsingService
     session.sessionConfiguration.timeoutIntervalForRequest = timeout
   }
 }
 
 // MARK: - Sessionable
 extension TourApiSessionProvider: Sessionable {
-  /// Tour API에서 제공하는 json의 벨류는 \n이 존재할 수 있습니다. json에서 value에 escape문자는 parsing을 방해한다고 합니다...
-  /// 역시나 JSONDecoder을 사용할떄 init(from:) 의 컨테이너에서 특정 타입을 디코딩할 때 에러가 던져집니다.
-  /// JSONSerialization 등을 사용해도 에러가 던져지는건 마찬가지입니다. JSON에서 escape character
-  ///   중 특히 \n을 사용해 깔끔히 보여 지게 하기위해 사용되는것 같기 때문입니다.
-  /// swift에선 \\n을 사용한다면 json value에서 사용될 수 있지만.. \n이 온다는 사실 ㅠㅠ
-  /// 한가지 해결 책은 \n -> \\n으로 바꾸려고햇으나 이 또한 escape 문자로 해당되서 아예 escape를 제거하는 방식으로 구현하기로 했습니다...
-  /// \n 뿐 아니라 다른 escape character가 온다면 마찬가지로 제거해야합니다.
   func request<R, E>(endpoint: E) -> Future<R, AFError>
   where R: Decodable,
         E: NetworkInteractionable,
@@ -36,31 +42,6 @@ extension TourApiSessionProvider: Sessionable {
             do {
               guard let session = self?.session else { return promise(.failure(.sessionInvalidated(error: nil)))}
               let request = try endpoint.makeRequest(from: session)
-              request
-                .validate(statusCode: 200...299)
-                .responseData { response in
-                  switch response.result {
-                  case .success(let data):
-                    guard
-                      let editedDataWithoutEscapeCharacter = String(data: data, encoding: .utf8)?
-                      .replacingOccurrences(of: "\n", with: "")
-                      .data(using: .utf8)
-                    else {
-                      promise(.failure(AFError.responseSerializationFailed(reason: .inputDataNilOrZeroLength)))
-                      return
-                    }
-                    do {
-                      let responseDTO = try JSONDecoder().decode(R.self, from: editedDataWithoutEscapeCharacter)
-                      promise(.success(responseDTO))
-                    } catch {
-                      promise(
-                        .failure(AFError.responseSerializationFailed(
-                          reason: .customSerializationFailed(error: error))))
-                    }
-                  case .failure(let error):
-                    promise(.failure(error))
-                  }
-                }
             } catch let err as AFError {
               return promise(.failure(err))
             } catch {
@@ -68,4 +49,52 @@ extension TourApiSessionProvider: Sessionable {
             }
           }
         }
+}
+
+// MARK: - Private Helpers
+extension TourApiSessionProvider {
+  private func handleDecodingError<R: Decodable>(
+    _ error: Swift.DecodingError,
+    promise: @escaping Future<R, AFError>.Promise
+  ) {
+    // TODO: - 로그 남기기
+    if case .dataCorrupted(let context) = error {
+      handleDataCorrupedDecodingError(error, promise: promise)
+    }
+    
+    if case .typeMismatch(let type, let context) = error {
+      // TODO: - 그 3개 타입 만들어서 비교해야함.
+      promise(.failure(.explicitlyCancelled))
+    }
+    
+    promise(.failure(AFError.responseSerializationFailed(reason: .customSerializationFailed(error: error))))
+  }
+  
+  private func handleDataCorrupedDecodingError<R: Decodable>(
+    _ error: Swift.DecodingError,
+    promise: @escaping Future<R, AFError>.Promise
+  ) {
+    // TODO: - 로그 남기기
+    if case .dataCorrupted(let context) = error {
+      // 이떄 파서로 파싱 고고
+      let xmlParsingSubscription = xmlParsingService
+        .xmlParserNotifier
+        .sink { completion in
+          if case .failure(let error) = completion {
+            promise(.failure(
+              AFError.responseSerializationFailed(reason: .decodingFailed(error: error))))
+          }
+        } receiveValue: { attributes in
+          if let errorCode = attributes["returnReasonCode"],
+             let tourApiError = TourAPIError(code: errorCode) {
+            promise(.failure(
+              AFError.responseSerializationFailed(reason: .decodingFailed(error: tourApiError))))
+          }
+          let unexpectedError = TourAPIError.unexpectedDecodingErrorFromPublicDataPortal
+          promise(.failure(
+            AFError.responseSerializationFailed(reason: .decodingFailed(error: unexpectedError))))
+        }
+      subscriptions.insert(xmlParsingSubscription)
+    }
+  }
 }
