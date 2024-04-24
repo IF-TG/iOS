b7126d2532742f8b2d1473d0d60556e45880eabe
shcommit <happysh_s2@naver.com>
2024-04-24 19:24:12 +0900

    [Fix] TourApiSessionProvider에서 xmlparser 내부에 초기화, 해제로 필요할때만 메모리 점유하도록 로직 변경

--- travelPlan/Source/Data/Common/TourApiSessionProvider.swift
+++ travelPlan/Source/Data/Common/TourApiSessionProvider.swift
@@ -13,7 +13,7 @@ final class TourApiSessionProvider {
   // MARK: - Properties
   private let session: Session
   
-  private let xmlParsingService: XMLParsingServiceProtocol
+  var xmlParsingService: XMLParsingServiceProtocol?
   
   private var subscriptions = Set<AnyCancellable?>()
   
@@ -22,12 +22,10 @@ final class TourApiSessionProvider {
   // MARK: - Lifecycle
   init(
     session: Session = .default,
-    xmlParsingService: XMLParsingServiceProtocol,
     timeout: Double = 30
   ) {
     self.session = session
     self.timeout = timeout
-    self.xmlParsingService = xmlParsingService
     session.sessionConfiguration.timeoutIntervalForRequest = timeout
   }
 }
@@ -42,6 +40,22 @@ extension TourApiSessionProvider: Sessionable {
             do {
               guard let session = self?.session else { return promise(.failure(.sessionInvalidated(error: nil)))}
               let request = try endpoint.makeRequest(from: session)
+              request
+                .validate(statusCode: 200...299)
+                .responseData { response in
+                  switch response.result {
+                  case .success(let data):
+                    // 일반적인 경우고, 이거마저도 header 확인해주어야한다.
+                    do {
+                      let responseDTO = try JSONDecoder().decode(R.self, from: data)
+                      promise(.success(responseDTO))
+                    } catch let error as Swift.DecodingError {
+                      handleDecodingError(error, from: data, promise: promise)
+                    }
+                  case .failure(let error):
+                    promise(.failure(error))
+                  }
+                }
             } catch let err as AFError {
               return promise(.failure(err))
             } catch {
@@ -55,16 +69,16 @@ extension TourApiSessionProvider: Sessionable {
 extension TourApiSessionProvider {
   private func handleDecodingError<R: Decodable>(
     _ error: Swift.DecodingError,
+    from data: Data,
     promise: @escaping Future<R, AFError>.Promise
   ) {
-    // TODO: - 로그 남기기
-    if case .dataCorrupted(let context) = error {
-      handleDataCorrupedDecodingError(error, promise: promise)
+    // TODO: - 로그 남기기 (컨텍스트, 타입 등)
+    if case .dataCorrupted = error {
+      handleDataCorrupedDecodingError(error, from: data, promise: promise)
     }
     
-    if case .typeMismatch(let type, let context) = error {
-      // TODO: - 그 3개 타입 만들어서 비교해야함.
-      promise(.failure(.explicitlyCancelled))
+    if case .typeMismatch = error {
+      let errorResponseDTO = try? JSONDecoder().decode(TourApiErrorResponseDTO.self, from: data)
     }
     
     promise(.failure(AFError.responseSerializationFailed(reason: .customSerializationFailed(error: error))))
@@ -72,29 +86,31 @@ extension TourApiSessionProvider {
   
   private func handleDataCorrupedDecodingError<R: Decodable>(
     _ error: Swift.DecodingError,
+    from data: Data,
     promise: @escaping Future<R, AFError>.Promise
   ) {
-    // TODO: - 로그 남기기
-    if case .dataCorrupted(let context) = error {
-      // 이떄 파서로 파싱 고고
-      let xmlParsingSubscription = xmlParsingService
-        .xmlParserNotifier
-        .sink { completion in
-          if case .failure(let error) = completion {
-            promise(.failure(
-              AFError.responseSerializationFailed(reason: .decodingFailed(error: error))))
-          }
-        } receiveValue: { attributes in
-          if let errorCode = attributes["returnReasonCode"],
-             let tourApiError = TourAPIError(code: errorCode) {
-            promise(.failure(
-              AFError.responseSerializationFailed(reason: .decodingFailed(error: tourApiError))))
-          }
-          let unexpectedError = TourAPIError.unexpectedDecodingErrorFromPublicDataPortal
+    xmlParsingService = XMLParsingService(parser: XMLParser(data: data))
+    // 이떄 파서로 파싱 고고
+    let xmlParsingSubscription = xmlParsingService?
+      .xmlParserNotifier
+      .sink { [weak self] completion in
+        self?.xmlParsingService = nil
+        if case .failure(let error) = completion {
           promise(.failure(
-            AFError.responseSerializationFailed(reason: .decodingFailed(error: unexpectedError))))
+            AFError.responseSerializationFailed(reason: .decodingFailed(error: error))))
         }
-      subscriptions.insert(xmlParsingSubscription)
-    }
+      } receiveValue: { [weak self] attributes in
+        self?.xmlParsingService = nil
+        if let errorCode = attributes["returnReasonCode"],
+           let tourApiError = TourAPIError(code: errorCode) {
+          promise(.failure(
+            AFError.responseSerializationFailed(reason: .decodingFailed(error: tourApiError))))
+        }
+        let unexpectedError = TourAPIError.unexpectedDecodingErrorFromPublicDataPortal
+        promise(.failure(
+          AFError.responseSerializationFailed(reason: .decodingFailed(error: unexpectedError))))
+      }
+    subscriptions.insert(xmlParsingSubscription)
+
   }
 }
