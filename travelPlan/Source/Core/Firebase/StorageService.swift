3e472dd91368a52598ca00def36b2a08a5c5f41d
shcommit <happysh_s2@naver.com>
2024-04-26 00:43:04 +0900

    [Feat] StorageService에서 git  s

--- travelPlan/Source/Core/Firebase/StorageService.swift
+++ travelPlan/Source/Core/Firebase/StorageService.swift
@@ -6,3 +6,65 @@
 //
 
 import Foundation
+import FirebaseStorage
+import Combine
+
+public struct StorageService {
+  // MARK: - Helpers
+  func uploadImage(_ imageData: Data, type: UploadType) async throws -> String {
+    let fileName = NSUUID().uuidString
+    let reference = Storage.storage().reference(withPath: "/\(type.path)/\(fileName)")
+    _ = try await reference.putDataAsync(imageData)
+    return (try await reference.downloadURL()).absoluteString
+  }
+  
+  func fetchImage(_ url: String, type: UploadType) -> AnyPublisher<Data, Error> {
+    return Future { promise in
+      guard let url = URL(string: url) else {
+        promise(.failure(ReferenceError.invalidReference))
+        return
+      }
+      do {
+        try Storage.storage()
+          .reference(for: url)
+          .getData(maxSize: type.maxSize) { data, error in
+            if let error {
+              promise(.failure(error))
+            } else if let data {
+              promise(.success(data))
+            }
+          }
+      } catch {
+        promise(.failure(error))
+      }
+    }.eraseToAnyPublisher()
+  }
+}
+
+// MARK: - Nested
+extension StorageService {
+  @frozen enum UploadType {
+    case profileImage
+    case postImage
+    
+    var path: String {
+      switch self {
+      case .profileImage:
+        "profile_images"
+      case .postImage:
+        "post_images"
+      }
+    }
+    
+    var maxSize: Int64 {
+      switch self {
+      case .profileImage:
+        /// 프로필 최대 30MB 제한
+        30*1024*1024
+      case .postImage:
+        /// 포스트 이미지 최대 30MB로 제한
+        100*1024*1024
+      }
+    }
+  }
+}
