7c2c509cafb93bfb722bd8c99310f22c31b90323
shcommit <happysh_s2@naver.com>
2024-04-26 00:50:42 +0900

    [Feat] ImageStorageServiceProtocol 선언 및 upload, fetch 이미지 관련 함수 두개 선언!

---
+++ travelPlan/Source/Core/Firebase/ImageStorageServiceProtocol.swift
@@ -0,0 +1,19 @@
+//
+//  ImageStorageServiceProtocol.swift
+//  travelPlan
+//
+//  Created by 양승현 on 4/26/24.
+//
+
+import Foundation
+import Combine
+@frozen enum ImageStorageServiceType {
+  case profileImage
+  case postImage
+}
+
+protocol ImageStorageServiceProtocol {
+  func uploadImage(_ imageData: Data, type: ImageStorageServiceType) async throws -> String
+  
+  func fetchImage(_ url: String, type: ImageStorageServiceType) -> AnyPublisher<Data, Error>
+}
