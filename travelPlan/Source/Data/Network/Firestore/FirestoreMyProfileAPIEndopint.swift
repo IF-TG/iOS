67f71e01236a3323d0097eb4b1df9e1031cef536
shcommit <happysh_s2@naver.com>
2024-04-25 18:25:31 +0900

    [Style] profile -> profileImage로 네이밍 명확하게 수정

--- travelPlan/Source/Data/Network/Firestore/FirestoreMyProfileAPIEndopint.swift
+++ travelPlan/Source/Data/Network/Firestore/FirestoreMyProfileAPIEndopint.swift
@@ -1,5 +1,5 @@
 //
-//  FirestoreUserAPIEndopint.swift
+//  FirestoreMyProfileAPIEndopint.swift
 //  travelPlan
 //
 //  Created by 양승현 on 4/25/24.
@@ -9,8 +9,8 @@ import Foundation
 
 struct VoidResponseDTO: Decodable { }
 
-struct FirestoreUserAPIEndopint {
-  static func fetchUserEndpoint(userUID: String) -> FirestoreEndpoint<VoidResponseDTO> {
+struct FirestoreMyProfileAPIEndopint {
+  static func fetchUserProfileEndpoint(userUID: String) -> FirestoreEndpoint<VoidResponseDTO> {
     return .init(
       method: .get,
       requestType: .users(.userDocument(.user(userUID))))
---
+++ travelPlan/Source/Data/Network/Firestore/ResponseDTO/UserProfileResponseDTO.swift
@@ -0,0 +1,8 @@
+//
+//  UserProfileResponseDTO.swift
+//  travelPlan
+//
+//  Created by 양승현 on 4/25/24.
+//
+
+import Foundation
