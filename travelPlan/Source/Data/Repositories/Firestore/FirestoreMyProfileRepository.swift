d7b4a8fe9840446c772be1edb25d4a9d844b6eae
shcommit <happysh_s2@naver.com>
2024-04-25 22:11:38 +0900

    [Feat] FirestoreMyProfileRepository에서 fetchProfile함수 구현

--- travelPlan/Source/Data/Repositories/Firestore/FirestoreMyProfileRepository.swift
+++ travelPlan/Source/Data/Repositories/Firestore/FirestoreMyProfileRepository.swift
@@ -7,8 +7,53 @@
 
 import Foundation
 import Combine
+import SHFirestoreService
 
-final class FirestoreMyProfileRepository: MyProfileRepository {
+final class FirestoreMyProfileRepository {
+  typealias Endpoint = FirestoreMyProfileAPIEndopint
+  
+  // MARK: - Dependencies
+  private let backgroundQueue: DispatchQueue
+  private let service: FirestoreServiceProtocol
+  private let loggedInUserRepository: LoggedInUserRepository
+  
+  // MARK: - Properties
+  private var subscriptions = Set<AnyCancellable?>()
+  
+  // MARK: - Lifecycle
+  init(
+    service: FirestoreServiceProtocol,
+    loggedInUserRepository: LoggedInUserRepository,
+    backgroundQueue: DispatchQueue = .global(qos: .userInitiated)
+  ) {
+    self.service = service
+    self.loggedInUserRepository = loggedInUserRepository
+    self.backgroundQueue = backgroundQueue
+  }
+}
+
+// MARK: - MyProfileRepository
+extension FirestoreMyProfileRepository: MyProfileRepository {
+  var isProfileSavedInServer: Bool {
+    loggedInUserRepository.isSavedProfileInServer
+  }
+  
+  func fetchProfile(with userId: String) -> AnyPublisher<UserEntity, any Error> {
+    let endpoint = Endpoint.fetchUserProfileEndpoint(userUID: userId)
+    return Future { [weak self, backgroundQueue] promise in
+      let subscription = self?.service.request(endpoint: endpoint)
+        .subscribe(on: backgroundQueue)
+        .sink { completion in
+          if case .failure(let error) = completion {
+            promise(.failure(error))
+          }
+        } receiveValue: { responseDTO in
+          promise(.success(responseDTO.toDomain()))
+        }
+      self?.subscriptions.insert(subscription)
+    }.eraseToAnyPublisher()
+  }
+  
   func checkIfUserNicknameDuplicate(with name: String) -> AnyPublisher<Bool, any Error> {
     fatalError("아직 미구현")
   }
@@ -32,8 +77,4 @@ final class FirestoreMyProfileRepository: MyProfileRepository {
   func fetchProfileImage() -> AnyPublisher<ProfileImageEntity, any Error> {
     fatalError("아직 미구현")
   }
-  
-  var isProfileSavedInServer: Bool
-  
-  
 }
