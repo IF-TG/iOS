5775e95614bdd86d7d1290034784d20085494ab7
shcommit <happysh_s2@naver.com>
2024-04-26 16:09:14 +0900

    [Feat] DefaultLoginRepository.performLogin(type:)에 firestore로 로그인할 경우 사용자의 이미지를 userDefaults에 저장하는 로직 추가

--- travelPlan/Source/Data/Repositories/Server/DefaultLoginRepository.swift
+++ travelPlan/Source/Data/Repositories/Server/DefaultLoginRepository.swift
@@ -7,25 +7,38 @@
 
 import Combine
 import Foundation
+import FirebaseAuth
+import SHFirestoreService
 
 enum DefaultLoginRepositoryError: Error {
   case tokensSavingFailed
   case taskAlreadyCancelled
   case invalidPlatform
   case loginResultSavingFailed
+  case invalidFirebaseAuthCurrentUserUID
 }
 
 final class DefaultLoginRepository {
   // MARK: - Properties
-  private var subscriptions = Set<AnyCancellable>()
+  private var subscriptions = Set<AnyCancellable?>()
   private let loginResponseStorage = KeychainLoginResponseStorage()
   private let loginResultStorage: LoginResultStorage
   private let authService: AuthenticationService
+  private let loggedInUserRepository: LoggedInUserRepository
+  private let firestoreService: FirestoreServiceProtocol
+  private let firestoreStorageService = FiresabseStorageService()
 
   // MARK: - LifeCycle
-  init(authService: AuthenticationService, loginResultStorage: LoginResultStorage) {
+  init(
+    authService: AuthenticationService,
+    loginResultStorage: LoginResultStorage,
+    loggedInUserRepository: LoggedInUserRepository,
+    firestoreService: FirestoreServiceProtocol
+  ) {
     self.authService = authService
     self.loginResultStorage = loginResultStorage
+    self.loggedInUserRepository = loggedInUserRepository
+    self.firestoreService = firestoreService
   }
   
   deinit {
@@ -43,29 +56,59 @@ extension DefaultLoginRepository: LoginRepository {
       authService.setLoginStrategy(GoogleLoginStrategyWithFirebase())
     }
     // TODO: - 여기에서 case 추가하고 service에 구체 Strategy객체 주입
-    
-    return authService.performLogin()
-      .receive(on: DispatchQueue.global(qos: .userInitiated))
-      .tryMap { [weak self] jwtDTO in
-        guard let self = self else {
-          throw DefaultLoginRepositoryError.taskAlreadyCancelled
-        }
-        if let jwtDTO {
-          guard self.loginResponseStorage.saveTokens(jwtDTO: jwtDTO) else {
-            throw DefaultLoginRepositoryError.tokensSavingFailed
+    return Future<Bool, Error> { [weak self] promise in
+      let authServiceSubscription = self?.authService.performLogin()
+        .receive(on: DispatchQueue.global(qos: .userInitiated))
+        .sink { completion in
+          if case .failure(let error) = completion {
+            promise(.failure(error))
           }
-        } else {
-          /// 사용자는 FirebaseAuth를 이용.
-          // TODO: - 여기서 해당 사용자 도큐먼트에 컬랙션 문서있느지호확ㅇ니하고엇ㅂ으면 추가.
+        } receiveValue: { jwtDTO in
+          if let jwtDTO {
+            guard self?.loginResponseStorage.saveTokens(jwtDTO: jwtDTO) == true else {
+              promise(.failure(DefaultLoginRepositoryError.tokensSavingFailed))
+              return
+            }
+            // TODO: - DefaultLoggedInUserRepository를 통해 save합니다.
+            /// loggedInUserRepository.setUser(with: <#T##UserEntity#>)
+            
+            promise(.success(true))
+          } else {
+            /// 파이어 베이스 auth를 통해 로그인한 사용자입니다.
+            guard let userUid = Auth.auth().currentUser?.uid else {
+              promise(.failure(DefaultLoginRepositoryError.invalidFirebaseAuthCurrentUserUID))
+              return
+            }
+            
+            let endpoint = FirestoreMyProfileAPIEndopint.fetchUserProfileEndpoint(userUID: userUid)
+            let firestoreServiceSubscription = self?.firestoreService.request(endpoint: endpoint)
+              .sink { completion in
+                if case .failure(let error) = completion {
+                  promise(.failure(error))
+                }
+              } receiveValue: { responseDTO in
+                if responseDTO.profileImagePath == "" {
+                  self?.loggedInUserRepository.setUser(with: responseDTO.toDomain(with: nil))
+                  promise(.success(true))
+                } else {
+                  let firestoreStorageSubscription = self?.firestoreStorageService
+                    .fetchImage(responseDTO.profileImagePath, type: .profileImage)
+                    .sink { completion in
+                      if case .failure(let error) = completion {
+                        promise(.failure(error))
+                      }
+                    } receiveValue: { data in
+                      self?.loggedInUserRepository.setUser(with: responseDTO.toDomain(with: data))
+                      promise(.success(true))
+                    }
+                  self?.subscriptions.insert(firestoreStorageSubscription)
+                  
+                }
+              }
+            self?.subscriptions.insert(firestoreServiceSubscription)
+          }
         }
-        // TODO: - loginResultStorage를 통해 save합니다.
-//          if loginResultStorage.save() {
-//            return true
-//          } else {
-//            throw DefaultLoginRepositoryError.loginResultSavingFailed
-//          }
-        return true
-      }
-      .eraseToAnyPublisher()
+      self?.subscriptions.insert(authServiceSubscription)
+    }.eraseToAnyPublisher()
   }
 }
