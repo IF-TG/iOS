b0982f43b3c71d12c32588061b242dc18c3dbf9e
seokhyun <letushyun@gmail.com>
2024-04-28 16:54:48 +0900

    [Feat] 레포츠 response DTO 추가

---
+++ travelPlan/Source/Data/Network/TourAPI/ResponseDTO/LeportsResponseDTO.swift
@@ -0,0 +1,30 @@
+//
+//  LeportsResponseDTO.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 4/28/24.
+//
+
+import Foundation
+
+struct LeportsResponseDTO: Decodable {
+  let canLoanBabyCarriage: String?
+  let canAccompanyPet: String?
+  let openPeriod: String?
+  let parkingFacility: String?
+  let parkingFee: String?
+  let ageLimit: String?
+  let fee: String?
+  let availableTime: String?
+  
+  enum CodingKeys: String, CodingKey {
+    case canLoanBabyCarriage = "chkbabycarriageleports"
+    case canAccompanyPet = "chkpetleports"
+    case openPeriod = "openperiod"
+    case parkingFacility = "parkingleports"
+    case parkingFee = "parkingfeeleports"
+    case ageLimit = "expagerangeleports"
+    case fee = "usefeeleports"
+    case availableTime = "usetimeleports"
+  }
+}
