b586a4c93411a95268ae257db43ced44caf82b75
shcommit <happysh_s2@naver.com>
2024-04-30 00:11:20 +0900

    [Feat] FirestoreReviewWritingSaveRequestDTO에 createAt Timestamp로 변경.

--- travelPlan/Source/Data/Network/Firestore/RequestDTO/FirestoreReviewWritingSaveRequestDTO.swift
+++ travelPlan/Source/Data/Network/Firestore/RequestDTO/FirestoreReviewWritingSaveRequestDTO.swift
@@ -6,15 +6,14 @@
 //
 
 import Foundation
+import FirebaseFirestore
 
 struct FirestoreReviewWritingSaveRequestDTO: Encodable {
   var reviewWritingSaveRequestDTO: ReviewWritingSaveRequestDTO
   let postId: String
-  
   let authorId: String
-  
   let likeNum: Int = 0
-  let createAt: String
+  let createAt: Timestamp
   
   func encode(to encoder: Encoder) throws {
     var container = encoder.container(keyedBy: CodingKeys.self)
@@ -52,14 +51,10 @@ extension FirestoreReviewWritingSaveRequestDTO {
     postId: String,
     authorId: String
   ) -> Self {
-    let formatter = DateFormatter()
-    formatter.dateFormat = "yyyy.MM.dd"
-    let curDate = Date()
-    let curDateString = formatter.string(from: curDate)
     return .init(
       reviewWritingSaveRequestDTO: ReviewWritingSaveRequestDTO.makeRequestDTO(entity: entity),
       postId: postId,
       authorId: authorId,
-      createAt: curDateString)
+      createAt: Timestamp(date: Date()))
   }
 }
