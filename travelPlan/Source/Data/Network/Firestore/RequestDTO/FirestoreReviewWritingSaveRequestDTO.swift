2950b018673775e4e0c44ff811c477087df993ea
shcommit <happysh_s2@naver.com>
2024-04-29 15:50:47 +0900

    [Feat] FirestoreReviewWritingSaveRequestDTO에서 createAt로 정렬할때 문자열끼리는 뭐가 최근 생성된지 식별 불가능하므로 Timestamp로 변 경. 이제 포스트 받을때 데이터 포멧 써서 변경하면됩니닷

--- travelPlan/Source/Data/Network/Firestore/RequestDTO/FirestoreReviewWritingSaveRequestDTO.swift
+++ travelPlan/Source/Data/Network/Firestore/RequestDTO/FirestoreReviewWritingSaveRequestDTO.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Firebase
 
 struct FirestoreReviewWritingSaveRequestDTO: Encodable {
   var reviewWritingSaveRequestDTO: ReviewWritingSaveRequestDTO
@@ -14,7 +15,7 @@ struct FirestoreReviewWritingSaveRequestDTO: Encodable {
   let authorId: String
   
   let likeNum: Int = 0
-  let createAt: String
+  let createAt: Timestamp
   
   func encode(to encoder: Encoder) throws {
     var container = encoder.container(keyedBy: CodingKeys.self)
@@ -52,14 +53,10 @@ extension FirestoreReviewWritingSaveRequestDTO {
     postId: String,
     authorId: String
   ) -> Self {
-    let formatter = DateFormatter()
-    formatter.dateFormat = "yyyy.MM.dd"
-    let curDate = Date()
-    let curDateString = formatter.string(from: curDate)
     return .init(
       reviewWritingSaveRequestDTO: ReviewWritingSaveRequestDTO.makeRequestDTO(entity: entity),
       postId: postId,
       authorId: authorId,
-      createAt: curDateString)
+      createAt: Timestamp(date: Date()))
   }
 }
