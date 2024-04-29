1ce1bcea5c401dede656dfa56f80f48d04d5a488
shcommit <happysh_s2@naver.com>
2024-04-29 12:46:12 +0900

    [Feat] FirestoreReviewWritingSaveRequestDTO 추가. author 관련 프로퍼티들은 넣지 않는것으로 결정. 추후 사용자 profile변경됬을떄 관리 어렵다는 단점

---
+++ travelPlan/Source/Data/Network/Firestore/RequestDTO/FirestoreReviewWritingSaveRequestDTO.swift
@@ -0,0 +1,54 @@
+//
+//  FirestoreReviewWritingSaveRequestDTO.swift
+//  travelPlan
+//
+//  Created by 양승현 on 4/29/24.
+//
+
+import Foundation
+
+struct FirestoreReviewWritingSaveRequestDTO: Encodable {
+  let reviewWritingSaveRequestDTO: ReviewWritingSaveRequestDTO
+  let postId: String
+  
+  let authorId: String
+  
+  let likeNum: Int
+  let createAt: String
+  
+  func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encode(reviewWritingSaveRequestDTO, forKey: .reviewWritingSaveRequestDTO)
+    try container.encode(postId, forKey: .postId)
+    try container.encode(authorId, forKey: .authorId)
+    try container.encode(likeNum, forKey: .likeNum)
+    try container.encode(createAt, forKey: .createAt)
+  }
+  
+  enum CodingKeys: String, CodingKey {
+    case reviewWritingSaveRequestDTO
+    case postId
+    case authorId
+    case likeNum
+    case createAt
+  }
+}
+
+extension FirestoreReviewWritingSaveRequestDTO {
+  static func makeRequestDTO(
+    entity: ReviewWritingEntity,
+    postId: String,
+    authorId: String
+  ) -> Self {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy.MM.dd"
+    let curDate = Date()
+    let curDateString = formatter.string(from: curDate)
+    return .init(
+      reviewWritingSaveRequestDTO: ReviewWritingSaveRequestDTO.makeRequestDTO(entity: entity),
+      postId: postId,
+      authorId: authorId,
+      likeNum: 0,
+      createAt: curDateString)
+  }
+}
