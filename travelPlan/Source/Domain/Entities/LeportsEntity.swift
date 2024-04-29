5f97ebc99f197011393730f7939318f586696862
seokhyun <letushyun@gmail.com>
2024-04-29 14:05:25 +0900

    [Feat] 관광타입 6개에 대한 Entity 추가

---
+++ travelPlan/Source/Domain/Entities/LeportsEntity.swift
@@ -0,0 +1,19 @@
+//
+//  LeportsEntity.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 4/29/24.
+//
+
+import Foundation
+
+struct LeportsEntity {
+  let canLoanBabyCarriage: String?
+  let canAccompanyPet: String?
+  let openPeriod: String?
+  let parkingFacility: String?
+  let parkingFee: String?
+  let ageLimit: String?
+  let fee: String?
+  let availableTime: String?
+}
