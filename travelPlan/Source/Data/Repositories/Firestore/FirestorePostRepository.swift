4fdc778d5b700124a6cff66b3a28521f95bf911b
shcommit <happysh_s2@naver.com>
2024-04-30 13:06:27 +0900

    [Feat] FirestorePostRepository.fetchPosts 페이징 query들 구현.

---
+++ travelPlan/Source/Data/Repositories/Firestore/FirestorePostRepository.swift
@@ -0,0 +1,139 @@
+//
+//  FirestorePostRepository.swift
+//  travelPlan
+//
+//  Created by 양승현 on 4/29/24.
+//
+
+import Foundation
+import SHFirestoreService
+import FirebaseFirestore
+import Combine
+
+final class FirestorePostRepository {
+  typealias Endpoint = FirestorePostAPIEndpoint
+  
+  // MARK: - Dependencies
+  private let service: FirestoreServiceProtocol
+  
+  // MARK: - Properties
+  private var subscription = Set<AnyCancellable?>()
+  
+  // MARK: - Lifecycle
+  init(service: FirestoreServiceProtocol) {
+    self.service = service
+  }
+}
+
+// MARK: - PostRepository
+extension FirestorePostRepository: PostRepository {
+  func fetchPosts(
+    page: Int32,
+    perPage: Int32,
+    category: PostCategory
+  ) -> AnyPublisher<PostsPage, any Error> {
+    let isFirstPage = page == 1
+    
+    let endpoint = Endpoint.fetchPostsEndpoint()
+    
+    return Future { [weak self] promise in
+      guard let self else {
+        promise(.failure(ReferenceError.invalidReference))
+        return
+      }
+      let serviceSubscription = service.paginate(
+        endpoint: endpoint,
+        makeQuery: { ref in
+          let query = self.makeBaseQuery(
+            ref,
+            perPage: perPage,
+            withOrderBy: category.orderBy)
+          self.appendMainThemeCategoryQuery(
+            query,
+            withMainTheme: category.mainTheme)
+          return query
+        },
+        isFirstPagination: isFirstPage)
+        .sink { completion in
+          if case .failure(let error) = completion {
+            switch error as FirestoreServiceError {
+            case .documentNotFound, .noMorePage:
+              promise(.success(PostsPage(totalPosts: 0, posts: [], thumbnails: [], hasMorePage: false)))
+            default:
+              promise(.failure(error))
+            }
+          }
+        } receiveValue: { responseDTO in
+          // TODO: - 리스폰스.. 어, 잘 활용해보자.
+          fatalError("구현하기")
+        }
+      subscription.insert(serviceSubscription)
+    }.eraseToAnyPublisher()
+  }
+  
+  /// 둘 중 하나의 정렬은 반드시 들어갑니다.
+  private func makeBaseQuery(
+    _ ref: CollectionReference,
+    perPage: Int32,
+    withOrderBy orderBy: TravelOrderType
+  ) -> Query {
+    let query = ref.order(by: "createAt", descending: true)
+      .limit(to: Int(perPage))
+    if case .popularity = orderBy {
+      return query.order(by: "likeNum", descending: true)
+    }
+    return query
+  }
+  
+  private func appendMainThemeCategoryQuery(
+    _ query: Query,
+    withMainTheme main: TravelMainThemeType
+  ) {
+    /// all, cateogryDevelop의 경우 부수적인 qeury가 없습니다.
+    guard let mainThemeField = TravelMainThemeTypeMapper.toFirestoreField(main) else { return }
+    switch main {
+    case .all,
+         .categoryDevelop:
+      break
+    case .season(let season):
+      if let seasion = season {
+        query.whereField(mainThemeField, arrayContains: SeasonMapper.toDTO(seasion))
+      } else {
+        query.whereField(mainThemeField, isGreaterThan: [])
+      }
+    case .region(let travelRegion):
+      if let travelRegion {
+        query.whereField(mainThemeField, arrayContains: TravelRegionMapper.toDTO(travelRegion))
+      } else {
+        query.whereField(mainThemeField, isGreaterThan: [])
+      }
+    case .travelTheme(let travelTheme):
+      if let travelTheme {
+        query.whereField(mainThemeField, arrayContains: TravelThemeMapper.toDTO(travelTheme))
+      } else {
+        query.whereField(mainThemeField, isGreaterThan: [])
+      }
+    case .partner(let travelPartner):
+      if let travelPartner {
+        query.whereField(mainThemeField, arrayContains: TravelPartnerMapper.toDTO(travelPartner))
+      } else {
+        query.whereField(mainThemeField, isGreaterThan: [])
+      }
+    }
+  }
+}
+
+// MARK: - PostRepository
+extension FirestorePostRepository {
+  func fetchComments(page: Int32, perPage: Int32, postId: String) -> AnyPublisher<PostCommentContainerEntity, any Error> {
+    fatalError("미 구현")
+  }
+  
+  func fetchLikedPostsByLoggedInUser(page: Int32, perPage: Int32) -> AnyPublisher<PostsPage, any Error> {
+    fatalError("미 구현")
+  }
+  
+  func searchPosts(keyword: String, page: Int32, perPage: Int32, isTitle: Bool, isContent: Bool) -> AnyPublisher<[Post], any Error> {
+    fatalError("미 구현")
+  }
+}
