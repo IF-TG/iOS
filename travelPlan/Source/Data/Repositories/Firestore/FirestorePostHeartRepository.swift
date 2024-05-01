602577fddc1f30ad61d572040ba3032f4db72045
shcommit <happysh_s2@naver.com>
2024-05-02 00:45:50 +0900

    [Style] FirestorePostHeartRepository.fetchHeartUsers(_:) 내부 불필요한 주석 제 거

--- travelPlan/Source/Data/Repositories/Firestore/FirestorePostHeartRepository.swift
+++ travelPlan/Source/Data/Repositories/Firestore/FirestorePostHeartRepository.swift
@@ -30,25 +30,13 @@ final class FirestorePostHeartRepository {
 }
 
 extension FirestorePostHeartRepository: PostHeartRepository {
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  // TODO: - Unit test. 없는 컬랙션은 에러? 아니면 빈 문자열?. 컬랙션에 값 있으면 그거받아져오는지 두개
   func fetchHeartUsers(
     _ postId: String
   ) -> AnyPublisher<[String], any Error> {
     let endpoint = Endpoint.makeFetchHeartUsersEndpoint(postId)
     return Future { [weak self, backgroundQueue] promise in
       let requestSubscription = self?.service
-        .request(endpoint: endpoint)
+        .retrieveDocumentIDs(endpoint: endpoint)
         .subscribe(on: backgroundQueue)
         .sink { completion in
           if case .failure(let error) = completion {
