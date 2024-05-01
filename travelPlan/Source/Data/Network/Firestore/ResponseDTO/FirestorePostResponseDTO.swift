b5273dae161c41151033d1ac4206273e8cf05838
shcommit <happysh_s2@naver.com>
2024-05-01 13:30:37 +0900

    [Feat] FirestorePostResponseDTO에 toDomain() 함수들  구현

--- travelPlan/Source/Data/Network/Firestore/ResponseDTO/FirestorePostResponseDTO.swift
+++ travelPlan/Source/Data/Network/Firestore/ResponseDTO/FirestorePostResponseDTO.swift
@@ -21,6 +21,7 @@ struct FirestorePostResponseDTO: Decodable {
   
   let createAt: Timestamp
   let likes: Int
+  let comments: Int
   
   let mapX: Double
   let mapY: Double
@@ -46,12 +47,18 @@ struct FirestorePostResponseDTO: Decodable {
     case regions
     case seasons
     case partners = "companions"
+    case comments = "commentNum"
   }
 }
 
 struct FirestorePostContentResponseDTO: Decodable {
   let sort: Int
   let text: String
+  
+  // MARK: - Mappings DTO
+  func toDomain() -> Post.PostContent {
+    return .init(sort: sort, text: text)
+  }
 }
 
 struct FirestorePostImageFileResponseDTO: Decodable {
@@ -65,5 +72,54 @@ struct FirestorePostImageFileResponseDTO: Decodable {
     case url = "img"
     case imageType
     case thumbnail
+  }
+  
+  // MARK: - Mappings DTO
+  func toDomain(with imageData: Data) -> Post.PostImage {
+    return .init(imageData: imageData, sort: Int32(sort))
+  }
+}
+
+// MARK: - Mappings DTO
+extension FirestorePostResponseDTO {
+  func toDomain() -> Post.Detail<[Post.PostContent]> {
+    return Post.Detail(
+      postID: postId,
+      title: title,
+      content: content.map { $0.toDomain() },
+      likes: Int32(likes),
+      comments: Int32(comments),
+      location: .init(x: mapX, y: mapY),
+      createAt: TimestampConverter.toString(from: createAt),
+      tripDate: .init(start: startDate, end: endDate))
+  }
+  
+  func toDomain(liked: Bool, authorImageData: Data?, authorName: String, postImages: [Post.PostImage]) -> Post {
+    return .init(
+      liked: liked,
+      detail: toDomain(),
+      author: toDomain(with: authorImageData, authorName: authorName),
+      highResolveImages: postImages,
+      category: toDomain())
+  }
+  
+  func toDomain(with authorImageData: Data?, authorName: String) -> Post.Author {
+    return .init(
+      profileImageData: authorImageData,
+      nickname: authorName,
+      authorId: authorId)
+  }
+  
+  func toDomain() -> Post.Category {
+    let mappedThemes = themes.compactMap { TravelThemeMapper.toDomain($0) }
+    let mappedRegions = regions.compactMap { TravelRegionMapper.toDomain($0) }
+    let mappedSeasons = seasons.compactMap { SeasonMapper.toDomain($0) }
+    let mappedPartners = partners.compactMap { TravelPartnerMapper.toDomain($0) }
+    
+    return Post.Category(
+      themes: mappedThemes,
+      regions: mappedRegions,
+      seasons: mappedSeasons,
+      partners: mappedPartners)
   }
 }
