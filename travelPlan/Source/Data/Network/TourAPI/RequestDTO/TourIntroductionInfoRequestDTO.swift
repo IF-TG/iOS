2be10408fe9863a0dc93b2cd53b35d6a2714ad6f
seokhyun <letushyun@gmail.com>
2024-05-02 19:29:13 +0900

    [Feat] 소개정보조회 TourIntroductionInfoRequestDTO 정의

---
+++ travelPlan/Source/Data/Network/TourAPI/RequestDTO/TourIntroductionInfoRequestDTO.swift
@@ -0,0 +1,32 @@
+//
+//  TourIntroductionInfoRequestDTO.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 5/2/24.
+//
+
+import Foundation
+
+/// 소개정보조회
+final class TourIntroductionInfoRequestDTO: TourApiBaseRequestDTO {
+  let contentId: Int
+  let contentTypeId: Int
+  
+  init(contentId: Int, contentTypeId: Int) {
+    self.contentId = contentId
+    self.contentTypeId = contentTypeId
+  }
+  
+  enum CodingKeys: CodingKey {
+    case contentId
+    case contentTypeId
+  }
+  
+  override func encode(to encoder: any Encoder) throws {
+    try super.encode(to: encoder)
+    
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encode(self.contentId, forKey: .contentId)
+    try container.encode(self.contentTypeId, forKey: .contentTypeId)
+  }
+}
