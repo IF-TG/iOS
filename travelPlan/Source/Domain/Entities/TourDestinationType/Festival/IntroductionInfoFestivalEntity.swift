5f476a082286a6e85a0abc02cbac5208486886de
seokhyun <letushyun@gmail.com>
2024-05-02 19:31:10 +0900

    [Feat] 소개정보조회에서 축제/행사를 요청시 받는 Entity 정의

---
+++ travelPlan/Source/Domain/Entities/TourDestinationType/Festival/IntroductionInfoFestivalEntity.swift
@@ -0,0 +1,17 @@
+//
+//  IntroductionInfoFestivalEntity.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 5/2/24.
+//
+
+import Foundation
+
+struct IntroductionInfoFestivalEntity {
+  let startDate: Date
+  let endDate: Date
+  let fee: String
+  let place: String
+  let ageLimit: String
+  let showTime: String
+}
