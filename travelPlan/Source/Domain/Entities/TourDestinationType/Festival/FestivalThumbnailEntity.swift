bc1af91be33116a49b82be9e8fa97491296eefdf
seokhyun <letushyun@gmail.com>
2024-05-03 01:03:40 +0900

    [Feat] 검색 탭 축체 section에서 사용되는 FestivalThumbnailEntity 구현

---
+++ travelPlan/Source/Domain/Entities/TourDestinationType/Festival/FestivalThumbnailEntity.swift
@@ -0,0 +1,15 @@
+//
+//  FestivalThumbnailEntity.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 4/30/24.
+//
+
+import Foundation
+
+struct FestivalThumbnailEntity {
+  let title: String
+  let startDate: Date
+  let endDate: Date
+  let imageData: Data
+}
