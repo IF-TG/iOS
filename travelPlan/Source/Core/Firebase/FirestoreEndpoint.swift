959ff73ae50c75e00f5b4709a7609b40e89173ec
shcommit <happysh_s2@naver.com>
2024-05-03 12:29:43 +0900

    [Feat] SHFirestoreService version 1.2.1->1.2.2로 변경되며 여러 init 타입 초기화 추가.

--- travelPlan/Source/Core/Firebase/FirestoreEndpoint.swift
+++ travelPlan/Source/Core/Firebase/FirestoreEndpoint.swift
@@ -10,18 +10,45 @@ import SHFirestoreService
 
 final class FirestoreEndpoint<ResponseDTO>: FirestoreEndopintable where ResponseDTO: Decodable {
   // MARK: - Properties
+  var requestDTODictionary: [String : Any]?
   var requestDTO: (any Encodable)?
   var method: FirestoreMethod
   var requestType: any FirestoreAccessible
   
   // MARK: - Lifecycle
   init(
+    requestDTODictionary: [String: Any]? = nil,
     requestDTO: (any Encodable)? = nil,
     method: FirestoreMethod,
     requestType: FirestoreRequestType
   ) {
+    self.requestDTODictionary = requestDTODictionary
     self.requestDTO = requestDTO
     self.method = method
     self.requestType = requestType
+  }
+  
+  convenience init(
+    requestDTODictionary: [String: Any]?,
+    method: FirestoreMethod,
+    requestType: FirestoreRequestType
+  ) {
+    self.init(
+      requestDTODictionary: requestDTODictionary,
+      requestDTO: nil,
+      method: method,
+      requestType: requestType)
+  }
+  
+  convenience init(
+    requestDTO: (any Encodable)?,
+    method: FirestoreMethod,
+    requestType: FirestoreRequestType
+  ) {
+    self.init(
+      requestDTODictionary: nil,
+      requestDTO: requestDTO,
+      method: method,
+      requestType: requestType)
   }
 }
