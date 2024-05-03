5b37ccf4d9265c5b608bf69b8f1449e6298e7912
shcommit <happysh_s2@naver.com>
2024-05-03 12:42:57 +0900

    [Feat] PostHeartRepository에 fetchPostHearts(_:) 정의 및 주석 추가

--- travelPlan/Source/Domain/Interfaces/Repositories/Server/PostHeartRepository.swift
+++ travelPlan/Source/Domain/Interfaces/Repositories/Server/PostHeartRepository.swift
@@ -9,8 +9,18 @@ import Foundation
 import Combine
 
 protocol PostHeartRepository {
+  /// 포스트 좋아요 한 사용자들의 ID를 반환합니다.
   func fetchHeartUsers(_ postId: String) -> AnyPublisher<[String], Error>
+  
+  /// 포스트 좋아요 한 개수를 반환합니다.
+  func fetchPostHearts(_ postId: String) -> AnyPublisher<Int, Error>
+  
+  /// 사용자가 포스트를 하트 표시 합니다.
   func heartPost(_ postId: String, userId: String) -> AnyPublisher<Void, Error>
+  
+  /// 사용자가 포스트 하트표시 하지 않습니다.
   func hatePost(_ postId: String, userId: String) -> AnyPublisher<Void, Error>
+  
+  /// DB내부에서 serial하게 포스트 좋아요 개수를 증가 또는 감소 시킵니다.
   func togglePostHearts(_ postId: String, willHeartPost: Bool) -> AnyPublisher<Void, Error>
 }
