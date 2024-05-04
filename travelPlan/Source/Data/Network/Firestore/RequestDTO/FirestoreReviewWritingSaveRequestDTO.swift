3a736025e8a437550c9c7c1ca9d5562e6d7bcf9d
shcommit <happysh_s2@naver.com>
2024-05-04 18:31:34 +0900

    [Fix] Server에서도 호출가능한 ReviewWritingSaveRequestDTO의 content 프로퍼티는 그대로, firestore에서 사용할땐 코딩키 contents로 추가

--- travelPlan/Source/Data/Network/Firestore/RequestDTO/FirestoreReviewWritingSaveRequestDTO.swift
+++ travelPlan/Source/Data/Network/Firestore/RequestDTO/FirestoreReviewWritingSaveRequestDTO.swift
@@ -19,7 +19,7 @@ struct FirestoreReviewWritingSaveRequestDTO: Encodable {
   func encode(to encoder: Encoder) throws {
     var container = encoder.container(keyedBy: CodingKeys.self)
     try container.encode(reviewWritingSaveRequestDTO.companions, forKey: .companions)
-    try container.encode(reviewWritingSaveRequestDTO.contents, forKey: .content)
+    try container.encode(reviewWritingSaveRequestDTO.content, forKey: .contents)
     try container.encode(reviewWritingSaveRequestDTO.endDate, forKey: .endDate)
     try container.encode(reviewWritingSaveRequestDTO.imgFileList, forKey: .imgFileList)
     try container.encode(reviewWritingSaveRequestDTO.mapX, forKey: .mapX)
@@ -43,7 +43,7 @@ struct FirestoreReviewWritingSaveRequestDTO: Encodable {
     case commentNum
     case createAt
     case title
-    case content
+    case contents
     case startDate
     case endDate
     case themes
--- travelPlan/Source/Data/Network/Server/RequestDTO/ReviewWritingSaveRequestDTO.swift
+++ travelPlan/Source/Data/Network/Server/RequestDTO/ReviewWritingSaveRequestDTO.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct ReviewWritingSaveRequestDTO: Encodable {
   let title: String
-  let contents: [TextListDTO]
+  let content: [TextListDTO]
   let startDate: String
   let endDate: String
   let themes: [String]
@@ -64,7 +64,7 @@ extension ReviewWritingSaveRequestDTO {
     }
     return .init(
       title: entity.title,
-      contents: textListDTO,
+      content: textListDTO,
       startDate: entity.tripDate.start,
       endDate: entity.tripDate.end,
       themes: entity.category.themes.map { TravelThemeMapper.toDTO($0) },
