3bab5fa0cb3f6f495c63b1507a287e2ec06af7d6
shcommit <happysh_s2@naver.com>
2024-05-06 16:39:45 +0900

    [Test] FirestorePostCommentHeartRepositoryTestsㅇ에서 fetchCommentHearts 호출시 예상되는 값과 실제 받아온 값이 일치하는지 테스트. request type path 테스트

--- travelPlan/Source/Data/Tests/FirestorePostCommentHeartRepositoryTests.swift
+++ travelPlan/Source/Data/Tests/FirestorePostCommentHeartRepositoryTests.swift
@@ -10,6 +10,7 @@ import Combine
 
 @testable import travelPlan
 @testable import SHFirestoreService
+@testable import FirebaseFirestore
 
 final class FirestorePostCommentHeartRepositoryTests: XCTestCase {
   var sut = {
@@ -82,5 +83,47 @@ extension FirestorePostCommentHeartRepositoryTests {
     // Assert
     checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "hateComment")
     XCTAssertTrue(hasReceivedResult, "좋아요한 사용자가 파이어스토어에 성공적으로 제거된 후 빈 값을 반환해야하지만 예상치 못한 동작이 발생됬습니다")
+  }
+  
+  func test_fetchCommentHearts호출시_성공저으로값을반환하는지() {
+    // Arrange
+    let prevExpectation = expectation(description: "사전 준비 작업")
+    var receivedCommentHearts: Int?
+    var expectedCommentHearts: Int?
+    var unexpectedError: Error?
+    Firestore.firestore()
+      .collection("posts")
+      .document(testPostId)
+      .collection("comments")
+      .document(testCommentId)
+      .collection("comment-hearts")
+      .getDocuments()
+      .sink {
+        if case .failure = $0 {
+          XCTAssert(false, "testComment를 좋아한 사용자 개수를 받아오는 사전 작업에서 에러가 발생됬습니다.")
+        }
+      } receiveValue: { snapshot in
+        expectedCommentHearts = snapshot.documents.count
+        prevExpectation.fulfill()
+      }.store(in: &subscriptions)
+    wait(for: [prevExpectation], timeout: 7.777)
+    
+    // Act
+    sut.fetchCommentHearts(with: testPostId, commentId: testCommentId)
+      .sink { completion in
+        if case .failure(let error) = completion {
+          unexpectedError = error
+          self.expectation.fulfill()
+        }
+      } receiveValue: { result in
+        receivedCommentHearts = result
+        self.expectation.fulfill()
+      }.store(in: &subscriptions)
+    
+    wait(for: [expectation], timeout: 7.777)
+    
+    // Assert
+    checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "fetchCommentHearts")
+    XCTAssertEqual(expectedCommentHearts, receivedCommentHearts, "예상된 좋아요 사용자 개수와 실제로 sut.fetchCommentHearts에서 받아오는 데이터가 일치해야하지만 일치하지 않습니다.")
   }
 }
