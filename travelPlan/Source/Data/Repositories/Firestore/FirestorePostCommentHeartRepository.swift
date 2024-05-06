8ee77d93722e9e07bb1edbd57ca9230202334fa4
shcommit <happysh_s2@naver.com>
2024-05-06 16:22:03 +0900

    [Style] FirestorePostCommentHeartRepository에서 todo 추가

--- travelPlan/Source/Data/Repositories/Firestore/FirestorePostCommentHeartRepository.swift
+++ travelPlan/Source/Data/Repositories/Firestore/FirestorePostCommentHeartRepository.swift
@@ -81,6 +81,7 @@ extension FirestorePostCommentHeartRepository: PostCommentHeartRepository {
   ) -> AnyPublisher<Void, any Error> {
     let endpoint = Endpoint.makeCommentHeartEndpoint(with: postId, commentId: commentId, userId: userId)
     return Future { [weak self, backgroundQueue] promise in
+      // TODO: - 이전 PR 머지되면 BackgroundTask로 백그라운드 전환해도 동작되도록 기능 추가해야합니다.
       let saveSubscription = self?.service
         .saveDocument(endpoint: endpoint)
         .subscribe(on: backgroundQueue)
@@ -104,6 +105,7 @@ extension FirestorePostCommentHeartRepository: PostCommentHeartRepository {
   ) -> AnyPublisher<Void, any Error> {
     let endpoint = Endpoint.makeCommentHateEndpoint(with: postId, commentId: commentId, userId: userId)
     return Future { [weak self, backgroundQueue] promise in
+      // TODO: - 이전 PR 머지되면 BackgroundTask로 백그라운드 전환해도 동작되도록 기능 추가해야합니다.
       let deleteSubscription = self?.service
         .request(endpoint: endpoint)
         .subscribe(on: backgroundQueue)
@@ -128,6 +130,7 @@ extension FirestorePostCommentHeartRepository: PostCommentHeartRepository {
   ) -> AnyPublisher<Void, any Error> {
     let endpoint = Endpoint.makePostHeartsToggleEndpoint(with: postId, commentId: commentId)
     return Future { [weak self, backgroundQueue] promise in
+      // TODO: - 이전 PR 머지되면 BackgroundTask로 백그라운드 전환해도 동작되도록 기능 추가해야합니다.
       let subscription = self?.service
         .performTransaction { transaction in
           guard let docRef = endpoint.requestType.documentRef else {
