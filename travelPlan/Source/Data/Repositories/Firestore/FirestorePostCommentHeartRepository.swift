828a3a6107901e5618c679f210ce74459351a47a
shcommit <happysh_s2@naver.com>
2024-05-06 14:29:56 +0900

    [Feat] FirestorePostCommentHeartRepository 객체 정의 및 PostCommentHeartRepository protocol 준수만!

---
+++ travelPlan/Source/Data/Repositories/Firestore/FirestorePostCommentHeartRepository.swift
@@ -0,0 +1,67 @@
+//
+//  FirestorePostCommentHeartRepository.swift
+//  travelPlan
+//
+//  Created by 양승현 on 5/6/24.
+//
+
+import Combine
+import Foundation
+import SHFirestoreService
+
+final class FirestorePostCommentHeartRepository {
+  // MARK: - Dependencies
+  private let service: FirestoreServiceProtocol
+  
+  // MARK: - Properties
+  private var subscriptions = Set<AnyCancellable?>()
+  
+  // MARK: - Lifecycle
+  init(
+    service: FirestoreServiceProtocol
+  ) {
+    self.service = service
+  }
+}
+
+// MARK: - PostCommentHeartRepository
+extension FirestorePostCommentHeartRepository: PostCommentHeartRepository {
+  func fetchCommentHeartUsers(
+    _ postId: String,
+    _ commentId: String
+  ) -> AnyPublisher<[UserIdentifier], any Error> {
+    fatalError("미구현")
+  }
+  
+  func fetchCommentHearts(
+    _ postId: String,
+    _ commentId: String
+  ) -> AnyPublisher<Int, any Error> {
+    fatalError("미구현")
+  }
+  
+  func heartComment(
+    _ postId: String,
+    commentId: String,
+    userId: String
+  ) -> AnyPublisher<Void, any Error> {
+    fatalError("미구현")
+  }
+  
+  func hateComment(
+    _ postId: String,
+    commentId: String,
+    userId: String
+  ) -> AnyPublisher<Void, any Error> {
+    fatalError("미구현")
+  }
+  
+  func togglePostHearts(
+    _ postId: String,
+    commentId: String,
+    userId: String,
+    willHeartComment: Bool
+  ) -> AnyPublisher<Void, any Error> {
+    fatalError("미구현")
+  }
+}
