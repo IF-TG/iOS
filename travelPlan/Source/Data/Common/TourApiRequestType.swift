e93fa86c1946dd4af58155082e51aad6d433d18d
shcommit <happysh_s2@naver.com>
2024-05-07 00:50:46 +0900

    [Feat] TourApiReqeustType에 detailImage case 및 path 추가, TourImageInfoAPIEndpoint 객체 구현 및 이미지 검색 endpoint 구현

--- travelPlan/Source/Data/Common/TourApiRequestType.swift
+++ travelPlan/Source/Data/Common/TourApiRequestType.swift
@@ -10,6 +10,7 @@ import Foundation
 @frozen enum TourApiRequestType: String, RelativeURLPathProvidable {
   /// 공통정보조회
   case detailCommon = "detailCommon1"
+  case detailImage = "detailImage1"
   
   private var prefixPath: String {
     return "/B551011/KorService1"
@@ -19,6 +20,8 @@ import Foundation
     let prefix = "B551011/KorService1"
     let relativePath = switch self {
     case .detailCommon:
+      self.rawValue
+    case .detailImage:
       self.rawValue
     }
     return prefix + "/\(relativePath)"
