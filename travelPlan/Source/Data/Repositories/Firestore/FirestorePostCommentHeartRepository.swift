6eda790868f0439da5b8cec0fd8797088a9b48ef
shcommit <happysh_s2@naver.com>
2024-05-06 14:51:42 +0900

    [Feat] FirestorePostCommentHeartRepository에 커스텀 background queue 추가 및 fetchCommentHearts구현

--- travelPlan/Source/Data/Repositories/Firestore/FirestorePostCommentHeartRepository.swift
+++ travelPlan/Source/Data/Repositories/Firestore/FirestorePostCommentHeartRepository.swift
@@ -14,15 +14,18 @@ final class FirestorePostCommentHeartRepository {
   
   // MARK: - Dependencies
   private let service: FirestoreServiceProtocol
+  private let backgroundQueue: DispatchQueue
   
   // MARK: - Properties
   private var subscriptions = Set<AnyCancellable?>()
   
   // MARK: - Lifecycle
   init(
-    service: FirestoreServiceProtocol
+    service: FirestoreServiceProtocol,
+    backgroundQueue: DispatchQueue = DispatchQueue(label: "PostCommentHeart", qos: .default, attributes: .concurrent)
   ) {
     self.service = service
+    self.backgroundQueue = backgroundQueue
   }
 }
 
@@ -33,9 +36,11 @@ extension FirestorePostCommentHeartRepository: PostCommentHeartRepository {
     _ commentId: String
   ) -> AnyPublisher<[UserIdentifier], any Error> {
     let endpoint = Endpoint.makeCommentHeartUsersFetchEndpoint(postId, commentId)
-    return Future { [weak self] promise in
+    return Future { [weak self, backgroundQueue] promise in
       let retrieveSubscription = self?.service
         .retrieveDocumentIDs(endpoint: endpoint)
+        .subscribe(on: backgroundQueue)
+        .receive(on: backgroundQueue)
         .sink { completion in
           if case .failure(let error) = completion {
             promise(.failure(error))
@@ -51,7 +56,19 @@ extension FirestorePostCommentHeartRepository: PostCommentHeartRepository {
     _ postId: String,
     _ commentId: String
   ) -> AnyPublisher<Int, any Error> {
-    fatalError("미구현")
+    return Future { [weak self, backgroundQueue] promise in
+      let commentHeartFetchSubscription = self?.fetchCommentHeartUsers(postId, commentId)
+        .subscribe(on: backgroundQueue)
+        .receive(on: backgroundQueue)
+        .sink { completion in
+          if case .failure(let error) = completion {
+            promise(.failure(error))
+          }
+        } receiveValue: { commentHeartUsers in
+          promise(.success(commentHeartUsers.count))
+        }
+      self?.subscriptions.insert(commentHeartFetchSubscription)
+    }.eraseToAnyPublisher()
   }
   
   func heartComment(
