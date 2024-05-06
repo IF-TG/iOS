2d744aadefac79041642b0557c68714d81d4d78b
shcommit <happysh_s2@naver.com>
2024-05-07 00:59:16 +0900

    [Feat] TourApiRetrievedImageResponseDTO 객체 내부 구조 수정

--- travelPlan/Source/Data/Network/TourAPI/ResponseDTO/TourApiRetrievedImageResponseDTO.swift
+++ travelPlan/Source/Data/Network/TourAPI/ResponseDTO/TourApiRetrievedImageResponseDTO.swift
@@ -10,34 +10,38 @@ import Foundation
 struct TourApiRetrievedImageResponseDTO: Decodable {
   let contentId: String
   let image: Image
-  
-  /// 저작권?!
-  let cpyrhtDivCd: String
-  let serialNumber: String
+  let copyright: ImageCopyright
   
   enum CodingKeys: String, CodingKey {
     case contentId
-    case originimgurl
-    case smallimageurl
-    case cpyrhtDivCd
-    case serialNumber = "serialnum"
   }
   
   init(from decoder: any Decoder) throws {
     let container = try decoder.container(keyedBy: CodingKeys.self)
     contentId = try container.decode(String.self, forKey: .contentId)
-    cpyrhtDivCd = try container.decode(String.self, forKey: .cpyrhtDivCd)
-    serialNumber = try container.decode(String.self, forKey: .serialNumber)
     image = try Image(from: decoder)
+    copyright = try ImageCopyright(from: decoder)
   }
   
   struct Image: Decodable {
+    let name: String
     let originalUrl: String
     let thumbnailUrl: String
     
     enum CodingKeys: String, CodingKey {
       case originalUrl = "originimgurl"
       case thumbnailUrl = "smallimageurl"
+      case name = "imgname"
+    }
+  }
+  
+  struct ImageCopyright: Decodable {
+    let divisionCode: String
+    let serialNumber: String
+    
+    enum CodingKeys: String, CodingKey {
+      case divisionCode = "cpyrhtDivCd"
+      case serialNumber = "serialnum"
     }
   }
 }
