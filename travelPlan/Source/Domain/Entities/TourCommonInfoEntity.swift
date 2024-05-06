87edf372cbb11ba33f484894ededfde58857549d
seokhyun <letushyun@gmail.com>
2024-05-06 22:29:44 +0900

    [Feat] TourCommonInfoEntity Image프로퍼티를 String 타입에서 Data? 타입으로 변경

--- travelPlan/Source/Domain/Entities/TourCommonInfoEntity.swift
+++ travelPlan/Source/Domain/Entities/TourCommonInfoEntity.swift
@@ -45,7 +45,7 @@ extension TourCommonInfoEntity {
   
   /// **이미지 정보 조회**
   struct ImageInfo {
-    let originalImageURL: String
-    let thumbnailImageURL:  String
+    let originalImageData: Data?
+    let thumbnailImageData: Data?
   }
 }
