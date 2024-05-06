c710da4f1607b1c2105b02986d12234749734e9b
seokhyun <letushyun@gmail.com>
2024-05-06 23:53:49 +0900

    [Rebuild] DefaultTourCommonInfoRepository 수정

--- travelPlan/Source/Data/Repositories/TourAPI/DefaultTourCommonInfoRepository.swift
+++ travelPlan/Source/Data/Repositories/TourAPI/DefaultTourCommonInfoRepository.swift
@@ -15,7 +15,6 @@ final class DefaultTourCommonInfoRepository: TourCommonInfoRepository {
   // MARK: - Dependencies
   private let service: Sessionable
   private let backgroundQueue: DispatchQueue
-  private var subscriptions = Set<AnyCancellable?>()
   
   // MARK: - Lifecycle
   init(service: Sessionable, backgroundQueue: DispatchQueue = .global(qos: .userInitiated)) {
@@ -32,44 +31,39 @@ final class DefaultTourCommonInfoRepository: TourCommonInfoRepository {
     let endpoint = Endpoint.makeCommonInfoAPIEndpoint(with: requestDTO)
     let imageConverter = ImageConverter()
     
-    return Future { [weak self, backgroundQueue] promise in
-      let subscription = self?.service.request(endpoint: endpoint)
-        .subscribe(on: backgroundQueue)
-        .mapConnectionError()
-        .flatMap {
-          let resultCode = $0.response.header.resultCode
-          
-          guard resultCode == "0000" else {
-            return Fail<TourCommonInfoEntity, any Error> (error: TourAPIError.unexpectedErrorFromSuccessfulResponseData(resultCode)).eraseToAnyPublisher()
-          }
-          
-          guard let item = $0.response.body.items.item.first else {
-            return Fail<TourCommonInfoEntity, any Error> (error: TourAPIError.tourAPIProviderInstitutionError(.noDataError)).eraseToAnyPublisher()
-          }
-          
-          let imagePublisher = imageConverter
-            .request(imageURL: item.firstimage, queue: backgroundQueue)
-            .mapError { $0 as Error }
-            .eraseToAnyPublisher()
-          let thumbnailPublisher = imageConverter
-            .request(imageURL: item.firstimage2, queue: backgroundQueue)
-            .mapError { $0 as Error }
-            .eraseToAnyPublisher()
-          
-          return imagePublisher.zip(thumbnailPublisher)
-            .map { (imageDate, thumbnailData) in
-              return item.toDomain(firstImageData: imageDate, thumbnailImageDate: thumbnailData)
-            }.eraseToAnyPublisher()
+    return service.request(endpoint: endpoint)
+      .subscribe(on: backgroundQueue)
+      .mapConnectionError()
+      .flatMap { [backgroundQueue] in
+        let resultCode = $0.response.header.resultCode
+        
+        guard resultCode == "0000" else {
+          return Fail<TourCommonInfoEntity, any Error> (
+            error: TourAPIError.publicDataPortalError(.init(code: String(resultCode.suffix(2))))
+          )
+          .eraseToAnyPublisher()
         }
-        .sink(receiveCompletion: { completion in
-          if case .failure(let error) = completion {
-            promise(.failure(error))
-          }
-        }, receiveValue: { entity in
-          promise(.success(entity))
-        })
-      
-      self?.subscriptions.insert(subscription)
-    }.eraseToAnyPublisher()
+        
+        guard let item = $0.response.body.items.item.first else {
+          return Fail<TourCommonInfoEntity, any Error> (
+            error: TourAPIError.tourAPIProviderInstitutionError(.noDataError)
+          )
+          .eraseToAnyPublisher()
+        }
+        
+        let imagePublisher = imageConverter
+          .request(imageURL: item.firstimage, queue: backgroundQueue)
+          .mapError { $0 as Error }
+          .eraseToAnyPublisher()
+        let thumbnailPublisher = imageConverter
+          .request(imageURL: item.firstimage2, queue: backgroundQueue)
+          .mapError { $0 as Error }
+          .eraseToAnyPublisher()
+        
+        return imagePublisher.zip(thumbnailPublisher)
+          .map { (imageDate, thumbnailData) in
+            return item.toDomain(firstImageData: imageDate, thumbnailImageDate: thumbnailData)
+          }.eraseToAnyPublisher()
+      }.eraseToAnyPublisher()
   }
 }
