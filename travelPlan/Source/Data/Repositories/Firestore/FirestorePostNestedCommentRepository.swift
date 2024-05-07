634fb6c2ae2243651e6fb0130094135747b82857
shcommit <happysh_s2@naver.com>
2024-05-07 22:41:20 +0900

    [Feat] FirestorePostNestedCommentRepository에서 대댓보내기 로직 구현!

--- travelPlan/Source/Data/Repositories/Firestore/FirestorePostNestedCommentRepository.swift
+++ travelPlan/Source/Data/Repositories/Firestore/FirestorePostNestedCommentRepository.swift
@@ -7,6 +7,7 @@
 
 import Combine
 import Foundation
+import FirebaseFirestore
 import SHFirestoreService
 
 final class FirestorePostNestedCommentRepository {
@@ -38,22 +39,48 @@ extension FirestorePostNestedCommentRepository: PostAtomicNestedCommentRepositor
     postId: String,
     commentId: String
   ) -> AnyPublisher<[PostAtomicNestedCommentEntity], any Error> {
-    
+    fatalError("아직 미구현입니다.")
   }
   
   func sendNestedComment(
     ownerId: String,
     postId: String,
-    commentId: String
-  ) -> AnyPublisher<[PostAtomicNestedCommentEntity], any Error> {
-    fatalError("아직 미구현입니다.")
+    commentId: String,
+    comment: String
+  ) -> AnyPublisher<PostAtomicNestedCommentEntity, any Error> {
+    let nestedCommentId = UUID().uuidString
+    let requestDTO = FirestorePostNestedCommentSendRequestDTO(
+      nestedCommentId: nestedCommentId, authorId: ownerId,
+      comment: comment, hearts: 0, createAt: Timestamp(date: Date()))
+    let endpoint = Endpoint.makeNestedCommentSendEndpoint(
+      withPostId: postId, commentId: commentId, nestedCommentId: nestedCommentId, requestDTO: requestDTO)
+    return Future { [weak self, backgroundQueue] promise in
+      guard let self else {
+        promise(.failure(ReferenceError.invalidReference))
+        return
+      }
+      let saveSubscription = service.saveDocument(endpoint: endpoint)
+        .subscribe(on: backgroundQueue)
+        .receive(on: backgroundQueue)
+        .sink { completion in
+          if case .failure(let error) = completion {
+            promise(.failure(error))
+          }
+        } receiveValue: { _ in
+          let atomicEntity = PostAtomicNestedCommentEntity(
+            nestedCommentId: nestedCommentId, authorId: ownerId, comment: comment,
+            createAt: requestDTO.createAt.dateValue(), hearts: 0)
+          promise(.success(atomicEntity))
+        }
+      subscriptions.insert(saveSubscription)
+    }.eraseToAnyPublisher()
   }
   
   func updateNestedComment(
     ownerId: String,
     postId: String,
     commentId: String
-  ) -> AnyPublisher<[PostAtomicNestedCommentEntity], any Error> {
+  ) -> AnyPublisher<Void, any Error> {
     fatalError("아직 미구현입니다.")
   }
   
