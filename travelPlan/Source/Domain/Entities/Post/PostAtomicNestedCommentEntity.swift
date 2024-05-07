65ba6ad766b443b08fd40c7e279fd7d31b811606
shcommit <happysh_s2@naver.com>
2024-05-07 21:39:51 +0900

    [Feat] PostAtomicNestedCommentEntity 구현

---
+++ travelPlan/Source/Domain/Entities/Post/PostAtomicNestedCommentEntity.swift
@@ -0,0 +1,16 @@
+//
+//  PostAtomicNestedCommentEntity.swift
+//  travelPlan
+//
+//  Created by 양승현 on 5/7/24.
+//
+
+import Foundation
+
+struct PostAtomicNestedCommentEntity {
+  let nestedCommentId: String
+  let authorId: String
+  let comment: String
+  let createAt: Date
+  let hearts: Int
+}
