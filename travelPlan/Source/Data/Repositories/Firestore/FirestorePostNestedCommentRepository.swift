9cf2c38f5b410994c7b47603e2e9cc511e946bfa
shcommit <happysh_s2@naver.com>
2024-05-07 22:25:57 +0900

    [Feat] FirestorePostNestedCommentRepository 객체 생성 및 PostAtomicNestedCommentRepository 준수만!!

---
+++ travelPlan/Source/Data/Repositories/Firestore/FirestorePostNestedCommentRepository.swift
@@ -0,0 +1,68 @@
+//
+//  FirestorePostNestedCommentRepository.swift
+//  travelPlan
+//
+//  Created by 양승현 on 5/7/24.
+//
+
+import Combine
+import Foundation
+import SHFirestoreService
+
+final class FirestorePostNestedCommentRepository {
+  typealias Endpoint = FirestoreUserProfileAPIEndpoint
+  
+  // MARK: - Dependencies
+  private let backgroundQueue: DispatchQueue
+  private let service: FirestoreServiceProtocol
+  private let firebaseStorageService: ImageStorageServiceProtocol
+  
+  // MARK: - Properties
+  private var subscriptions = Set<AnyCancellable?>()
+  
+  // MARK: - Lifecycle
+  init(
+    service: FirestoreServiceProtocol,
+    backgroundQueue: DispatchQueue = .global(qos: .userInitiated),
+    firebaseStorageService: ImageStorageServiceProtocol
+  ) {
+    self.service = service
+    self.backgroundQueue = backgroundQueue
+    self.firebaseStorageService = firebaseStorageService
+  }
+}
+
+// MARK: - PostAtomicNestedCommentRepository
+extension FirestorePostNestedCommentRepository: PostAtomicNestedCommentRepository {
+  func fetchNestedComments(
+    postId: String,
+    commentId: String
+  ) -> AnyPublisher<[PostAtomicNestedCommentEntity], any Error> {
+    fatalError("아직 미구현입니다.")
+  }
+  
+  func sendNestedComment(
+    ownerId: String,
+    postId: String,
+    commentId: String
+  ) -> AnyPublisher<[PostAtomicNestedCommentEntity], any Error> {
+    fatalError("아직 미구현입니다.")
+  }
+  
+  func updateNestedComment(
+    ownerId: String,
+    postId: String,
+    commentId: String
+  ) -> AnyPublisher<[PostAtomicNestedCommentEntity], any Error> {
+    fatalError("아직 미구현입니다.")
+  }
+  
+  func deleteNestedComment(
+    hasCommentDeleted: Bool,
+    postId: String,
+    commentId: String,
+    nestedCommentId: String
+  ) -> AnyPublisher<Void, any Error> {
+    fatalError("아직 미구현입니다.")
+  }
+}
