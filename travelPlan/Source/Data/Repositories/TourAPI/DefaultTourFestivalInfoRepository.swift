3fa528ccc7049e39e498c95578a84c7a60fa9fe8
seokhyun <letushyun@gmail.com>
2024-05-08 01:12:07 +0900

    [Feat] 데이터 변경 error 추가

--- travelPlan/Source/Data/Repositories/TourAPI/DefaultTourFestivalInfoRepository.swift
+++ travelPlan/Source/Data/Repositories/TourAPI/DefaultTourFestivalInfoRepository.swift
@@ -29,7 +29,10 @@ extension DefaultTourFestivalInfoRepository: TourFestivalInfoRepository {
   func fetchFestivalList() -> AnyPublisher<[FestivalThumbnailEntity], any Error> {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "yyyyMMdd"
-    let formattedDate = Int(dateFormatter.string(from: Date()))!
+    guard let formattedDate = Int(dateFormatter.string(from: Date())) else {
+      return Fail(error: TransformationError.dateToInt)
+        .eraseToAnyPublisher()
+    }
     
     let requestDTO = TourAPIFestivalRequestDTO(eventStartDate: formattedDate)
     let endpoint = TourAPIFestivalEndpoints.fetchFestivalList(with: requestDTO)
