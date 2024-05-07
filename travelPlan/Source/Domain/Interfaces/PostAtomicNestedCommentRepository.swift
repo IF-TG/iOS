7b6d6ff3c0158062ea07fd56950c9c443cf05e69
shcommit <happysh_s2@naver.com>
2024-05-07 22:22:36 +0900

    [Feat] PostAtomicNestedCommentRepository 프로토콜 선언 및 대댓글 fetch, send, update, delete 함수 추가

---
+++ travelPlan/Source/Domain/Interfaces/PostAtomicNestedCommentRepository.swift
@@ -0,0 +1,36 @@
+//
+//  PostAtomicNestedCommentRepository.swift
+//  travelPlan
+//
+//  Created by 양승현 on 5/7/24.
+//
+
+import Foundation
+import Combine
+
+protocol PostAtomicNestedCommentRepository {
+  func fetchNestedComments(
+    postId: String,
+    commentId: String
+  ) -> AnyPublisher<[PostAtomicNestedCommentEntity], Error>
+  
+  func sendNestedComment(
+    ownerId: String,
+    postId: String,
+    commentId: String
+  ) -> AnyPublisher<[PostAtomicNestedCommentEntity], Error>
+  
+  func updateNestedComment(
+    ownerId: String,
+    postId: String,
+    commentId: String
+  ) -> AnyPublisher<[PostAtomicNestedCommentEntity], Error>
+  
+  /// 댓글이 삭제됬는지 고려해야 합니다.
+  func deleteNestedComment(
+    hasCommentDeleted: Bool,
+    postId: String,
+    commentId: String,
+    nestedCommentId: String
+  ) -> AnyPublisher<Void, Error>
+}
