fbb9f4ac9977b5184bd0139243a9c34bd22a1c95
seokhyun <letushyun@gmail.com>
2024-05-08 17:59:55 +0900

    [Test] DefaultTourIntroductionInfoRepositoryTests 구현

--- travelPlan/travelPlanTests/Data/DefaultTourIntroductionInfoRepositoryTests.swift
+++ travelPlan/travelPlanTests/Data/DefaultTourIntroductionInfoRepositoryTests.swift
@@ -8,6 +8,7 @@
 import XCTest
 import Combine
 @testable import travelPlan
+
 final class DefaultTourIntroductionInfoRepositoryTests: XCTestCase {
   // MARK: - Properties
   var sut: DefaultTourIntroductionInfoRepository!
@@ -31,5 +32,30 @@ final class DefaultTourIntroductionInfoRepositoryTests: XCTestCase {
 }
 
 extension DefaultTourIntroductionInfoRepositoryTests {
-  func test_fetchFestival메소드_호출시_value로
+  func test_fetchFestival메소드_호출시_value로_IntroductionInfoFestivalEntity를_내려주면_shouldReturnTrue() {
+    // Arrange
+    var unexpectedError: Error?
+    var receivedResult = false
+    let tourContentId = TourContentId(contentId: 3114721, contentTypeId: 15)
+    
+    // Act
+    sut.fetchFestival(tourContentId: tourContentId)
+      .sink { [weak self] completion in
+        if case let .failure(error) = completion {
+          unexpectedError = error
+          self?.expectation.fulfill()
+        }
+      } receiveValue: { [weak self] entity in
+        print("entity: \(entity)")
+        receivedResult = true
+        self?.expectation.fulfill()
+      }
+      .store(in: &subscriptions)
+    
+    waitForExpectations(timeout: 10)
+    
+    // Assert
+    checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "fetchFestival")
+    XCTAssertTrue(receivedResult, "receiveValue가 들어와야하는데 들어오지 않음")
+  }
 }
