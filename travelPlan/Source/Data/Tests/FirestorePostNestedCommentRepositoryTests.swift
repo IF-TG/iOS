d070b86002eadd9755b5dc2a683823a5dbdb9bc1
shcommit <happysh_s2@naver.com>
2024-05-08 21:13:06 +0900

    [Test] 대댓글 받아오는 레포지토리 함수 fetchNestedComments 테스트

--- travelPlan/Source/Data/Tests/FirestorePostNestedCommentRepositoryTests.swift
+++ travelPlan/Source/Data/Tests/FirestorePostNestedCommentRepositoryTests.swift
@@ -131,4 +131,29 @@ extension FirestorePostNestedCommentRepositoryTests {
     XCTAssertTrue(receivedResult)
   }
   
+  func test_fetchNestedComments호출시관련Entity를받는가_ShouldReturnTrue() {
+    // Arrange
+    var receivedResult = false
+    var unexpectedError: Error?
+    
+    // Act
+    sut.fetchNestedComments(postId: testPostId, commentId: testCommentId)
+    .receive(on: DispatchQueue.main)
+    .sink { completion in
+      if case .failure(let error) = completion {
+        unexpectedError = error
+        self.expectation.fulfill()
+      }
+    } receiveValue: { entity in
+      print("DEBUG: 값을 성공적으로 받았습니다. \(entity)")
+      receivedResult.toggle()
+      self.expectation.fulfill()
+    }.store(in: &subscriptions)
+    wait(for: [expectation], timeout: 7.777)
+    
+    // Assert
+    checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "updateNestedComment")
+    XCTAssertTrue(receivedResult)
+  }
+  
 }
