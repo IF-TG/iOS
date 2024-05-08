334608bb1bd27d7a135de764150d4a1e1060d45a
seokhyun <letushyun@gmail.com>
2024-05-08 17:05:25 +0900

    [Feat] TourContentId 정의

---
+++ travelPlan/Source/Domain/Entities/TourContentId.swift
@@ -0,0 +1,13 @@
+//
+//  TourContentId.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 5/8/24.
+//
+
+import Foundation
+
+struct TourContentId {
+  let contentId: Int
+  let contentTypeId: Int
+}
