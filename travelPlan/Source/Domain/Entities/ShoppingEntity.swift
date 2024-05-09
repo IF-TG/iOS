de3600346267346866352dcb76cb34f7f1d77108
seokhyun <letushyun@gmail.com>
2024-05-10 01:28:58 +0900

    [Style] ShoppingEntity 프로퍼티 타입 명확하게 수정

--- travelPlan/Source/Domain/Entities/ShoppingEntity.swift
+++ travelPlan/Source/Domain/Entities/ShoppingEntity.swift
@@ -10,9 +10,9 @@ import Foundation
 struct ShoppingEntity {
   let canLoanBabyCarriage: String?
   let canAccompanyPet: String?
-  let fairDate: String?
-  let openDate: String?
+  let fairDate: Date?
+  let openDate: Date?
   let openTime: String?
-  let restDate: String?
+  let restDate: Date?
   let saleItem: String?
 }
