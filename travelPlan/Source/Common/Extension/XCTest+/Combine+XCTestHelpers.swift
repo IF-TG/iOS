354ee84c9b6b4b624c2adfdee19c300434f14bab
shcommit <happysh_s2@naver.com>
2024-05-10 17:01:18 +0900

    [Fix] sink 테스트 전용 만들때 @escaping 클로저는 inout 인자값 캡쳐 불가능하기 때문에 클로저로 변경

--- travelPlan/Source/Common/Extension/XCTest+/Combine+XCTestHelpers.swift
+++ travelPlan/Source/Common/Extension/XCTest+/Combine+XCTestHelpers.swift
@@ -12,37 +12,44 @@ import Combine
 import XCTest
 
 /// 비동기적으로 작동하는 퍼블리셔로부터 데이터를 잘 받았는지, 에러를 방출하는지를 구독하는 함수입니다.
+/// 결과가 성공이든 에러든 expectation.fulfill()을 호출합니다.
+/// expectation을 방출하기 전에 sink의 receivedValue를 받았는지, 아니면 에러를 받았는지를 resultHandler의 인자값으로 알수있습니다.
 extension XCTestCase {
+  typealias UnexpectedError = Error
+  typealias HasReceivedResult = Bool
+  typealias TestResultHandler = (UnexpectedError?, HasReceivedResult) -> Void
+  
   func sink(
     fromPublisher taskPublisher: any Publisher,
     withExpectation expectation: XCTestExpectation,
-    unexpectedError: inout Error?,
-    hasReceivedResult: inout Bool
+    resultHandler: @escaping TestResultHandler
   ) -> AnyCancellable {
     return taskPublisher
       .sink(
         withExpectation: expectation,
-        unexpectedError: &unexpectedError,
-        hasReceivedResult: &hasReceivedResult)
+        resultHandler: resultHandler)
   }
 }
 
 fileprivate extension Publisher {
+  typealias UnexpectedError = Error
+  typealias HasReceivedResult = Bool
+  typealias TestResultHandler = (UnexpectedError?, HasReceivedResult) -> Void
+  
   func sink(
     withExpectation expectation: XCTestExpectation,
-    unexpectedError: inout Error?,
-    hasReceivedResult: inout Bool
+    resultHandler: @escaping TestResultHandler
   ) -> AnyCancellable {
     return self
       .receive(on: DispatchQueue.main)
       .sink { completion in
       if case .failure(let error) = completion {
-        unexpectedError = error
+        resultHandler(error, false)
         expectation.fulfill()
       }
     } receiveValue: { receivedValue in
       NSLog("DEBUG: 값을 성공적으로 받았습니다:\n\(receivedValue)\n\n")
-      hasReceivedResult = true
+      resultHandler(nil, true)
       expectation.fulfill()
     }
   }
