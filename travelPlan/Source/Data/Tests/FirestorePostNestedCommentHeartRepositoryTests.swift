1564191be27de0db37bfb2f7ee4c4031a04041f4
shcommit <happysh_s2@naver.com>
2024-05-10 17:23:28 +0900

    [Test] FriestorePostNestedCommentHeartRepositoryTests에서 대댓글 싫어요할 경우에 commeht-hearts에서 사라지는지 테스트

--- travelPlan/Source/Data/Tests/FirestorePostNestedCommentHeartRepositoryTests.swift
+++ travelPlan/Source/Data/Tests/FirestorePostNestedCommentHeartRepositoryTests.swift
@@ -56,7 +56,42 @@ extension FirestorePostNestedCommentHeartRepositoryTests {
     wait(for: [expectation], timeout: 7.777)
     
     // Assert
-    checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "fetchProfile")
+    checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "heartNestedComment")
+    XCTAssertTrue(hasReceivedResult, "대댓글에 좋아요한 사용자가 추가된 후에 성공적으로 사용자 id를 받아야하지만 받지 못했습니다")
+  }
+  
+  func test_hateNestedComment호출시성공적으로사용자가제거되는지() {
+    // Arrange
+    var unexpectedError: Error?
+    var hasReceivedResult: Bool = false
+    let testUserName = "testUser12345"
+    var prevExpectation = expectation(description: "사전 준비작업으로 위에서 테스트한 함수가 사용됩니다")
+    sut.heartNestedComment(
+      with: testPostId, commentId: testCommentId, nestedCommentId: testNestedCommentId, userId: testUserName)
+    .sink {
+      if case .failure = $0 {
+        XCTFail("대댓글 좋아요한 사용자 삭제 테스트하기 위해 사전작업으로 좋아요한 사용자 넣는 도중 에러 발생")
+        prevExpectation.fulfill()
+      }
+    } receiveValue: { _ in
+      prevExpectation.fulfill()
+    }.store(in: &subscriptions)
+    wait(for: [prevExpectation], timeout: 7.777)
+    
+    // Act
+    let taskPublisher = sut.hateNestedComment(
+      with: testPostId, commentId: testCommentId,
+      nestedCommentId: testNestedCommentId, userId: testUserName)
+    sink(
+      fromPublisher: taskPublisher,
+      withExpectation: expectation) { error, result in
+        unexpectedError = error
+        hasReceivedResult = result
+      }.store(in: &subscriptions)
+    wait(for: [expectation], timeout: 7.777)
+    
+    // Assert
+    checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "hateNestedComment")
     XCTAssertTrue(hasReceivedResult, "대댓글에 좋아요한 사용자가 추가된 후에 성공적으로 사용자 id를 받아야하지만 받지 못했습니다")
   }
 }
