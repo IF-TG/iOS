506e1e2062dfcd0759627e9d5ff90402331a56d4
shcommit <happysh_s2@naver.com>
2024-05-10 16:42:27 +0900

    [Feat] 하트좋아요 테스트 로직 추가

--- travelPlan/Source/Data/Tests/FirestorePostNestedCommentHeartRepositoryTests.swift
+++ travelPlan/Source/Data/Tests/FirestorePostNestedCommentHeartRepositoryTests.swift
@@ -6,30 +6,58 @@
 //
 
 import XCTest
+import Combine
+@testable import travelPlan
+@testable import SHFirestoreService
 
 final class FirestorePostNestedCommentHeartRepositoryTests: XCTestCase {
+  // MARK: - Properties
+  var sut: PostNestedCommentHeartRepository!
+  var expectation: XCTestExpectation!
+  var subscriptions = Set<AnyCancellable>()
+  let testPostId = "ABEB803F-DD54-41A4-B8BF-487A210BD1EC"
+  let testCommentId = "12181109-6CDE-46E5-AD4F-04E824E89581"
+  let testNestedCommentId = "941C554D-CB1E-4B7E-A749-9DA76E67D960"
+  
+  override func setUp() {
+    super.setUp()
+    let service = FirestoreService()
+    
+    sut = FirestorePostNestedCommentHeartRepository(
+      service: service)
+    
+    expectation = XCTestExpectation(description: "Finish")
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+    expectation = nil
+    subscriptions.removeAll()
+  }
+}
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+extension FirestorePostNestedCommentHeartRepositoryTests {
+  func test_heartNestedComment호출시성공적으로반환값을받는지() {
+    // Arrange
+    var unexpectedError: Error?
+    var hasReceivedResult: Bool = false
+    
+    // Act
+    let taskPublisher = sut.heartNestedComment(
+      with: testPostId, commentId: testCommentId,
+      nestedCommentId: testNestedCommentId, userId: "testUser1234")
+    sink(
+      fromPublisher: taskPublisher,
+      withExpectation: expectation, 
+      unexpectedError: &unexpectedError,
+      hasReceivedResult: &hasReceivedResult)
+    .store(in: &subscriptions)
+    wait(for: [expectation], timeout: 7.777)
+    
+    
+    // Assert
+    checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "fetchProfile")
+    XCTAssertTrue(hasReceivedResult, "대댓글에 좋아요한 사용자가 추가된 후에 성공적으로 사용자 id를 받아야하지만 받지 못했습니다")
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  }
 }
