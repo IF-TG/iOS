66adcee00f2060d343d10b5fd339dc720094089e
seokhyun <letushyun@gmail.com>
2024-05-12 08:36:13 +0900

    [Feat] 소개정보조회 관광지 entity 구현

---
+++ travelPlan/Source/Domain/Entities/TourDestinationType/IntroductionInfoAttractionEntity.swift
@@ -0,0 +1,18 @@
+//
+//  IntroductionInfoAttractionEntity.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 5/12/24.
+//
+
+import Foundation
+
+struct IntroductionInfoAttractionEntity {
+  let tourContentId: TourContentId
+  let restDateString: String
+  let telNumber: String
+  let availableTime: String
+  let canPark: String
+  let canAccompanyDog: String
+  let experienceInfo: String
+}
