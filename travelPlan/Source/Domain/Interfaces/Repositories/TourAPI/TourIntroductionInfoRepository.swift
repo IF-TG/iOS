5abb636f71d96ccc01a451cf90b816e6c9bf5f06
seokhyun <letushyun@gmail.com>
2024-05-12 01:10:05 +0900

    [Feat] fetchRestaurant 추가

--- travelPlan/Source/Domain/Interfaces/Repositories/TourAPI/TourIntroductionInfoRepository.swift
+++ travelPlan/Source/Domain/Interfaces/Repositories/TourAPI/TourIntroductionInfoRepository.swift
@@ -9,6 +9,7 @@ import Foundation
 import Combine
 
 protocol TourIntroductionInfoRepository {
+  func fetchRestaurant(tourContentId: TourContentId) -> AnyPublisher<IntroductionInfoRestaurantEntity, any Error>
   func fetchFestival(tourContentId: TourContentId) -> AnyPublisher<IntroductionInfoFestivalEntity, any Error>
   func fetchShopping(tourContentId: TourContentId) -> AnyPublisher<IntroductionInfoShoppingEntity, any Error>
 }
