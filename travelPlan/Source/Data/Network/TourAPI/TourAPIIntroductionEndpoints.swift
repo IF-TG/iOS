15d824a6babc01a43b5d883a4eb0888d36df947d
seokhyun <letushyun@gmail.com>
2024-05-12 16:18:19 +0900

    [Rebuild] requestType이 공통이므로, 간편화

--- travelPlan/Source/Data/Network/TourAPI/TourAPIIntroductionEndpoints.swift
+++ travelPlan/Source/Data/Network/TourAPI/TourAPIIntroductionEndpoints.swift
@@ -8,27 +8,34 @@
 import Foundation
 
 struct TourAPIIntroductionEndpoints {
+  private static let tourApiRequestType = TourApiRequestType.introduction
   private init() { }
+  
+  static func fetchCultureFacility(
+    with requestDTO: TourAPIIntroductionInfoRequestDTO
+  ) -> TourApiEndpoint<TourApiCommonResponseDTO<TourIntroductionInfoCultureFacilityResponseDTO>> {
+    return .init(parameters: .query(requestDTO), requestType: tourApiRequestType)
+  }
   
   static func fetchAttraction(
     with requestDTO: TourAPIIntroductionInfoRequestDTO
   ) -> TourApiEndpoint<TourApiCommonResponseDTO<TourIntroductionInfoAttractionResponseDTO>> {
-    return .init(parameters: .query(requestDTO), requestType: .introduction)
+    return .init(parameters: .query(requestDTO), requestType: tourApiRequestType)
   }
   
   static func fetchRestaurant(
     with requestDTO: TourAPIIntroductionInfoRequestDTO
   ) -> TourApiEndpoint<TourApiCommonResponseDTO<TourIntroductionInfoRestaurantResponseDTO>> {
-    return .init(parameters: .query(requestDTO), requestType: .introduction)
+    return .init(parameters: .query(requestDTO), requestType: tourApiRequestType)
   }
   
   static func fetchFestival(
     with requestDTO: TourAPIIntroductionInfoRequestDTO
   ) -> TourApiEndpoint<TourApiCommonResponseDTO<TourIntroductionInfoFestivalResponseDTO>> {
-    return .init(parameters: .query(requestDTO), requestType: .introduction)
+    return .init(parameters: .query(requestDTO), requestType: tourApiRequestType)
   }
   
   static func fetchShopping(with requestDTO: TourAPIIntroductionInfoRequestDTO) -> TourApiEndpoint<TourApiCommonResponseDTO<TourIntroductionInfoShoppingResponseDTO>> {
-    return .init(parameters: .query(requestDTO), requestType: .introduction)
+    return .init(parameters: .query(requestDTO), requestType: tourApiRequestType)
   }
 }
