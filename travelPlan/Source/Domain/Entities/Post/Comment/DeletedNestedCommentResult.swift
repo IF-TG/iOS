9606dc22231180c9bdacb08056ac5767d922b92d
shcommit <happysh_s2@naver.com>
2024-05-14 01:21:05 +0900

    [Style] DeletedNestedCommentResult 네이밍 명확하게 수정

--- travelPlan/Source/Domain/Entities/Post/Comment/DeletedNestedCommentResult.swift
+++ travelPlan/Source/Domain/Entities/Post/Comment/DeletedNestedCommentResult.swift
@@ -9,7 +9,7 @@ import Foundation
 
 @frozen enum DeletedNestedCommentResult {
   /// 댓글이 삭제되지 않은 상태에서 대댓글 제거 완료
-  case deletedJustNestedComment
+  case justANestedCommentDeleted
   /// 대댓글도 제거, 해당 대댓글을 소유한 댓글도 제거 완료
-  case deletedACommentAndAllNestedComments
+  case ACommentAndAllNestedCommentsDeleted
   }
