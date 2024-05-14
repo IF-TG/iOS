1fb6bd069462f82d5cf98a58ad3f12b9f150c0a6
seokhyun <letushyun@gmail.com>
2024-05-14 22:32:56 +0900

    [Test] FestivalUseCase의 fetchFestivalDetail 메소드 test
    테스트 결과 FestivalEntity를 성공적으로 받아오는 것을 확인.

--- travelPlan/Source/Domain/Tests/FestivalUseCaseTests.swift
+++ travelPlan/Source/Domain/Tests/FestivalUseCaseTests.swift
@@ -21,14 +21,17 @@ final class FestivalUseCaseTests: XCTestCase {
     self.sut = DefaultFestivalUseCase(
       tourFestivalInfoRepository: DefaultTourFestivalInfoRepository(
         service: SessionProvider(),
-        imageService: ImageSessionProvider()
+        imageService: ImageSessionProvider(),
+        backgroundQueue: DispatchQueue.main
       ),
       tourCommonInfoRepository: DefaultTourCommonInfoRepository(
         service: SessionProvider(),
-        imageService: ImageSessionProvider()
+        imageService: ImageSessionProvider(),
+        backgroundQueue: DispatchQueue.main
       ),
       tourIntroductionInfoRepository: DefaultTourIntroductionInfoRepository(
-        service: SessionProvider()
+        service: SessionProvider(),
+        backgroundQueue: DispatchQueue.main
       )
     )
     self.expectation = .init(description: "비동기 처리")
@@ -70,5 +73,33 @@ extension FestivalUseCaseTests {
     // Assert
     checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "fetchFestivalList")
     XCTAssertTrue(receivedResult, "receviedValue로 FestivalThumbnailEntity배열을 받지 못함.")
+  }
+  
+  func test_fetchFestivalDetail메소드_호출시_receivedValue로FestivalEntity받아오는지() {
+    // Arrange
+    var unexpectedError: Error?
+    var receivedResult = false
+    let festivalId = TourContentId(contentId: 531391, contentTypeId: TourType.festival.rawValue)
+    
+    // Act
+    sut.fetchFestivalDetail(tourContentId: festivalId)
+      .receive(on: RunLoop.main)
+      .sink { [weak self] completion in
+        if case .failure(let error) = completion {
+          unexpectedError = error
+          self?.expectation.fulfill()
+        }
+      } receiveValue: { [weak self] festivalEntity in
+        receivedResult = true
+        print("festivalEntity: \(festivalEntity)")
+        self?.expectation.fulfill()
+      }
+      .store(in: &subscriptions)
+
+    wait(for: [expectation], timeout: 10)
+    
+    // Assert
+    checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "fetchFestivalDetail")
+    XCTAssertTrue(receivedResult, "receivedValue로 FestiavlEntity를 받아야하는데 받지 못함.")
   }
 }
