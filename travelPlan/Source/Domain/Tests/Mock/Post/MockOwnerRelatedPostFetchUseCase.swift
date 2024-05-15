4951d66714b5114ffe05b5ca050ec259212cc614
shcommit <happysh_s2@naver.com>
2024-05-16 01:07:31 +0900

    [Refactor] MockPostUseCaseForPaging 객체 제거. MockPostsGeneratorForPaging, MockPostFetchUseCase, MockOwnerRelatedPostFetchUseCase 로 분리

---
+++ travelPlan/Source/Domain/Tests/Mock/Post/MockOwnerRelatedPostFetchUseCase.swift
@@ -0,0 +1,51 @@
+//
+//  MockOwnerRelatedPostFetchUseCase.swift
+//  travelPlan
+//
+//  Created by 양승현 on 5/16/24.
+//
+
+import Combine
+import Foundation
+
+final class MockOwnerRelatedPostFetchUseCase: OwnerRelatedPostFetchUseCase {
+  var mockPostsGenerator1 = MockPostsGeneratorForPaging()
+  var mockPostsGenerator2 = MockPostsGeneratorForPaging()
+  
+  func fetchOwnerLikedPosts(page: Int32, perPage: Int32) -> AnyPublisher<PostsPage, any Error> {
+    if mockPostsGenerator1.index > mockPostsGenerator1.totalPage {
+      return Fail(error: PostUseCaseError.noMorePage).eraseToAnyPublisher()
+    }
+    let responseData = {
+      let nextPosts = (mockPostsGenerator1.index..<mockPostsGenerator1.index+5)
+        .map { mockPostsGenerator1.mockPostPage.posts[$0] }
+      let nextThumbnails = (mockPostsGenerator1.index..<mockPostsGenerator1.index+5)
+        .map { mockPostsGenerator1.mockPostPage.thumbnails[$0] }
+      return PostsPage(totalPosts: Int64(mockPostsGenerator1.totalPage), posts: nextPosts, thumbnails: nextThumbnails)
+    }()
+    mockPostsGenerator1.index+=5
+    return Just(responseData)
+      .delay(for: .seconds(0.28), scheduler: DispatchQueue.global(qos: .background))
+      .setFailureType(to: Error.self)
+      .eraseToAnyPublisher()
+  }
+  
+  func fetchOwnerWrotePosts(isFirstPage: Bool, perPage: Int32) -> AnyPublisher<PostsPage, any Error> {
+    if mockPostsGenerator1.index > mockPostsGenerator1.totalPage {
+      return Fail(error: PostUseCaseError.noMorePage).eraseToAnyPublisher()
+    }
+    let responseData = {
+      let nextPosts = (mockPostsGenerator1.index..<mockPostsGenerator1.index+5)
+        .map { mockPostsGenerator1.mockPostPage.posts[$0] }
+      let nextThumbnails = (mockPostsGenerator1.index..<mockPostsGenerator1.index+5)
+        .map { mockPostsGenerator1.mockPostPage.thumbnails[$0] }
+      return PostsPage(totalPosts: Int64(mockPostsGenerator1.totalPage), posts: nextPosts, thumbnails: nextThumbnails)
+    }()
+    mockPostsGenerator1.index+=5
+    return Just(responseData)
+      .delay(for: .seconds(0.28), scheduler: DispatchQueue.global(qos: .background))
+      .setFailureType(to: Error.self)
+      .eraseToAnyPublisher()
+    
+  }
+}
