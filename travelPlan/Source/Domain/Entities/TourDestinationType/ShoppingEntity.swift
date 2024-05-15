5a256da82db94eea302395d990fc06d6a07d1c12
seokhyun <letushyun@gmail.com>
2024-05-15 19:35:57 +0900

    [Feat] ShoppingEntity 구현

---
+++ travelPlan/Source/Domain/Entities/TourDestinationType/ShoppingEntity.swift
@@ -0,0 +1,20 @@
+//
+//  ShoppingEntity.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 5/15/24.
+//
+
+import Foundation
+
+struct ShoppingEntity {
+  let canPark: String
+  let fairDay: String
+  let openTime: String
+  let restDay: String
+  let saleItem: String
+  let telNumber: String
+  let imageDataList: [Data?]
+  let title: String
+  let overview: String
+}
