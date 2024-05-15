7219f70bc53f94898bc576298d783033a6ac66af
shcommit <happysh_s2@naver.com>
2024-05-16 00:04:31 +0900

    [Style] 오타수정

--- travelPlan/Source/Domain/UseCases/Protocol/Post/OwnerRelatedPostFetchUseCase.swift
+++ travelPlan/Source/Domain/UseCases/Protocol/Post/OwnerRelatedPostFetchUseCase.swift
@@ -5,13 +5,13 @@
 //  Created by 양승현 on 5/15/24.
 //
 
-import Foundation
 import Combine
+import Foundation
 
 protocol OwnerRelatedPostFetchUseCase {
   func fetchOwnerLikedPosts(
     page: Int32,
-    perIage: Int32
+    perPage: Int32
   ) -> AnyPublisher<PostsPage, Error>
   
   func fetchOwnerWrotePosts(
