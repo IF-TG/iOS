a7970968132320fe500f4311c0796139b081980d
shcommit <happysh_s2@naver.com>
2024-05-15 12:10:34 +0900

    [Feat] PostFetchAtomicRepository 생성및 함수들 추가

--- travelPlan/Source/Domain/Interfaces/Repositories/Post/PostFetchAtomicRepository.swift
+++ travelPlan/Source/Domain/Interfaces/Repositories/Post/PostFetchAtomicRepository.swift
@@ -1,8 +1,23 @@
 //
-//  PostAtomicRepository.swift
+//  PostFetchAtomicRepository.swift
 //  travelPlan
 //
 //  Created by 양승현 on 5/15/24.
 //
 
+import Combine
 import Foundation
+
+protocol PostFetchAtomicRepository {
+  /// Category에 따른 query를 반환하빈다.
+  func fetchFilteredPosts(
+    page: Int32,
+    perPage: Int32,
+    category: PostCategory
+  ) -> AnyPublisher<AtomicPost, any Error>
+  
+  func fetchLikedPostsByLoggedInUser(
+    page: Int32,
+    perPage: Int32
+  ) -> AnyPublisher<AtomicPost, any Error>
+}
