09953ecdcb1b926769dfa597341d6f5497a54e19
seokhyun <letushyun@gmail.com>
2024-05-15 20:53:24 +0900

    [Rebuild] shoppingList메소드는 SearchResultUseCase에서 사용할것이므로 제거

--- travelPlan/Source/Domain/UseCases/Protocol/ShoppingUseCase.swift
+++ travelPlan/Source/Domain/UseCases/Protocol/ShoppingUseCase.swift
@@ -9,8 +9,6 @@ import Foundation
 import Combine
 
 protocol ShoppingUseCase {
-  /// 쇼핑 리스트를 가져옵니다.
-  func fetchShoppingList() -> AnyPublisher<[ShoppingThumbnailEntity], any Error>
   /// 쇼핑 상세 정보를 가져옵니다.
-  func fetchShopping(tourContentId: TourContentId) -> AnyPublisher<ShoppingEntity, any Error>
+  func fetchShoppingDetail(tourContentId: TourContentId) -> AnyPublisher<ShoppingEntity, any Error>
 }
