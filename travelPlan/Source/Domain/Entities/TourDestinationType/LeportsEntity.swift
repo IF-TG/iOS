1444f58953266978bab4006075fc2bc6cb25f1c1
seokhyun <letushyun@gmail.com>
2024-05-15 22:01:46 +0900

    [Feat] LeportsEntity 정의

---
+++ travelPlan/Source/Domain/Entities/TourDestinationType/LeportsEntity.swift
@@ -0,0 +1,22 @@
+//
+//  LeportsEntity.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 5/15/24.
+//
+
+import Foundation
+
+struct LeportsEntity {
+  let tourContentId: TourContentId
+  let operationPeriod: String
+  let telNumber: String
+  let restDay: String
+  let availableTime: String
+  let canPark: String
+  let canAccompanyDog: String
+  let fee: String
+  let title: String
+  let overview: String
+  let imageDataList: [Data?]
+}
