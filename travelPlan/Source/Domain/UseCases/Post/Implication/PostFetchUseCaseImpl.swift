d4aef9b5730a5f989560531393fe1fa871642d35
shcommit <happysh_s2@naver.com>
2024-05-16 21:26:07 +0900

    [Feat] PostFetchUseCaseImpl에서 fetchFilteredPosts(with:) 구현

--- travelPlan/Source/Domain/UseCases/Post/Implication/PostFetchUseCaseImpl.swift
+++ travelPlan/Source/Domain/UseCases/Post/Implication/PostFetchUseCaseImpl.swift
@@ -11,6 +11,8 @@ import Foundation
 final class PostFetchUseCaseImpl {
   typealias IndexedUserEntity = (index: Int, user: UserEntity)
   typealias IndexedUserPublisher = AnyPublisher<IndexedUserEntity, any Error>
+  typealias FilteredPostsOutput = AnyPublisher<PostsPage, any Error>
+  typealias PostIdentifier = String
   
   // MARK: - Dependencies
   private let postFetchAtomicRepository: PostFetchAtomicRepository
@@ -19,6 +21,10 @@ final class PostFetchUseCaseImpl {
   
   private let postHeartRepository: PostHeartRepository
   
+  private let ownerHeartPostRepository: OwnerHeartPostRepository
+  
+  private let backgroundQueue: DispatchQueue
+  
   // MARK: - Properties
   private let pageSize = 10
   
@@ -26,11 +32,16 @@ final class PostFetchUseCaseImpl {
   init(
     postFetchAtomicRepository: PostFetchAtomicRepository,
     userProfileRepository: UserProfileRepository,
-    postHeartRepository: PostHeartRepository
+    postHeartRepository: PostHeartRepository,
+    ownerHeartPostRepository: OwnerHeartPostRepository,
+    backgroundQueue: DispatchQueue = DispatchQueue(
+      label: "PostFetcuUseCaseImpl.com", qos: .userInitiated, attributes: .concurrent)
   ) {
     self.postFetchAtomicRepository = postFetchAtomicRepository
     self.userProfileRepository = userProfileRepository
     self.postHeartRepository = postHeartRepository
+    self.ownerHeartPostRepository = ownerHeartPostRepository
+    self.backgroundQueue = backgroundQueue
   }
 }
 
@@ -38,44 +49,73 @@ final class PostFetchUseCaseImpl {
 extension PostFetchUseCaseImpl: PostFetchUseCase {
   func fetchFilteredPosts(
     with page: PostFetchRequestValue
-  ) -> AnyPublisher<PostsPage, any Error> {
-    return postFetchAtomicRepository
-      .fetchFilteredPosts(page: page.page, perPage: Int32(pageSize), category: page.category)
-      .flatMap { atomicPosts in
-        let collectCount = atomicPosts.count
-        let indexedUserPublishers: [IndexedUserPublisher] = atomicPosts
-          .enumerated()
-          .map { index, atomicPost in
-            return userProfileRepository
-              .fetchProfile(with: atomicPost.authorId)
-              .map { userEntity -> IndexedUserEntity in
-                return (index, userEntity)
-              }.eraseToAnyPublisher()
-          }
-        
-        // TODO: - Owner가 해당 포스트 좋아했는지 가져와야합니다.
-        let hasLikedPost: [Bool] = atomicPosts.map { atomicPost in
-          atomicPost.authorId 
-        }
-        
-        
-        
-        return Publishers.MergeMany(indexedUserPublishers)
-          .collect(collectCount)
-          .eraseToAnyPublisher()
-          .map { indexedUserEntities -> [UserEntity] in
-            return indexedUserEntities
-              .sorted(by: {$0.index < $1.index })
-              .map { $0.user }
-          }.eraseToAnyPublisher()
-          .map { postAuthors in
-            let posts: [Post] = (0..<postAuthors.count).map { index in
-              let atomicPost = atomicPosts[index]
-              let postAuthor = postAuthors[index]
-              return Post(liked: <#T##Bool#>, atomicPost: atomicPost, postAuthor: postAuthor)
-            }
-          }
-          
+  ) -> FilteredPostsOutput {
+    return Publishers.Zip(
+      makeFilteredPostsFetchPublisher(with: page),
+      makeOwnerHeartPostIdentifiersFetchPublisher())
+    .receive(on: backgroundQueue)
+    .flatMap { [weak self] atomicPosts, ownerheartPostIdentifiers -> FilteredPostsOutput in
+      guard let self else {
+        return Fail(error: ReferenceError.invalidReference).eraseToAnyPublisher()
       }
+      
+      let hasHeartPost: [Bool] = atomicPosts.map { atomicPost -> Bool in
+        return self.hasOwnerHeartPost(ownerheartPostIdentifiers, postId: atomicPost.detail.postID)
+      }
+      
+      let collectCount = atomicPosts.count
+      let indexedUserPublishers: [IndexedUserPublisher] = atomicPosts
+        .enumerated()
+        .compactMap { [weak self] index, atomicPost -> AnyPublisher<IndexedUserEntity, any Error>? in
+          return self?.userProfileRepository
+            .fetchProfile(with: atomicPost.authorId)
+            .map { userEntity -> IndexedUserEntity in
+              return (index, userEntity)
+            }.eraseToAnyPublisher()
+        }
+
+      return Publishers.MergeMany(indexedUserPublishers)
+        .collect(collectCount)
+        .eraseToAnyPublisher()
+        .map { indexedUserEntities -> PostsPage in
+          let authors = indexedUserEntities
+            .sorted(by: {$0.index < $1.index })
+            .map { $0.user }
+          
+          let posts: [Post] = (0..<authors.count).map { index in
+            let atomicPost = atomicPosts[index]
+            let author = authors[index]
+            let hasHeart = hasHeartPost[index]
+            let postAuthor = Post.Author(
+              profileImageData: author.profileImageData,
+              nickname: author.nickname,
+              authorId: author.id)
+            
+            return Post(liked: hasHeart, atomicPost: atomicPost, postAuthor: postAuthor)
+          }
+          let postPage = PostsPage(posts: posts, thumbnails: [])
+          return postPage
+        }.eraseToAnyPublisher()
+    }.eraseToAnyPublisher()
+  }
+}
+
+// MARK: - Helpers
+private extension PostFetchUseCaseImpl {
+  func makeFilteredPostsFetchPublisher(
+    with page: PostFetchRequestValue
+  ) -> AnyPublisher<[AtomicPost], any Error> {
+    return postFetchAtomicRepository.fetchFilteredPosts(
+      page: page.page,
+      perPage: Int32(pageSize),
+      category: page.category)
+  }
+  
+  func makeOwnerHeartPostIdentifiersFetchPublisher() -> AnyPublisher<[PostIdentifier], any Error> {
+    return ownerHeartPostRepository.fetchOwnerHeartPostIdentifiers()
+  }
+  
+  func hasOwnerHeartPost(_ ownerHeartPostIdentifiers: [PostIdentifier], postId: PostIdentifier) -> Bool {
+    return ownerHeartPostIdentifiers.contains { $0 == postId }
   }
 }
