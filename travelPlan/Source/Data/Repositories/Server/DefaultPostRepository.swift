a9116ef5cfcebd3de11bca85e0d41a8d5b2722d1
shcommit <happysh_s2@naver.com>
2024-05-16 15:00:28 +0900

    [Fix] DefaultPostRepository에서 totalPages 받지못하면 에러 던지는거 로직 제거. 더이상 totalPages필요 없음으로.

--- travelPlan/Source/Data/Repositories/Server/DefaultPostRepository.swift
+++ travelPlan/Source/Data/Repositories/Server/DefaultPostRepository.swift
@@ -57,11 +57,6 @@ extension DefaultPostRepository: PostRepository {
           }
         } receiveValue: { responseDTO in
           let postContainers = responseDTO.result.map { $0.toDomain() }
-          guard let totalPages = postContainers.first?.totalPosts else {
-            // 반드시 필요로한 totalPage가 없는 경우
-            promise(.failure(ConnectionError.missingRequiredData))
-            return
-          }
           let postsPage = PostsPage(
             posts: postContainers.map { $0.post },
             thumbnails: postContainers.map { $0.thumbnail })
@@ -128,11 +123,6 @@ extension DefaultPostRepository: PostRepository {
           }
         } receiveValue: { responseDTO in
           let postContainers = responseDTO.result.map { $0.toDomain() }
-          guard let totalPages = postContainers.first?.totalPosts else {
-            // 반드시 필요로한 totalPage가 없는 경우
-            promise(.failure(ConnectionError.missingRequiredData))
-            return
-          }
           let postsPage = PostsPage(
             posts: postContainers.map { $0.post },
             thumbnails: postContainers.map { $0.thumbnail })
