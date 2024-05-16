60e7069fd49dcd35b02dce3dc26ad3cb647f22d3
shcommit <happysh_s2@naver.com>
2024-05-16 23:25:51 +0900

    [Test] fetchFilteredPosts 함수 호출시 성공적으로 엔터티를 받는지 테스트. Stub으로 구성된 레포들이지만,, 0.011초..

--- travelPlan/Source/Domain/Tests/Post/Impl/PostFetchUseCaseImplTests.swift
+++ travelPlan/Source/Domain/Tests/Post/Impl/PostFetchUseCaseImplTests.swift
@@ -17,10 +17,10 @@ final class PostFetchUseCaseImplTests: XCTestCase {
   // MARK: - Lifecycle
   override func setUp() {
     super.setUp()
-
+    
     sut = PostFetchUseCaseImpl(
       postFetchAtomicRepository: StubPostFetchAtomicRepository(),
-      userProfileRepository: MockUserProfileRepository(),
+      userProfileRepository: StubUserProfileRepository(),
       postHeartRepository: StubPostHeartRepository(),
       ownerHeartPostRepository: StubOwnerHeartPostRepository())
     expectation = XCTestExpectation(description: "Finish")
@@ -31,5 +31,25 @@ final class PostFetchUseCaseImplTests: XCTestCase {
     sut = nil
     subscription = nil
     expectation = nil
+  }
+ 
+  func test_fetchFilteredPosts함수호출시_성공적으로entity를받는지() {
+    // Arrange
+    var hasReceivedResult: Bool = false
+    var unexpectedError: Error?
+    let postCategory = PostCategory(mainTheme: .all, orderBy: .newest)
+    let requestValue = PostFetchRequestValue(page: 1, perPage: 10, category: postCategory)
+    
+    // Act
+    let filteredPostsFetchPublisher = sut.fetchFilteredPosts(with: requestValue)
+    subscription = sink(fromPublisher: filteredPostsFetchPublisher, withExpectation: expectation) { err, res in
+      hasReceivedResult = res
+      unexpectedError = err
+    }
+    wait(for: [expectation], timeout: 7.777)
+    
+    // Assert
+    checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "fetchFilteredPosts")
+    XCTAssertTrue(hasReceivedResult, notReceivedErrorMessage)
   }
 }
