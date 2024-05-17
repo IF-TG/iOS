0d6b2fce10f182e3ba4a06381cec3aa8d476a122
seokhyun <letushyun@gmail.com>
2024-05-18 03:21:53 +0900

    [Feat] DefaultTourIntroductionInfoRepository에 fetchCourse메소드 추가

--- travelPlan/Source/Data/Repositories/TourAPI/DefaultIntroductionInfoRepository.swift
+++ travelPlan/Source/Data/Repositories/TourAPI/DefaultIntroductionInfoRepository.swift
@@ -22,6 +22,20 @@ final class DefaultTourIntroductionInfoRepository {
 
 // MARK: - IntroductionInfoRepository
 extension DefaultTourIntroductionInfoRepository: TourIntroductionInfoRepository {
+  /// 여행 코스
+  func fetchCourse(tourContentId: TourContentId)
+  -> AnyPublisher<IntroductionInfoCourseEntity, any Error> {
+    let requestDTO = TourAPIIntroductionInfoRequestDTO(contentId: tourContentId.contentId,
+                                                       contentTypeId: tourContentId.contentTypeId)
+    let endpoint = TourAPIIntroductionEndpoints.fetchCourse(with: requestDTO)
+    
+    return service.request(endpoint: endpoint)
+      .subscribe(on: backgroundQueue)
+      .tourAPITryMapResponseDTO()
+      .map { $0.toDomain() }
+      .eraseToAnyPublisher()
+  }
+  
   /// 숙박
   func fetchAccommodation(tourContentId: TourContentId)
   -> AnyPublisher<IntroductionInfoAccommodationEntity, any Error> {
