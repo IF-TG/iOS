5064d79788463b9cbeb79ad04bbbe7e7ec06e7f0
shcommit <happysh_s2@naver.com>
2024-05-17 23:37:24 +0900

    [Test] OwnerRelatedPostFetchUseCaseImpl.fetchOwnerLikedPosts 호출시 지정된 로직에의해 entity를 받는지

--- travelPlan/Source/Domain/Tests/Post/Impl/OwnerRelatedPostFetchUseCaseImplTests.swift
+++ travelPlan/Source/Domain/Tests/Post/Impl/OwnerRelatedPostFetchUseCaseImplTests.swift
@@ -6,30 +6,50 @@
 //
 
 import XCTest
+import Combine
+@testable import travelPlan
 
 final class OwnerRelatedPostFetchUseCaseImplTests: XCTestCase {
+  var sut: OwnerRelatedPostFetchUseCaseImpl!
+  var subscription: AnyCancellable?
+  var expectation: XCTestExpectation!
+  
+  // MARK: - Lifecycle
+  override func setUp() {
+    super.setUp()
+    
+    sut = OwnerRelatedPostFetchUseCaseImpl(
+      postAtomicRepository: StubPostFetchAtomicRepository(),
+      ownerHeartPostRepository: StubOwnerHeartPostRepository(),
+      userProfileRepository: StubUserProfileRepository(),
+      ownerRepository: DefaultLoggedInUserRepository(storage: StubOwnerStorage()))
+    expectation = XCTestExpectation(description: "테스트 시작!")
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+    sut = nil
+    subscription = nil
+    expectation = nil
+  }
+}
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+extension OwnerRelatedPostFetchUseCaseImplTests {
+  func test_fetchOwnerLikedPosts호출시_조합된entity를성공적으로받는지() {
+    // Arrange
+    var receivedResult = false
+    var unexpectedError: Error?
+    
+    // Act
+    let sutPublisher = sut.fetchOwnerLikedPosts(page: 1, perPage: 5).receive(on: RunLoop.current)
+    subscription = sink(fromPublisher: sutPublisher, withExpectation: expectation) { error, result in
+      receivedResult = result
+      unexpectedError = error
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    wait(for: [expectation], timeout: 7.777)
+    
+    // Assert
+    checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "fetchOwnerLikedPosts")
+    XCTAssert(receivedResult, notReceivedErrorMessage)
+  }
 }
