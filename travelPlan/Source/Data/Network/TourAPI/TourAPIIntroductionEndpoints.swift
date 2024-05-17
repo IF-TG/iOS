b0c3ebc2286d33bab03687d7a71a19a42123194f
seokhyun <letushyun@gmail.com>
2024-05-18 03:20:45 +0900

    [Feat] 소개정보조회 Endpoint에 fetchCourse함수 추가

--- travelPlan/Source/Data/Network/TourAPI/TourAPIIntroductionEndpoints.swift
+++ travelPlan/Source/Data/Network/TourAPI/TourAPIIntroductionEndpoints.swift
@@ -11,6 +11,12 @@ struct TourAPIIntroductionEndpoints {
   private static let tourApiRequestType = TourApiRequestType.introduction
   private init() { }
   
+  static func fetchCourse(
+    with requestDTO: TourAPIIntroductionInfoRequestDTO
+  ) -> TourApiEndpoint<TourApiCommonResponseDTO<TourIntroductionInfoCourseResponseDTO>> {
+    return .init(parameters: .query(requestDTO), requestType: tourApiRequestType)
+  }
+  
   static func fetchAccommodation(
     with requestDTO: TourAPIIntroductionInfoRequestDTO
   ) -> TourApiEndpoint<TourApiCommonResponseDTO<TourIntroductionInfoAccommodationResponseDTO>> {
