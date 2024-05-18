e1fcc95f65b52238557382271a06726de1e61564
shcommit <happysh_s2@naver.com>
2024-05-18 16:12:46 +0900

    [Refactor] DefaultTourImageRetreiveInfoRepositoryTests 에서 테스트 로직들 간편하게 개선

--- travelPlan/Source/Data/Tests/DefaultTourImageRetrieveInfoRepositoryTests.swift
+++ travelPlan/Source/Data/Tests/DefaultTourImageRetrieveInfoRepositoryTests.swift
@@ -57,20 +57,10 @@ extension DefaultTourImageRetrieveInfoRepositoryTests {
     var unexpectedError: Error?
     
     // Act
-    sut.retrieveAtomicImages(
-      contentId: contentId,
-      numOfRows: 10,
-      pageNo: 1)
-    .receive(on: DispatchQueue.main)
-    .sink { completion in
-      if case .failure(let error) = completion {
-        unexpectedError = error
-        self.expectation.fulfill()
-      }
-    } receiveValue: { entities in
-      print("Reveiced result: \(entities.description)")
-      hasReceivedResult = true
-      self.expectation.fulfill()
+    let retrieveAtomicImagesPublisher = sut.retrieveAtomicImages(contentId: contentId, numOfRows: 10, pageNo: 1)
+    sink(fromPublisher: retrieveAtomicImagesPublisher, withExpectation: expectation) { error, res in
+      hasReceivedResult = res
+      unexpectedError = error
     }.store(in: &self.subscriptions)
     
     wait(for: [expectation], timeout: 7.777)
@@ -86,28 +76,16 @@ extension DefaultTourImageRetrieveInfoRepositoryTests {
     var unexpectedError: Error?
     
     // Act
-    sut.retrieveImages(
-      contentId: contentId,
-      numOfRows: 10,
-      pageNo: 1)
-
-    .receive(on: DispatchQueue.main)
-    .sink { completion in
-      if case .failure(let error) = completion {
-        unexpectedError = error
-        self.expectation.fulfill()
-      }
-    } receiveValue: { entities in
-      print("Reveiced result: \(entities.description)")
-      hasReceivedResult = true
-      self.expectation.fulfill()
-    }.store(in: &self.subscriptions)
-    
+    let retrieveIamgesPublisher = sut.retrieveImages(contentId: contentId, numOfRows: 10, pageNo: 1)
+    sink(fromPublisher: retrieveIamgesPublisher, withExpectation: expectation) { error, res in
+      hasReceivedResult = res
+      unexpectedError = error
+    }.store(in: &subscriptions)
     wait(for: [expectation], timeout: 50)
     
     // Assert
     checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "retrieveImages")
-    XCTAssert(hasReceivedResult, "RetrieveImages 함수 호출시 성공적으로 엔터티를 받아야하지만 제공받지 못함")
+    XCTAssert(hasReceivedResult, notReceivedErrorMessage)
   }
   
   func test_retrieveOriginalImages호출시_원본이미지데이터들관련Entity를받는가() {
@@ -117,7 +95,6 @@ extension DefaultTourImageRetrieveInfoRepositoryTests {
     
     // Act
     let sutPublisher = sut.retrieveOriginalImages(contentId: contentId, numOfRows: 10, pageNo: 1)
-    .receive(on: DispatchQueue.main)
     sink(fromPublisher: sutPublisher, withExpectation: expectation) { error, res in
       hasReceivedResult = res
       unexpectedError = error
