cea86aa3bca67c1a1650837ebba992c440ddd166
shcommit <happysh_s2@naver.com>
2024-05-19 14:10:08 +0900

    [Feat] OwnerError에 nickname이 없을 경우 발생되는 에러 추가 및 Error에서 캐스팅 추가

--- travelPlan/Source/Common/Error/OwnerError.swift
+++ travelPlan/Source/Common/Error/OwnerError.swift
@@ -11,4 +11,6 @@ import Foundation
 @frozen enum OwnerError: LocalizedError {
   /// 로그인한 사용자는 로그인한 아이디가 있어야하는데 없다면 로그인 화면으로 나가서 다시 로그인하는 처리를 해야할것 같습니다.
   case invalidOwnerId
+  /// 로그인한 사용자는 닉네임이 있어야하는데 없다면 로그인 화면으로 나가서 다시 로그인하는 처리를 해야할것 같습니다.
+  case invalidOwnerNickname
 }
