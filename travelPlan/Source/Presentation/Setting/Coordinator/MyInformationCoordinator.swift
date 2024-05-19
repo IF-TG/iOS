ba1636a120cd041b9fa38d389ca50f3af7f9ab81
shcommit <happysh_s2@naver.com>
2024-05-19 15:29:28 +0900

    [Feat] MyInformationCoordinator에서 뷰모델 필요한 의존성 주입햇는데 목이 필요한데.. DI container를 만들어야겠습니다

--- travelPlan/Source/Presentation/Setting/Coordinator/MyInformationCoordinator.swift
+++ travelPlan/Source/Presentation/Setting/Coordinator/MyInformationCoordinator.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 import SHCoordinator
-import Alamofire
 import Combine
+import SHFirestoreService
 
 // protocol MyInformationCoordinatorDelegate: FlowCoordinatorDelegate {
 //  func showConfirmationAlertPage()
@@ -26,28 +26,33 @@ final class MyInformationCoordinator: FlowCoordinator {
   
   var alubmImageChoiceSubscription: AnyCancellable?
   
+  private let profileImageMemoryCache = ImageMemoryCache()
+  
   init(presenter: UINavigationController?) {
     self.presenter = presenter
   }
   
   func start() {
-    let sessionConfiguration = URLSessionConfiguration.default
-    sessionConfiguration.timeoutIntervalForRequest = 5
-    let monitor = ClosureEventMonitor()
-    monitor.requestDidResume = { request in print("MyInformationVC task 요청 시자그!: \(request)") }
-    monitor.requestDidFinish = { request in print("MyInformationVC 요청 끝: \(request)") }
-    monitor.taskDidComplete = { _, _, error in
-      if let error = error {
-        print("Task completed with error: \(error)")
-      } else {
-        print("Task completed successfully")
-      }
-    }
-    // let session = Session(configuration: sessionConfiguration, eventMonitors: [monitor])
-    let mockMyProfileUseCase = MockMyProfileUseCase()
+    let firestoreService = FirestoreService()
+    let firebaseStorageService = FirebaseStorageService()
     let stubOwnerStorage = StubOwnerStorage()
     let loggedInUserRepository = DefaultLoggedInUserRepository(storage: stubOwnerStorage)
     let loggedInUserUseCase = DefaultLoggedInUserUseCase(loggedInUserRepository: loggedInUserRepository)
+    
+    let userProfileSettingRepository = FirestoreUserProfileSettingRepository(
+      service: firestoreService,
+      firebaseStorageService: firebaseStorageService,
+      ownerStorage: stubOwnerStorage,
+      imageCache: profileImageMemoryCache)
+    let userProfileImageSettingUseCase = DefaultUserProfileImageSettingUseCase(
+      userProfileSettingRepository: userProfileSettingRepository)
+    
+    let userNicknameSettingUseCase = DefaultUserNicknameSettingUseCase(
+      userProfileSettingRepository: userProfileSettingRepository)
+    
+    let nicknameValidationUseCase = DefaultNicknameValidationUseCase(
+      userProfileSettingRepository: userProfileSettingRepository,
+      ownerStorage: stubOwnerStorage)
     
     let actions = MyInformationViewModelActions { [weak self] in
       self?.showConfirmationAlertPage()
@@ -62,7 +67,9 @@ final class MyInformationCoordinator: FlowCoordinator {
     }
 
     let viewModel = MyInformationViewModel(
-      myProfileUseCase: mockMyProfileUseCase,
+      userNicknameSettingUseCase: userNicknameSettingUseCase,
+      userProfileImageSettingUseCase: userProfileImageSettingUseCase,
+      nicknameValidationUseCase: nicknameValidationUseCase,
       loggedInUserUseCase: loggedInUserUseCase,
       actions: actions)
     let viewController = MyInformationViewController(viewModel: viewModel)
