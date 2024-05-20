f193fe51181aa68517b6afa03c4a565668e069c5
shcommit <happysh_s2@naver.com>
2024-05-20 17:03:32 +0900

    [Common] Publisher+Helpers에 sink(promise:receivedValue:) 함수 추가

--- travelPlan/Source/Common/Combine/Extension/Combine+Helpers.swift
+++ travelPlan/Source/Common/Combine/Extension/Combine+Helpers.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Combine
 
-extension Publisher {
+public extension Publisher {
   func subscribeAndReceive(
     on queue: DispatchQueue
   ) -> AnyPublisher<Output, Failure> {
@@ -16,13 +16,31 @@ extension Publisher {
       .subscribe(on: queue)
       .eraseToAnyPublisher()
       .receive(on: queue)
+      
       .eraseToAnyPublisher()
   }
 }
 
-extension Publisher where Self.Failure == Never {
+public extension Publisher where Self.Failure == Never {
   /// Just를 사용할때 any Error와 type erase를 해야할 경우 사용하면 편리합니다.
   func setAnyErrorAndEraseToAnyPublisher() -> AnyPublisher<Self.Output, any Error> {
     return self.setFailureType(to: (any Error).self).eraseToAnyPublisher()
+  }
+}
+
+/// promise를 사용할 때 에러는 promise 방출을 보장합니다.
+/// 테스트할때 이 지점에 디버깅체크를 하면 수월하게 테스트를 할 수 있습니다.
+public extension Publisher where Failure == Error {
+  func sink(
+    promise: @escaping Future<Output, Error>.Promise,
+    receivedValue: @escaping ((Output) -> Void)
+  ) -> AnyCancellable {
+    return self.sink { completion in
+      if case .failure(let error) = completion {
+        promise(.failure(error))
+      }
+    } receiveValue: { output in
+      receivedValue(output)
+    }
   }
 }
