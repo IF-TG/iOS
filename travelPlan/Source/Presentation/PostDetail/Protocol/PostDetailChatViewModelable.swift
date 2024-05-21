6dd2d501236a7921f3920584644bee5dd7c5197f
shcommit <happysh_s2@naver.com>
2024-05-21 11:27:28 +0900

    [Rebuild] PostDetailViewModelable -> PostDetailChatViewModelable로역할 세분화.

---
+++ travelPlan/Source/Presentation/PostDetail/Protocol/PostDetailChatViewModelable.swift
@@ -0,0 +1,94 @@
+//
+//  PostDetailChatViewModelable.swift
+//  travelPlan
+//
+//  Created by 양승현 on 5/21/24.
+//
+
+import Combine
+import Foundation
+
+// MARK: - Page
+protocol PostDetailChatViewModelPageDelegate: AnyObject {
+  func showAlertForError(with description: String, completion: (() -> Void)?)
+  
+  func showCommentOption(section: PostDetailSection)
+  func showNestedCommentOption(indexPath: IndexPath)
+}
+
+// MARK: - Actions
+struct PostDetailChatViewModelActions {
+  typealias isCommentOwner = Bool
+  
+  let showReviewWriting: (ReviewWritingEntity) -> Void
+  let showAlertForError: (String, (() -> Void)?) -> Void
+  let showAnAlertToAskWhetherToCancelWriting: (PostDetailWritingCacnelType, ((Bool) -> Void)?) -> Void
+  let showCommentOption: (isCommentOwner, ((PostDetailCommentOption) -> Void)?) -> Void
+}
+
+// MARK: - Input
+struct PostDetailChatViewModelInput {
+  typealias UserInputText = String
+  
+  let keyboardHideNotifier = PassthroughSubject<Void, Never>()
+  let commentSendHandler = PassthroughSubject<UserInputText, Never>()
+  let replyStartNotifier = PassthroughSubject<Int, Never>()
+}
+
+// MARK: - State
+@frozen enum PostDetailChatViewModelState {
+  case none
+  case networkProcessing
+  case unexpectedError(description: String)
+  case nestedComment(PostDetailNestedCommentState)
+  case comment(PostDetailCommentState)
+  
+  case keyboard(PostDetailKeyboardState)
+}
+
+@frozen enum PostDetailKeyboardState {
+  typealias WrittenComemnt = String
+  /// 댓글 작성시
+  case willShow
+  // 댓글, 대댓글 편집 시
+  case willShowWhenCommentEditStart(WrittenComemnt)
+  
+  /// 댓글 작성, 댓, 대댓 편집 취소
+  case hideToWritingCancel
+  /// 댓글 작성, 댓, 대댓 편집 진행
+  case writingContinue
+}
+
+@frozen enum PostDetailNestedCommentState {
+  
+  typealias Section = Int
+  typealias WrittenComemnt = String
+  
+  case sentSuccessfully(Section)
+  
+  /// 대댓글 삭제 후
+  case reload(IndexPath)
+  case reloadWhenLastNestedCommentDelete(IndexPath)
+  case reloadWhenCommentUpdate(IndexPath)
+}
+
+@frozen enum PostDetailCommentState {
+  typealias WrittenComemnt = String
+  
+  /// 초기에 실행됩니다.
+  case reloadedComment
+  /// 댓글 삭제 후 대댓글이 없는 경우
+  case reloadWhenCommentDelete(Int)
+  /// 댓글 삭제 후 대댓글이 있는 경우
+  case reloadWithNestedCommentsWhenCommentDelete(Int)
+  
+  case reloadWhenCommentUpdate(Int)
+}
+
+// MARK: - ViewModelable
+protocol PostDetailCommentViewModelable: ViewModelable
+where Input == PostDetailChatViewModelInput,
+      State == PostDetailChatViewModelState {
+  typealias UserInputText = String
+  typealias Section = Int
+}
--- travelPlan/Source/Presentation/PostDetail/Protocol/PostDetailViewModelable.swift
+++ travelPlan/Source/Presentation/PostDetail/Protocol/PostDetailViewModelable.swift
@@ -13,8 +13,6 @@ import SHCoordinator
 protocol PostDetailViewModelPageDelegate: AnyObject {
   func showAlertForError(with description: String, completion: (() -> Void)?)
   func showPostOption()
-  func showCommentOption(section: Int)
-  func showNestedCommentOption(indexPath: IndexPath)
   func showCategory()
   func showPostReportResult()
   func showReviewWriting()
@@ -23,13 +21,9 @@ protocol PostDetailViewModelPageDelegate: AnyObject {
 // MARK: - Actions
 /// 뷰 모델에서 사용할 타입. -> 코디네이터에서 구현
 struct PostDetailViewModelActions {
-  typealias isCommentOwner = Bool
-  let showReviewWriting: (ReviewWritingEntity) -> Void
   let showAlertForError: (String, (() -> Void)?) -> Void
-  let showAnAlertToAskWhetherToCancelWriting: (PostDetailWritingCacnelType, ((Bool) -> Void)?) -> Void
   let showPostOption: (((PostDetailOption) -> Void)?) -> Void
   
-  let showCommentOption: (isCommentOwner, ((PostDetailCommentOption) -> Void)?) -> Void
   let showPostAuthorBlock: (String, ((Bool) -> Void)?) -> Void
   /// 신고하기 종류 추가.
   let showPostReport: (((PostReportType) -> Void)?) -> Void
@@ -39,12 +33,8 @@ struct PostDetailViewModelActions {
 
 // MARK: - Input
 struct PostDetailViewModelInput {
-  typealias UserInputText = String
   
   let viewDidLoad = PassthroughSubject<Void, Never>()
-  let commentSendHandler = PassthroughSubject<UserInputText, Never>()
-  let replyStartNotifier = PassthroughSubject<Int, Never>()
-  let keyboardHideNotifier = PassthroughSubject<Void, Never>()
   let postReportNotifier = PassthroughSubject<PostReportType, Never>()
   let postAuthorBlockNotifier = PassthroughSubject<Void, Never>()
 }
@@ -56,11 +46,7 @@ struct PostDetailViewModelInput {
   
   case viewDidLoad(PostDetailViewDidLoadState)
   case unexpectedError(description: String)
-  case nestedComment(PostDetailNestedCommentState)
-  case comment(PostDetailCommentState)
   case postReport
-  
-  case keyboard(PostDetailKeyboardState)
 }
 
 @frozen enum PostDetailViewDidLoadState {
@@ -70,45 +56,6 @@ struct PostDetailViewModelInput {
   case loggedInUserInfo(userProfile: Data?, isPostOwner: Bool)
   case reloadedCommentsWithPostFavoriteInfo(Bool)
   case naviTitleInfo((Title, Duration))
-}
-
-@frozen enum PostDetailKeyboardState {
-  typealias WrittenComemnt = String
-  /// 댓글 작성시
-  case willShow
-  // 댓글, 대댓글 편집 시
-  case willShowWhenCommentEditStart(WrittenComemnt)
-  
-  /// 댓글 작성, 댓, 대댓 편집 취소
-  case hideToWritingCancel
-  /// 댓글 작성, 댓, 대댓 편집 진행
-  case writingContinue
-}
-
-@frozen enum PostDetailNestedCommentState {
-  
-  typealias Section = Int
-  typealias WrittenComemnt = String
-  
-  case sentSuccessfully(Section)
-  
-  /// 대댓글 삭제 후
-  case reload(IndexPath)
-  case reloadWhenLastNestedCommentDelete(IndexPath)
-  case reloadWhenCommentUpdate(IndexPath)
-}
-
-@frozen enum PostDetailCommentState {
-  typealias WrittenComemnt = String
-  
-  /// 초기에 실행됩니다.
-  case reloadedComment
-  /// 댓글 삭제 후 대댓글이 없는 경우
-  case reloadWhenCommentDelete(Int)
-  /// 댓글 삭제 후 대댓글이 있는 경우
-  case reloadWithNestedCommentsWhenCommentDelete(Int)
-  
-  case reloadWhenCommentUpdate(Int)
 }
 
 @frozen enum PostDetailOptionState {
