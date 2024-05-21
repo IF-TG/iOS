27ffd10d086e9f952ee649693cb718b656c3efd5
shcommit <happysh_s2@naver.com>
2024-05-21 11:35:50 +0900

    [Rebuild] PostDetailChatDataSource protocol 구현

--- travelPlan/Source/Presentation/PostDetail/Protocol/PostDetailChatDataSource.swift
+++ travelPlan/Source/Presentation/PostDetail/Protocol/PostDetailChatDataSource.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+protocol PostDetailChatDataSource: AnyObject {
+  func postContentItem(at row: Int) -> PostContentEntity
+  
+  func commentItem(in section: Int) -> PostCommentInfo
+  func replyItem(at indexPath: IndexPath) -> PostReplyInfo
+}
