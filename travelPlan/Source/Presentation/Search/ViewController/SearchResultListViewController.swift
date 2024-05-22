3020b674459e0ce43fb02249298e7a43d1e03967
seokhyun <letushyun@gmail.com>
2024-05-23 02:09:13 +0900

    [Feat] SearchResultListViewController 임시로 살짝 구현

--- travelPlan/Source/Presentation/Search/ViewController/SearchResultListViewController.swift
+++ travelPlan/Source/Presentation/Search/ViewController/SearchResultListViewController.swift
@@ -5,4 +5,47 @@
 //  Created by SeokHyun on 5/20/24.
 //
 
-import Foundation
+import UIKit
+import Combine
+
+final class SearchResultListViewController: UIViewController {
+  // MARK: - Properties
+  private lazy var collectionView = UICollectionView(
+    frame: .zero,
+    collectionViewLayout: UICollectionViewFlowLayout()
+  ).set {
+    $0.register(TravelDestinationCell.self, forCellWithReuseIdentifier: TravelDestinationCell.id)
+//    $0.dataSource = self
+  }
+//    .set {
+//    $0.register(TravelDestinationCell.self, forCellWithReuseIdentifier: TravelDestinationCell.id)
+//    $0.delegate = self
+//    $0.dataSource = self
+//  })
+  
+  // MARK: - LifeCycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
+
+// MARK: - UICollectionViewDataSource
+//extension SearchResultListViewController: UICollectionViewDataSource {
+//  func collectionView(
+//    _ collectionView: UICollectionView,
+//    cellForItemAt indexPath: IndexPath
+//  ) -> UICollectionViewCell {
+//    guard let cell = collectionView.dequeueReusableCell(
+//      withReuseIdentifier: TravelDestinationCell.id,
+//      for: indexPath
+//    ) as? TravelDestinationCell else { return .init() }
+//    
+//    cell.configure(
+//      with: TravelDestinationCellInfo(id: <#T##Int#>,
+//                                      place: <#T##String#>,
+//                                      secondText: <#T##String#>,
+//                                      thirdText: <#T##String#>, isSelectedButton: <#T##Bool#>)
+//    )
+//  }
+//}
