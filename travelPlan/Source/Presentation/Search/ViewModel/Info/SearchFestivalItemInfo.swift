1d94884debdb0e5e14617d53565353c8b6437f6a
seokhyun <letushyun@gmail.com>
2024-05-23 02:08:14 +0900

    [Feat] 검색화면 필요한 Model 추가

---
+++ travelPlan/Source/Presentation/Search/ViewModel/Info/SearchFestivalItemInfo.swift
@@ -0,0 +1,15 @@
+//
+//  SearchFestivalItemInfo.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 5/21/24.
+//
+
+import Foundation
+
+struct SearchFestivalItemInfo {
+  let title: String
+  let period: String
+  let imageData: Data?
+  var isSelectedButton: Bool
+}
---
+++ travelPlan/Source/Presentation/Search/ViewModel/Info/TravelDestinationItemInfo.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDestinationItemInfo.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 5/21/24.
+//
+
+import Foundation
+
+struct TravelDestinationItemInfo {
+  let place: String
+  let category: String
+  let location: String
+  var isButtonSelected: Bool
+  let imageData: Data?
+  let id: Int
+}
