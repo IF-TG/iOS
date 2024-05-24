3995835d2f2177cbf7f9da729b253a7dc88ef702
seokhyun <letushyun@gmail.com>
2024-05-25 02:40:04 +0900

    [Feat] 검색결과리스트 tag인 SearchResultCategotyCell 초기 구현

---
+++ travelPlan/Source/Presentation/Search/View/SearchResultCategotyCell.swift
@@ -0,0 +1,51 @@
+//
+//  SearchResultCategotyCell.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 5/25/24.
+//
+
+import UIKit
+import SnapKit
+
+final class SearchResultCategotyCell: UICollectionViewCell {
+  // MARK: - Properties
+  static let id = String(describing: TravelDestinationCell.self)
+  
+  private let tagLabel = PostSearchTagLabel().set {
+    $0.textColor = UIColor.YG.highlight
+  }
+  
+  // MARK: - LifeCycle
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: - Helpers
+extension SearchResultCategotyCell {
+  func configure(with textString: String) {
+    tagLabel.text = textString
+  }
+}
+
+// MARK: - LayoutSupport
+extension SearchResultCategotyCell: LayoutSupport {
+  func addSubviews() {
+    contentView.addSubview(tagLabel)
+  }
+  
+  func setConstraints() {
+    tagLabel.snp.makeConstraints {
+      $0.trailing.equalToSuperview().inset(13)
+      $0.leading.equalToSuperview().inset(13)
+      $0.top.equalToSuperview().inset(4)
+      $0.bottom.equalToSuperview().inset(4)
+    }
+  }
+}
