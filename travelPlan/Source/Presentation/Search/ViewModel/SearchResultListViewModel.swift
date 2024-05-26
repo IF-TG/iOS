675566e094c81a2b5271f8d01d13a619f161ea3a
seokhyun <letushyun@gmail.com>
2024-05-27 00:19:34 +0900

    [Feat] starButton input 및 state 추가

--- travelPlan/Source/Presentation/Search/ViewModel/SearchResultListViewModel.swift
+++ travelPlan/Source/Presentation/Search/ViewModel/SearchResultListViewModel.swift
@@ -29,6 +29,7 @@ struct SearchResultListViewModelInput {
 enum SearchResultListViewModelState {
   case none
   case reloadData
+  case reloadItems(IndexPath)
 }
 
 final class DefaultSearchResultListViewModel: SearchResultListViewModel {
@@ -54,36 +55,66 @@ final class DefaultSearchResultListViewModel: SearchResultListViewModel {
 extension DefaultSearchResultListViewModel {
   private func viewDidLoadStream(_ input: Input) -> Output {
     return input.viewDidLoad
-      .map { [weak self] _ in
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
-          self?.dataSource.append(.category(["전체"] + TourType.allCases.map { $0.toString }))
-          
-          let travelInfos = [
-            TravelDestinationInfo(place: "타이틀1", category: TourType.accommodation.toString, location: "강릉",
-                                  isButtonSelected: true, imageData: TempSource.imageData, id: 123),
-            TravelDestinationInfo(place: "타이틀2", category: TourType.attraction.toString, location: "인천",
-                                  isButtonSelected: false, imageData: TempSource.imageData, id: 234),
-            TravelDestinationInfo(place: "타이틀3", category: TourType.cultureFacility.toString, location: "대전",
-                                  isButtonSelected: false, imageData: TempSource.imageData, id: 345),
-            TravelDestinationInfo(place: "타이틀4", category: TourType.leports.toString, location: "전주",
-                                  isButtonSelected: true, imageData: TempSource.imageData, id: 456),
-            TravelDestinationInfo(place: "타이틀4", category: TourType.leports.toString, location: "전주",
-                                  isButtonSelected: true, imageData: TempSource.imageData, id: 456),
-            TravelDestinationInfo(place: "타이틀4", category: TourType.leports.toString, location: "전주",
-                                  isButtonSelected: true, imageData: TempSource.imageData, id: 456),
-            TravelDestinationInfo(place: "타이틀4", category: TourType.leports.toString, location: "전주",
-                                  isButtonSelected: true, imageData: TempSource.imageData, id: 456),
-          ]
-          self?.dataSource.append(.destination(travelInfos))
-        }
-        return State.reloadData
+      .flatMap { [weak self] _ in
+        return Future { promise in
+          DispatchQueue.global().asyncAfter(deadline: .now() + 2.0) {
+            self?.dataSource.append(.category(["전체"] + TourType.allCases.map { $0.toString }))
+            
+            let travelInfos = [
+              TravelDestinationInfo(place: "타이틀1", category: TourType.accommodation.toString, location: "강릉",
+                                    isButtonSelected: true, imageData: TempSource.imageData, id: 123),
+              TravelDestinationInfo(place: "타이틀2", category: TourType.attraction.toString, location: "인천",
+                                    isButtonSelected: false, imageData: TempSource.imageData, id: 234),
+              TravelDestinationInfo(place: "타이틀3", category: TourType.cultureFacility.toString, location: "대전",
+                                    isButtonSelected: false, imageData: TempSource.imageData, id: 345),
+              TravelDestinationInfo(place: "타이틀4", category: TourType.leports.toString, location: "전주",
+                                    isButtonSelected: true, imageData: TempSource.imageData, id: 456),
+              TravelDestinationInfo(place: "타이틀4", category: TourType.leports.toString, location: "전주",
+                                    isButtonSelected: true, imageData: TempSource.imageData, id: 456),
+              TravelDestinationInfo(place: "타이틀4", category: TourType.leports.toString, location: "전주",
+                                    isButtonSelected: true, imageData: TempSource.imageData, id: 456),
+              TravelDestinationInfo(place: "타이틀4", category: TourType.leports.toString, location: "전주",
+                                    isButtonSelected: true, imageData: TempSource.imageData, id: 456),
+            ]
+            self?.dataSource.append(.destination(travelInfos))
+            promise(.success(State.reloadData))
+          }
+        }.eraseToAnyPublisher()
       }
       .eraseToAnyPublisher()
   }
   
   private func didTapStarButtonStream(_ input: Input) -> Output {
     return input.didTapStarButton
-      .map { _ in State.none }
+      .flatMap { [weak self] indexPath in
+        guard let self = self else { return Just(State.none).eraseToAnyPublisher() }
+          return self.saveButtonState(indexPath: indexPath)
+      }
       .eraseToAnyPublisher()
+  }
+  
+  private func saveButtonState(indexPath: IndexPath) -> AnyPublisher<State, Never> {
+    // TODO: - id값을 통해 서버에 데이터 저장을 요청하고, 성공 시 하트버튼의 색깔을 변경해야 합니다.
+    return Future { [weak self] promise in
+      // fake network. 추후 네트워크 통신 이후, promise로 값을 방출해야 합니다.
+      DispatchQueue.global().asyncAfter(wallDeadline: .now() + 0.5) {
+        DispatchQueue.main.async {
+          print("DEBUG: FakeNetwork 통신 성공!")
+          guard let self = self else {
+            promise(.success(.none))
+            return
+          }
+          
+          if case .destination(var infos) = self.dataSource[indexPath.section] {
+            infos[indexPath.item].isButtonSelected.toggle()
+            self.dataSource[indexPath.section] = .destination(infos)
+            
+            promise(.success(.reloadItems(indexPath)))
+          }
+          promise(.success(.none))
+        }
+      }
+    }
+    .eraseToAnyPublisher()
   }
 }
