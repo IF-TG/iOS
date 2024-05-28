fc7acedbafe2c062abbe66d4f56233e71cde0478
shcommit <happysh_s2@naver.com>
2024-05-28 15:18:47 +0900

    [Feat] FeedPostVM에서 공유에 필요한 데이터 반환하도록 추가

--- travelPlan/Source/Presentation/Feed/ViewModel/FeedPostViewModel.swift
+++ travelPlan/Source/Presentation/Feed/ViewModel/FeedPostViewModel.swift
@@ -76,6 +76,17 @@ extension FeedPostViewModel: FeedPostViewModelable {
 
 // MARK: - Private Helpers
 private extension FeedPostViewModel {
+  // TODO: - 포스트 아이디 Int로 변환해야함.
+  func postShareSubjectStream(_ input: Input) -> Output {
+    return input.postShareSubject.map { [weak self] indexPath, imageData -> State in
+      guard let item = self?.postItem(at: indexPath.item) else {
+        return .unexpectedError(description: "앱 내부 서비스 에러가 발생됬습니다.")
+      }
+      let title = item.header.contentInfo.title
+      return .share(title, Int(item.postId)!, imageData)
+    }.eraseToAnyPublisher()
+  }
+  
   func postBlockSubjectStream(_ input: Input) -> Output {
     return input.postBlockSubject.map { [weak self] blockedPostId -> State in
       let blockedPostIdIndex = self?.posts.firstIndex(where: {
