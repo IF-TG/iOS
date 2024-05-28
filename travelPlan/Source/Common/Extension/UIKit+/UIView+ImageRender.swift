805c89f94381a08ea22ff7cff775c4be12dce70e
shcommit <happysh_s2@naver.com>
2024-05-28 14:34:43 +0900

    [Feat] UIView -> toImagedata(size:) 함수 추가

--- travelPlan/Source/Common/Extension/UIKit+/UIView+ImageRender.swift
+++ travelPlan/Source/Common/Extension/UIKit+/UIView+ImageRender.swift
@@ -12,10 +12,18 @@ public extension UIView {
   /// UIView를 이미지로 변환합니다.
   ///
   /// - Parameters size: 주어진 크기 기반으로 UIImage로 render합니다. nil인 경우 현제 뷰가 갖고 있는 크기를 바탕으로 이미지로 변환합니다.
-  func image(withSize size: CGSize? = nil) -> UIImage {
+  func toImage(withSize size: CGSize? = nil) -> UIImage {
     var _size: CGSize = size ?? self.layer.bounds.size
     return UIGraphicsImageRenderer(size: _size).image { context in
       self.layer.render(in: context.cgContext)
     }
+  }
+  
+  /// Redner Data from UIView's UIImage
+  /// UIView의 이미지로 데이터로 변환합니다.
+  ///
+  /// - Parameters size: 주어진 크기 기반으로 UIImage로 render합니다. nil인 경우 현제 뷰가 갖고 있는 크기를 바탕으로 이미지를 데이터로 변환합니다.
+  func toImageData(withSize size: CGSize? = nil) -> Data {
+    return toImage(withSize: size).jpegData(compressionQuality: 1) ?? Data()
   }
 }
