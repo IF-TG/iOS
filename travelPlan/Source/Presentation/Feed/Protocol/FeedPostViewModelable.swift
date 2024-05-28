d0a68d61d551d8c5df63887e48d04126f4f97799
shcommit <happysh_s2@naver.com>
2024-05-28 15:16:18 +0900

    [Feat] FeedPostViewModelable에 share input, state 추가

--- travelPlan/Source/Presentation/Feed/Protocol/FeedPostViewModelable.swift
+++ travelPlan/Source/Presentation/Feed/Protocol/FeedPostViewModelable.swift
@@ -9,6 +9,7 @@ import Combine
 import Foundation
 
 struct FeedPostViewModelInput {
+  typealias ImageData = Data
   let feedRefresh: PassthroughSubject<Void, Never> = .init()
   let nextPage: PassthroughSubject<Void, Never> = .init()
   let viewDidLoad: PassthroughSubject<Void, Never> = .init()
@@ -16,6 +17,7 @@ struct FeedPostViewModelInput {
   let notifiedMainThemeFilterRequest: PassthroughSubject<TravelMainThemeType, Never>
   let specificPostTapped: PassthroughSubject<Int, Never> = .init()
   let postBlockSubject: PassthroughSubject<Int32, Never> = .init()
+  let postShareSubject: PassthroughSubject<(IndexPath, ImageData), Never> = .init()
   
   init(
     notifiedOrderFilterRequest: PassthroughSubject<TravelOrderType, Never>,
@@ -27,12 +29,17 @@ struct FeedPostViewModelInput {
 }
 
 @frozen enum FeedPostViewModelState {
+  typealias Title = String
+  typealias PostId = Int
+  typealias PostImageData = Data
+  
   case viewDidLoad
   case refresh
   case pagination(FeedPostViewModelPaginationState)
   case unexpectedError(description: String)
   case networking
   case postFilterLoaded
+  case share(Title, PostId, PostImageData)
   ///
   case detailPostShow(post: Post, category: Post.Category)
   case deleteBlockedPost(IndexPath)
