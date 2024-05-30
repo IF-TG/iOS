cc01f26ff63ae05b107841f6874ef6608da66006
shcommit <happysh_s2@naver.com>
2024-05-30 16:16:19 +0900

    [Feat] AppDelegate+AppsFlyerHelpers에 extractPostId(from:) 추가

--- travelPlan/Source/Support/AppDelegate+AppsFlyerHelpers.swift
+++ travelPlan/Source/Support/AppDelegate+AppsFlyerHelpers.swift
@@ -11,16 +11,12 @@ import AppsFlyerLib
 extension AppDelegate {
   internal func configureAppsFlyer() {
     guard
-      let propertiesPath = Bundle.main.path(forResource: "afdevkey_donotpush", ofType: "plist"),
-      let properties = NSDictionary(contentsOfFile: propertiesPath) as? [String:String] 
-    else { fatalError("Cannot find `afdevkey_donotpush`") }
-    guard
-      let appsFlyerDevKey = properties["appsFlyerDevKey"],
-      let appleAppID = properties["appleAppID"] 
+      let appsFlyerDevKey = SecretManager.shared.get(with: .appsFlyerDevKey),
+      let appleAppID = SecretManager.shared.get(with: .appleAppId)
     else { fatalError("Cannot find `appsFlyerDevKey` or `appleAppID` key") }
     
-    AppsFlyerLib.shared().appsFlyerDevKey = SecretManager.shared.get(with: .appsFlyerDevKey)!
-    AppsFlyerLib.shared().appleAppID = SecretManager.shared.get(with: .appleAppId)!
+    AppsFlyerLib.shared().appsFlyerDevKey = appsFlyerDevKey
+    AppsFlyerLib.shared().appleAppID = appleAppID
     
     #if DEBUG
     AppsFlyerLib.shared().isDebug = true
@@ -39,9 +35,24 @@ extension AppDelegate {
   }
   
   func goToPostDetailScene(with PostId: String) {
-    
-    // TODO: - 앱 최초 실행일 경우 신 델리게이트 -> 앱 델리게이트 호출이어서, 여기서 이제 신 델리게이트에서 내부에 앱코디 변수 추가하고
+    print("hi")
+    // TODO: - 앱 최초 실행일 경우 신 델리게이트 -> 앱스플라이어에 의해 앱 델리게이트 호출이어서, 여기서 이제 신 델리게이트에서 내부에 앱코디 변수
+    // 추가하고
     // 공유하기에의해들어올떄 로직도 추가해주자.
+  }
+  
+  /// Universal link의 url의 링크에 도메인을 제외한 path를 받으면, postId를 찾아 반환합니다.
+  func extractPostId(from path: String?) -> Int? {
+    guard let path = path else {
+      return nil
+    }
+    let parameters = path.split { $0=="/" }
+    /// 포스트아이디 위치 다음 parameter가 존재하는가?
+    if let postIdIndex = parameters.firstIndex(of: "postId"), postIdIndex + 1 < parameters.count {
+      let postId = parameters[postIdIndex + 1]
+      return Int(postId)
+    }
+    return nil
   }
 }
 
