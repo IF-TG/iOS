8a52b43de934d4964cbd194294ebc635378a3b6d
shcommit <happysh_s2@naver.com>
2024-05-31 02:58:17 +0900

    [Feat] PostDetailCoordinator에서 변경된 postVM init 반영

--- travelPlan/Source/Presentation/PostDetail/Coordinator/PostDetailCoordinator.swift
+++ travelPlan/Source/Presentation/PostDetail/Coordinator/PostDetailCoordinator.swift
@@ -52,7 +52,7 @@ final class PostDetailCoordinator: NSObject, FlowCoordinator {
   
   var blockedPost: ((PostId) -> Void)?
   
-  init(presenter: UINavigationController?, post: Post, category: Post.Category) {
+  init(presenter: UINavigationController?, post: Post?, postId: PostId? = nil) {
     self.presenter = presenter
     super.init()
     let mockPostRepository = MockPostRepository()
@@ -88,15 +88,21 @@ final class PostDetailCoordinator: NSObject, FlowCoordinator {
       showCommentOption: { [weak self] isCommentOwner, optionCallBack in
         self?.showCommentOption(isCommentOwner: isCommentOwner, handler: optionCallBack)
       })
+    if post == nil, postId == nil {
+      NSLog("포스트, 포스트id 둘 중하나는 존재해야합니다")
+      return
+    }
     let postDetailVM = PostDetailViewModel(
       post: post,
-      category: category,
+      postId: postId,
       postFetchUsecase: defaultPostFetchUseCase,
       ownerRepository: loggedInUserRepository,
       userBlockUseCase: userBlockUseCase,
       actions: actions)
+    
+    // TODO: - identifier Int32로 변경하기
     let postDetailChatVM = PostDetailChatViewModel(
-      postId: post.detail.postID,
+      postId: post?.detail.postID ?? String(postId!),
       postCommentsAndPostLikeStateFetchUseCase: defaultPostCommetnsAndPostLikeStateFetchUseCase,
       postCommentUseCase: postCommentUseCase,
       postNestedCommentUseCase: postNestedCommentUseCase,
