0097449be16b0955621134b78c58bda5536ab8e0
shcommit <happysh_s2@naver.com>
2024-05-31 03:07:15 +0900

    [Feat] SceneDelegate에서 유니버셜 링크 타고올 때 앱델리게이트에서 처리하도록 appsFlyer 헨들함수 호출.

--- travelPlan/Source/Support/SceneDelegate.swift
+++ travelPlan/Source/Support/SceneDelegate.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import AppsFlyerLib
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  /// 이 객체를 참조하지 않아도 되지만, 앱스플라이어에서는 AppDelegate에서 one link를 처리하기에 이 객체 인스턴스를 선언했습니다..
+  var appCoordinator: ApplicationCoordinator?
+  
   func scene(
     _ scene: UIScene,
     willConnectTo session: UISceneSession,
@@ -15,22 +19,41 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   ) {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     let window = UIWindow(windowScene: windowScene)
-    ApplicationCoordinator(window: window).set { $0.start() }
+    appCoordinator = ApplicationCoordinator(window: window)
+    appCoordinator?.start()
     window.makeKeyAndVisible()
     
     if #available(iOS 13.0, *) {
       window.overrideUserInterfaceStyle = .light
     }
+    
+    if let userActivity = connectionOptions.userActivities.first {
+      AppsFlyerLib.shared().continue(userActivity, restorationHandler: nil)
+    } else if let url = connectionOptions.urlContexts.first?.url {
+      if url.scheme == "https" || url.scheme == "http" {
+        AppsFlyerLib.shared().handleOpen(url, options: nil)
+      } else {
+        // 일반 URL 스킴. 그러나 유니버셜 링크 사용해서 사용X.
+      }
+    }
   }
   
-  // TODO: - 여가 앱이 열려있을때에 url에 의해 접근되는 경우도 생각해야함
+  /// universal link에 의해 켜져있다가 background -> foreground로 오는 경우
   func scene(
     _ scene: UIScene,
     continue userActivity: NSUserActivity
   ) {
-    if userActivity.activityType == NSUserActivityTypeBrowsingWeb {
-      guard let url = userActivity.webpageURL else { return }
-      
+    AppsFlyerLib.shared().continue(userActivity, restorationHandler: nil)
+  }
+  
+  /// URLSceme
+  func scene(
+    _ scene: UIScene,
+    openURLContexts URLContexts: Set<UIOpenURLContext>
+  ) {
+    // URI scheme - Background -> foreground
+    if let url = URLContexts.first?.url {
+      AppsFlyerLib.shared().handleOpen(url, options: nil)
     }
   }
   
