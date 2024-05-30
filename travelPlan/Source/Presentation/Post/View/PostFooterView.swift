f0fe7a3d526639beb2439aab712a42dc75009be7
shcommit <happysh_s2@naver.com>
2024-05-31 02:56:00 +0900

    [Feat] PostFooterView에 공유하기 버튼 여백 추가

--- travelPlan/Source/Presentation/Post/View/PostFooterView.swift
+++ travelPlan/Source/Presentation/Post/View/PostFooterView.swift
@@ -33,24 +33,15 @@ class PostFooterView: UIView {
     $0.label.text = "0"
   }
   
-  private lazy var shareButton = {
-    $0.translatesAutoresizingMaskIntoConstraints = false
+  private lazy var shareButton = WrappedPaddingButton(
+    padding: UIEdgeInsets(top: 7, left: 7, bottom: 7, right: 7)
+  ) {
     let image = UIImage(named: "feedShare")
-    if #available(iOS 15.0, *) {
-      var config = UIButton.Configuration.plain()
-      config.contentInsets = NSDirectionalEdgeInsets(top: 7, leading: 7, bottom: 7, trailing: 7)
-      config.image = image
-      config.imagePadding = 0
-      $0.configuration = config
-    } else {
-      $0.imageEdgeInsets = UIEdgeInsets(top: 7, left: 7, bottom: 7, right: 7)
-      $0.setImage(image?.setColor(.yg.gray4), for: .normal)
-    }
+    $0.setImage(image?.setColor(.yg.gray4), for: .normal)
     $0.setImage(image?.setColor(.yg.gray4.withAlphaComponent(0.5)), for: .highlighted)
     $0.imageView?.contentMode = .scaleAspectFit
     $0.addTarget(self, action: #selector(didTapShare), for: .touchUpInside)
-    return $0
-  }(UIButton())
+  }
   
   var shareTapNotifier: EventNotifier?
   
