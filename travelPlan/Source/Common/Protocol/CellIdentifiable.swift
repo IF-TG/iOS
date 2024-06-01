ccc5f03cdaf6bd2652fdb293818db92b10b27e1d
shcommit <happysh_s2@naver.com>
2024-06-01 19:11:32 +0900

    [Feat] Common에 cellIdentifier 추가. PostProtocol에 PostCellLayouter 추가

---
+++ travelPlan/Source/Common/Protocol/CellIdentifiable.swift
@@ -0,0 +1,22 @@
+//
+//  CellIdentifiable.swift
+//  travelPlan
+//
+//  Created by 양승현 on 6/1/24.
+//
+
+import UIKit
+
+protocol CellIdentifiable {}
+
+extension CellIdentifiable where Self: UICollectionViewCell {
+  static var identifier: String {
+    return String(describing: self)
+  }
+}
+
+extension CellIdentifiable where Self: UITableViewCell {
+  static var identifier: String {
+    return String(describing: self)
+  }
+}
