56ab2dfa425368b0d62b05d8e2c68bd8874b2030
shcommit <happysh_s2@naver.com>
2024-06-02 14:01:23 +0900

    [Feat] HeaderFooterView Resuable, Identifiable protocol 추가

---
+++ travelPlan/Source/Common/Protocol/HeaderFooterViewResuable.swift
@@ -0,0 +1,20 @@
+//
+//  HeaderFooterViewResuable.swift
+//  travelPlan
+//
+//  Created by 양승현 on 6/2/24.
+//
+
+import UIKit
+
+protocol HeaderFooterViewResuable {}
+
+extension HeaderFooterViewResuable where Self: UITableView, Self: HeaderFooterViewIdentifiable {
+  func dequeueResuableHeaderFooterView<R>(
+    type: R.Type
+  ) -> R? where R: HeaderFooterViewIdentifiable, R: UITableViewHeaderFooterView {
+    return self.dequeueReusableHeaderFooterView(
+      withIdentifier: R.identifier
+    ) as? R
+  }
+}
