b29700ba755cb23791d12c1f1a93bf466d5f828c
shcommit <happysh_s2@naver.com>
2024-06-04 20:51:28 +0900

    [Feat] Register 함수에 반환타입 컴파일러 무시하도록 어노테이션추가ㅏㅏ:

--- travelPlan/Source/Application/DIContainer/Container+.swift
+++ travelPlan/Source/Application/DIContainer/Container+.swift
@@ -14,6 +14,7 @@ public extension Container {
   ///   - serviceType: 등록해야할 서비스의 Metatype을 as a value로 전달합니다.
   ///   - name: protocol을 등록시 String이 아닌 ResolveType의 Raw representable을 등록해 오류를 최소화 합니다.
   ///   - factory: service's type이 dependencies와 함께 resolved되는 방법을 명시합니다.
+  @discardableResult
   @inline(__always)
   func register<Service>(
     _ serviceType: Service.Type,
