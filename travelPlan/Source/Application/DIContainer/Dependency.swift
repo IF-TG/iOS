8fc5a824baeb677bf0d056e337730818dc80e507
shcommit <happysh_s2@naver.com>
2024-06-04 18:37:30 +0900

    [Feat] DIContainer Dependency wrapper 및 YeoGaAsssembler 객체 추가

---
+++ travelPlan/Source/Application/DIContainer/Dependency.swift
@@ -0,0 +1,42 @@
+//
+//  Dependency.swift
+//  travelPlan
+//
+//  Created by 양승현 on 6/4/24.
+//
+
+import Foundation
+import Swinject
+
+@propertyWrapper
+final class Dependency<Value> {
+  // MARK: - Properties
+  private var value: Value?
+  private let engine: Assembler
+  
+  // MARK: - Lifecycle
+  init(value: Value? = nil, engine: Assembler) {
+    self.value = value
+    self.engine = engine
+  }
+  
+  convenience init() {
+    self.init(engine: YeoGaAssembler.default.assembler)
+  }
+  
+  // MARK: - Wrapped
+  var wrappedValue: Value {
+    get {
+      if let value {
+        return value
+      }
+      if let value: Value = engine.resolver.resolve(Value.self) {
+        self.value = value
+        return value
+      }
+      fatalError("\(Value.self)를 resolve할 수 없습니다. 사전에 등록해야 합니다.")
+    } set {
+      value = newValue
+    }
+  }
+}
