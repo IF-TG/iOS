63b01aefa7cd3d4e71965e48054bf0ce6aa13b4d
shcommit <happysh_s2@naver.com>
2024-06-04 20:41:09 +0900

    [Feat] ResolveType추가

--- travelPlan/Source/Application/DIContainer/ResolveType.swift
+++ travelPlan/Source/Application/DIContainer/ResolveType.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+@frozen enum ResolveType {
+  case implementation(ImplementationResolveType)
+  case testDouble(TestDoubleResolveType)
+}
+
+
+@frozen enum ImplementationResolveType: String {
+  /// Spring server와 통신하는 구현체.
+  case `default`
+  /// Firestore와 통신하는 구현체
+  case firestore
+}
+
+@frozen enum TestDoubleResolveType: String {
+  case mock, stub, dummy
+}
