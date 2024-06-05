090777495f1be01b1b46e655fc5919e49f049259
shcommit <happysh_s2@naver.com>
2024-06-05 19:50:58 +0900

    [Feat] AppDIContainer에 FeedCoordinatorDependencies 추가

--- travelPlan/Source/Application/DIContainer/AppDIContainer.swift
+++ travelPlan/Source/Application/DIContainer/AppDIContainer.swift
@@ -1,12 +1,13 @@
 //
-//  YeoGaAssembler.swift
+//  AppDIContainer.swift
 //  travelPlan
 //
 //  Created by 양승현 on 6/4/24.
 //
 
-import Foundation
+import UIKit
 import Swinject
+import SHCoordinator
 
 /// Assembler는 단 하나의 인스턴스만 생겨야 합니다. 또한 앱이 종료되기 전까지 메모리에 로드되어 있음이 보장되야 합니다.
 /// Yeoga앱이 실행되기 위해 필요로되는 Layer별 assembly들을 한 곳에 모아 dependency를 관리합니다.
@@ -17,7 +18,7 @@ final class AppDIContainer {
     assembler.resolver
   }
   
-  static var `default` = AppDIContainer()
+  static var shared = AppDIContainer()
   
   // MARK: - Lifecycle
   private init() {
@@ -29,8 +30,41 @@ final class AppDIContainer {
       PresentationAssembly(),
       FlowCoordinatorAssembly()])
   }
+}
+
+// MARK: - FeedCoordinatorDependencies
+extension AppDIContainer: FeedCoordinatorDependencies {
+  func makeFeedViewController() -> FeedViewController {
+    #if DEBUG
+    return resolver.resolve(FeedViewController.self, name: .testDouble(.mock))!
+    #else
+    return resolver.resolve(FeedViewController.self, name: .implementation(.default))!
+    #endif
+  }
   
-  func resolve<Service>(_: Service.Type, name: ServiceName?) -> Service? {
-    return resolver.resolve(Service.self, name: name)
+  func makePostDetailCoordinator(
+    presenter: UINavigationController,
+    post: Post?,
+    postId: Int32
+  ) -> PostDetailCoordinator {
+    return resolver.resolve(PostDetailCoordinator.self, arguments: presenter, post, postId)!
+  }
+  
+  func makePostSearchCoordinator(
+    presenter: UINavigationController
+  ) -> PostSearchCoordinator {
+    return resolver.resolve(PostSearchCoordinator.self, argument: presenter)!
+  }
+  
+  func makeNotificationCoordinator(
+    presenter: UINavigationController
+  ) -> NotificationCenterCoordinator {
+    return resolver.resolve(NotificationCenterCoordinator.self, argument: presenter)!
+  }
+  
+  func makeReviewWritingCoordinator(
+    presenter: UINavigationController
+  ) -> ReviewWritingCoordinator {
+    return resolver.resolve(ReviewWritingCoordinator.self, argument: presenter)!
   }
 }
