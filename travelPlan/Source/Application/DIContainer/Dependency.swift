ea5cc123cdd8aa6b555121c77aba5b9add2975d6
shcommit <happysh_s2@naver.com>
2024-06-05 12:03:34 +0900

    [Style] Dependency에 주석 추가

--- travelPlan/Source/Application/DIContainer/Dependency.swift
+++ travelPlan/Source/Application/DIContainer/Dependency.swift
@@ -8,6 +8,10 @@
 import Foundation
 import Swinject
 
+/// 실험중인 객체입니다.
+/// 원래 dependency를 등록할때 Assembly에서 등록하기에 아직까지 이 Wrapper 객체를 사용한다고 이점이 없는것 같습니다.
+/// Dependency를 container에 register할 때 protocol인 경우 Mock, Stub, Default etc... 의존성이 등록되고 name으로 가져와야하지만,
+/// @Dependency 프로퍼티를 다시 init시점에 초기화할 때 두번 초기화가 되게 됩니다.
 @propertyWrapper
 final class Dependency<Value> {
   // MARK: - Properties
