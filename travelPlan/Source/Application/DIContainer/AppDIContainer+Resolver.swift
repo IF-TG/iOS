17bca0958c6104b7bd3d98d1934529077ac7fd4b
shcommit <happysh_s2@naver.com>
2024-06-05 19:51:24 +0900

    [Feat] AppDIContainer에 resolve 함수들 (인자값 3개까지 넣을수있도록 추가

--- travelPlan/Source/Application/DIContainer/AppDIContainer+Resolver.swift
+++ travelPlan/Source/Application/DIContainer/AppDIContainer+Resolver.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+extension AppDIContainer {  
+  func resolve<Service>(_: Service.Type, name: ServiceName?) -> Service? {
+    return resolver.resolve(Service.self, name: name)
+  }
+  
+  /// 뷰컨트롤러나 코디네이터를 사용할 때 목, 스터브 객체가 없는 경우에 호출합니다.
+  func resolve<Service>(_: Service.Type, name: String? = nil) -> Service? {
+    return resolver.resolve(Service.self, name: name)
+  }
+  
+  func resolve<Service, Arg1>(_: Service.Type, name: String? = nil, argument: Arg1) -> Service? {
+    return resolver.resolve(Service.self, name: name, argument: argument)
+  }
+  
+  func resolve<Service, Arg1, Arg2>(
+    _: Service.Type, name: String? = nil, arguments arg1: Arg1, _ arg2: Arg2
+  ) -> Service? {
+    return resolver.resolve(Service.self, name: name, arguments: arg1, arg2)!
+  }
+  
+  func resolve<Service, Arg1, Arg2, Arg3>(
+    _: Service.Type, name: String? = nil, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3
+  ) -> Service? {
+    return resolver.resolve(Service.self, name: name, arguments: arg1, arg2, arg3)!
+  }
+}
