ac6f7c210e2047d0fa286dfced24d99058120159
shcommit <happysh_s2@naver.com>
2024-06-07 20:17:10 +0900

    [Feat] Notification+ 에서 name추가

---
+++ travelPlan/Source/Presentation/Post/Common/Extension/Notification+.swift
@@ -0,0 +1,12 @@
+//
+//  Notification+.swift
+//  travelPlan
+//
+//  Created by 양승현 on 6/7/24.
+//
+
+import Foundation
+
+extension Notification.Name {
+  static let hasPostBlocked = Notification.Name("postBlocked")
+}
