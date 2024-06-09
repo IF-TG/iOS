abfc971423b1d59c97be5dfc9bbd69ba17edcfe6
shcommit <happysh_s2@naver.com>
2024-06-09 16:00:03 +0900

    [Rebuild] PostDetailCoordinator에서 옵션 선택시 관련 알림창 호출 함수들 전부 PostOptionCoordinatable, ALertCoordinatable에서 담당하도록 수정

--- travelPlan/Source/Presentation/Post/PostDetail/Coordinator/PostDetailCoordinator.swift
+++ travelPlan/Source/Presentation/Post/PostDetail/Coordinator/PostDetailCoordinator.swift
@@ -34,7 +34,7 @@ import SHCoordinator
 }
 
 // MARK: - PostDetailCoordinator
-final class PostDetailCoordinator: NSObject, FlowCoordinator {
+final class PostDetailCoordinator: NSObject, FlowCoordinator, PostOptionCoordinatable, AlertCoordinatable {
   typealias PostId = Int32
   
   var parent: FlowCoordinator?
@@ -146,29 +146,8 @@ extension PostDetailCoordinator {
     addChild(with: reviewWritingCoordinator)
   }
   
-  func showAlertForError(with description: String, completion: (() -> Void)?) {
-    let alert = UIAlertController(title: nil, message: description, preferredStyle: .alert).set {
-      $0.addAction(title: "OK", style: .default) { _ in completion?() }
-    }
-    postDetailViewController?.present(alert, animated: true)
-  }
-  
   func showAnAlertToAskWhetherToCancelWriting(type: PostDetailWritingCacnelType, completion: ((Bool) -> Void)?) {
-    let alert = UIAlertController(title: type.alertMessage, message: nil, preferredStyle: .alert).set {
-      $0.addAction(title: "아니요", style: .cancel) { _ in completion?(false) }
-      $0.addAction(title: "예", style: .default) { _ in completion?(true) }
-    }
-    postDetailViewController?.present(alert, animated: true)
-  }
-  
-  func showOption(handler: ((PostOption) -> Void)?) {
-    /// 액션시트에서 cancel은 하나밖에 안됩니다.
-    let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-    PostOption.allCases.forEach { option in
-      alert.addAction(title: option.rawValue, style: .destructive) { _ in handler?(option) }
-    }
-    alert.addAction(title: "취소", style: .cancel, handler: nil)
-    presenter?.present(alert, animated: true)
+    showAlertWithYesNo(with: type.alertMessage, message: nil, completion: completion)
   }
   
   func showCommentOption(isCommentOwner: Bool, handler: ((PostDetailCommentOption) -> Void)?) {
@@ -189,39 +168,6 @@ extension PostDetailCoordinator {
     presenter?.present(alert, animated: true)
   }
   
-  func showPostAuthorBlock(_ authorName: String, handler: ((Bool) -> Void)?) {
-    let alert = UIAlertController(
-      title: "‘\(authorName)’님을 차단하시겠습니까?",
-      message: "이 유저의 모든 게시물이 보이지 않고\n회원님에게 좋아요, 댓글을 남길 수 없으며\n팔로우가 취소됩니다.",
-      preferredStyle: .alert
-    ).set {
-      $0.addAction(title: "취소", style: .cancel) { _ in handler?(false) }
-      $0.addAction(title: "차단", style: .destructive) { _ in handler?(true) }
-    }
-    presenter?.present(alert, animated: true)
-  }
-  
-  func showPostReport(handler: ((PostReportType) -> Void)?) {
-    let alert = UIAlertController(title: "신고하기", message: nil, preferredStyle: .alert)
-    PostReportType.allCases.forEach { report in
-      var isStoppedRequest = false
-      if report == .stopRequest { isStoppedRequest = true }
-      alert.addAction(title: report.toKorean, style: isStoppedRequest ? .destructive : .default) { _ in
-        handler?(report)
-      }
-    }
-    presenter?.present(alert, animated: true, completion: nil)
-  }
-  
-  func showPostReportResult(wtih option: PostOption) {
-    switch option {
-    case .postBlock:
-      presenter?.present(PostOptionResultAlertController(type: .postAuthorBlock), animated: true)
-    case .postReport:
-      presenter?.present(PostOptionResultAlertController(type: .postReport), animated: true)
-    }
-  }
-  
   func showCategory(with categories: [String]) {
     let categoryViewController = PostDetailCategoryViewController(style: .plain, dataSource: categories)
     presenter?.pushViewController(categoryViewController, animated: true)
@@ -230,13 +176,6 @@ extension PostDetailCoordinator {
   func showFeedAfterBlockingFeed(_ postId: Int32) {
     blockedPost?(postId)
     finish(withAnimated: true)
-  }
-}
-
-// MARK: - Fileprivate Helpers
-fileprivate extension UIAlertController {
-  func addAction(title: String, style: UIAlertAction.Style, handler: ((UIAlertAction) -> Void)?) {
-    self.addAction(UIAlertAction(title: title, style: style, handler: handler))
   }
 }
 
