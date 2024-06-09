53d2de2367c5d9acdb1d876806392820fce78e03
shcommit <happysh_s2@naver.com>
2024-06-09 15:39:52 +0900

    [Feat] AlertCoordinatable 구현

--- travelPlan/Source/Common/Protocol/AlertCoordinatable.swift
+++ travelPlan/Source/Common/Protocol/AlertCoordinatable.swift
@@ -5,4 +5,16 @@
 //  Created by 양승현 on 6/9/24.
 //
 
-import Foundation
+import UIKit
+import SHCoordinator
+
+protocol AlertCoordinatable {}
+
+extension AlertCoordinatable where Self: FlowCoordinator {
+  func showAlertForError(with description: String, completion: (() -> Void)?) {
+    let alert = UIAlertController(title: nil, message: description, preferredStyle: .alert).set {
+      $0.addAction(title: "OK", style: .default) { _ in completion?() }
+    }
+    presenter?.present(alert, animated: true)
+  }
+}
