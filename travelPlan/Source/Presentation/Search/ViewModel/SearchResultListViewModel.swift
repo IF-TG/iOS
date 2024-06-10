56a8ef25be0165f5e3f694abbef3298c6c937d83
seokhyun <letushyun@gmail.com>
2024-06-11 04:31:46 +0900

    [Feat] SearchResultListVM input에 대한 output 구현

--- travelPlan/Source/Presentation/Search/ViewModel/SearchResultListViewModel.swift
+++ travelPlan/Source/Presentation/Search/ViewModel/SearchResultListViewModel.swift
@@ -14,7 +14,7 @@ struct SearchResultListViewModelActions {
 }
 
 enum SearchResultSectionModel {
-  case category([String])
+  case category([TravelDestinationCategoryInfo])
   case destination([TravelDestinationInfo])
 }
 
@@ -28,15 +28,17 @@ where Input == SearchResultListViewModelInput,
 
 struct SearchResultListViewModelInput {
   let viewDidLoad: PassthroughSubject<Void, Never> = .init()
-  let didTapStarButton: PassthroughSubject<IndexPath, Never> = .init()
+  let didTapStarButton: PassthroughSubject<(IndexPath, Int), Never> = .init()
   let didTapSearchButton: PassthroughSubject<String, Never> = .init()
   let didTapBackButton: PassthroughSubject<Void, Never> = .init()
   let didChangeSearchTextField: AnyPublisher<String, Never>
+  let didTapCategoryItem: PassthroughSubject<(Int, Int?), Never> = .init()
 }
 
 enum SearchResultListViewModelState {
   case none
   case firstReloadData
+  case reloadSection(Int)
   case reloadItems(IndexPath)
   case changeButtonColor(Bool)
 }
@@ -44,6 +46,7 @@ enum SearchResultListViewModelState {
 final class DefaultSearchResultListViewModel: SearchResultListViewModel {
   // MARK: - Properties
   var dataSource = [SearchResultSectionModel]()
+  private var originalDestinationInfos = [TravelDestinationInfo]()
   private let actions: SearchResultListViewModelActions
   
   // MARK: - LifeCycle
@@ -63,7 +66,8 @@ final class DefaultSearchResultListViewModel: SearchResultListViewModel {
       didTapStarButtonStream(input),
       didTapDetailStream(input),
       didChangeSearchTextFieldStream(input),
-      didTapSearchButtonStream(input)
+      didTapSearchButtonStream(input),
+      didTapCategoryItem(input)
     )
     .eraseToAnyPublisher()
   }
@@ -85,24 +89,35 @@ extension DefaultSearchResultListViewModel {
       .flatMap { [weak self] _ in
         return Future { promise in
           DispatchQueue.global().asyncAfter(deadline: .now() + 1.0) {
-            self?.dataSource.append(.category(["전체"] + TourType.allCases.map { $0.toString }))
+            self?.dataSource.append(.category(
+              [TravelDestinationCategoryInfo(categoryId: nil, title: "전체")] +
+              TourType.allCases.map { TravelDestinationCategoryInfo(categoryId: $0.rawValue, title: $0.toString) }
+            ))
             
             let travelInfos = [
-              TravelDestinationInfo(place: "타이틀1", category: TourType.accommodation.toString, location: "강릉",
-                                    isButtonSelected: true, imageData: TempSource.imageData, id: 123),
-              TravelDestinationInfo(place: "타이틀2", category: TourType.attraction.toString, location: "인천",
-                                    isButtonSelected: false, imageData: TempSource.imageData, id: 234),
-              TravelDestinationInfo(place: "타이틀3", category: TourType.cultureFacility.toString, location: "대전",
-                                    isButtonSelected: false, imageData: TempSource.imageData, id: 345),
-              TravelDestinationInfo(place: "타이틀4", category: TourType.leports.toString, location: "전주",
-                                    isButtonSelected: true, imageData: TempSource.imageData, id: 456),
-              TravelDestinationInfo(place: "타이틀4", category: TourType.leports.toString, location: "전주",
-                                    isButtonSelected: true, imageData: TempSource.imageData, id: 456),
-              TravelDestinationInfo(place: "타이틀4", category: TourType.leports.toString, location: "전주",
-                                    isButtonSelected: true, imageData: TempSource.imageData, id: 456),
-              TravelDestinationInfo(place: "타이틀4", category: TourType.leports.toString, location: "전주",
-                                    isButtonSelected: true, imageData: TempSource.imageData, id: 456),
+              TravelDestinationInfo(place: "숙박 mock", categoryId: TourType.accommodation.rawValue, 
+                                    category: TourType.accommodation.toString, location: "강릉",
+                                    isButtonSelected: true, imageData: TempSource.imageData, id: 1),
+              TravelDestinationInfo(place: "관광지 mock", categoryId: TourType.attraction.rawValue, 
+                                    category: TourType.attraction.toString, location: "인천",
+                                    isButtonSelected: false, imageData: TempSource.imageData, id: 2),
+              TravelDestinationInfo(place: "문화시설 mock", categoryId: TourType.cultureFacility.rawValue,
+                                    category: TourType.cultureFacility.toString, location: "대전",
+                                    isButtonSelected: false, imageData: TempSource.imageData, id: 3),
+              TravelDestinationInfo(place: "레포츠 mock", categoryId: TourType.leports.rawValue,
+                                    category: TourType.leports.toString, location: "전주",
+                                    isButtonSelected: true, imageData: TempSource.imageData, id: 4),
+              TravelDestinationInfo(place: "레포츠 mock2", categoryId: TourType.leports.rawValue,
+                                    category: TourType.leports.toString, location: "전주",
+                                    isButtonSelected: true, imageData: TempSource.imageData, id: 5),
+              TravelDestinationInfo(place: "레포츠 mock3", categoryId: TourType.leports.rawValue,
+                                    category: TourType.leports.toString, location: "전주",
+                                    isButtonSelected: true, imageData: TempSource.imageData, id: 6),
+              TravelDestinationInfo(place: "레포츠 mock4", categoryId: TourType.leports.rawValue,
+                                    category: TourType.leports.toString, location: "전주",
+                                    isButtonSelected: true, imageData: TempSource.imageData, id: 7)
             ]
+            self?.originalDestinationInfos = travelInfos
             self?.dataSource.append(.destination(travelInfos))
             promise(.success(State.firstReloadData))
           }
@@ -113,14 +128,14 @@ extension DefaultSearchResultListViewModel {
   
   private func didTapStarButtonStream(_ input: Input) -> Output {
     return input.didTapStarButton
-      .flatMap { [weak self] indexPath in
+      .flatMap { [weak self] indexPath, id in
         guard let self = self else { return Just(State.none).eraseToAnyPublisher() }
-          return self.saveButtonState(indexPath: indexPath)
+        return self.saveButtonState(indexPath: indexPath, id: id)
       }
       .eraseToAnyPublisher()
   }
   
-  private func saveButtonState(indexPath: IndexPath) -> AnyPublisher<State, Never> {
+  private func saveButtonState(indexPath: IndexPath, id: Int) -> AnyPublisher<State, Never> {
     // TODO: - id값을 통해 서버에 데이터 저장을 요청하고, 성공 시 하트버튼의 색깔을 변경해야 합니다.
     return Future { [weak self] promise in
       // fake network. 추후 네트워크 통신 이후, promise로 값을 방출해야 합니다.
@@ -134,6 +149,12 @@ extension DefaultSearchResultListViewModel {
           
           if case .destination(var infos) = self.dataSource[indexPath.section] {
             infos[indexPath.item].isButtonSelected.toggle()
+            for (i, info) in self.originalDestinationInfos.enumerated()
+            where self.originalDestinationInfos[i].id == id {
+              self.originalDestinationInfos[i].isButtonSelected.toggle()
+              break
+            }
+    
             self.dataSource[indexPath.section] = .destination(infos)
             
             promise(.success(.reloadItems(indexPath)))
@@ -163,6 +184,28 @@ extension DefaultSearchResultListViewModel {
     return input.didTapSearchButton
       .map { [weak self] text in
         print("search: \(text)")
+        return State.none
+      }
+      .eraseToAnyPublisher()
+  }
+  
+  private func didTapCategoryItem(_ input: Input) -> Output {
+    return input.didTapCategoryItem
+      .map { [weak self] item, categoryId in
+        if case .destination(let infos) = self?.dataSource[1] {
+          guard let self = self else { return State.none }
+          
+          if item == .zero {
+            self.dataSource[1] = .destination(originalDestinationInfos)
+          } else {
+            self.dataSource[1] = .destination(
+              originalDestinationInfos.filter {
+                $0.categoryId == categoryId
+              }
+            )
+          }
+          return State.reloadSection(1)
+        }
         return State.none
       }
       .eraseToAnyPublisher()
