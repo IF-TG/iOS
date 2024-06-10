2132a883ce52ec1ff671d0ac90db8202b449525e
shcommit <happysh_s2@naver.com>
2024-06-10 20:59:59 +0900

    [Feat] Resolver, Container에 ServiceName을 name으로 등록 및 꺼낼때 인자값 5개인 경우도 추가.

--- travelPlan/Source/Application/DIContainer/Resolver+.swift
+++ travelPlan/Source/Application/DIContainer/Resolver+.swift
@@ -39,10 +39,18 @@ public extension Resolver {
     return resolve(Service.self, name: name?.rawValue, arguments: arg1, arg2, arg3)
   }
   
+  func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5>(
+    _: Service.Type,
+    name: ServiceName?,
+    arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5
+  ) -> Service? {
+    return resolve(Service.self, name: name?.rawValue, arguments: arg1, arg2, arg3, arg4, arg5)
+  }
+  
   func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7>(
     _: Service.Type,
     name: ServiceName?,
-    arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7travelPlan/Source/Application/DIContainer/Container+.swift
+    arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7
   ) -> Service? {
     return resolve(Service.self, name: name?.rawValue, arguments: arg1, arg2, arg3, arg4, arg5, arg6, arg7)
   }
