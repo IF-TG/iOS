036b4c25ade256a9a4bbd322c32dabaf53f0a6da
shcommit <happysh_s2@naver.com>
2024-06-10 20:58:07 +0900

    [Feat] Container, Resolver에 인자값 7개로 등록 및 꺼낼수있는 함수 추가.

--- travelPlan/Source/Application/DIContainer/Container+.swift
+++ travelPlan/Source/Application/DIContainer/Container+.swift
@@ -47,6 +47,17 @@ public extension Container {
     return _register(serviceType, factory: factory, name: name.rawValue)
   }
   
+  /// Container에 a service register할 때 인자값이 7개 요구되는 경우에 사용합니다.
+  @discardableResult
+  @inline(__always)
+  func register<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7>(
+    _ serviceType: Service.Type,
+    name: ServiceName,
+    factory: @escaping (Resolver, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7) -> Service
+  ) -> ServiceEntry<Service> {
+    return _register(serviceType, factory: factory, name: name.rawValue)
+  }
+  
   @discardableResult
   @inline(__always)
   func register<Service>(
--- travelPlan/Source/Application/DIContainer/Resolver+.swift
+++ travelPlan/Source/Application/DIContainer/Resolver+.swift
@@ -38,4 +38,12 @@ public extension Resolver {
   ) -> Service? {
     return resolve(Service.self, name: name?.rawValue, arguments: arg1, arg2, arg3)
   }
+  
+  func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7>(
+    _: Service.Type,
+    name: ServiceName?,
+    arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7travelPlan/Source/Application/DIContainer/Container+.swift
+  ) -> Service? {
+    return resolve(Service.self, name: name?.rawValue, arguments: arg1, arg2, arg3, arg4, arg5, arg6, arg7)
+  }
 }
