902a1bc9a9cba09d32e85ab43458c09cdefe7529
shcommit <happysh_s2@naver.com>
2024-06-11 18:22:39 +0900

    [Feat] FeedPostViewAdapterDataSource에서 옵션클릭시 옵션에 필요한 데이터 전달할 수 있는 함수 추가.

--- travelPlan/Source/Presentation/Feed/Protocol/PostView/FeedPostViewAdapterDataSource.swift
+++ travelPlan/Source/Presentation/Feed/Protocol/PostView/FeedPostViewAdapterDataSource.swift
@@ -5,6 +5,10 @@
 //  Created by 양승현 on 10/22/23.
 //
 
+import Foundation
+
 protocol FeedPostViewAdapterDataSource: PostViewAdapterDataSource {
   var headerItem: PostFilterOptions { get }
+  
+  func postInfoForPostOption(from indexPath: IndexPath) -> (postId: Int32, authorId: Int32, authorName: String)
 }
