1903c5955788552be40eca22ef5da9c58f395de9
shcommit <happysh_s2@naver.com>
2024-06-12 13:17:34 +0900

    [Style] PR리뷰 반영. Repository Assembly -> SpringServerRepository로 파일 세분화

--- travelPlan/Source/Data/Assembly/RepositoryAssembly.swift
+++ travelPlan/Source/Data/Assembly/RepositoryAssembly.swift
@@ -92,31 +92,5 @@ final class RepositoryAssembly: Swinject.Assembly {
         firebaseStorageService: firestoreStorageService,
         ownerStorage: ownerStorage)
     }
-    
-    // TODO: - Tour API
-    
-    // TODO: - SpringServer
-    
-    // TODO: - SpringServer User
-    container.register(LoggedInUserRepository.self, name: .implementation(.default)) { _ in
-      return DefaultLoggedInUserRepository(storage: Dependency(name: .implementation(.default)))
-    }
-    
-    container.register(LoggedInUserRepository.self, name: .testDouble(.stub)) { _ in
-      return DefaultLoggedInUserRepository(storage: Dependency(name: .testDouble(.stub)))
-    }
-    
-    // TODO: - SpringServer Post
-    container.register(PostRepository.self, name: .implementation(.default)) { r in
-      let defaultOwnerStorage = r.resolve(OwnerStorage.self, name: .implementation(.default))!
-      let defaultSessionProvider = r.resolve(Sessionable.self, name: .implementation(.default))!
-      return DefaultPostRepository(service: defaultSessionProvider, ownerStorage: defaultOwnerStorage)
-    }
-    
-    container.register(PostRepository.self, name: .implementation(.interceptedDefault)) { r in
-      let stubOwnerStorage = r.resolve(OwnerStorage.self, name: .testDouble(.stub))!
-      let mockSessionProvider = r.resolve(Sessionable.self, name: .testDouble(.mock))!
-      return DefaultPostRepository(service: mockSessionProvider, ownerStorage: stubOwnerStorage)
-    }
   }
 }
