382133ab1845d335f7e7ec85ee3b9255eb6ddbcf
shcommit <happysh_s2@naver.com>
2024-06-15 19:23:08 +0900

    [Feat] PostNotificationManger 객체 구현

---
+++ travelPlan/Source/Presentation/Post/Common/PostNotificationManager.swift
@@ -0,0 +1,38 @@
+//
+//  PostNotificationManager.swift
+//  travelPlan
+//
+//  Created by 양승현 on 6/15/24.
+//
+
+import Foundation
+
+// MARK: - Notification.Name + Post
+extension Notification.Name {
+  static let hasPostBlocked = Notification.Name("PostBlocked")
+  static let postShareFromPostOptionActionSheet = Notification.Name("PostShareFromPostOptionActionSheet")
+}
+
+final class PostNotificationManager {
+  static let shared = PostNotificationManager()
+  
+  private init() { }
+}
+
+// MARK: - For post option
+extension PostNotificationManager {
+  func notifyPostHasBlocked(postId: Int32, postOptionLocation: PostOptionLocation) {
+    NotificationCenter.default.post(
+      name: .hasPostBlocked,
+      object: nil,
+      userInfo: ["postId": postId,
+                 "postOptionLocation": postOptionLocation])
+  }
+  
+  func notifyUserWantToSharePost(postId: Int32) {
+    NotificationCenter.default.post(
+      name: .postShareFromPostOptionActionSheet,
+      object: nil,
+      userInfo: ["postId": postId])
+  }
+}
