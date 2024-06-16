66cdb4c00f9c891574e43b174c0553097a7420d9
shcommit <happysh_s2@naver.com>
2024-06-17 00:25:20 +0900

    [Style] Swiftlint 준수

--- travelPlan/Source/Presentation/Feed/ViewController/FeedPostViewController.swift
+++ travelPlan/Source/Presentation/Feed/ViewController/FeedPostViewController.swift
@@ -161,7 +161,8 @@ extension FeedPostViewController: ViewBindCase {
     case .share(let title, let postId):
       let item = PostActivityItemSource(title: title, postId: postId)
       
-      coordinator?.showPostShareSheet(with: item)    }
+      coordinator?.showPostShareSheet(with: item)
+    }
   }
   
   func handlePaginationState(_ state: FeedPostViewModelPaginationState) {
