ffaccca621175af14b3e49b5a47736305ac83dd1
shcommit <happysh_s2@naver.com>
2024-06-18 02:49:12 +0900

    [Test] Firestore에서 whatsnew notification 받아오는 작업 테스트 성공

--- travelPlan/Source/Data/Tests/FirestoreWhatsNewNotificationRepoTests.swift
+++ travelPlan/Source/Data/Tests/FirestoreWhatsNewNotificationRepoTests.swift
@@ -6,30 +6,35 @@
 //
 
 import XCTest
+import Combine
+import SHFirestoreService
+@testable import travelPlan
 
-final class FirestoreWhatsNewNotificationRepoTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+final class FirestoreWhatsNewNotificationRepoTests: BaseXCTestCase {
+  var sut: FirestoreWhatsNewNotificationRepository!
+  
+  override func setUp() {
+    super.setUp()
+    sut = FirestoreWhatsNewNotificationRepository(service: FirestoreService())
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+    sut = nil
+  }
+  
+  // MARK: - Test
+  
+  func test_whenFetchWhatsNewNotifications_ShouldReturnTrue() {
+    // Arrange
+    
+    // Act
+    let testPublisher = sut.fetchNotices()
+    execute(fromPublisher: testPublisher).store(in: &subscriptions)
+    wait(for: [expectation], timeout: 7.777)
+    
+    // Assert
+    checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "fetchNotices")
+    XCTAssert(hasReceivedResult, notReceivedErrorMessage)
+  }
 }
