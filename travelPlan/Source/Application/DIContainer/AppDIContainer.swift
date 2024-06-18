f33566319255008514596450f466cf0342e0e4a9
shcommit <happysh_s2@naver.com>
2024-06-18 17:50:17 +0900

    [Style] 코드정릿

--- travelPlan/Source/Application/DIContainer/AppDIContainer.swift
+++ travelPlan/Source/Application/DIContainer/AppDIContainer.swift
@@ -133,12 +133,12 @@ extension AppDIContainer: NotificationCenterCoordinatorDependencies {
   /// 업데이트마다 보여지는 공지사항은  spring server를 활용한 레포를 구현했지만, 해당 기능 제공이 불확실해서
   ///   Firestore로도 구현했습니다. firestore에서 로그인하지 않아도 공지사항을 read할 수 있도록 특정 컬랙션 규칙을 수정했습니다.
   func makeNotificationCenterViewController() -> NotificationCenterViewController {
-#if DEBUG
+    #if DEBUG
     return resolver.resolve(NotificationCenterViewController.self, name: .implementation(.firestore))!
     // return resolver.resolve(NotificationCenterViewController.self, name: .implementation(.interceptedDefault))!
-#else
+    #else
     return resolver.resolve(NotificationCenterViewController.self, name: .implementation(.firestore))!
     // return resolver.resolve(NotificationCenterViewController.self, name: .implementation(.default))!
-#endif
+    #endif
   }
 }
