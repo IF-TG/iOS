f2032d24c7fa099ac06e11a864478e12936321e3
shcommit <happysh_s2@naver.com>
2024-06-18 17:13:55 +0900

    [Style] NoticeUseCase네이밍 변경

--- travelPlan/Source/Domain/UseCases/DefaultNoticeUseCase.swift
+++ travelPlan/Source/Domain/UseCases/DefaultNoticeUseCase.swift
@@ -10,27 +10,27 @@ import Foundation
 
 final class DefaultNoticeUseCase: NoticeUseCase {
   // MARK: - Dependencies
-  private let notificationRepository: WhatsNewNotificationRepository
+  private let whatsNewNotificationRepository: WhatsNewNotificationRepository
   
   // MARK: - Properties
-  var noticeEntities: CurrentValueSubject<[NoticeEntity], any Error> = .init([])
+  var whatsNewNoticeEntities: CurrentValueSubject<[NoticeEntity], any Error> = .init([])
   
   private var subscriptions = Set<AnyCancellable>()
   
   // MARK: - Lifecycle
-  init(notificationRepository: WhatsNewNotificationRepository) {
-    self.notificationRepository = notificationRepository
+  init(whatsNewNotificationRepository: WhatsNewNotificationRepository) {
+    self.whatsNewNotificationRepository = whatsNewNotificationRepository
   }
   
-  func fetchNotices() {
-    notificationRepository
+  func fetchWhatsNewNotices() {
+    whatsNewNotificationRepository
       .fetchNotices()
       .sink(receiveCompletion: { [weak self] completion in
         if case .failure(let error) = completion {
-          self?.noticeEntities.send(completion: .failure(error))
+          self?.whatsNewNoticeEntities.send(completion: .failure(error))
         }
       }, receiveValue: { [weak self] noticeEntities in
-        self?.noticeEntities.send(noticeEntities)
+        self?.whatsNewNoticeEntities.send(noticeEntities)
       }).store(in: &subscriptions)
   }
 }
--- travelPlan/Source/Domain/UseCases/Protocol/NoticeUseCase.swift
+++ travelPlan/Source/Domain/UseCases/Protocol/NoticeUseCase.swift
@@ -8,7 +8,7 @@
 import Combine
 
 protocol NoticeUseCase {
-  var noticeEntities: CurrentValueSubject<[NoticeEntity], any Error> { get }
+  var whatsNewNoticeEntities: CurrentValueSubject<[NoticeEntity], any Error> { get }
   
-  func fetchNotices()
+  func fetchWhatsNewNotices()
 }
