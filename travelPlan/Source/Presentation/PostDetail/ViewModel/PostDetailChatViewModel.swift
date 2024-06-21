554c8df4af1ab6e11a22198a211aa61d52c00402
shcommit <happysh_s2@naver.com>
2024-06-21 13:05:55 +0900

    [Style] 주석 수정

--- travelPlan/Source/Presentation/PostDetail/ViewModel/PostDetailChatViewModel.swift
+++ travelPlan/Source/Presentation/PostDetail/ViewModel/PostDetailChatViewModel.swift
@@ -71,9 +71,9 @@ final class PostDetailChatViewModel {
   
   private let actions: PostDetailChatViewModelActions
   
-  /// Flow1: 앱스플라이엉 디퍼드 딥 링크에 의해 접속될 경우
-  /// - 사용자가 댓글, 대댓글 작성 및 제거할 때 post Footer info만 새로 개선해주고, 피드화면에서는 알려주지 않아도 됩니다.
-  /// Flow2: 피드 화면에서 특정한 여행 후기 summary(thumbnail)를 클릭해 여행 후기 상세 화면으로 이동됬을 때
+  /// Flow1: 앱스플라이어 디퍼드 딥 링크에 의해 접속될 경우
+  /// - 사용자가 댓글, 대댓글 작성 및 제거할 때 postDetailChatDataSource's post Footer info만 새로 개선해주고, 피드화면에서는 알려주지 않아도 됩니다.
+  /// Flow2: 피드 화면에서 특정한 여행 후기 summary(thumbnail)를 클릭해 여행 후기 상세 화면으로 이동했을 때
   /// - hasEnteredByDefferedDeepLink는 false이므로 이때 노티피케이션을 통해 상세화면 들어오기 이전 post thumbnail cell에서 대댓글 전체 개수를
   ///   증가 혹은 감소합니다.
   private var postDetailChatInfo: PostDetailChatViewModelInfo
