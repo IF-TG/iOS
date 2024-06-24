abdec402baf2f0d44a65289a6b3b5c17e80c3ba5
seokhyun <letushyun@gmail.com>
2024-06-24 22:28:40 +0900

    [Feat] PersistentStorageAssembly에 LoginResultStorage 객체 등록

--- travelPlan/Source/Data/Assembly/PersistentStorageAssembly.swift
+++ travelPlan/Source/Data/Assembly/PersistentStorageAssembly.swift
@@ -21,5 +21,8 @@ final class PersistentStorageAssembly: Swinject.Assembly {
     }
     
     // TODO: - PersistentStorages LoginResponseStorage
+    container.register(LoginResultStorage.self, name: .implementation(.default)) { _ in
+      UserDefaultsLoginResultStorage()
+    }
   }
 }
