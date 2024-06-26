e019b755d1211e02903ef8c7a581608660ff0858
seokhyun <letushyun@gmail.com>
2024-06-26 23:36:55 +0900

    [Style] 코드 간편화

--- travelPlan/Source/Presentation/Assembly/PresentationAssembly.swift
+++ travelPlan/Source/Presentation/Assembly/PresentationAssembly.swift
@@ -12,17 +12,7 @@ import Swinject
 final class PresentationAssembly: Assembly {
   // swiftlint:disable:next function_body_length
   func assemble(container: Swinject.Container) {
-    // TODO: - Login Page
-    container.register((any LoginViewModel).self) { r in
-      let loginRepository = r.resolve(LoginRepository.self)!
-      let loginUseCase = r.resolve(LoginUseCase.self)!
-      return DefaultLoginViewModel(loginUseCase: loginUseCase)
-    }
-    
-    container.register(LoginViewController.self) { r in
-      let loginViewModel = r.resolve((any LoginViewModel).self)!
-      return LoginViewController(viewModel: loginViewModel)
-    }
+    loginPage(container: container)
     
     // TODO: - PostDetail Page
     
@@ -153,5 +143,21 @@ final class PresentationAssembly: Assembly {
     // TODO: - Plan Page
     
     // TODO: - ReviewWriting Page
+  }
+}
+
+// MARK: - Login Page
+private extension PresentationAssembly {
+  func loginPage(container: Container) {
+    container.register((any LoginViewModel).self) { r in
+      let loginRepository = r.resolve(LoginRepository.self)!
+      let loginUseCase = r.resolve(LoginUseCase.self)!
+      return DefaultLoginViewModel(loginUseCase: loginUseCase)
+    }
+    
+    container.register(LoginViewController.self) { r in
+      let loginViewModel = r.resolve((any LoginViewModel).self)!
+      return LoginViewController(viewModel: loginViewModel)
+    }
   }
 }
