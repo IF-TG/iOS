726e7b12ad07619317af4769177704ec2c706da6
shcommit <happysh_s2@naver.com>
2024-06-27 23:36:53 +0900

    [Add] 파일 생성!

---
+++ travelPlan/Source/Presentation/Plan/PlanCategorySelection/ViewController/PlanCategorySelectionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PlanCategoryPageViewController.swift
+//  travelPlan
+//
+//  Created by 양승현 on 6/27/24.
+//
+
+import UIKit
+
+final class PlanCategorySelectionViewController: UIViewController {
+  
+}
