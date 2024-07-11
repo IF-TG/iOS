d70da264f75cc277970619228af1caeee6ddcc5e
seokhyun <letushyun@gmail.com>
2024-07-11 22:01:12 +0900

    [Rename] DestinationScrapRequestDTO에서 DestinationScrapToggleRequestDTO 파일 네이밍 변경

---
+++ travelPlan/Source/Data/Network/Server/RequestDTO/DestinationScrapToggleRequestDTO.swift
@@ -0,0 +1,13 @@
+//
+//  DestinationScrapToggleRequestDTO.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 6/30/24.
+//
+
+import Foundation
+
+struct DestinationScrapToggleRequestDTO: Encodable {
+  let objectId: Int64
+  let forderName: String
+}
