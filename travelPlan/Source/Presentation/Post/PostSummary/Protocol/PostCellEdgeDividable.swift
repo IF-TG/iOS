2c105be0d52396763fb4dc9028cf8eabe6b203d7
shcommit <happysh_s2@naver.com>
2024-07-13 17:36:41 +0900

    [Feat] PostFooterView에 하트 개수 외부에서 수정할수있도록 변경.

---
+++ travelPlan/Source/Presentation/Post/PostSummary/Protocol/PostCellEdgeDividable.swift
@@ -0,0 +1,12 @@
+//
+//  PostCellEdgeDividable.swift
+//  travelPlan
+//
+//  Created by 양승현 on 7/13/24.
+//
+
+import Foundation
+
+protocol PostCellEdgeDividable: AnyObject {
+  func hideCellDivider()
+}
