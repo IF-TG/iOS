52bde5879f793810a6641a1700953d312bc379b3
shcommit <happysh_s2@naver.com>
2024-07-13 14:25:14 +0900

    [Rebuild] PostRepostiroyDecorator 리빌딩: )

--- travelPlan/Source/Domain/Tests/Mock/PostRepositoryDecorator.swift
+++ travelPlan/Source/Domain/Tests/Mock/PostRepositoryDecorator.swift
@@ -33,23 +33,11 @@ final class PostRepositoryDecorator: PostRepository {
       let mockData = MockResponseType.post(.whenPostContainerResponse).mockDataLoader
       return ((HTTPURLResponse(), mockData))
     }
-    return Future { promise in
-      DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 0.1) { [weak self] in
-        let subscription = self?.postRepository
-          .fetchPosts(page: page, perPage: perPage, category: category)
-          .sink(receiveCompletion: { completion in
-            if case .failure(let error) = completion {
-              promise(.failure(error))
-            }
-          }, receiveValue: { postsPage in
-            promise(.success(postsPage))
-          })
-        self?.subscriptions.insert(subscription)
-      }
-    }.eraseToAnyPublisher()
+    return postRepository
+      .fetchPosts(page: page, perPage: perPage, category: category)
+      .eraseToAnyPublisherWithDelay(for: .seconds(0.0777), scheduler: RunLoop.current)
   }
   
-  // swiftlint:disable:next function_body_length
   func fetchComments(
     page: Int32,
     perPage: Int32,
@@ -59,61 +47,9 @@ final class PostRepositoryDecorator: PostRepository {
       let mockData = MockResponseType.post(.whenPostCommentContainerResponse).mockDataLoader
       return ((HTTPURLResponse(), mockData))
     }
-    // MARK: - 이 시점은 mock json에 base64이미지 str이 담긴게 아니라 에셋에 있는 이미지 경로를 담았기에 포스트 상세 화면에서
-    // 댓글, 대댓글 작성자 이미지는 nil이 됩니다.
-    return Future { promise in
-      DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 0.1) { [weak self] in
-        let subscription = self?.postRepository
-          .fetchComments(page: page, perPage: perPage, postId: postId)
-          .sink(receiveCompletion: { completion in
-            if case .failure(let error) = completion {
-              promise(.failure(error))
-            }
-          }, receiveValue: { postCommentContainerEntity in
-            let json파일에asset경로다시한번UIImage_named_로변환한데이터 = postCommentContainerEntity
-              .comments.map {
-                let str = $0.userProfileImageData!.base64EncodedString()
-                if self?.cache[str] == nil {
-                  self?.cache[str] = UIImage(named: str)?.jpegData(compressionQuality: 1)
-                }
-                let commentUserImage = self?.cache[str]
-                let nestedCommentUserImages = $0.nestedComments.map {
-                  let nStr = $0.userProfileImageData!.base64EncodedString()
-                  if self?.cache[nStr] == nil {
-                    self?.cache[nStr] = UIImage(named: nStr)?.jpegData(compressionQuality: 1)
-                  }
-                  return self?.cache[nStr]
-                }
-                return PostCommentEntity(
-                  commentId: $0.commentId,
-                  authorId: $0.authorId,
-                  userProfileImageData: commentUserImage,
-                  userName: $0.userName,
-                  timestamp: $0.timestamp,
-                  comment: $0.comment,
-                  isDeleted: $0.isDeleted,
-                  isOnHeart: $0.isOnHeart,
-                  isBlocked: $0.isBlocked,
-                  hearts: $0.hearts,
-                  nestedComments: $0.nestedComments.enumerated().map { i, nestedComment in
-                    PostNestedCommentEntity(
-                      nestedCommentId: nestedComment.nestedCommentId,
-                      authorId: nestedComment.authorId,
-                      userProfileImageData: nestedCommentUserImages[i],
-                      nickname: nestedComment.nickname,
-                      timestamp: nestedComment.timestamp,
-                      comment: nestedComment.comment,
-                      hearts: nestedComment.hearts,
-                      isOnHeart: nestedComment.isOnHeart)
-                  })
-              }
-            promise(.success(.init(
-              comments: json파일에asset경로다시한번UIImage_named_로변환한데이터,
-              isFavorited: postCommentContainerEntity.isFavorited)))
-          })
-        self?.subscriptions.insert(subscription)
-      }
-    }.eraseToAnyPublisher()
+    return postRepository
+      .fetchComments(page: page, perPage: perPage, postId: postId)
+      .eraseToAnyPublisherWithDelay(for: .seconds(0.07), scheduler: RunLoop.current)
   }
   
   func fetchLikedPostsByLoggedInUser(
@@ -124,20 +60,7 @@ final class PostRepositoryDecorator: PostRepository {
       let mockData = MockResponseType.post(.whenPostContainerResponse).mockDataLoader
       return ((HTTPURLResponse(), mockData))
     }
-    return Future { promise in
-      DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 0.1) { [weak self] in
-        let subscription = self?.postRepository
-          .fetchLikedPostsByLoggedInUser(page: page, perPage: perPage)
-          .sink(receiveCompletion: { completion in
-            if case .failure(let error) = completion {
-              promise(.failure(error))
-            }
-          }, receiveValue: { postsPage in
-            promise(.success(postsPage))
-          })
-        self?.subscriptions.insert(subscription)
-      }
-    }.eraseToAnyPublisher()
+    return postRepository.fetchLikedPostsByLoggedInUser(page: page, perPage: perPage)
   }
   
   func searchPosts(
@@ -151,23 +74,18 @@ final class PostRepositoryDecorator: PostRepository {
       let mockData = MockResponseType.post(.whenPostsSearchResponse).mockDataLoader
       return ((.init(), mockData))
     }
-    return Future { promise in
-      DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 0.05) { [weak self] in
-        let subscription = self?.postRepository
-          .searchPosts(keyword: keyword, page: page, perPage: perPage, isTitle: isTitle, isContent: isContent)
-          .sink { completion in
-            if case .failure(let error) = completion {
-              promise(.failure(error))
-            }
-          } receiveValue: { posts in
-            promise(.success(posts))
-          }
-        self?.subscriptions.insert(subscription)
-      }
-    }.eraseToAnyPublisher()
+    return postRepository
+      .searchPosts(keyword: keyword, page: page, perPage: perPage, isTitle: isTitle, isContent: isContent)
+      .eraseToAnyPublisherWithDelay(for: .seconds(0.06), scheduler: RunLoop.current)
   }
   
   func togglePostHeart(postId: Int64) -> AnyPublisher<Bool, any Error> {
-    return Just(true).setAnyErrorAndEraseToAnyPublisher()
+    MockUrlProtocol.requestHandler = { _ in
+      let mock = MockResponseType.postComment(.whenCommentHeartToggle).mockDataLoader
+      return ((HTTPURLResponse(), mock))
+    }
+    return postRepository
+      .togglePostHeart(postId: 777)
+      .eraseToAnyPublisherWithDelay(for: .seconds(0.06), scheduler: RunLoop.current)
   }
 }
