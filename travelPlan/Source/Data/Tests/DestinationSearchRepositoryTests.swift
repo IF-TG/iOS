d80c737f9686ca2d49b71d197341f2b24b1f46e1
seokhyun <letushyun@gmail.com>
2024-07-15 03:41:50 +0900

    [Test] DestinationSearchRepository testcase 작성
    결과 성공

---
+++ travelPlan/Source/Data/Tests/DestinationSearchRepositoryTests.swift
@@ -0,0 +1,44 @@
+//
+//  DestinationSearchRepositoryTests.swift
+//  travelPlanTests
+//
+//  Created by SeokHyun on 7/15/24.
+//
+
+import XCTest
+@testable import travelPlan
+
+final class DestinationSearchRepositoryTests: BaseXCTestCase {
+  // MARK: - Properties
+  private var sut: JsonMockDestinationSearchRepository!
+  
+  // MARK: - LifeCycle
+  override func setUp() {
+    super.setUp()
+    sut = JsonMockDestinationSearchRepository(backgroundQueue: .main)
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+    sut = nil
+    
+  }
+}
+
+// MARK: - Tests
+extension DestinationSearchRepositoryTests {
+  func test_fetchDestinationList호출시_검색결과여행지썸네일리스트반환하는지() {
+    // Arrange
+    let keyword = "테스트 키워트"
+    
+    // Act
+    let publisher = sut.fetchDestinationList(by: keyword)
+    execute(fromPublisher: publisher).store(in: &subscriptions)
+    wait(for: [expectation], timeout: 7.77)
+    
+    // Assert
+    checkIfUnexpectedErrorOccurred(unexpectedError, functionName: "fetchDestinationList")
+    XCTAssert(hasReceivedResult, notReceivedErrorMessage)
+  }
+}
+
