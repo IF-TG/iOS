2e8a7f335d19a9b62cc5a3b722291851a2853283
seokhyun <letushyun@gmail.com>
2024-07-15 02:59:40 +0900

    [Feat] JsonMockDestinationSearchRepository 구현

---
+++ travelPlan/Source/Data/Tests/JsonMock/JsonMockDestinationSearchRepository.swift
@@ -0,0 +1,39 @@
+//
+//  JsonMockDestinationSearchRepository.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 7/15/24.
+//
+
+import Foundation
+import Combine
+
+final class JsonMockDestinationSearchRepository {
+   // MARK: - Properties
+  private let repository: DestinationSearchRepository
+  
+  // MARK: - LifeCycle
+  init(backgroundQueue: DispatchQueue = .global(qos: .userInitiated)) {
+    self.repository = DefaultDestinationSearchRepository(
+      service: SessionProvider(session: MockSession.default),
+      backgroundQueue: backgroundQueue
+    )
+  }
+}
+
+extension JsonMockDestinationSearchRepository: DestinationSearchRepository {
+  func fetchDestinationList(
+    by keyword: String,
+    page: Int32? = nil,
+    perPage: Int32? = nil
+  ) -> AnyPublisher<[ThumbnailDestination], any Error> {
+    MockUrlProtocol.requestHandler = { _ in
+      let mockData = MockResponseType
+        .destinationScrap(.getAllByKeyword)
+        .mockDataLoader
+      return (HTTPURLResponse(), mockData)
+    }
+    
+    return repository.fetchDestinationList(by: keyword, page: page, perPage: perPage)
+  }
+}
