cd02c374611a18bd30e7358a1cfcb7d11a012a7f
seokhyun <letushyun@gmail.com>
2024-07-15 00:04:29 +0900

    [Feat] DefaultDestinationSearchRepository 구현

--- travelPlan/Source/Data/Repositories/SpringServer/Search/Protocol/DestinationSearchRepository.swift
+++ travelPlan/Source/Data/Repositories/SpringServer/Search/Protocol/DestinationSearchRepository.swift
@@ -10,5 +10,9 @@ import Combine
 
 protocol DestinationSearchRepository {
   /// 검색 결과를 기반으로 여행지 리스트를 받아옵니다.
-  func fetchDestinationList(by keyword: String) -> AnyPublisher<Void, Never>
+  func fetchDestinationList(
+    by keyword: String,
+    page: Int32?,
+    perPage: Int32?
+  ) -> AnyPublisher<[ThumbnailDestination], any Error>
 }
