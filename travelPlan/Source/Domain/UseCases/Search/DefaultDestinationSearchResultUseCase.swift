b4abc11f450e8b52202dd02328a12c79b5f98f5d
seokhyun <letushyun@gmail.com>
2024-07-15 17:28:30 +0900

    [Feat] DefaultDestinationSearchResultUseCase 초기 구현

---
+++ travelPlan/Source/Domain/UseCases/Search/DefaultDestinationSearchResultUseCase.swift
@@ -0,0 +1,41 @@
+//
+//  DefaultDestinationSearchResultUseCase.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 7/15/24.
+//
+
+import Foundation
+import Combine
+final class DefaultDestinationSearchResultUseCase {
+  // MARK: - Dependencies
+  private let searchRepository: DestinationSearchRepository
+  private let scrapRepository: DestinationScrapRepository
+  
+  // MARK: - Properties
+  
+  // MARK: - LifeCycle
+  init(
+    destinationSearchRepository: DestinationSearchRepository,
+    destinationScrapRepository: DestinationScrapRepository
+  ) {
+    self.searchRepository = destinationSearchRepository
+    self.scrapRepository = destinationScrapRepository
+  }
+}
+
+extension DefaultDestinationSearchResultUseCase: DestinationSearchResultUseCase {
+  func fetchDestinationList(
+    keyword: String,
+    page: Int32?,
+    perPage: Int32?
+  ) -> AnyPublisher<[ThumbnailDestination], any Error> {
+    return searchRepository.fetchDestinationList(by: keyword, page: page, perPage: perPage)
+  }
+  
+
+
+  func toggleScrap(id: Int64, folderName: String?) {
+    
+  }
+}
