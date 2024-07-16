3d609557b4180404b31f2a318e2e530a49aeab9c
shcommit <happysh_s2@naver.com>
2024-07-16 23:21:52 +0900

    [Feat] FavoriteDirectoryEntity 내부 객체 접근제어 변경

--- travelPlan/Source/Domain/Entities/Favorite/FavoriteDirectoryEntity.swift
+++ travelPlan/Source/Domain/Entities/Favorite/FavoriteDirectoryEntity.swift
@@ -14,12 +14,12 @@ final class FavoriteDirectoryEntity: Object {
   @Persisted var title: String
   
   /// 디렉터리의 대표 이미지
-  @Persisted private var imageThumbnails = List<Data>()
+  @Persisted var imageThumbnails = List<Data>()
   
   /// 내부 디렉터리에서 보여질 수 잇는 데이터들의 id만 저장합니다.
   /// 이는 찜 바텀시트가 ui가 올라올 때 유용합니다. (이미 찜해져있는지 여부를 파악해야합니다.)
-  @Persisted private var postIdentifiers = List<PostIdentifier>()
-  @Persisted private var destinationIdentifiers = List<Int64>()
+  @Persisted var postIdentifiers = List<PostIdentifier>()
+  @Persisted var destinationIdentifiers = List<Int64>()
   
   /// 시간복잡도 O(1). 내부 포스트, 여행지 들의 총 개수를 반환합니다.
   var innerItemCount: Int {
