8a17fbf2a6b8710fbb99ba633dbcd488b2f2f628
seokhyun <letushyun@gmail.com>
2024-07-23 16:35:59 +0900

    [Feat] entity 추가

---
+++ travelPlan/Source/Domain/Entities/RecentSearchHistory.swift
@@ -0,0 +1,13 @@
+//
+//  RecentSearchHistory.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 7/18/24.
+//
+
+import Foundation
+
+struct RecentSearchHistory {
+  let keyword: String
+  let createAt: Date?
+}
