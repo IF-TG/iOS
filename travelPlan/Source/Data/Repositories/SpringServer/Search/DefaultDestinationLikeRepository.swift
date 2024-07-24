9b9bb06000847fcbea637dbd9cb456f3a7ec6130
seokhyun <letushyun@gmail.com>
2024-07-24 16:44:27 +0900

    [Feat] DestinationLikeRepository 추가

---
+++ travelPlan/Source/Data/Repositories/SpringServer/Search/DefaultDestinationLikeRepository.swift
@@ -0,0 +1,28 @@
+//
+//  DefaultDestinationLikeRepository.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 7/24/24.
+//
+
+import Foundation
+import Combine
+
+final class DefaultDestinationLikeRepository {
+  // MARK: - Dependencies
+  private let service: Sessionable
+  private let backgroundQueue: DispatchQueue
+  
+  // MARK: - LifeCycle
+  init(service: Sessionable, backgroundQueue: DispatchQueue = .global(qos: .userInitiated)) {
+    self.service = service
+    self.backgroundQueue = backgroundQueue
+  }
+}
+
+// MARK: - DestinationLikeRepository
+extension DefaultDestinationLikeRepository: DestinationLikeRepository {
+  func toggleDestinationLike(destinationId: Int) -> AnyPublisher<Void, Never> {
+    <#code#>
+  }
+}
