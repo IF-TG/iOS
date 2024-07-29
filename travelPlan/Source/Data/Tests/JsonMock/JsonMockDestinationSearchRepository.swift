6a32675a37cdbb55f3b75fb7f8062d4814e29219
seokhyun <letushyun@gmail.com>
2024-07-29 22:33:35 +0900

    [Style] 자잘한 코드 변경

--- travelPlan/Source/Data/Tests/JsonMock/JsonMockDestinationSearchRepository.swift
+++ travelPlan/Source/Data/Tests/JsonMock/JsonMockDestinationSearchRepository.swift
@@ -29,7 +29,7 @@ extension JsonMockDestinationSearchRepository: DestinationSearchRepository {
   ) -> AnyPublisher<[ThumbnailDestination], any Error> {
     MockUrlProtocol.requestHandler = { _ in
       let mockData = MockResponseType
-        .destinationScrap(.getAllByKeyword)
+        .destination(.getAllByKeyword)
         .mockDataLoader
       return (HTTPURLResponse(), mockData)
     }
