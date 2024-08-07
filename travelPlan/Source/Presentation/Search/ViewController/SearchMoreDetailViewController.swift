a9c6b9d25fffa8ca53852fe3edd8d64638df659c
seokhyun <letushyun@gmail.com>
2024-08-07 23:37:55 +0900

    [Comment] 주석 추가

--- travelPlan/Source/Presentation/Search/ViewController/SearchMoreDetailViewController.swift
+++ travelPlan/Source/Presentation/Search/ViewController/SearchMoreDetailViewController.swift
@@ -298,7 +298,7 @@ extension SearchMoreDetailViewController: UICollectionViewDelegate {
     input.didSelectItem.send(indexPath)
     
     // TODO: - will erase
-    let viewModel = DefaultDestinationDetailViewModel()
-    navigationController?.pushViewController(DestinationDetailViewController(viewModel: viewModel, type: .festival), animated: true)
+//    let viewModel = DefaultDestinationDetailViewModel()
+//    navigationController?.pushViewController(DestinationDetailViewController(viewModel: viewModel, type: .festival), animated: true)
   }
 }
