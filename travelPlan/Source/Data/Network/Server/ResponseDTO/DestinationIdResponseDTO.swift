bbef26cc41a79be08aa38f214e08cc31d4d3cffc
seokhyun <letushyun@gmail.com>
2024-08-08 05:00:15 +0900

    [Feat] 공통 responseDTO 추가

---
+++ travelPlan/Source/Data/Network/Server/ResponseDTO/DestinationIdResponseDTO.swift
@@ -0,0 +1,13 @@
+//
+//  DestinationIdResponseDTO.swift
+//  travelPlan
+//
+//  Created by SeokHyun on 8/8/24.
+//
+
+import Foundation
+
+struct DestinationIdResponseDTO: Decodable {
+  let id: Int
+  let contentTypeId: Int
+}
