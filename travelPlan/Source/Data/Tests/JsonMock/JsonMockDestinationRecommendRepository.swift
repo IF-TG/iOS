9e4250060b78849de827e09ec44f99d8de727e1d
seokhyun <letushyun@gmail.com>
2024-08-09 02:51:14 +0900

    [Feat] jsonMockRepository 구현

--- travelPlan/Source/Data/Tests/JsonMock/JsonMockDestinationRecommendRepository.swift
+++ travelPlan/Source/Data/Tests/JsonMock/JsonMockDestinationRecommendRepository.swift
@@ -14,13 +14,14 @@ final class JsonMockDestinationRecommendRepository {
   
   // MARK: - LifeCycle
   init(backgroundQueue: DispatchQueue = .global(qos: .userInitiated)) {
-    self.repository = DefaultDestinationRepository(
+    self.repository = DefaultDestinationRecommendRepository(
       service: SessionProvider(session: MockSession.default),
       backgroundQueue: backgroundQueue
     )
   }
 }
 
+// MARK: - DestinationRecommendRepository
 extension JsonMockDestinationRecommendRepository: DestinationRecommendRepository {
   func fetchRecommendationDestinationList(
     page: Int? = nil,
@@ -28,17 +29,38 @@ extension JsonMockDestinationRecommendRepository: DestinationRecommendRepository
   ) -> AnyPublisher<[DestinationRecommendSection], any Error> {
     MockUrlProtocol.requestHandler = { _ in
       let mockData = MockResponseType
-        .destination(<#T##MockResponseType.Destination#>)
+        .destinationRecommend(.getDestinationRecommend)
         .mockDataLoader
       return (HTTPURLResponse(), mockData)
     }
     
-    repository.fetchRecommendationDestinationList(page: page, perPage: perPage)
-    
-      .map {
-        let sections = $0.map {
-          $0.destinations
+    return repository.fetchRecommendationDestinationList(page: page, perPage: perPage)
+      .tryMap {
+        let sections = try $0.map {
+          let destinations = try $0.destinations.map {
+            guard let url = Bundle.main.url(forResource: "restaurant_1", withExtension: "png")
+            else { throw TransformationError.notExistedUrl }
+            
+            guard let data = try? Data(contentsOf: url)
+            else { throw TransformationError.cannotConvertData }
+            
+            return DestinationRecommendSection.Destination(
+              destinationId: $0.destinationId,
+              title: $0.title,
+              thumbnailData: data,
+              address: $0.address,
+              category: $0.category,
+              isScaped: $0.isScaped
+            )
+          }
+          return DestinationRecommendSection(
+            title: $0.title,
+            destinations: destinations
+          )
         }
+        
+        return sections
       }
+      .eraseToAnyPublisher()
   }
 }
