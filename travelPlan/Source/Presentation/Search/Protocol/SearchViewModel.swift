311845fbe6d6f86adc2353ed3bc611ad58400d83
seokhyun <letushyun@gmail.com>
2024-08-11 02:35:08 +0900

    [Feat] 프로토콜 위치 변경

--- travelPlan/Source/Presentation/Search/Protocol/SearchViewModel.swift
+++ travelPlan/Source/Presentation/Search/Protocol/SearchViewModel.swift
@@ -10,3 +10,10 @@ import Foundation
 protocol SearchViewModel: ViewModelable, SearchViewModelDataSourceable
 where Input == SearchViewModelInput,
       State == SearchViewModelState {}
+
+protocol SearchViewModelDataSourceable {
+  func getCellViewModels(in section: Int) -> SearchItemType
+  func fetchHeaderTitle(in section: Int) -> String
+  func numberOfItemsInSection(in section: Int) -> Int
+  func numberOfSections() -> Int
+}
