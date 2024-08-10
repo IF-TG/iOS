63b9428bc64875ec62f60ba23500571523c0d1a8
shcommit <happysh_s2@naver.com>
2024-08-10 19:14:42 +0900

    [Style] 페이징을 위한 목객체 컨텐츠 글 추가

--- travelPlan/Source/Domain/Tests/Mock/Post/MockPostsGeneratorForPaging.swift
+++ travelPlan/Source/Domain/Tests/Mock/Post/MockPostsGeneratorForPaging.swift
@@ -146,7 +146,9 @@ final class MockPostsGeneratorForPaging {
       let postDetail = Post.Detail(
         postID: PostIdentifier(i),
         title: titles[i],
-        content: [Post.PostContent(sort: 1+i*i, text: postContentTexts[i])],
+        content: [Post.PostContent(sort: 1, text: postContentTexts[i]),
+                  Post.PostContent(sort: 4, text: postContentTexts[(i+3)%postContentTexts.count]),
+                  Post.PostContent(sort: 7, text: postContentTexts[(i+5)%postContentTexts.count])],
         likes: Int32(postHearts[i]),
         comments: Int32(postComments[i]),
         location: .init(x: -1.0, y: -1.0),
