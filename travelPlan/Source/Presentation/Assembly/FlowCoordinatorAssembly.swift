88ebdcaf068fb372b0708800dea499b39fd1b1ac
shcommit <happysh_s2@naver.com>
2024-08-16 01:59:23 +0900

    [Feat] FlowCoordaintorAssembly에 MyInformationCoordinator추가

--- travelPlan/Source/Presentation/Assembly/FlowCoordinatorAssembly.swift
+++ travelPlan/Source/Presentation/Assembly/FlowCoordinatorAssembly.swift
@@ -61,9 +61,14 @@ final class FlowCoordinatorAssembly: Assembly {
       return MainCoordinator(tabBarController: tabBarController, dependencies: appDIContainer)
     }
     
-    // FIXME: - Setting Flow Coordinator.
+    // MARK: - Setting Flow Coordinator.
     container.register(SettingCoordinator.self) { (_, presenter: UINavigationController) in
       SettingCoordinator(presenter: presenter, dependencies: appDIContainer)
+    }
+    
+    // MARK: - MyInformationCoordinator
+    container.register(MyInformationCoordinator.self) { (_, presenter: UINavigationController?) in
+      MyInformationCoordinator(presenter: presenter, dependencies: appDIContainer)
     }
     
     // FIXME: - Favorite Flow Coordinator
