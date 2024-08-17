48439d5a4e24c5604b44f6e3beefc77be1c8461d
seokhyun <letushyun@gmail.com>
2024-08-18 04:17:15 +0900

    [Feat] 하트버튼 클릭 시 로직 구현

--- travelPlan/Source/Presentation/Destination/ViewModel/DefaultDestinationDetailViewModel.swift
+++ travelPlan/Source/Presentation/Destination/ViewModel/DefaultDestinationDetailViewModel.swift
@@ -10,7 +10,6 @@ import Combine
 
 enum DestinationDetailSection {
   case main(Main)
-  case temp
   case content([Content])
   
   struct Main {
@@ -31,14 +30,23 @@ enum DestinationDetailSection {
   }
 }
 
+enum DestinationDetailSectionIndex: Int {
+  case main
+  case content
+}
+
 struct DestinationDetailViewModelInput {
   let viewDidLoad: PassthroughSubject<Void, Never> = .init()
   let didTapCopyAddressButton: PassthroughSubject<Void, Never> = .init()
+  let didTapStarButton: PassthroughSubject<Bool, Never> = .init()
+  let didTapHeartButton: PassthroughSubject<IndexPath, Never> = .init()
 }
 
 enum DestinationDetailViewModelState {
+  case updateScrap(isSelected: Bool)
   case none
-  case reloadData
+  case loadData(isScraped: Bool)
+  case reloadItem(indexPath: IndexPath)
   case appearCopyAlert
   case unexpectedError(description: String)
 }
@@ -47,15 +55,15 @@ struct DestinationDetailViewModelActions {
   let pop: () -> Void
 }
 
-final class DefaultDestinationDetailViewModel: DestinationDetailViewModelDataSourceable {
+final class DefaultDestinationDetailViewModel {
   // MARK: - Dependencies
   private let useCase: any DestinationDetailUseCase
-  // TODO: - destinationId를 통해서 usecase 호출
   private let destinationId: DestinationIdEntity
   private let actions: DestinationDetailViewModelActions
   
   // MARK: - Properties
-  var dataSource = [DestinationDetailSection]()
+  private var dataSource = [DestinationDetailSection]()
+  
   private let emptyInfoString = "해당 데이터를 제공하지 않습니다."
   private let destinationMapper = DestinationDetailContentMapper()
   
@@ -76,15 +84,57 @@ extension DefaultDestinationDetailViewModel: DestinationDetailViewModelable {
   func transform(_ input: Input) -> Output {
     return Publishers.MergeMany([
       viewDidLoadStream(input),
-      didTapCopyAddressButtonStream(input)
+      didTapCopyAddressButtonStream(input),
+      didTapStarButtonStream(input),
+      didTapHeartButtonStream(input)
     ])
       .eraseToAnyPublisher()
-    
   }
 }
 
 // MARK: - Private Helpers
 extension DefaultDestinationDetailViewModel {
+  private func didTapStarButtonStream(_ input: Input) -> Output {
+    input.didTapStarButton
+      .flatMap { [weak self] isSelected in
+        guard let self = self else { return Just(State.none).eraseToAnyPublisher() }
+        
+        return isSelected
+        ? toggleScrapPublisher(id: destinationId.id, folderName: nil)
+        : toggleScrapPublisher(id: destinationId.id, folderName: "전체")
+      }
+      .eraseToAnyPublisher()
+  }
+  
+  private func didTapHeartButtonStream(_ input: Input) -> Output {
+    input.didTapHeartButton
+      .flatMap { [weak self] indexPath in
+        guard let self = self else { return Just(State.none).eraseToAnyPublisher() }
+        
+        return useCase.toggleLike(id: destinationId.id)
+          .map { [weak self] in
+            guard let self = self else { return State.none }
+            
+            if case .main(var mainDataSource) = dataSource[indexPath.section] {
+              mainDataSource.isSelectedHeart = $0.isSelected
+              
+              if $0.isSelected {
+                mainDataSource.heartCount += 1
+              } else {
+                mainDataSource.heartCount -= 1
+              }
+              dataSource[DestinationDetailSectionIndex.main.rawValue] = .main(mainDataSource)
+              
+              return State.reloadItem(indexPath: indexPath)
+            }
+            return State.none
+          }
+          .catch { _ in return Just(State.none).eraseToAnyPublisher() }
+          .eraseToAnyPublisher()
+      }
+      .eraseToAnyPublisher()
+  }
+  
   private func didTapCopyAddressButtonStream(_ input: Input) -> Output {
     return input.didTapCopyAddressButton
       .map { _ in
@@ -106,18 +156,26 @@ extension DefaultDestinationDetailViewModel {
               address: destinationEntity.address.address1,
               isSelectedHeart: destinationEntity.liked,
               heartCount: destinationEntity.likeCount,
-              headerInfo: DestinationDetailSection.Main.Header(imageDatas: [destinationEntity.imageData])
+              headerInfo: DestinationDetailSection.Main.Header(imageDatas: destinationEntity.imageDatas)
             ))
             self.dataSource.append(firstSection)
-            
             let contents = self.destinationMapper.makeDestinationDetail(detail: destinationEntity.detail)
             self.dataSource.append(contents)
             
-            return State.reloadData
+            return State.loadData(isScraped: destinationEntity.isScraped)
           }
           .catch { Just(State.unexpectedError(description: $0.localizedDescription)).eraseToAnyPublisher() }
           .eraseToAnyPublisher()
       }
+      .eraseToAnyPublisher()
+  }
+  
+  private func toggleScrapPublisher(id: Int, folderName: String?) -> AnyPublisher<State, Never> {
+    return useCase.toggleScrap(id: destinationId.id, folderName: nil)
+      .map { toggler in
+        return State.updateScrap(isSelected: toggler.isSelected)
+      }
+      .catch { _ in return Just(State.none).eraseToAnyPublisher() }
       .eraseToAnyPublisher()
   }
 }
@@ -129,3 +187,21 @@ extension DefaultDestinationDetailViewModel: DestinationDetailViewModelPageDeleg
   }
 }
 
+extension DefaultDestinationDetailViewModel: DestinationDetailViewModelDataSourceable {
+  func numberOfSections() -> Int {
+    return dataSource.count
+  }
+  
+  func numberOfItemsInSection(sectionIndex: Int) -> Int {
+    switch dataSource[sectionIndex] {
+    case .main:
+      return 1
+    case .content(let infos):
+      return infos.count
+    }
+  }
+  
+  func destinationDetailSection(sectionIndex: Int) -> DestinationDetailSection {
+    return dataSource[sectionIndex]
+  }
+}
