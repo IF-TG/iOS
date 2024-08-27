67cc1aa440d1b27ac5f564c3421469bce270830d
seokhyun <letushyun@gmail.com>
2024-08-28 04:07:46 +0900

    [Style] 코드 수정

--- travelPlan/Source/Domain/UseCases/Protocol/ReviewWritingUseCase.swift
+++ travelPlan/Source/Domain/UseCases/Protocol/ReviewWritingUseCase.swift
@@ -7,9 +7,10 @@
 
 import Foundation
 import Combine
+import Photos
 
 protocol ReviewWritingUseCase {
   func savePost(entity: ReviewWritingEntity) -> AnyPublisher<Bool, any Error>
   func updatePost(requestValue: ReviewWritingUseCaseUpdateRequestValue) -> AnyPublisher<Post?, any Error>
-  func requestAuthorization() -> AnyPublisher<PHAuthorizationStatus, any Never>
+  func requestAuthorization() -> AnyPublisher<PHAuthorizationStatus, Never>
 }
