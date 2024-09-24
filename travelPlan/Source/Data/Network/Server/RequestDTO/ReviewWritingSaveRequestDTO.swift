caca1b74b5c159d7b7b82214a2dd92183eab629a
seokhyun <letushyun@gmail.com>
2024-09-24 21:39:29 +0900

    [Style] 주석 제거

--- travelPlan/Source/Data/Network/Server/RequestDTO/ReviewWritingSaveRequestDTO.swift
+++ travelPlan/Source/Data/Network/Server/RequestDTO/ReviewWritingSaveRequestDTO.swift
@@ -31,48 +31,6 @@ extension ReviewWritingSaveRequestDTO {
 }
 
 extension ReviewWritingSaveRequestDTO {
-//  static func makeRequestDTO(entity: ReviewWritingEntity) -> Self {
-//    var textListDTO = [TextListDTO]()
-//    var imageFileListDTO = [ImageFileListDTO]()
-//    var order: Int32 = 1
-//    var imageOrder = 1
-//    
-//    entity.contents.forEach { content in
-//      
-//      
-//      switch content {
-//      case .text(let textString):
-//        textListDTO.append(.init(sort: order, text: textString))
-//      case let .image(data):
-//        // 정확한 비즈니스로직이 정해질 때까지 임시적으로 선착순 5개만 thumbnail로 구현
-//        let isThumbnail = imageOrder <= 5 ? true : false
-//        
-//        imageFileListDTO.append(.init(
-//          sort: order,
-////          imageType: imageType.rawValue,
-//          imageType: "jpeg",
-//          thumbnail: isThumbnail,
-//          img: data.base64EncodedString())
-//        )
-//        imageOrder += 1
-//      }
-//      order += 1
-//    }
-//    return .init(
-//      title: entity.title,
-//      content: textListDTO,
-//      startDate: DateTimeConverter.toString(from: entity.tripDate.startDate),
-//      endDate: DateTimeConverter.toString(from: entity.tripDate.endDate),
-//      themes: entity.category.themes.map { TravelThemeMapper.toDTO($0) },
-//      regions: entity.category.regions.map { TravelRegionMapper.toDTO($0) },
-//      seasons: entity.category.seasons.map { SeasonMapper.toDTO($0) },
-//      companions: entity.category.partners.map { TravelPartnerMapper.toDTO($0) },
-//      imgFileList: imageFileListDTO,
-//      mapX: entity.mapX,
-//      mapY: entity.mapY
-//    )
-//  }
-  
   static func makeRequestDTO(entity: ReviewWritingEntity) -> Self {
     var textContent = ""
     var countForTextAndImage = 0
